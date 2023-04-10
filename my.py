6145c0631df1aea075745ce707342e0267dc138c
Jhonny <gonzodzen@mail.ru>
2023-04-10 23:05:54 +0300

    Perfect Number Verifier

--- my.py
+++ my.py
@@ -1,9 +1,20 @@
-def hotpo(n,r=0):
-    if(n==1):
-        return r
-    if(n%2)==0:
-        return hotpo(n/2,r+1)
-    if(n%2)!=0:
-        return hotpo(n*3+1,r+1)
+from functools import reduce
+import math
 
-print(hotpo(6))
+def r(a,b):
+    return a+b
+
+def is_perfect(n):
+    if(n==1):
+        return False
+    a=[]
+    i=1
+    while i<math.sqrt(n):
+        if(n%i)==0:
+            a.append(i)
+            if(n/i)<=n/2:
+                a.append(n/i)
+        i+=1
+    return  reduce(r,a)==n
+
+print(is_perfect(28))
