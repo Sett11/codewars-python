621ea567269fe007236c1ceb50c42aa6a17946bd
Jhonny <gonzodzen@mail.ru>
2023-04-10 20:48:32 +0300

    Is n divisible by x and y?

--- my.py
+++ my.py
@@ -1,11 +1,4 @@
-def compare(a,b):
-    c=str(a)
-    d=str(b)
-    if(c[0]==d[0] and c[1]!=d[1] or c[1]==d[0] and c[0]!=d[1] or c[1]==d[1] and c[0]!=d[0] or c[0]==d[1] and c[1]!=d[0]):
-        return '50%'
-    if(c[0]==d[0] and c[1]==d[1] or c[0]==d[1] and c[1]==d[0]):
-        return '100%'
-    else:
-        return '0%'
+def is_divisible(n,x,y):
+    return n%x==0 and n%y==0
 
-print(compare(58,75))
+print(is_divisible(12,3,4))
