172a280a1aba75110759b904a325d9d7bf611bbd
Jhonny <gonzodzen@mail.ru>
2023-04-10 20:34:27 +0300

    Compare 2 digit numbers

--- my.py
+++ my.py
@@ -1,9 +1,11 @@
-def fix_the_meerkat(a):
-    b=a[0]
-    c=a[2]
-    a[0]=c
-    a[2]=b
-    return a
-        
+def compare(a,b):
+    c=str(a)
+    d=str(b)
+    if(c[0]==d[0] and c[1]!=d[1] or c[1]==d[0] and c[0]!=d[1] or c[1]==d[1] and c[0]!=d[0] or c[0]==d[1] and c[1]!=d[0]):
+        return '50%'
+    if(c[0]==d[0] and c[1]==d[1] or c[0]==d[1] and c[1]==d[0]):
+        return '100%'
+    else:
+        return '0%'
 
-print(fix_the_meerkat(["tail", "body", "head"]))
+print(compare(58,75))
