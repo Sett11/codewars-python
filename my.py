f4e83e808f18ad135d0060dd8c910383b78ddcf3
Jhonny <gonzodzen@mail.ru>
2023-04-10 22:13:32 +0300

    Collatz Conjecture (3n+1)

--- my.py
+++ my.py
@@ -1,11 +1,9 @@
-def collatz(n,r=1):
-    if(n==73567465519280238573):
-        return 362
+def hotpo(n,r=0):
     if(n==1):
         return r
     if(n%2)==0:
-        return collatz(n/2,r+1)
+        return hotpo(n/2,r+1)
     if(n%2)!=0:
-        return collatz(n*3+1,r+1)
+        return hotpo(n*3+1,r+1)
 
-print(collatz(73567465519280238573))
+print(hotpo(6))
