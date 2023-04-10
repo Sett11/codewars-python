da68462ce75c0a78d3ecfb84de6c37ceb8674eca
Jhonny <gonzodzen@mail.ru>
2023-04-10 23:28:02 +0300

    Fibonacci

--- my.py
+++ my.py
@@ -1,2 +1,7 @@
-def if_chuck_says_so():
-    return 1==2
+def fibonacci(n: int) -> int:
+    a=[0,1]
+    while len(a)<=n:
+        a.append(a[len(a)-1]+a[len(a)-2])
+    return a[n]
+
+print(fibonacci(34))
