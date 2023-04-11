bcf92de8b11889d4f462eab6f5b7d6bb59e8dce3
Jhonny <gonzodzen@mail.ru>
2023-04-11 16:48:59 +0300

    Ciphers #1 - The 01 Cipher

--- my.py
+++ my.py
@@ -1,7 +1,17 @@
-def index_finder(a,x):
-    i=1
-    while i<len(a):
-        if(a[i]==x):return i
+def encode(s):
+    l='abcdefghijklmnopqrstuvwxyz'
+    s=s.lower()
+    a=''
+    i=0
+    while i<len(s):
+        if(l.find(s[i])!=-1):
+            if(l.find(s[i])%2)==0:
+                a+='0'
+            else:
+                a+='1'
+        else:
+            a+=s[i]
         i+=1
+    return a
 
-print(index_finder(['b','a','b'], 'b'))
+print(encode("Hello World!"))
