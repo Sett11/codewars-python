a804ebe18ba8f45b332a5ff693b5c63f5296781b
Jhonny <gonzodzen@mail.ru>
2023-04-11 21:13:06 +0300

    Debug Sum of Digits of a Number

--- my.py
+++ my.py
@@ -1,10 +1,6 @@
-def cleaned_counts(a):
-    m=0
-    r=a.copy()
-    for i in range(len(r)):
-        m=max(m,r[i])
-        if(r[i]<m):
-            r[i]=m
-    return r
+def f(e):return int(e)
+def get_sum_of_digits(num):
+    d=map(f,list(str(num)))
+    return sum(list(d))
 
-print(cleaned_counts([5, 5, 6, 5, 5, 5, 5, 6]))
+print(get_sum_of_digits(123))
