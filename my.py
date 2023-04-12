cc927a501d86ae0bba875586cc29a1df474a481b
Jhonny <gonzodzen@mail.ru>
2023-04-13 00:19:29 +0300

    Simple Fun #11: Swap Adjacent Bits

--- my.py
+++ my.py
@@ -1,9 +1,10 @@
-def kill_kth_bit(n,k):
-    r=list(bin(n)[slice(2,100)])
-    r.reverse()
-    if(k>=len(r)):k%=len(r)
-    r[k-1]='0'
-    r.reverse()
-    return int(''.join(r),2)
+def swap_adjacent_bits(n):
+    n=list(str(bin(n)))[slice(2,100)];i=0
+    while len(n)%8!=0:
+        n.insert(0,'0')
+    while i<len(n)-1:
+        n[i],n[i+1]=n[i+1],n[i]
+        i+=2
+    return int(''.join(n),2)
 
-print(kill_kth_bit(2147483647,16))
+print(swap_adjacent_bits(83748))
