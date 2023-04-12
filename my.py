9dccd4a8bb58f789f6fbf4246893c6215a2cb772
Jhonny <gonzodzen@mail.ru>
2023-04-13 00:32:54 +0300

    Do I get a bonus?

--- my.py
+++ my.py
@@ -1,10 +1,4 @@
-def swap_adjacent_bits(n):
-    n=list(str(bin(n)))[slice(2,100)];i=0
-    while len(n)%8!=0:
-        n.insert(0,'0')
-    while i<len(n)-1:
-        n[i],n[i+1]=n[i+1],n[i]
-        i+=2
-    return int(''.join(n),2)
+def bonus_time(s,b,c='$'):
+    return c+str(s*10) if b else c+str(s)
 
-print(swap_adjacent_bits(83748))
+print(bonus_time(10000,True))
