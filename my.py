ede95a35aa80e5b80e000f762b46995549e2b47a
Jhonny <gonzodzen@mail.ru>
2023-04-12 19:56:02 +0300

    Grasshopper - If/else syntax debug

--- my.py
+++ my.py
@@ -1,11 +1,5 @@
-def f(e,alf='abcdefghijklmnopqrstuvwxyz'):
-    return alf.index(e)+1
-
-def add_letters(*letters):
-    alf='abcdefghijklmnopqrstuvwxyz'
-    if(letters=='z' or len(letters)==0):return 'z'
-    g=sum(list(map(f,letters)))
-    g=g%len(alf)
-    return alf[g-1]
-
-print(add_letters('y', 'c', 'b'))
+def check_alive(health):
+    if(health <= 0):
+        return 1==2
+    else:
+        return 1==1
