86bcbdc40a672e9c8d1fd079f944eca18e8b0e07
Jhonny <gonzodzen@mail.ru>
2023-04-12 20:42:59 +0300

    Halving Sum

--- my.py
+++ my.py
@@ -1,5 +1,9 @@
-def check_alive(health):
-    if(health <= 0):
-        return 1==2
-    else:
-        return 1==1
+import math
+def halving_sum(n):
+    c=n
+    while n!=1:
+        n=math.floor(n/2)
+        c+=n
+    return c
+
+print(halving_sum(127))
