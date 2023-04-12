bfe38dfc995a22485beabfafa0c48cf3c86d45bd
Jhonny <gonzodzen@mail.ru>
2023-04-12 19:18:00 +0300

    Car Crash! ==== O='`o

--- my.py
+++ my.py
@@ -1,3 +1,15 @@
-a = "code"
-b = "wa.rs"
-name = a + b
+def f(e):
+    return e.find('O')!=-1
+
+def car_crash(r):
+    if(r.find('O')==-1):return False
+    r=r.split('\n')
+    r=list(filter(f,r))
+    r=''.join(r)
+    g=r.find('O')
+    return r.find('X',g)!=-1
+
+print(car_crash("""
+                      X
+            X   O='`o
+                      X   """.strip()))
