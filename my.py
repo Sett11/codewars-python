9e30a192cad8f32a314f48417a1cbeec8696539d
Jhonny <gonzodzen@mail.ru>
2023-04-12 18:09:43 +0300

    Sort array by string length

--- my.py
+++ my.py
@@ -1,24 +1,2 @@
-def f(e):
-    if(e==''):return False
-    else:return True
-
-def is_defended(a,d):
-    c=sum(a)
-    b=sum(d)
-    r=min(len(a),len(d))
-    i=0
-    while i<r:
-        if(a[i]>d[i]):
-            d[i]=''
-        else:
-            a[i]=''
-        i+=1
-    a=list(filter(f,a))
-    d=list(filter(f,d))
-    if(len(d)>len(a)):return True
-    if(len(d)==len(a)):
-        if(c>b):return False
-        if(c<b or c==b):return True
-    return False
-    
-print(is_defended([], [1,2,3]))
+def sort_by_length(a):
+    return sorted(a,key=len)
