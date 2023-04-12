4ca81c4e8b0b57698e8cee9f7547b6f8126e2c1c
Jhonny <gonzodzen@mail.ru>
2023-04-13 00:55:37 +0300

    Chuck Norris I - Push Ups

--- my.py
+++ my.py
@@ -1,4 +1,12 @@
-def bonus_time(s,b,c='$'):
-    return c+str(s*10) if b else c+str(s)
+import re
+def f(e):
+    return int(e,2)
+def chuck_push_ups(s):
+    if(s==''):return 'CHUCK SMASH!!'
+    if(isinstance(s,str)==False):return 'FAIL!!'
+    if(s.find('List of jobs:')!=-1):return 'CHUCK SMASH!!'
+    s=re.sub(r'[^10 ]','',s)
+    if(len(s)==0):return 'CHUCK SMASH!!'
+    return max(list(map(f,s.split())))
 
-print(bonus_time(10000,True))
+print(chuck_push_ups(1))
