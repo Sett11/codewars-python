a3d637973f341452694ace7c8e9eaee2b7ca8a72
Jhonny <gonzodzen@mail.ru>
2023-04-13 20:45:19 +0300

    Give me a Crazy Formula!

--- my.py
+++ my.py
@@ -1,11 +1,17 @@
-def to_bytes(n):
-    n=bin(n)[slice(2,100)];a=[]
-    while len(n)%8!=0:
-        n='0'+n
-    i=0
-    while i<len(n):
-        a.append(n[slice(i,i+8)])
-        i+=8
-    return a
+import math
+def crazy_formula(n):
+    n=list(map(int,str(n)))
+    if(len(n)%2)==0:
+        n=n[slice(0,len(n)-1)]
+    if(n[math.floor(len(n)/2)]%2!=0):
+        n[math.floor(len(n)/2)]=-n[math.floor(len(n)/2)]
+    if(n[math.floor(len(n)/2)]%2)==0 and (n[len(n)-1]%2)==0:
+        n[len(n)-1]=-n[len(n)-1]
+    if(n[math.floor(len(n)/2)]%2)==0 and (n[len(n)-1]%2)!=0:
+        n[0]=n[0]**2
+    n=abs(sum(n))
+    return n if len(str(n))==1 else crazy_formula(n)
 
-print(to_bytes(0xffff))
+print(crazy_formula(214))
+print(crazy_formula(2234))
+print(crazy_formula(77633658797932970))
