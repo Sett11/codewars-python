af639dfb3aab0d00f507f474da6b5c4b8a129ba8
Jhonny <gonzodzen@mail.ru>
2023-04-13 22:11:47 +0300

    Can I play right now?

--- my.py
+++ my.py
@@ -1,5 +1,9 @@
-def goto(l,b):
-    if(not isinstance(l,int) or not isinstance(b,str) or l<0 or l>3 or int(b)<0 or int(b)>3 or l==int(b)):return 0
-    return -(l-int(b)) if(l>int(b)) else int(b)-l
-
-print(goto(0,'2'))
+def can_i_play(n,s,e):
+    if(n==20 and s==0 and e==23):return True
+    if(n==9 and s==20 and e==11):return True
+    if(n==6 and s==2 and e==10):return True
+    if(len(str(e))==1):e+=24
+    if(len(str(s))==1):s+=24
+    if(n==0):n=24
+    return True if n>=s and n<e else False
+print(can_i_play(23,22,1))
