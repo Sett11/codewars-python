2c1d92657538d65e4f4c36845ec9723c7fdc2f91
Jhonny <gonzodzen@mail.ru>
2023-04-13 20:54:53 +0300

    Knight or Knave (From the Motion Picture: The Labyrinth)

--- my.py
+++ my.py
@@ -1,17 +1,5 @@
-import math
-def crazy_formula(n):
-    n=list(map(int,str(n)))
-    if(len(n)%2)==0:
-        n=n[slice(0,len(n)-1)]
-    if(n[math.floor(len(n)/2)]%2!=0):
-        n[math.floor(len(n)/2)]=-n[math.floor(len(n)/2)]
-    if(n[math.floor(len(n)/2)]%2)==0 and (n[len(n)-1]%2)==0:
-        n[len(n)-1]=-n[len(n)-1]
-    if(n[math.floor(len(n)/2)]%2)==0 and (n[len(n)-1]%2)!=0:
-        n[0]=n[0]**2
-    n=abs(sum(n))
-    return n if len(str(n))==1 else crazy_formula(n)
+def knight_or_knave(s):
+    if(isinstance(s,str)):s=eval(s)
+    return 'Knight!' if s else 'Knave! Do not trust.'
 
-print(crazy_formula(214))
-print(crazy_formula(2234))
-print(crazy_formula(77633658797932970))
+print(knight_or_knave('2+2==4'))
