9ce5d0bca9ce688208b4dc85405af0b95eb2f6bb
Jhonny <gonzodzen@mail.ru>
2023-04-13 21:39:58 +0300

    Simple elevator

--- my.py
+++ my.py
@@ -1,5 +1,5 @@
-def is_kiss(w):
-    w=list(map(len,w.split()))
-    return 'Good work Joe!' if len(w)>=max(w) else 'Keep It Simple Stupid'
+def goto(l,b):
+    if(not isinstance(l,int) or not isinstance(b,str) or l<0 or l>3 or int(b)<0 or int(b)>3 or l==int(b)):return 0
+    return -(l-int(b)) if(l>int(b)) else int(b)-l
 
-print(is_kiss('Joe had a bad day'))
+print(goto(0,'2'))
