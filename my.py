58a93a59899d611094d99cfdd35e913b6b8bbb45
Jhonny <gonzodzen@mail.ru>
2023-04-13 22:41:48 +0300

    Hello World - Without Strings

--- my.py
+++ my.py
@@ -1,9 +1,8 @@
-def can_i_play(n,s,e):
-    if(n==20 and s==0 and e==23):return True
-    if(n==9 and s==20 and e==11):return True
-    if(n==6 and s==2 and e==10):return True
-    if(len(str(e))==1):e+=24
-    if(len(str(s))==1):s+=24
-    if(n==0):n=24
-    return True if n>=s and n<e else False
-print(can_i_play(23,22,1))
+from functools import reduce
+def f(a,b):
+    a+=b
+    return a
+def hello_world():
+    return reduce(f,map(chr,[72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33]))
+
+print(hello_world())
