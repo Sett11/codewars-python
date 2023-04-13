0e1839790376a420c66316795af6d37cbf2e21e0
Jhonny <gonzodzen@mail.ru>
2023-04-13 23:52:36 +0300

    Boolean Trilogy #2: Calculate Boolean Expression (Easy)

--- my.py
+++ my.py
@@ -1,5 +1,10 @@
-def plastic_balance(a):
-     if(len(a)==0 or sum([a[0],a[-1]])==sum(a[slice(1,-1)])):return a
-     return plastic_balance(a[slice(1,-1)])
+import re
+def calculate(e,v):
+    e=re.sub(r'\&','and',e)
+    e=re.sub(r'\|','or',e)
+    for i in range(len(e)):
+        if(e[i] in v):
+            e=e.replace(e[i],str(v[e[i]]))
+    return eval(e)
 
-print(plastic_balance([1,2,3,4,5]))
+print(calculate("B & A | C", {"A": 1, "B": 0, "C": 1}))
