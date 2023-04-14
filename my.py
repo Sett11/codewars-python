51d888b0d1081479ab9277434cb528b20ef47e33
Jhonny <gonzodzen@mail.ru>
2023-04-14 12:00:59 +0300

    Wealth equality, finally!

--- my.py
+++ my.py
@@ -1,5 +1,8 @@
-def is_smooth(a):
-    return a[0]==sum(a[slice(len(a)//2-1,-len(a)//2+1)]) and a[0]==a[len(a)-1] if len(a)%2==0 else a[0]==a[len(a)//2] and a[0]==a[len(a)-1]
+def redistribute_wealth(a):
+    b=sum(a)/len(a);l=len(a);i=0
+    a.clear()
+    while i<l:
+        a.append(b)
+        i+=1
 
-print(is_smooth([-12, 34, 40, -5, -12, 4, 0, 0, -12]))
-print(is_smooth([7, 2, 2, 5, 10, 7]))
+print(redistribute_wealth([2,2,3]))
