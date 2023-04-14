bbc43360aab267704ee7fe7571948654f20068e0
Jhonny <gonzodzen@mail.ru>
2023-04-14 15:07:17 +0300

    Simple Fun #32: Create Anagram

--- my.py
+++ my.py
@@ -1,16 +1,10 @@
-def passer_rating(att, yds, comp, td, ints):
-    a=((comp/att)-0.3)*5
-    b=((yds/att)-3)*0.25
-    c=(td/att)*20
-    d=2.375-((ints/att)*25)
-    if(a>2.375):a=2.375
-    if(b>2.375):b=2.375
-    if(c>2.375):c=2.375
-    if(d>2.375):d=2.375
-    if(a<0):a=0
-    if(b<0):b=0
-    if(c<0):c=0
-    if(d<0):d=0
-    return round(((a+b+c+d)/6)*100,1)
+def create_anagram(s,t,i=0):
+    s=list(s);t=list(t)
+    while i<len(s):
+        if(t[i] in s):
+            s[s.index(t[i])]='&'
+        i+=1
+    return len(list(filter(lambda e:e!='&',s)))
 
-print(passer_rating(432, 3554, 291, 28, 2))
+print(create_anagram("AABAA", "BBAAA"))
+print(create_anagram("OVGHK", "RPGUC"))
