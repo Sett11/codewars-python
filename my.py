c9b020c1752d4efbbf5f99b4952fe93bb21f4023
Jhonny <gonzodzen@mail.ru>
2023-04-14 16:20:15 +0300

    Barbell weight

--- my.py
+++ my.py
@@ -1,8 +1,6 @@
-def S2N(m,n,c=0,i=0):
-    l=list(range(0,m+1))
-    while i<=n:
-        c+=sum(map(lambda e: e**i,l))
-        i+=1
-    return c
+import re
+def barbell_weight(b):
+    o={'R':25,'B':20,'Y':15,'G':10,'W':5,'r':2.5,'b':2,'y':1.5,'g':1,'w':0.5,'c':2.5}
+    return int(sum(list(map(lambda e:o[e], list(re.sub(r'\-','',b[slice(0,10)])))))*2+20)
 
-print(S2N(10,9))
+print(barbell_weight('-------bcr--------------------rcb-------'))
