90cb4e151452205a5d12a325df44a865c8b39c2d
Jhonny <gonzodzen@mail.ru>
2023-04-15 19:59:17 +0300

    Password Check - Binary to String

--- my.py
+++ my.py
@@ -1,6 +1,10 @@
 def binary_to_string(s):
     def f(e):
         return chr(int(e,2))
-    return ''.join(list(map(f,filter(lambda e:e,s.replace('0b','&').split('&')))))
+    return ''.join(list(map(f,filter(lambda e:e,s.split()))))
 
-print(binary_to_string('0b10000110b11000010b1110100'))
+def decode_pass(a,s):
+    s=binary_to_string(s)
+    return s if s in a else False
+
+print(decode_pass(['password123', 'admin', 'admin1'], '01110000 01100001 01110011 01110011 01110111 01101111 01110010 01100100 00110001 00110010 00110011'))
