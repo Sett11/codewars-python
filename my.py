2e8bfa8e547f7c818953339dd22f79aad202b45b
Jhonny <gonzodzen@mail.ru>
2023-04-15 12:36:56 +0300

    noobCode 04: HOT SINGLES...compare two arrays, return the unpaired items !

--- my.py
+++ my.py
@@ -1,6 +1,9 @@
-def is_bouncy(n):
-    a=sorted(list(map(int,str(n))))
-    b=sorted(a,reverse=True)
-    c=list(map(int,str(n)))
-    return c!=a and c!=b
-print(is_bouncy(120))
+def hot_singles(a,b):
+    r=[]
+    x=list(filter(lambda e: e not in b,a))
+    x.extend(list(filter(lambda e: e not in a,b)))
+    for i in x:
+        if(i not in r):r.append(i)
+    return r
+
+print(hot_singles([10, 200, 30], [10, 20, 3, 4, 5, 5, 5, 200]))
