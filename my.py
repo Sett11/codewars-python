5f9969c3313d2098b3cad0ce55cf80fa4a31a560
Jhonny <gonzodzen@mail.ru>
2023-04-15 12:05:28 +0300

    Bouncy Numbers

--- my.py
+++ my.py
@@ -1,16 +1,6 @@
-def substring_test(s1,s2):
-    a=[s1,s2]
-    a.sort(key=len)
-    a[0]=a[0].lower();a[1]=a[1].lower();i=0;j=i+2;m=min([len(a[0]),len(a[1])])
-    while i<m-1:
-        while j<m+1:
-            if(a[1].find(a[0][slice(i,j)])!=-1):
-                return True
-            j+=1
-        i+=1
-        j=i+2
-    return False
-
-
-
-print(substring_test('Audn3uheNkr8hlB', 'G4lpnK'))
+def is_bouncy(n):
+    a=sorted(list(map(int,str(n))))
+    b=sorted(a,reverse=True)
+    c=list(map(int,str(n)))
+    return c!=a and c!=b
+print(is_bouncy(120))
