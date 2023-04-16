d11bb90a7387f157fad0e498c002e05e82cb31a0
Jhonny <gonzodzen@mail.ru>
2023-04-16 18:18:42 +0300

    Pernicious Numbers

--- my.py
+++ my.py
@@ -1,9 +1,20 @@
-def hop_across(a,i=0,j=0,c=0):
-    while i<len(a):
-        i+=a[i];c+=1
-    a.reverse()
-    while j<len(a):
-        j+=a[j];c+=1
-    return c
+import math
+def f(x):
+        if(x<2):return False
+        if(x==2):return True
+        j=2
+        while j<math.sqrt(x)+1:
+            if(x%j==0):
+                 return False
+            j+=1
+        return True
 
-print(hop_across([2,2,3,1,1,2,1]))
+def pernicious(n):
+    i=2;a=[]
+    while i<=n:
+         if(f(sum(map(int,list(bin(i)[slice(2,len(bin(i)))]))))):
+              a.append(i)
+         i+=1
+    return a if len(a) else "No pernicious numbers"
+
+print(pernicious(5))
