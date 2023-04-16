8fe769449751cdfc6d61ef77ffc363cad918caea
Jhonny <gonzodzen@mail.ru>
2023-04-17 00:54:00 +0300

    Mirroring cipher

--- my.py
+++ my.py
@@ -1,4 +1,7 @@
-def string_to_int_list(s):
-    return list(map(int,filter(lambda e:e,s.split(','))))
+def mirror(s,c='abcdefghijklmnopqrstuvwxyz'):
+    s=list(s.lower())
+    for i in range(len(s)):
+        s[i]=c[len(c)-c.index(s[i])-1] if s[i] in c else s[i]
+    return ''.join(s)
 
-print(string_to_int_list("21,,12,23,34,45"))
+print(mirror("hello", "abcdefgh"))
