c485518a79e9631b10dd3a342b6bc54bde600c73
Jhonny <gonzodzen@mail.ru>
2023-04-16 14:24:30 +0300

    Chuck Norris III - Cage Match

--- my.py
+++ my.py
@@ -1,3 +1,9 @@
-def one_punch(s):return 'Broken!' if isinstance(s,int) or len(s)==0 or type(s)==list else ' '.join(list(map(lambda e: e.replace('a','').replace('e','').replace('A','').replace('E',''),sorted(s.split()))))
+def head_smash(a):
+    if(isinstance(a,int)):return "This isn't the gym!!"
+    return  'Gym is empty' if len(a)==0 else list(map(lambda e:e.replace('O',' '),a))
 
-print(one_punch('Friend Beer Beard Monkey Laptop'))
+print(head_smash([
+'*****************************************',
+'**  _O_   *   _O_   *   _O_   *   _O_  **',
+'** /(.)J  *  C(.)J  *  /(.)J  *  C(.)J **',
+'** _| |_  *  _| |_  *  _( )_  *  _( )_ *']))
