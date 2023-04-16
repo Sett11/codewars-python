6b4dc37b8ef62d381dd10b411fd31b14fb5f000f
Jhonny <gonzodzen@mail.ru>
2023-04-16 15:15:20 +0300

    Find the motif in DNA sequence.

--- my.py
+++ my.py
@@ -1,9 +1,8 @@
-def head_smash(a):
-    if(isinstance(a,int)):return "This isn't the gym!!"
-    return  'Gym is empty' if len(a)==0 else list(map(lambda e:e.replace('O',' '),a))
+def motif_locator(s,c,i=0):
+    r=[]
+    while i<len(s)-len(c)-1:
+        r.append(s.find(c,i)+1)
+        i+=1
+    return list(filter(lambda e:e,sorted(list(set(r)))))
 
-print(head_smash([
-'*****************************************',
-'**  _O_   *   _O_   *   _O_   *   _O_  **',
-'** /(.)J  *  C(.)J  *  /(.)J  *  C(.)J **',
-'** _| |_  *  _| |_  *  _( )_  *  _( )_ *']))
+print(motif_locator("ACGTTACAACGTTAG", "ACGT"))
