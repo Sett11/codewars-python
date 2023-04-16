96d6a464fdc930531fe44a7a2772e079deed49bf
Jhonny <gonzodzen@mail.ru>
2023-04-16 15:41:52 +0300

    DNA Sequence Tester

--- my.py
+++ my.py
@@ -1,8 +1,8 @@
-def motif_locator(s,c,i=0):
-    r=[]
-    while i<len(s)-len(c)-1:
-        r.append(s.find(c,i)+1)
-        i+=1
-    return list(filter(lambda e:e,sorted(list(set(r)))))
+def check_DNA(s1,s2):
+    s2=list(s2)
+    s2.reverse()
+    s2=''.join(s2).replace('T','1').replace('C','2').replace('G','3').replace('A','4')
+    s1=s1.replace('A','1').replace('T','4').replace('C','3').replace('G','2')
+    return s1==s2 or s1.find(s2)!=-1 or s2.find(s1)!=-1
 
-print(motif_locator("ACGTTACAACGTTAG", "ACGT"))
+print(check_DNA('GTCTTAGTGTAGCTATGCATGC','GCATGCATAGCTACACTACGAC'))
