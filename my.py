225711aac924106bdb974d50287a1ca90082c303
Jhonny <gonzodzen@mail.ru>
2023-04-16 20:44:08 +0300

    Loose Change!

--- my.py
+++ my.py
@@ -1,6 +1,7 @@
-def esrever(s):
-    if(len(s)==0):return ''
-    c=s[len(s)-1];s=s[:-1]
-    return ' '.join(list(map(lambda e: ''.join(list(e)[::-1]), s.split()))[::-1])+c
+CHANGE={'penny': 0.01, 'nickel': 0.05, 'dime': 0.1, 'quarter': 0.25, 'dollar': 1.0}
 
-print(esrever('hello world.'))
+def change_count(c):
+    c='$'+str(round(sum(list(map(lambda e:CHANGE[e], c.split()))),2))
+    return c+'0' if(len(c[slice(c.find('.')+1,len(c))])==1) else c
+
+print(change_count('quarter quarter'))
