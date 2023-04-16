899b0382f46632f3ad77c4b4c27d0c26396cdc47
Jhonny <gonzodzen@mail.ru>
2023-04-16 11:11:55 +0300

    Inertial Array

--- my.py
+++ my.py
@@ -1,5 +1,10 @@
-import re
-def guess_my_number(s,n='123-451-2345'):
-    return re.sub(r'[^{}-]'.format(s),'#',n)
+def is_inertial(a):
+    if(len(a)==0):return False
+    l=len(list(filter(lambda e: e%2!=0,a)))
+    t=max(a)
+    r=list(filter(lambda e: e!=t,a))
+    z=list(filter(lambda e: e%2==0,r))
+    v=list(filter(lambda e: e%2!=0,r))
+    return min(v,default=0)>max(z,default=0) and t%2==0 and l>0
 
-print(guess_my_number('01234'))
+print(is_inertial([11, 4, 20, 9, 2, 8]))
