471ad7bb79a08f6ef622198fb2141b0fbdabae29
Jhonny <gonzodzen@mail.ru>
2023-04-17 01:20:47 +0300

    Moving Average

--- my.py
+++ my.py
@@ -1,4 +1,9 @@
-def rank_of_element(a,i):
-    return len(list(filter(lambda e:e<=a[i],a[slice(0,i)])))+len(list(filter(lambda e:e<a[i],a[slice(i+1,len(a))])))
+def moving_average(a,n):
+    if(n<1):return None
+    i=0;r=[]
+    while i<=len(a)-n:
+        r.append(sum(a[slice(i,i+n)])/n)
+        i+=1
+    return r if r else None
 
-print(rank_of_element([2,1,2,1,2],2))
+print(moving_average([40, 30, 50, 46, 39, 44],4))
