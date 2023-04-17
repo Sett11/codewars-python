17ad96229fb4b76f6433d5fab712a311a4eab386
Jhonny <gonzodzen@mail.ru>
2023-04-17 18:20:05 +0300

    BASIC: Making Six Toast.

--- my.py
+++ my.py
@@ -1,9 +1,4 @@
-def moving_average(a,n):
-    if(n<1):return None
-    i=0;r=[]
-    while i<=len(a)-n:
-        r.append(sum(a[slice(i,i+n)])/n)
-        i+=1
-    return r if r else None
+def six_toast(n):
+    return n-6 if n>6 else 6-n
 
-print(moving_average([40, 30, 50, 46, 39, 44],4))
+print(six_toast(3))
