b59f6824f990c24c84284e8397fb59abf53fc36a
Jhonny <gonzodzen@mail.ru>
2023-04-18 00:42:35 +0300

    Drunk friend

--- my.py
+++ my.py
@@ -1,4 +1,9 @@
-def wdm(s):
-    return ' '.join(s.replace('puke','').replace('hiccup','').split())
+def f(e):
+    a=ord(e)
+    if(a>64 and a<91):return chr((26-(a-65))+64)
+    if(a>96 and ord(e)<123):return chr((26-(a-96))+97)
+    return e
+def decode(s):
+    return 'Input is not a string' if not isinstance(s,str) else ''.join(list(map(f,list(s))))
 
-print(wdm("puke All's well hiccup     that ends hiccup well puke"))
+print(decode("Ovg'h hdrn rm gsv ulfmgzrm!"))
