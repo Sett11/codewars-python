2c4be73cf01b784c38e98ac0709e801f7d31364a
Jhonny <gonzodzen@mail.ru>
2023-04-17 23:48:33 +0300

    Simple Fun #88: Sort By Height

--- my.py
+++ my.py
@@ -1,5 +1,8 @@
-import re
-def evenator(s):
-    return ' '.join(list(map(lambda e: e+e[len(e)-1] if len(e)%2!=0 else e,re.sub(r'[.,?!_]','',s).split())))
+def sort_by_height(a):
+    b=list(map(lambda e:'&' if e!=-1 else e,a));c=sorted(list(filter(lambda e:e>-1,a)))
+    for i in range(len(b)):
+        if(b[i]=='&'):
+            b[i]=c.pop(0)
+    return b
 
-print(evenator('underscore is not considered a word..in this case,'))
+print(sort_by_height([-1, 150, 190, 170, -1, -1, 160, 180]))
