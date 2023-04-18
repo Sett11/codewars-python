d3f6a9263059eca5b932fce57af1eb4bbac66005
Jhonny <gonzodzen@mail.ru>
2023-04-18 18:44:58 +0300

    Stern-Brocot Sequence Part I

--- my.py
+++ my.py
@@ -1,7 +1,8 @@
-def range_function(*a):
-    b=[*a]
-    if(len(b)==1):return list(range(1,b[0]+1))
-    if(len(b)==2):return list(range(b[0],b[1]+1))
-    if(len(b)==3):return list(range(b[0],b[2]+1,b[1]))
+def stern_brocot(n):
+    a=[1,1];c=1
+    while n not in a:
+            a.extend([a[len(a)-c]+a[len(a)-c-1],a[len(a)-c]])
+            c+=1
+    return a.index(n)
 
-print(range_function(2,3,15))
+print(stern_brocot(19))
