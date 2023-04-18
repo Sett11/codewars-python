6cfe31d978844bc0995fe1636b98de9384996c0e
Jhonny <gonzodzen@mail.ru>
2023-04-18 23:06:54 +0300

    Networking 101/4: Determine the IPv4 network prefix in CIDR notation

--- my.py
+++ my.py
@@ -1,4 +1,12 @@
-def ipv4_to_binary(ipv4_addr: str)->str:
-    return '.'.join(list(map(lambda e:'0'*(8-len(bin(int(e))[2:]))+bin(int(e))[2:],ipv4_addr.split('.'))))
+def network_cidr(ipv4_addr: str, net_mask: str) -> str:
+    one=list('.'.join(list(map(lambda e:'0'*(8-len(bin(int(e))[2:]))+bin(int(e))[2:],ipv4_addr.split('.')))))
+    two=list('.'.join(list(map(lambda e:'0'*(8-len(bin(int(e))[2:]))+bin(int(e))[2:],net_mask.split('.')))))
+    three=list(''.join(two).replace('1','0'))
+    i=0
+    while i<len(one):
+        if(one[i]=='1' and two[i]=='1'):
+            three[i]='1'
+        i+=1
+    return '.'.join(list(map(lambda e:str(int(e,2)),''.join(three).split('.'))))+'/'+str(''.join(two).count('1'))
 
-print(ipv4_to_binary("192.168.0.1"))
+print(network_cidr("192.168.1.0",   "255.255.254.0"))
