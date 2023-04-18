e24533425f47522cc8dc5f75f19c7e7a30a8355c
Jhonny <gonzodzen@mail.ru>
2023-04-19 01:06:14 +0300

    Pairing brackets

--- my.py
+++ my.py
@@ -1,8 +1,19 @@
-def interlockable(a,b):
-    a=bin(a)[2:][::-1];b=bin(b)[2:][::-1];i=0
-    while i<min(len(a),len(b)):
-        if(a[i]=='1' and b[i]=='1'):return False
+def bracket_pairs(s):
+    a=[];i=0;j=i+1;c=0
+    while i<len(s):
+        if(s[i]=='('):
+            c+=1
+            j=i+1
+            while j<len(s):
+                if(s[j]=='('):
+                    c+=1
+                if(s[j]==')'):
+                    c-=1
+                if(c==0):
+                    a.append([i,j])
+                    break
+                j+=1
         i+=1
-    return True
+    return False if s.count('(')!=s.count(')') or c!=0 else dict(a)
 
-print(interlockable(3,6))
+print(bracket_pairs('))))))))(x)()x()))'))
