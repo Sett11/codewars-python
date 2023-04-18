25e838e505f5a1f61fc2faebb400bc81aa65443d
Jhonny <gonzodzen@mail.ru>
2023-04-18 20:42:42 +0300

    Consecutive Vowels in a String

--- my.py
+++ my.py
@@ -1,9 +1,14 @@
-from functools import reduce
-def string_color(s):
-    a=list(map(lambda e:ord(e),list(s)))
-    one=hex(sum(a)%256)[2:]
-    two=hex(reduce(lambda a,c:a*c,a,1)%256)[2:]
-    three=hex(abs(a[0]-sum(a[1:]))%256)[2:]
-    return ('0'*(2-len(one))+one+'0'*(2-len(two))+two+'0'*(2-len(three))+three).upper() if len(s)>2 else None
+def get_the_vowels(s):
+    v = list('aeiou')
+    i = 0
+    j = 0
+    c = 0
+    while i < len(s):
+        if (s[i] == v[j % len(v)]):
+            c += 1
+            j += 1
+        i += 1
+    return c
 
-print(string_color("John Doe"))
+
+print(get_the_vowels('akfheujfkgiaaaofmmfkdfuaiiie'))
