fa63be3a1b9ce8f822ef5c02b65553a44b978965
Jhonny <gonzodzen@mail.ru>
2023-04-20 00:07:43 +0300

    16+18=214

--- my.py
+++ my.py
@@ -1,9 +1,15 @@
-def least_larger(a,i):
-    r=a[i]
-    m=1000
-    for e in a:
-        if(e>r):
-            m=min(m,e)
-    return a.index(m) if m!=1000 else -1
+def add(n1,n2):
+    n1=list(map(int,list(str(n1))))
+    n2=list(map(int,list(str(n2))))
+    while len(n1)<len(n2):
+        n1.insert(0,0)
+    while len(n2)<len(n1):
+        n2.insert(0,0)
+    i=len(n1)-1
+    r=[]
+    while i>=0:
+        r.insert(0,n1[i]+n2[i])
+        i-=1
+    return int(''.join(list(map(str,r))))
 
-print(least_larger([4, 1, 3, 5, 6],0))
+print(add(2,11))
