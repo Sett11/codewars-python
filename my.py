696a7b78b75cea8b95b885e2d8ce0e140ce3b491
Jhonny <gonzodzen@mail.ru>
2023-04-19 20:31:33 +0300

    Dictionary of Factors

--- my.py
+++ my.py
@@ -1,14 +1,19 @@
-def switch_dict(d):
-    o={}
-    for i in d:
-        o[d[i]]=[]
-    for i in d:
-        o[d[i]].append(i)
-    return o
+import math
+def f(x):
+    i = 2
+    r = []
+    while i < math.sqrt(x)+1:
+        if (x % i == 0):
+            r.append(i)
+            r.append(x/i)
+        i += 1
+    r = sorted(set(filter(lambda e: e > 1 and e != x, map(int, r))))
+    return [x, r if len(r) else ['None']]
 
-print(switch_dict({
-          'Ice': 'Cream',
-          'Age': '21',
-          'Light': 'Cream',
-          'Double': 'Cream'
-          }))
+
+def factorsRange(n, m):
+    l = list(range(n, m+1))
+    return dict(list(map(f, l)))
+
+
+print(factorsRange(200, 220))
