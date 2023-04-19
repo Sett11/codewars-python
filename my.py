6192821d6275d1b225a0d410756da2d06a59b146
Jhonny <gonzodzen@mail.ru>
2023-04-19 18:50:26 +0300

    Even and Odd !

--- my.py
+++ my.py
@@ -1,19 +1,6 @@
-def count_duplicates(a,b,c):
-    i=0;r=[];x=0
-    while i<len(a):
-        r.append(a[i]+str(b[i])+str(c[i]))
-        i+=1
-    i=0;j=i+1
-    while i<len(r):
-        while j<len(r):
-            if(r[i]==r[j]):
-                x+=1
-                break
-            j+=1
-        i+=1;j=i+1
-    return x
-    
+def even_and_odd(n):
+    n = list(map(int, list(str(n))))
+    return tuple([int(''.join(list(map(str, filter(lambda e: e % 2 == 0, n)))) or 0), int(''.join(list(map(str, filter(lambda e: e % 2 != 0, n)))) or 0)])
 
-print(count_duplicates( ['Jack','Ben','Jack','Ben','Jack','Jack']
-        ,[25,25,34,25,25,34]
-        ,[180,180,200,180,180,200]))
+
+print(even_and_odd(4628))
