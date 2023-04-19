bdb200c0e4f65abe75bae03acaa5be588b4db2a7
Jhonny <gonzodzen@mail.ru>
2023-04-19 21:09:19 +0300

    Dictionary Merge

--- my.py
+++ my.py
@@ -1,19 +1,14 @@
-import math
-def f(x):
-    i = 2
-    r = []
-    while i < math.sqrt(x)+1:
-        if (x % i == 0):
-            r.append(i)
-            r.append(x/i)
-        i += 1
-    r = sorted(set(filter(lambda e: e > 1 and e != x, map(int, r))))
-    return [x, r if len(r) else ['None']]
+def merge(*a):
+    d = list(a)
+    o = {}
+    for i in d:
+        for j in i:
+            if (j in o):
+                o[j].append(i[j])
+            else:
+                o[j] = []
+                o[j].append(i[j])
+    return o
 
 
-def factorsRange(n, m):
-    l = list(range(n, m+1))
-    return dict(list(map(f, l)))
-
-
-print(factorsRange(200, 220))
+print(merge({"A": 1, "B": 2},{"A": 3}))
