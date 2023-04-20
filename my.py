9b41306aaee3e0e569664a1e33b8385f15f9fe7d
Jhonny <gonzodzen@mail.ru>
2023-04-20 21:56:40 +0300

    You are a Robot: Translating a Path

--- my.py
+++ my.py
@@ -1,21 +1,29 @@
-def f(x, y):
-    r = []
-    i = 1
-    while i <= x:
-        r.append(y//i)
-        i += 1
+def f(x):
+    r=''
+    if(x[0]=='^'):r='Take {} step up'.format(x[1])
+    if(x[0]=='v'):r='Take {} step down'.format(x[1])
+    if(x[0]=='<'):r='Take {} step left'.format(x[1])
+    if(x[0]=='>'):r='Take {} step right'.format(x[1])
+    if(x[1]>1):r=r.replace('step','steps')
     return r
+def walk(c):
+    a=[]
+    i=0
+    j=i+1
+    s=c+'a'
+    while i<len(s)-1:
+        if(s[i]==s[i+1]):
+            j=i+1
+            while j<len(s):
+                if(s[j]!=s[i]):
+                    a.append(s[i:j])
+                    i=j-1
+                    break
+                j+=1
+        else:
+            a.append(s[i])
+        i+=1
+    a=list(map(lambda e:[e[0],len(e)],a))
+    return '\n'.join(list(map(f,a))) if c else 'Paused'
 
-
-def distribute_seats(n, l):
-    if(l==(1,2,3)):return [1,1,2]
-    l = list(map(lambda e: f(n, e), l))
-    r = []
-    for i in l:
-        r.extend(i)
-    r.sort(reverse=True)
-    r = r[:n]
-    return list(map(lambda e: len(list(filter(lambda u: u in r, e))), l))
-
-
-print(distribute_seats(4, (1, 2, 3)))
+print(walk("<<>"))
