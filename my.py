b61e9980c483a047ffd2946053d0d432ca64d791
Jhonny <gonzodzen@mail.ru>
2023-04-22 01:06:22 +0300

    Noob Debug 1: Fix the string sum!

--- my.py
+++ my.py
@@ -1,13 +1,8 @@
-def f(x):
-    x=round(x)
-    n=x
-    m=x
-    while n%5!=0 and m%5!=0:
-        n-=1
-        m+=1
-    return list(filter(lambda e: e%5==0,[m,n]))[0]
+def add(s1,s2):
+    l=[list(s1),list(s2)]
+    a=[]
+    for i in l:
+        a.append(sum(list(map(lambda e:ord(e),i))))
+    return sum(a)
 
-def round_to_five(l):
-    return list(map(f,list(map(lambda e:e+0.01,l))))
-
-print(round_to_five([22.5, 544.9, 77.5]))
+print(add('aa','bb'))
