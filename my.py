9fee5beaf3485f2ee05e85e3a2be0778e9838cfa
Jhonny <gonzodzen@mail.ru>
2023-04-21 17:11:11 +0300

    Alternating sort

--- my.py
+++ my.py
@@ -1,4 +1,10 @@
-def multiply(n):
-    return n*(5**len(str(abs(n))))
+def alternate_sort(l):
+    a=sorted(list(filter(lambda e:e>=0,l)))
+    b=sorted(list(filter(lambda e:e<0,l)),reverse=True)
+    c=[]
+    while len(a) or len(b):
+        if(len(b)):c.append(b.pop(0))
+        if(len(a)):c.append(a.pop(0))
+    return c
 
-print(multiply(200))
+print(alternate_sort([5, -42, 2, -3, -4, 8, -9,]))
