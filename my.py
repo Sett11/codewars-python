13819c9df02238233373ce51ec858b3276777a0c
Jhonny <gonzodzen@mail.ru>
2023-04-22 00:25:05 +0300

    Grandma learning to text

--- my.py
+++ my.py
@@ -1,5 +1,5 @@
-import math
-def pop_shift(s):
-    return [s[math.floor(len(s)/2)+1:][::-1],s[:math.ceil(len(s)/2)-1],s[math.floor(len(s)/2)]] if len(s)%2!=0 else [s[int(len(s)/2):][::-1],s[:int(len(s)/2)],'']
+import re
+def textin(s):
+    return re.sub(r'too|two|to','2',s,flags=re.IGNORECASE)
 
-print(pop_shift('letstalkaboutjavascriptthebestlanguage'))
+print(textin('TwO ToO tWo tOo'))
