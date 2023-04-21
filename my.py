7e7972f0d52b99433f0d5bd04267631195564219
Jhonny <gonzodzen@mail.ru>
2023-04-22 00:55:07 +0300

    Round to nearest 0 or 5

--- my.py
+++ my.py
@@ -1,7 +1,13 @@
-def is_dd(n):
-    for i in str(n):
-        if(str(n).count(i)==int(i)):
-            return True
-    return False
+def f(x):
+    x=round(x)
+    n=x
+    m=x
+    while n%5!=0 and m%5!=0:
+        n-=1
+        m+=1
+    return list(filter(lambda e: e%5==0,[m,n]))[0]
 
-print(is_dd(664444309))
+def round_to_five(l):
+    return list(map(f,list(map(lambda e:e+0.01,l))))
+
+print(round_to_five([22.5, 544.9, 77.5]))
