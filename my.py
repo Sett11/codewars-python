115c37f5be0144bb5caf1f3b7c154042bcd11eb7
Jhonny <gonzodzen@mail.ru>
2023-04-22 21:44:52 +0300

    Populate hash with array keys and default value

--- my.py
+++ my.py
@@ -1,4 +1,4 @@
-def solution(t,i,d):
-    return t[i] if abs(i)<=len(t) else d
+def populate_dict(k,d):
+    return dict([[i,d] for i in k])
 
-print(solution(list(range(1, 4)),-5,'a'))
+print(populate_dict(["draft", "completed"], 0))
