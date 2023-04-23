8d386d8929f2faec2d0c92993f2650390f6dfc83
Jhonny <gonzodzen@mail.ru>
2023-04-23 11:05:47 +0300

    Speed Limit

--- my.py
+++ my.py
@@ -1,9 +1,13 @@
-def f(x, c=1):
-    return c if x == 1 else f(x-1, c*x)
+def speed_limit(s,l):
+    r=list(filter(lambda u:u>0,list(map(lambda e:s-e,l))))
+    c=0
+    for i in r:
+        if(i<20 and i>=10):
+            c+=100
+        if(i>=20 and i<30):
+            c+=250
+        if(i>=30):
+            c+=500
+    return c
 
-
-def sum_factorial(l):
-    return sum(map(f, l))
-
-
-print(sum_factorial([4, 6]))
+print(speed_limit(86, [120, 110, 100, 90, 80, 100]))
