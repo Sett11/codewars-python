f3d567d8b5726c46cb7d72b808217c6bb5ea716a
Jhonny <gonzodzen@mail.ru>
2023-04-24 12:01:02 +0300

    Elementary Arithmetic - Carries Count

--- my.py
+++ my.py
@@ -1,7 +1,26 @@
-import re
-def case_sensitive(s):
-    r=re.sub(r'[a-z]','',s)
-    return [False,[r]] if r else [True,[]]
-    
-print(case_sensitive('asd'))
-print(case_sensitive('cellS'))
+def f(x,y):
+    i=len(x)-1
+    c=0
+    t=0
+    while i>=0:
+        q=int(x[i])+int(y[i])
+        if(q+t>9):
+            t=1
+        if(q+t<10):
+            t=0
+        if(q+t>9):
+            c+=1
+            t=1
+        i-=1
+    return c
+
+def solve(s):
+    s=s.split()
+    i=0
+    r=[]
+    while i<len(s):
+        r.append(f(s[i],s[i+1]))
+        i+=2
+    return '\n'.join(map(lambda e: 'No carry operation' if not e else '{} carry operations'.format(e),r))
+
+print(solve("321 679\n098 805\n123 867"))
