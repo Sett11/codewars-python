54f3ec70081dc5d36b6e600f89c8b4bc270f6cff
Jhonny <gonzodzen@mail.ru>
2023-04-24 22:34:39 +0300

    Find twins

--- my.py
+++ my.py
@@ -1,5 +1,5 @@
-def majority(a):
-    a=sorted([[i,a.count(i)] for i in a],key=lambda e:e[1],reverse=True)
-    return a[0][0] if len(set(list(map(lambda u:u[0],list(filter(lambda e:e[1]==a[0][1],a))))))==1 else None
+def elimination(a):
+    a=list(filter(lambda e:e!='',[i if a.count(i)>1 else '' for i in a]))
+    return a[0] if len(a) else None
 
-print(majority(["A", "B", "A"]))
+print(elimination([2, 5, 34, 1, 22]))
