936b0124d6e61a380b4f5367f97b5123229ef3cd
Jhonny <gonzodzen@mail.ru>
2023-04-24 10:44:16 +0300

    Alphabetically ordered

--- my.py
+++ my.py
@@ -1,13 +1,6 @@
-def speed_limit(s,l):
-    r=list(filter(lambda u:u>0,list(map(lambda e:s-e,l))))
-    c=0
-    for i in r:
-        if(i<20 and i>=10):
-            c+=100
-        if(i>=20 and i<30):
-            c+=250
-        if(i>=30):
-            c+=500
-    return c
+def alphabetic(s):
+    r=list(map(ord,list(s)))
+    return r==sorted(r)
 
-print(speed_limit(86, [120, 110, 100, 90, 80, 100]))
+print(alphabetic('kata'))
+print(alphabetic('ant'))
