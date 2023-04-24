cf8d0a85d028bc9c20610e61e0ed9fd089aa95ce
Jhonny <gonzodzen@mail.ru>
2023-04-24 23:33:17 +0300

    What adds up

--- my.py
+++ my.py
@@ -1,26 +1,9 @@
-def f(x,y):
-    i=len(x)-1
-    c=0
-    t=0
-    while i>=0:
-        q=int(x[i])+int(y[i])
-        if(q+t>9):
-            t=1
-        if(q+t<10):
-            t=0
-        if(q+t>9):
-            c+=1
-            t=1
-        i-=1
-    return c
+def addsup(a1,a2,a3):
+    r=[]
+    for i in a1:
+        for j in a2:
+            if(i+j in a3):
+                r.append([i,j,i+j])
+    return r
 
-def number_of_carries(a,b):
-    a=str(a)
-    b=str(b)
-    if(len(a)<len(b)):
-        a='0'*(len(b)-len(a))+a
-    if(len(b)<len(a)):
-        b='0'*(len(a)-len(b))+b
-    return f(a,b)
-
-print(number_of_carries(9999,1))
+print(addsup([1,2,5],[3,1],[5,4,6]))
