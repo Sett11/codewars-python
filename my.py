6a8b2b63583f6d413abec2be04a3e335f56e7328
Jhonny <gonzodzen@mail.ru>
2023-04-24 10:48:55 +0300

    Case-sensitive!

--- my.py
+++ my.py
@@ -1,6 +1,7 @@
-def alphabetic(s):
-    r=list(map(ord,list(s)))
-    return r==sorted(r)
-
-print(alphabetic('kata'))
-print(alphabetic('ant'))
+import re
+def case_sensitive(s):
+    r=re.sub(r'[a-z]','',s)
+    return [False,[r]] if r else [True,[]]
+    
+print(case_sensitive('asd'))
+print(case_sensitive('cellS'))
