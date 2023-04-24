d73b08cd4c7a3cd85055c96eb28ea673e49c9f00
Jhonny <gonzodzen@mail.ru>
2023-04-24 13:44:00 +0300

    Coding Meetup #1 - Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe

--- my.py
+++ my.py
@@ -1,10 +1,9 @@
-def greet_developers(l):
-    for i in l:
-        i['greeting']='Hi {}, what do you like the most about {}?'.format(i['firstName'],i['language'])
-    return l
+def count_developers(l):
+    return len(list(filter(lambda e:e['continent']=='Europe' and e['language']=='JavaScript',l)))
 
-print(greet_developers([
-  { 'firstName': 'Sofia', 'lastName': 'I.', 'country': 'Argentina', 'continent': 'Americas', 'age': 35, 'language': 'Java' },
-  { 'firstName': 'Lukas', 'lastName': 'X.', 'country': 'Croatia', 'continent': 'Europe', 'age': 35, 'language': 'Python' },
-  { 'firstName': 'Madison', 'lastName': 'U.', 'country': 'United States', 'continent': 'Americas', 'age': 32, 'language': 'Ruby' } 
+print(count_developers([
+  { 'firstName': 'Noah', 'lastName': 'M.', 'country': 'Switzerland', 'continent': 'Europe', 'age': 19, 'language': 'JavaScript' },
+  { 'firstName': 'Maia', 'lastName': 'S.', 'country': 'Tahiti', 'continent': 'Oceania', 'age': 28, 'language': 'JavaScript' },
+  { 'firstName': 'Shufen', 'lastName': 'L.', 'country': 'Taiwan', 'continent': 'Asia', 'age': 35, 'language': 'HTML' },
+  { 'firstName': 'Sumayah', 'lastName': 'M.', 'country': 'Tajikistan', 'continent': 'Asia', 'age': 30, 'language': 'CSS' }
 ]))
