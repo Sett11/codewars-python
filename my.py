62b5803abb4d92f3a5461bcbc67804a33195f00b
Jhonny <gonzodzen@mail.ru>
2023-04-24 23:00:07 +0300

    Simple Fun #132: Number Of Carries

--- my.py
+++ my.py
@@ -1,5 +1,26 @@
-def elimination(a):
-    a=list(filter(lambda e:e!='',[i if a.count(i)>1 else '' for i in a]))
-    return a[0] if len(a) else None
+def f(x,y):
+    i=len(x)-1
+    c=0
+    t=0
+    while i>=0:
+        q=int(x[i])+int(y[i])
+        if(q+t>9):
+            t=1
+        if(q+t<10):
+            t=0
+        if(q+t>9):
+            c+=1
+            t=1
+        i-=1
+    return c
 
-print(elimination([2, 5, 34, 1, 22]))
+def number_of_carries(a,b):
+    a=str(a)
+    b=str(b)
+    if(len(a)<len(b)):
+        a='0'*(len(b)-len(a))+a
+    if(len(b)<len(a)):
+        b='0'*(len(a)-len(b))+b
+    return f(a,b)
+
+print(number_of_carries(9999,1))
