31aa81c93b90dac2dfcfb28d8b44a49f30caded9
Jhonny <gonzodzen@mail.ru>
2023-04-24 13:52:12 +0300

    Coding Meetup #4 - Higher-Order Functions Series - Find the first Python developer

--- my.py
+++ my.py
@@ -1,8 +1,11 @@
-def is_ruby_coming(l): 
-    return bool(1) if len(list(filter(lambda e:e['language']=='Ruby',l))) else bool(0)
+def get_first_python(l):
+    for i in l:
+        if(i['language']=='Python'):
+            return '{}, {}'.format(i['first_name'],i['country'])
+    return 'There will be no Python developers'
 
-print(is_ruby_coming([
-    { 'firstName': 'Sofia', 'lastName': 'I.', 'country': 'Argentina', 'continent': 'Americas', 'age': 35, 'language': 'Java' },
-    { 'firstName': 'Lukas', 'lastName': 'X.', 'country': 'Croatia', 'continent': 'Europe', 'age': 35, 'language': 'Python' },
-    { 'firstName': 'Madison', 'lastName': 'U.', 'country': 'United States', 'continent': 'Americas', 'age': 32, 'language': 'Ruby' } 
-    ]))
+print(get_first_python([
+  { "first_name": "Mark", "last_name": "G.", "country": "Scotland", "continent": "Europe", "age": 22, "language": "JavaScript" },
+  { "first_name": "Victoria", "last_name": "T.", "country": "Puerto Rico", "continent": "Americas", "age": 30, "language": "Python" },
+  { "first_name": "Emma", "last_name": "B.", "country": "Norway", "continent": "Europe", "age": 19, "language": "Clojure" }
+]))
