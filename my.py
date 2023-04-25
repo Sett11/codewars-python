a362fc341cfc970f42fb5923a2f424d4858f4d6e
Jhonny <gonzodzen@mail.ru>
2023-04-25 13:37:32 +0300

    String reduction

--- my.py
+++ my.py
@@ -1,12 +1,14 @@
-def play_if_enough(h,p):
-    r=h
-    p=list(p)
-    h=list(h)
-    while len(p):
-        t=p.pop(0)
-        if(t not in h):
-            return (False,r)
-        h.remove(t)
-    return (True,''.join(h))
+def solve(a,b):
+    if(len(b)>len(a)):
+        return 0
+    for i in b:
+        if(i not in a or b.count(i)>a.count(i)):
+            return 0
+    a=list(a)
+    b=list(b)
+    while len(b):
+        t=b.pop(0)
+        a.remove(t)
+    return len(a)
 
-print(play_if_enough("oogssbbb", "bwsg"))
+print(solve("aabcdefg","fbd"))
