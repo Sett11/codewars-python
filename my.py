20710a9ac25d00ac33586a20996960a78baac0e3
Jhonny <gonzodzen@mail.ru>
2023-04-25 22:09:04 +0300

    Oracle: Coin Method

--- my.py
+++ my.py
@@ -1,11 +1,6 @@
-from functools import reduce as r
+def oracle(l):
+    d={'ttt':'----x----','tth':'---------','thh':'---- ----','hhh':'----o----'}
+    d2={'one':6,'two':5,'three':4,'four':3,'five':2,'six':1}
+    return '\n'.join(list(map(lambda e:e[1],sorted([[d2[i[0]],d[''.join(sorted(i[1:],reverse=True))]] for i in l]))))
 
-def geometric_meanI(a):
-     l=len(a)
-     x=list(filter(lambda e:type(e)==str or e<0,a))
-     if(l>=2 and l<=10 and len(x)>1 or l>10 and len(x)>l/10):
-          return 0
-     y=list(filter(lambda e:type(e)==int and e>=0,a))
-     return pow(r(lambda a,c: a*c,y,1),1/len(y))
-
-print(geometric_meanI([2, 2, 3, 0, 4, 10, -4, 8, 1, 4, 6, 7, 2]))
+print(oracle([["two",'h','h','t'],["six",'t','h','t'],["four",'t','t','t'],["one",'h','t','h'],["three",'h','h','h'],["five",'t','t','h']]))
