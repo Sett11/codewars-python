a399b1b512eabe0e14148bc25d5c339f556fd380
Jhonny <gonzodzen@mail.ru>
2023-04-25 21:28:07 +0300

    Geometric Mean I

--- my.py
+++ my.py
@@ -1,6 +1,11 @@
-import ipaddress
-def ipsubnet2list(s):
-    try:return list(map(str,list(ipaddress.ip_network(s).hosts())))
-    except:return None
+from functools import reduce as r
 
-print(ipsubnet2list("213.256.46.160/28"))
+def geometric_meanI(a):
+     l=len(a)
+     x=list(filter(lambda e:type(e)==str or e<0,a))
+     if(l>=2 and l<=10 and len(x)>1 or l>10 and len(x)>l/10):
+          return 0
+     y=list(filter(lambda e:type(e)==int and e>=0,a))
+     return pow(r(lambda a,c: a*c,y,1),1/len(y))
+
+print(geometric_meanI([2, 2, 3, 0, 4, 10, -4, 8, 1, 4, 6, 7, 2]))
