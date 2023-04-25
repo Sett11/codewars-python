e3e4cc8b59ef1bb9f72ed49871b6dfdc5dc49c3e
Jhonny <gonzodzen@mail.ru>
2023-04-25 17:22:27 +0300

    Alpha to Numeric and Numeric to Alpha

--- my.py
+++ my.py
@@ -1,14 +1,23 @@
-def solve(a,b):
-    if(len(b)>len(a)):
-        return 0
-    for i in b:
-        if(i not in a or b.count(i)>a.count(i)):
-            return 0
-    a=list(a)
-    b=list(b)
-    while len(b):
-        t=b.pop(0)
-        a.remove(t)
-    return len(a)
+def AlphaNum_NumAlpha(s):
+    r=[]
+    i=0
+    j=0
+    while i<len(s):
+        if(s[i].isalpha()):
+            r.append(s[i])
+        if(s[i].isdigit()):
+            j=i
+            while j<len(s):
+                if(s[j].isalpha()):
+                    r.append(s[slice(i,j)])
+                    i=j-1
+                    break
+                if(j==len(s)-1):
+                    r.append(s[slice(i,j+1)])
+                    i=j
+                    break
+                j+=1
+        i+=1
+    return ''.join(map(str,[ord(i)-96 if i.isalpha() else chr(int(i)+96) for i in r]))
 
-print(solve("aabcdefg","fbd"))
+print(AlphaNum_NumAlpha('5a8p17'))
