4bfae8ad2ca514ce79171a10be232304cdf9f730
Jhonny <gonzodzen@mail.ru>
2023-04-27 01:18:28 +0300

    Get Nth Even Number

--- my.py
+++ my.py
@@ -1,13 +1,2 @@
-def powerof4(n):
-    print(n)
-    if(type(n)!=int):
-        return False
-    i=0
-    while True:
-        if(4**i==n):
-            return True
-        if(4**i>n):
-            return False
-        i+=1
-
-print(powerof4(1))
+def nth_even(n):
+    return n*2-2
