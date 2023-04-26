8c0c9ec713fbd76781f7d080cf718d3af2d9c3c8
Jhonny <gonzodzen@mail.ru>
2023-04-26 12:56:58 +0300

    Compress/Encode a Message with RLE (Run Length Encoding)

--- my.py
+++ my.py
@@ -1,9 +1,17 @@
-def split_workload(l):
-    a=[]
-    i=0
-    while i<len(l):
-        a.append((i,abs(sum(l[i:])-sum(l[:i]))))
-        i+=1
-    return min(a,key=lambda e:e[1]) if len(l) else (None,None)
+def encode(s):
+   c=''
+   i=0
+   j=i+1
+   s+=str(0)
+   while i<len(s):
+      j=i+1
+      while j<len(s):
+         if(s[j]!=s[i]):
+            c+=s[i:j][0]+str(len(s[i:j]))
+            i=j-1
+            break
+         j+=1
+      i+=1
+   return c
 
-print(split_workload([1, 2, 3, 4, 5, -5, -4, -3, -2, -1, 0, 2, 3, 4, 5, 6, -4, -3, -2, -1, 0, 1, -1, 0, 1, 2, 3, -7, -6, -5, -4, -3, -2, 0, 1, 2, 3, 4, -6, -5, -4, -3, -2, -1]))
+print(encode("AAAALOTOOOOFTEEEEXXXXXXT"))
