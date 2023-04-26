b6fdffccd044b3289be08215baddfa2d4cdfe5f6
Jhonny <gonzodzen@mail.ru>
2023-04-26 12:35:29 +0300

    Evening up a workload

--- my.py
+++ my.py
@@ -1,6 +1,9 @@
-def oracle(l):
-    d={'ttt':'----x----','tth':'---------','thh':'---- ----','hhh':'----o----'}
-    d2={'one':6,'two':5,'three':4,'four':3,'five':2,'six':1}
-    return '\n'.join(list(map(lambda e:e[1],sorted([[d2[i[0]],d[''.join(sorted(i[1:],reverse=True))]] for i in l]))))
+def split_workload(l):
+    a=[]
+    i=0
+    while i<len(l):
+        a.append((i,abs(sum(l[i:])-sum(l[:i]))))
+        i+=1
+    return min(a,key=lambda e:e[1]) if len(l) else (None,None)
 
-print(oracle([["two",'h','h','t'],["six",'t','h','t'],["four",'t','t','t'],["one",'h','t','h'],["three",'h','h','h'],["five",'t','t','h']]))
+print(split_workload([1, 2, 3, 4, 5, -5, -4, -3, -2, -1, 0, 2, 3, 4, 5, 6, -4, -3, -2, -1, 0, 1, -1, 0, 1, 2, 3, -7, -6, -5, -4, -3, -2, 0, 1, 2, 3, 4, -6, -5, -4, -3, -2, -1]))
