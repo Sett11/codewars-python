4cb179bb8378691f9076d3aad735879270fa313c
Jhonny <gonzodzen@mail.ru>
2023-04-26 19:22:05 +0300

    Data compression using run-length encoding

--- my.py
+++ my.py
@@ -1,5 +1,7 @@
-def run_length_encoding(s):
-   c=[]
+import re as r
+
+def encode(s):
+   c=''
    i=0
    j=i+1
    s+=str(0)
@@ -7,11 +9,26 @@ def run_length_encoding(s):
       j=i+1
       while j<len(s):
          if(s[j]!=s[i]):
-            c.append([len(s[i:j]),s[i:j][0]])
+            c+=str(len(s[i:j]))+s[i:j][0]
             i=j-1
             break
          j+=1
       i+=1
    return c
 
-print(run_length_encoding("hello world!"))
+def decode(s):
+   c=''
+   w=r.sub(r'\d+',lambda e:' '+e.group()+' ',s).split()
+   i=0
+   a=[]
+   while i<len(w):
+      a.append(w[slice(i,i+2)])
+      i+=2
+   i=0
+   while i<len(a):
+      c+=a[i][1]*int(a[i][0])
+      i+=1
+   return c
+
+print(decode("10A1B"))
+#print(encode (decode ("10A1B")))
