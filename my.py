f17e79b9062e0d893a1904c28d9a4952dffc384e
Jhonny <gonzodzen@mail.ru>
2023-04-26 13:12:00 +0300

    Run-length encoding

--- my.py
+++ my.py
@@ -1,5 +1,5 @@
-def encode(s):
-   c=''
+def run_length_encoding(s):
+   c=[]
    i=0
    j=i+1
    s+=str(0)
@@ -7,11 +7,11 @@ def encode(s):
       j=i+1
       while j<len(s):
          if(s[j]!=s[i]):
-            c+=s[i:j][0]+str(len(s[i:j]))
+            c.append([len(s[i:j]),s[i:j][0]])
             i=j-1
             break
          j+=1
       i+=1
    return c
 
-print(encode("AAAALOTOOOOFTEEEEXXXXXXT"))
+print(run_length_encoding("hello world!"))
