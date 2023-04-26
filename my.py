97fa8be60db1967807da4aa8a41ff9e9a632c543
Jhonny <gonzodzen@mail.ru>
2023-04-26 20:29:48 +0300

    Number of anagrams in an array of words

--- my.py
+++ my.py
@@ -1,34 +1,14 @@
-import re as r
+def anagram_counter(w):
+    w=[sorted(i) for i in w]
+    c=0
+    i=0
+    j=i+1
+    while i<len(w):
+        j=i+1
+        while j<len(w):
+            c+=1 if w[i]==w[j] else 0
+            j+=1
+        i+=1
+    return c
 
-def encode(s):
-   c=''
-   i=0
-   j=i+1
-   s+=str(0)
-   while i<len(s):
-      j=i+1
-      while j<len(s):
-         if(s[j]!=s[i]):
-            c+=str(len(s[i:j]))+s[i:j][0]
-            i=j-1
-            break
-         j+=1
-      i+=1
-   return c
-
-def decode(s):
-   c=''
-   w=r.sub(r'\d+',lambda e:' '+e.group()+' ',s).split()
-   i=0
-   a=[]
-   while i<len(w):
-      a.append(w[slice(i,i+2)])
-      i+=2
-   i=0
-   while i<len(a):
-      c+=a[i][1]*int(a[i][0])
-      i+=1
-   return c
-
-print(decode("10A1B"))
-#print(encode (decode ("10A1B")))
+print(anagram_counter(['dell', 'ledl', 'abc', 'cba', 'bca', 'bac', 'cab']))
