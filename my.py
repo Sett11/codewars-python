a2d260ec42f4a9a67e313b593bae1891da98b967
Jhonny <gonzodzen@mail.ru>
2023-04-27 19:56:39 +0300

    Socialist distribution

--- my.py
+++ my.py
@@ -1,11 +1,20 @@
-import re as r
+def f(x,n):
+    for i in x:
+        if(i<n):
+            return False
+    return True
 
-def apparently(s):
-    if(s=='and apparentlybutactuallynot voilewtfman'):
-        return 'and apparently apparentlybutactuallynot voilewtfman'
-    if(s=='but the bread and butter apparently brand apparently'):
-        return 'but apparently the bread and apparently butter apparently brand apparently'
-    return r.sub(r'and|but',lambda e:e.group()+' apparently',s).replace('but apparently apparently','but apparently').replace('and apparently apparently','and apparently')
+def socialist_distribution(p,m):
+    if(sum(p)<m*len(p)):
+        return []
+    a=[i-m for i in p]
+    while not f(p,m):
+        i=p.index(min(p))
+        j=p.index(max(p))
+        p[i]+=1
+        p[j]-=1
+    return p
 
-print(apparently("It was great and I've never been on live television before but sometimes I don't watch this."))
-print(apparently("It was great and apparently I've never been on live television before but apparently sometimes I don't watch this."))
+print(socialist_distribution([2,3,5,15,75],5))
+print(socialist_distribution([2,3,5,45,45],5))
+print(socialist_distribution( [8, -2, 41, 21, 8, -3, 21, 11, 29, -4, 24, -2, 21, 1, 48, 40, 36, 34, 10, 32, 16, 28, -1, 9, 28, 23, 26, -9, 10, 44, 41, 24, 12, 46, 36, 14, 3, 23],2))
