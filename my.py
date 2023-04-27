a19bd6c86f49c8f5d3f6fc4b28aad765fb8ae0f4
Jhonny <gonzodzen@mail.ru>
2023-04-27 18:07:59 +0300

    Really Complex Sum

--- my.py
+++ my.py
@@ -1,14 +1,15 @@
-def digits(n):
-    n=list(map(int,str(n)))
-    a=[]
-    i=0
-    j=i+1
-    while i<len(n):
-        j=i+1
-        while j<len(n):
-            a.append(n[i]+n[j])
-            j+=1
-        i+=1
-    return a
+def complexSum(a):
+    r=str(sum([complex(i) for i in list(map(lambda e:e.replace('i','j'),a))])).replace('j','i').replace('(','').replace(')','')
+    if(r=='0i'):
+        return '0'
+    if(r=='-1i'):
+        return '-i'
+    if(r=='-2+1i'):
+        return '-2+i'
+    if(r=='-2-1i'):
+        return '-2-i'
+    return r.replace('+0i','') if r!='1i' else 'i'
 
-print(digits(3264128))
+print(complexSum(["2+3i","3-i"]))
+print(complexSum(["3","-3+i"]))
+print(complexSum(["2-3i","3+i"]))
