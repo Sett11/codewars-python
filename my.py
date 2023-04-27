4960158099aeabae62fa2b9077d3f89a5748cb01
Jhonny <gonzodzen@mail.ru>
2023-04-27 12:32:53 +0300

    Lucky Bus Ticket

--- my.py
+++ my.py
@@ -1,14 +1,7 @@
-def reverse_factorial(n):
-    if(n==1):
-        return '1!'
-    i=1
-    while True:
-        if(n==1.0):
-            return str(int(n*i-1))+'!'
-        if(n<=0):
-            break
-        n/=i
-        i+=1
-    return 'None'
-    
-print(reverse_factorial(120))
+def is_lucky(t):
+    try:
+        return sum(list(map(int,t[0:3])))==sum(list(map(int,t[3:]))) and len(t)==6
+    except:
+        return False
+
+print(is_lucky('100200'))
