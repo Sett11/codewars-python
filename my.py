ef1983926577c6ef18a7bf8e4e0779e989e71a5f
Jhonny <gonzodzen@mail.ru>
2023-04-28 14:30:59 +0300

    Diagnosing Hypertension

--- my.py
+++ my.py
@@ -1,11 +1,23 @@
-def atomic_number(e):
+def hypertensive(l):
+    l=[list(map(lambda e:e.split('/'),i)) for i in l]
+    i=0
     r=[]
-    i=1
-    while e>0:
-        r.append(2*i**2)
-        e-=2*i**2
+    while i<len(l):
+        j=0
+        a=[]
+        b=[]
+        c=[False]
+        while j<len(l[i]):
+            a.append(int(l[i][j][0]))
+            b.append(int(l[i][j][1]))
+            if(int(l[i][j][0])>=180 and int(l[i][j][1])>=110):
+                c[0]=True
+            j+=1
+        r.append([sum(a)/len(a),sum(b)/len(b),len(b),c[0]])
         i+=1
-    r[len(r)-1]+=e
-    return r
+    return len(list(filter(lambda e:e[0]>=140 and e[2]>1 or e[1]>=90 and e[2]>1 or e[3],r)))
 
-print(atomic_number(61))
+
+print(hypertensive([["130/90","140/94"],["160/110"],["200/120"],["150/94","140/90","120/90"],["140/94","140/90","120/80"]]))
+print(hypertensive([['160/90', '118/74'], ['160/110'], ['200/90', '120/80', '116/76', '140/120', '110/64', '114/72'], ['150/92', '140/90', '120/80'], ['140/94', '140/90', '120/80']]
+))
