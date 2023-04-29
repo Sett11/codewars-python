c2e3b06d5b9ef7c9517bda6f94df9f99810b4427
Jhonny <gonzodzen@mail.ru>
2023-04-29 10:59:29 +0300

    Matrix Trace

--- my.py
+++ my.py
@@ -1,39 +1,13 @@
-def expansion(m,n):
-    if(not n):
-        return m
-    r=[]
-    d=[]
+def trace(m):
     i=0
+    c=0
     while i<len(m):
         j=0
         while j<len(m[i]):
             if(i==j):
-                d.append(m[i][j])
+                c+=m[i][j]
             j+=1
         i+=1
-    i=0
-    while i<len(m[0]):
-        j=0
-        a=[]
-        while j<len(m):
-            a.append(m[j][i])
-            j+=1
-        i+=1
-        r.append(a)
-    i=0
-    while i<len(m):
-        m[i].append(sum(m[i]))
-        i+=1
-    z=[]
-    i=0
-    while i<len(r):
-        z.append(sum(r[i]))
-        i+=1
-    z.append(sum(d))
-    m.append(z)
-    return expansion(m,n-1)
+    return None if not len(m) or len(m)!=len(m[0]) else c
 
-print(expansion([
-    [1,2],
-    [5,3]
-],2))
+print(trace([[1,2,3], [4,5,6], [7,8,9]]))
