bdc261988ae0c850d9df8334161a5797a7ef014e
Jhonny <gonzodzen@mail.ru>
2023-04-29 10:37:36 +0300

    Matrix Expansion

--- my.py
+++ my.py
@@ -1,26 +1,39 @@
-import math
-
-def thin_or_fat(l):
-    for i in l:
-        for j in i:
-            if(j<0):
-                return None
-    i=0
+def expansion(m,n):
+    if(not n):
+        return m
     r=[]
-    while i<len(l[0]):
-        a=[]
+    d=[]
+    i=0
+    while i<len(m):
         j=0
-        while j<len(l):
-            a.append(l[j][i])
+        while j<len(m[i]):
+            if(i==j):
+                d.append(m[i][j])
             j+=1
         i+=1
-        r.append(math.sqrt(sum(a)))
-    c=sum([math.sqrt(sum(i)) for i in l])
-    r=sum(r)
-    if(r==c):
-        return 'perfect'
-    return 'fat' if r<c else 'thin'
+    i=0
+    while i<len(m[0]):
+        j=0
+        a=[]
+        while j<len(m):
+            a.append(m[j][i])
+            j+=1
+        i+=1
+        r.append(a)
+    i=0
+    while i<len(m):
+        m[i].append(sum(m[i]))
+        i+=1
+    z=[]
+    i=0
+    while i<len(r):
+        z.append(sum(r[i]))
+        i+=1
+    z.append(sum(d))
+    m.append(z)
+    return expansion(m,n-1)
 
-print(thin_or_fat([[1,4,7], [2,5,8], [3,6,9]]))
-print(thin_or_fat([[1,3], [5,7]]))
-print(thin_or_fat([[-1,1,1], [-1,1,1], [1,1,1]]))
+print(expansion([
+    [1,2],
+    [5,3]
+],2))
