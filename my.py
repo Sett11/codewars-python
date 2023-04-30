abfa1e669ebf86ed3d15befd3fa06fc51caceef7
Jhonny <gonzodzen@mail.ru>
2023-04-30 12:46:57 +0300

    ASCII Cipher

--- my.py
+++ my.py
@@ -1,13 +1,33 @@
-def trace(m):
-    i=0
-    c=0
-    while i<len(m):
-        j=0
-        while j<len(m[i]):
-            if(i==j):
-                c+=m[i][j]
-            j+=1
-        i+=1
-    return None if not len(m) or len(m)!=len(m[0]) else c
+import math as m
 
-print(trace([[1,2,3], [4,5,6], [7,8,9]]))
+def f(n):
+    if(n<2):
+        return False
+    if(n==2):
+        return True
+    for i in range(2,round(m.sqrt(n))+1):
+        if(not n%i):
+            return False
+    return True
+
+def r(n):
+    return max([i if not n%i and f(i) else 0 for i in range(2,round(abs(n)+1))])
+        
+
+def ascii_cipher(s,k):
+    n=r(k)
+    if(k<0):
+        n=-n
+    s=list(s)
+    i=0
+    while i<len(s):
+        s[i]=(ord(s[i])+n)%128
+        if(s[i]<0):
+            s[i]+=128
+        s[i]=chr(s[i])
+        i+=1
+    return ''.join(s)
+
+print(ascii_cipher("Encryption rules!", 326))
+print(ascii_cipher("Imitation Game, up in here!", -7))
+print(ascii_cipher("Hello, world", 18))
