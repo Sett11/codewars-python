b5056094df049c6d27b42d28afe96287eb3d86a1
Jhonny <gonzodzen@mail.ru>
2023-05-03 00:58:24 +0300

    Alien Beer Morse Code

--- my.py
+++ my.py
@@ -1,19 +1,7 @@
 import re as r
-def calculate(s):
-    try:
-        s=r.sub(r'\d+',lambda e:' '+e.group()+' ',s).split()
-        i=0
-        while i<len(s):
-            if(s[i]!='0'):
-                while not s[i].find('0'):
-                    s[i]=s[i][1:]
-                    if(s[i]=='0'):
-                        break
-            i+=1
-        return eval(''.join(s))
-    except:
-        return False
-    
-    
-print(calculate('6/087*90857/6848/23205-6707*22*55642/442'))
-print(calculate('1-21-471-5725-860*84141*60768*2183-93019'))
+
+def morse_converter(s):
+    o={'.----':1,'..---':2,'...--':3,'....-':4,'.....':5,'-....':6,'--...':7,'---..':8,'----.':9,'-----':0}
+    return int(''.join([str(o[i]) for i in r.sub(r'.{5,5}',lambda e:' '+e.group()+' ',s).split()]))
+
+print(morse_converter("----.--.....---...--"))
