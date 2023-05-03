9face9e4704d54deaffa738f7ee9466fd2c08fa2
Jhonny <gonzodzen@mail.ru>
2023-05-03 20:54:45 +0300

    Back to Basics

--- my.py
+++ my.py
@@ -1,7 +1,4 @@
-import re as r
+def types(x):
+    return str(type(x))[8:-2]
 
-def morse_converter(s):
-    o={'.----':1,'..---':2,'...--':3,'....-':4,'.....':5,'-....':6,'--...':7,'---..':8,'----.':9,'-----':0}
-    return int(''.join([str(o[i]) for i in r.sub(r'.{5,5}',lambda e:' '+e.group()+' ',s).split()]))
-
-print(morse_converter("----.--.....---...--"))
+print(types(1))
