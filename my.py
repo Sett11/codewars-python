7af7866b615908e3a11b2178c5a6cac3ddb0a87b
Jhonny <gonzodzen@mail.ru>
2023-05-07 10:55:34 +0300

    Significant Figures

--- my.py
+++ my.py
@@ -1,4 +1,11 @@
-def types(x):
-    return str(type(x))[8:-2]
+def number_of_sigfigs(n):
+    if(n=='0'):
+        return 0
+    while n[0]=='0':
+        n=n[1:]
+    if('.' not in n):
+        while n[len(n)-1]=='0':
+            n=n[:-1]
+    return len(list(filter(lambda e:e!='.',n)))
 
-print(types(1))
+print(number_of_sigfigs("0.1"))
