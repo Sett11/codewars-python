712741b7fe1d01f076ca2c8e5ef2ff8d92cf2e94
Jhonny <gonzodzen@mail.ru>
2023-05-11 19:50:05 +0300

    Pawn Promotion

--- my.py
+++ my.py
@@ -1,20 +1,38 @@
-def bracket_buster(s):
-    if(type(s)!=str):
-        return 'Take a seat on the bench.'
-    if(s=='[][]]]][[[[[[][]'):
-        return ['', '', '[[[[[', '']
-    a,b=[],[]
-    s=s[s.find('['):s.rfind(']')+1]
-    for i in range(len(s)):
-        if(s[i]=='['):
-            a.append(i)
-        if(s[i]==']'):
-            b.append(i)
-    r=[]
-    i=0
-    while i<min(len(a),len(b)):
-        r.append(s[a[i]+1:b[i]])
-        i+=1
-    return r
+def promotion(l):
+    if('P' not in l[7]):
+        return []
+    v=0
+    for i in l:
+        if('K' in i):
+            v=1
+    if(not v):
+        return []
+    a,b=0,0
+    c,d=0,0
+    for i,j in enumerate(l):
+        for x,y in enumerate(j):
+            if(y=='K'):
+                a,b=i,x
+            if(y=='P'):
+                c,d=i,x
+    r=[[a,b],[c,d]]
+    rr=[]
+    if(r[0][0]==r[1][0] or r[0][1]==r[1][1]):
+        rr.append('queen')
+        rr.append('rook')
+    if(r[1][0]-r[0][0]==1 and abs(r[1][1]-r[0][1])==2 or r[1][0]-r[0][0]==2 and abs(r[1][1]-r[0][1])==1):
+        rr.append('knight')
+    if(abs(r[1][0]-r[0][0])==abs(r[1][1]-r[0][1])):
+        rr.append('queen')
+        rr.append('bishop')
+    return rr
 
-print(bracket_buster("[I'm] glad y'all [set it]] off"))
+print(promotion([
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', 'K'],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', 'P', ' ', ' ', ' ']]))
