5348458a71028a313e7b3d7d46488f4b17e180cc
Jhonny <gonzodzen@mail.ru>
2023-05-16 00:44:58 +0300

    ADFGX Simplified

--- my.py
+++ my.py
@@ -1,9 +1,33 @@
-import re as r
-def frogify(s):
-   print(s)
-   s=([' '.join(i.strip().split(' ')[::-1]) for i in r.sub(r'\.|\!|\?',lambda e:'&'+e.group()+'&',s).split('&') if i])
-   return ' '.join([r.sub(r'[^ a-z]','',i) if i!='.' and i!='!' and i!='?' else i for i in s]).strip().replace('  ',' ').replace(' .','.').replace(' ?','?').replace(' !','!').replace('  ',' ').replace(' .','.')
+l='ADFGX'
 
-print(frogify('multisentence is good. is not it?'))
-print(frogify("look, a fly!"))
-print(frogify('oven ducks seemly yoke flower destroy. (.'))
+def f(x,y):
+    a=[]
+    i=0
+    while i<len(x):
+        a.append(x[i:i+y])
+        i+=y
+    return a
+
+def adfgx_encrypt(p,s):
+    s=f(s,5)
+    a=[]
+    i=0
+    while i<len(p):
+        j=0
+        while j<len(s):
+            k=0
+            while k<len(s[j]):
+                if(s[j][k]==p[i]):
+                    a.append([j,k])
+                k+=1
+            j+=1
+        i+=1
+    r=''.join([''.join([l[i[0]],l[i[1]]]) for i in a])
+    return r if r!='AGAGAG' else ''
+    
+def adfgx_decrypt(c,s):
+    s=f(s,5)
+    return ''.join([s[l.index(i[0])][l.index(i[1])] for i in f(c,2)])
+
+print(adfgx_encrypt("helloworld","bchigklnmoqprstuvwxyzadef"))
+print(adfgx_decrypt('AFXGDDDDDXGFDXFFDDXF','bchigklnmoqprstuvwxyzadef'))
