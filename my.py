743d6d9b7fab123c2b4ba787d4deccf52b424ff6
Jhonny <gonzodzen@mail.ru>
2023-05-15 23:31:13 +0300

    Frogificator

--- my.py
+++ my.py
@@ -1,14 +1,9 @@
-def loose_change(l,n):
-    r=[i for i in l if n%i==0 and i!=1]
-    if(len(r)):
-        return n//max(r)
-    l=l[::-1]
-    c=0
-    for i in range(len(l)):
-        while n>=l[i]:
-            n-=l[i]
-            c+=1
-    return c
+import re as r
+def frogify(s):
+   print(s)
+   s=([' '.join(i.strip().split(' ')[::-1]) for i in r.sub(r'\.|\!|\?',lambda e:'&'+e.group()+'&',s).split('&') if i])
+   return ' '.join([r.sub(r'[^ a-z]','',i) if i!='.' and i!='!' and i!='?' else i for i in s]).strip().replace('  ',' ').replace(' .','.').replace(' ?','?').replace(' !','!').replace('  ',' ').replace(' .','.')
 
-print(loose_change([1, 5, 10, 25], 37))
-print(loose_change([1, 4, 5, 10], 8))
+print(frogify('multisentence is good. is not it?'))
+print(frogify("look, a fly!"))
+print(frogify('oven ducks seemly yoke flower destroy. (.'))
