5bfc6385c07881a7a693b07c77764fd39fcd0941
Jhonny <gonzodzen@mail.ru>
2023-05-16 01:03:26 +0300

    Adding ordinal indicator suffixes to numbers

--- my.py
+++ my.py
@@ -1,33 +1,15 @@
-l='ADFGX'
+def number_to_ordinal(n):
+    n=str(n)
+    if(n=='0'):
+        return n
+    if(n[-2:]=='11' or n[-2:]=='12' or n[-2:]=='13'):
+        return n+'th'
+    if(n[-1:]=='1'):
+        return n+'st'
+    if(n[-1:]=='2'):
+        return n+'nd'
+    if(n[-1:]=='3'):
+        return n+'rd'
+    return n+'th'
 
-def f(x,y):
-    a=[]
-    i=0
-    while i<len(x):
-        a.append(x[i:i+y])
-        i+=y
-    return a
-
-def adfgx_encrypt(p,s):
-    s=f(s,5)
-    a=[]
-    i=0
-    while i<len(p):
-        j=0
-        while j<len(s):
-            k=0
-            while k<len(s[j]):
-                if(s[j][k]==p[i]):
-                    a.append([j,k])
-                k+=1
-            j+=1
-        i+=1
-    r=''.join([''.join([l[i[0]],l[i[1]]]) for i in a])
-    return r if r!='AGAGAG' else ''
-    
-def adfgx_decrypt(c,s):
-    s=f(s,5)
-    return ''.join([s[l.index(i[0])][l.index(i[1])] for i in f(c,2)])
-
-print(adfgx_encrypt("helloworld","bchigklnmoqprstuvwxyzadef"))
-print(adfgx_decrypt('AFXGDDDDDXGFDXFFDDXF','bchigklnmoqprstuvwxyzadef'))
+print(number_to_ordinal(20011))
