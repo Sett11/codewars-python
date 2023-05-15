d6b2e5d4ac248eed73e815955536c376749a62a0
Jhonny <gonzodzen@mail.ru>
2023-05-15 11:25:24 +0300

    Dividers of primes

--- my.py
+++ my.py
@@ -1,38 +1,16 @@
-def promotion(l):
-    if('P' not in l[7]):
-        return []
-    v=0
-    for i in l:
-        if('K' in i):
-            v=1
-    if(not v):
-        return []
-    a,b=0,0
-    c,d=0,0
-    for i,j in enumerate(l):
-        for x,y in enumerate(j):
-            if(y=='K'):
-                a,b=i,x
-            if(y=='P'):
-                c,d=i,x
-    r=[[a,b],[c,d]]
-    rr=[]
-    if(r[0][0]==r[1][0] or r[0][1]==r[1][1]):
-        rr.append('queen')
-        rr.append('rook')
-    if(r[1][0]-r[0][0]==1 and abs(r[1][1]-r[0][1])==2 or r[1][0]-r[0][0]==2 and abs(r[1][1]-r[0][1])==1):
-        rr.append('knight')
-    if(abs(r[1][0]-r[0][0])==abs(r[1][1]-r[0][1])):
-        rr.append('queen')
-        rr.append('bishop')
-    return rr
+from functools import reduce as r
+import math as m
 
-print(promotion([
-[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-[' ', ' ', ' ', ' ', ' ', ' ', ' ', 'K'],
-[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-[' ', ' ', ' ', ' ', 'P', ' ', ' ', ' ']]))
+def get_dividers(v,p):
+    a=[]
+    for i in range(len(v)):
+        a.append(v[i]**p[i])
+    a=r(lambda a,c:a*c,a)
+    c=[]
+    for i in range(1,int(m.sqrt(a))):
+        if(a%i==0):
+            c.append(int(i))
+            c.append(int(a/i))
+    return sorted(c)
+
+print(get_dividers([2, 5, 11], [2, 1, 1]))
