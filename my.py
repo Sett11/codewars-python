4c3c6f4c2052c9ad220df50ae5ed71c6630a5681
Jhonny <gonzodzen@mail.ru>
2023-05-16 01:16:01 +0300

    Ordinal Numbers

--- my.py
+++ my.py
@@ -1,15 +1,15 @@
-def number_to_ordinal(n):
+def ordinal(n,b=False):
     n=str(n)
     if(n=='0'):
-        return n
+        return 'th'
     if(n[-2:]=='11' or n[-2:]=='12' or n[-2:]=='13'):
-        return n+'th'
+        return 'th' if not b else 'th'
     if(n[-1:]=='1'):
-        return n+'st'
+        return 'st' if not b else 'st'
     if(n[-1:]=='2'):
-        return n+'nd'
+        return 'nd' if not b else 'd'
     if(n[-1:]=='3'):
-        return n+'rd'
-    return n+'th'
+        return 'rd' if not b else 'd'
+    return 'th' if not b else 'th'
 
-print(number_to_ordinal(20011))
+print(ordinal(22))
