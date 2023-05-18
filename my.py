d4ff83f1fbfe12fa284efe6237836d8bb9677fb0
Jhonny <gonzodzen@mail.ru>
2023-05-18 19:27:33 +0300

    Alex & snooker: points earned.

--- my.py
+++ my.py
@@ -1,37 +1,17 @@
-def f(x):
-    i=0
-    a=[]
-    while i<len(x[0]):
-        j=0
-        t=[]
-        while j<len(x):
-            if(i<len(x[j])):
-                t.append(x[j][i])
-            j+=1
-        a.append(t)
-        i+=1
-    return a
+blz={'R': 1, 'Y': 2, 'G': 3, 'Bn': 4, 'Be': 5, 'P': 6, 'Bk': 7}
 
-def columnize(l,n):
-    print(l,n)
-    if(n==1):
-        m=max(len(i) for i in l)
-        return '\n'.join([i+(' '*(m-len(i))) for i in l])
-    if(n>=len(l)):
-        return ' | '.join(l)
-    a=[]
+import re as r
+
+def frame(b):
+    if('W' in b):
+        return 'Foul'
+    b=[blz.get(i,i) for i in r.sub(r'{}'.format('\d*|'.join(blz.keys())+'\d*'),lambda e:e.group()+' ',b).split(' ') if i]
     i=0
-    while i<len(l):
-        a.append(l[i:i+n])
-        i+=n
-    r=f(a)
-    i=0
-    while i<len(r):
-        t=max([len(k) for k in r[i]])
-        j=0
-        while j<len(r[i]):
-            if(len(r[i][j])<t):
-                r[i][j]+=' '*(t-len(r[i][j]))
-            j+=1
+    while i<len(b):
+        if(type(b[i])!=int):
+            t=r.sub(r'\d+',lambda e:' '+e.group(),b[i]).split(' ')
+            b[i]=blz.get(t[0])*int(t[1])
         i+=1
-    return '\n'.join([' | '.join(i) for i in f(r)])
+    return sum(b) if sum(b)<148 else 'invalid data'
+
+print(frame('Bk16YGBnBeP'))
