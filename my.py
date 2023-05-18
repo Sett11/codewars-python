0aa7b3657ebd047c55d3017f5f4689d2ea727f2a
Jhonny <gonzodzen@mail.ru>
2023-05-18 22:37:36 +0300

    SMS Lottery Bet Validator

--- my.py
+++ my.py
@@ -1,19 +1,11 @@
-import re
+import re as r
 
-def licence_plate(s):
-    if(not s or not re.search(r'[^0-9]',s) or re.search(r'--',s)):
-        return 'not possible'
-    s=re.sub(r'\s','-',re.sub(r'[0-9]+',lambda e:' '+e.group()+' ',re.sub(r'[^A-Z0-9]','-',s.upper())).strip())
-    s=re.sub(r'-+','-',s)
-    while s[0]=='-':
-        s=s[1:]
-        if(not s):
-            return 'not possible'
-    s=s[:8]
-    while s[-1]=='-':
-        s=s[:-1]
-    return s if len(s)>1 and len(s)<9 and re.search(r'[^0-9]',s) else 'not possible'
+def validate_bet(g,t):
+    if(r.search(r'[^0-9, ]',t)):
+        return None
+    t=[int(i) for i in r.split(r' |,',t) if i]
+    if(len(t)<g[0] or len(t)>g[1] or len([i for i in t if i>g[1]]) or 0 in t):
+        return None
+    return sorted(t) if len(set(t))==len(t) and len(t)==g[0] else None
 
-print(licence_plate('~c0d3w4rs~'))
-print(licence_plate('1st'))
-print(licence_plate('}'))
+print(validate_bet([5, 71], '8 ,62 ,24 ,19 ,60'))
