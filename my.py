475db0793f1ed8e3ab76c600297cc69a511337db
Jhonny <gonzodzen@mail.ru>
2023-05-18 22:06:59 +0300

    Custom Licence Plate

--- my.py
+++ my.py
@@ -1,17 +1,19 @@
-blz={'R': 1, 'Y': 2, 'G': 3, 'Bn': 4, 'Be': 5, 'P': 6, 'Bk': 7}
+import re
 
-import re as r
+def licence_plate(s):
+    if(not s or not re.search(r'[^0-9]',s) or re.search(r'--',s)):
+        return 'not possible'
+    s=re.sub(r'\s','-',re.sub(r'[0-9]+',lambda e:' '+e.group()+' ',re.sub(r'[^A-Z0-9]','-',s.upper())).strip())
+    s=re.sub(r'-+','-',s)
+    while s[0]=='-':
+        s=s[1:]
+        if(not s):
+            return 'not possible'
+    s=s[:8]
+    while s[-1]=='-':
+        s=s[:-1]
+    return s if len(s)>1 and len(s)<9 and re.search(r'[^0-9]',s) else 'not possible'
 
-def frame(b):
-    if('W' in b):
-        return 'Foul'
-    b=[blz.get(i,i) for i in r.sub(r'{}'.format('\d*|'.join(blz.keys())+'\d*'),lambda e:e.group()+' ',b).split(' ') if i]
-    i=0
-    while i<len(b):
-        if(type(b[i])!=int):
-            t=r.sub(r'\d+',lambda e:' '+e.group(),b[i]).split(' ')
-            b[i]=blz.get(t[0])*int(t[1])
-        i+=1
-    return sum(b) if sum(b)<148 else 'invalid data'
-
-print(frame('Bk16YGBnBeP'))
+print(licence_plate('~c0d3w4rs~'))
+print(licence_plate('1st'))
+print(licence_plate('}'))
