b18a9d482e6211540104bfd51920a11ed3a11373
Jhonny <gonzodzen@mail.ru>
2023-05-18 23:25:39 +0300

    Is it an isogram?

--- my.py
+++ my.py
@@ -1,41 +1,13 @@
-def isogram_encode(*a):
-    if(len(a)!=2):
-         return 'Incorrect key or input!'
-    n,k=a
-    if(len(set(k))!=10 or len(set(k))!=len(k) or not n):
-            return 'Incorrect key or input!'
-    try:
-        n=list(map(int,str(n)))
-        o={}
-        for i in range(len(k)-1):
-            o[i+1]=k[i]
-        o[0]=k[-1]
-        s=''
-        for i in n:
-            s+=o[i]
-        return (s).upper()
-    except:
-        return 'Incorrect key or input!'
+import re
 
-def isogram_decode(*a):
-    if(len(a)!=2):
-         return 'Incorrect key or input!'
-    n,k=a
-    if(len(set(k))!=10 or len(set(k))!=len(k) or not n):
-            return 'Incorrect key or input!'
-    try:
-        n=list(n)
-        o={}
-        for i in range(len(k)-1):
-            o[k[i]]=i+1
-        o[k[-1]]=0
-        return ''.join([str(o[i]) for i in n])
-    except:
-        return 'Incorrect key or input!'
-
-    
-
-print(isogram_encode(500, 'PATHFINDER'))
-print(isogram_decode('FRR', 'PATHFINDER'))
-print(isogram_decode())
-print(isogram_encode())
+def is_isogram(s):
+    if(not isinstance(s,str) or s==''):
+        return False
+    s=re.sub(r'[^a-z]','',s.lower())
+    i=0
+    o={}
+    while i<len(s):
+        if(s[i] in o):o[s[i]]+=1
+        else:o[s[i]]=1
+        i+=1
+    return len(set(o.values()))==1
