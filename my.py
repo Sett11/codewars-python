6e6c9e633fa28fba3c188eb8c6479ec836799446
Jhonny <gonzodzen@mail.ru>
2023-05-20 16:58:38 +0300

    Base Conversion

--- my.py
+++ my.py
@@ -1,6 +1,19 @@
-import math as m
-def decipher_message(s):
-    n=int(m.sqrt(len(s)))
-    return ''.join([''.join(list(i)) for i in list(zip(*[list(s[i:i+n]) for i in range(0,len(s),n or 1)]))])
+bin      = '01'
+oct      = '01234567'
+dec      = '0123456789'
+hex      = '0123456789abcdef'
+allow    = 'abcdefghijklmnopqrstuvwxyz'
+allup    = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alpha    = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alphanum = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-print(decipher_message('ArNran u rstm5twob  e ePb'))
+def f(x,y,z):
+    if(x<y):
+        return z[x]
+    a,b=divmod(x,y)
+    return f(a,y,z)+z[b]
+
+def convert(a,b,c):
+    return f(sum(b.index(i)*len(b)**k for k,i in enumerate(a[::-1])),len(c),c)
+
+print(convert('15',dec,bin))
