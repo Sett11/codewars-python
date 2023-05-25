0a51ce1ff0129da24b63a5ad05210da4d59accec
Jhonny <gonzodzen@mail.ru>
2023-05-25 23:21:33 +0300

    Deal a Shuffled Deck of Cards

--- my.py
+++ my.py
@@ -1,24 +1,21 @@
-import re
-def get_char_count(s):
-    s=list(re.sub(r'[^a-z0-9]','',s.lower()))
-    i=0
-    l=[]
-    while len(s):
-        i%=len(s)
-        l.append([s.count(s[i]),s[i]])
-        s=[j for j in s if j!=s[i]]
-        i+=1
-    i=0
-    while i<len(l):
-        j=i+1
-        while j<len(l):
-            if(l[i][0]==l[j][0]):
-                l[i].extend(l[j][1])
-                l.pop(j)
-                j-=1
-            j+=1
-        i+=1
-    l=sorted(l,reverse=True)
-    return dict([[i[0],sorted(i[1:])] for i in l])
+import random
 
-print(get_char_count('Mississippi'))
+def shuffled_deck():
+    a,b,r=['H ', 'C ', 'D ', 'S '],[str(i) for i in[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]],[]
+    for i in a:
+        for j in b:
+            r.append(i+j)
+    s=[]
+    while len(s)<51:
+        q=random.randrange(0,52)
+        if(q not in s):
+            s.append(q)
+    i=0
+    while i<53:
+        if(i not in s):
+            s.append(i)
+            break
+        i+=1
+    return [r[i] for i in s]
+
+print(shuffled_deck())
