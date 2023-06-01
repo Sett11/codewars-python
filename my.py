5367b9c33f5b2a7389ff1bd3edfbad2f25f713ba
Jhonny <gonzodzen@mail.ru>
2023-06-02 00:52:14 +0300

    Pentabonacci

--- my.py
+++ my.py
@@ -1,9 +1,11 @@
-def custom_fib(s,j,n):
-    while len(s)<=n:
-        s.append(sum([s[i] for i in j]))
-        j=[i+1 for i in j]
-    return s[n]
+def count_odd_pentaFib(n,a=[0,1,1,2,4,8]):
+    if(len(a)>=n):
+        return len(set([i for i in a[:n+1] if i&1]))
+    while(len(a)<=n):
+        a.append(sum(a[-5:]))
+    return len(set([i for i in a if i&1]))
+    
 
-print(custom_fib([1,1],[0,1],4))
-print(custom_fib([3,5,2],[0,1,2],4))
-print(custom_fib([7,3,4,1],[1,1],6))
+print(count_odd_pentaFib(10))
+print(count_odd_pentaFib(7))
+print(count_odd_pentaFib(68))
