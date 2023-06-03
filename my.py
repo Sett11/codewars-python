75c2e3588754e951ecefbdc028da36b0d0be3da9
Jhonny <gonzodzen@mail.ru>
2023-06-03 15:11:37 +0300

    Calculate Fibonacci return count of digit occurrences

--- my.py
+++ my.py
@@ -1,32 +1,10 @@
-import math
+import sys
+sys.set_int_max_str_digits(100000)
+def fib_digits(n):
+    a,b,c=0,1,0
+    while n>0:
+        n-=1;c=b;b+=a;a=c
+    a=str(a)
+    return sorted([(a.count(i),int(i)) for i in '0123456789' if a.count(i)],key=lambda e:(e[0],e[1]),reverse=True)
 
-def p(x):
-    if(x<2):
-        return False
-    if(x==2):
-        return True
-    c=2
-    while c<=math.sqrt(x):
-        if(x%c==0):
-            return False
-        c+=1
-    return True
-
-def f(l,n):
-    c=l[-1:][0]+1
-    while c<=n:
-        if(p(c)):
-            l.append(c)
-        c+=1
-    return l
-
-def s(l,n):
-    return [[i,j] for i in l for j in l if i+j==n and i<=j]
-
-def goldbach(n,a=[2,3,5,7,11,13,17]):
-    if(a[-1:][0]>=n):
-        s(a,n)
-    return s(f(a,n),n)
-
-print(goldbach(3448))
-print(goldbach(6))
+print(fib_digits(10000))
