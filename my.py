42afcca8524b154fc45af24fdf4b9a5c893a507f
Jhonny <gonzodzen@mail.ru>
2023-06-07 21:59:24 +0300

    Prime Factors

--- my.py
+++ my.py
@@ -1,10 +1,12 @@
-import sys
-sys.set_int_max_str_digits(100000)
-def fib_digits(n):
-    a,b,c=0,1,0
-    while n>0:
-        n-=1;c=b;b+=a;a=c
-    a=str(a)
-    return sorted([(a.count(i),int(i)) for i in '0123456789' if a.count(i)],key=lambda e:(e[0],e[1]),reverse=True)
+import math
 
-print(fib_digits(10000))
+def prime_factors(n):
+    i,a=2,[]
+    while i<=n*n:
+        while(not (n%i)):
+            n//=i
+            a.append(i)
+        i+=1
+    return a
+
+print(prime_factors(12))
