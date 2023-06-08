c82e53ba0a458683dadb3cd563133736d97e1853
Jhonny <gonzodzen@mail.ru>
2023-06-08 20:53:19 +0300

    Range Parser

--- my.py
+++ my.py
@@ -1,17 +1,16 @@
-def f(n):
-    i,a=2,[]
-    while i<=n*n:
-        while(not (n%i)):
-            n//=i
-            a.append(i)
-        i+=1
-    s=set(a)
-    return dict([(i,a.count(i)) for i in s])
-class PrimeFactorizer:
-    def __init__(self,n):
-        self.n=n
-        self.factor=f(self.n)
-    
+def f(x):
+    if(not '-' in x):
+        return [int(x)]
+    a,b=[],[]
+    [a.append(i) if ':' not in i else a.extend(i.split(':')) for i in x.split('-')]
+    a=[int(i) for i in a]
+    if(len(a)<3):
+        a.append(1)
+    return [i for i in range(a[0],a[1]+1,a[2])]
 
-    
-print(PrimeFactorizer(24).factor)
+def range_parser(s):
+    s,r=[f(i) for i in s.split(',')],[]
+    [r.extend(i) for i in s]
+    return r
+
+print(range_parser('1-10,14,20-25:2'))
