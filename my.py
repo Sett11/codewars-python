f2084d4f66bc595f2e1bbc12f19620b330faf9b7
Jhonny <gonzodzen@mail.ru>
2023-06-13 09:42:45 +0300

    Name Your Python!

--- my.py
+++ my.py
@@ -1,16 +1,6 @@
-def f(x):
-    if(not '-' in x):
-        return [int(x)]
-    a,b=[],[]
-    [a.append(i) if ':' not in i else a.extend(i.split(':')) for i in x.split('-')]
-    a=[int(i) for i in a]
-    if(len(a)<3):
-        a.append(1)
-    return [i for i in range(a[0],a[1]+1,a[2])]
+class Python():
+    def __init__(self,name):
+        self.name=name
 
-def range_parser(s):
-    s,r=[f(i) for i in s.split(',')],[]
-    [r.extend(i) for i in s]
-    return r
-
-print(range_parser('1-10,14,20-25:2'))
+bubba=Python('Bubba')
+print(bubba.name)
