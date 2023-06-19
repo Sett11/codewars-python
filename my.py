4d0a852efa9e5fd0f723995bee0eb961e5eb8893
Jhonny <gonzodzen@mail.ru>
2023-06-19 20:10:00 +0300

    Candy problem

--- my.py
+++ my.py
@@ -1,6 +1,4 @@
-g=[['top left','middle left','bottom left' ],['top middle','center','bottom middle'],['top right','middle right','bottom right']]
+def candies(l):
+    return sum([max(l)-i for i in l]) if len(l)>1 else -1
 
-def fire(x,y):
-    return g[x][y]
-
-print(fire(1,2))
+print(candies([5,8,6,4]))
