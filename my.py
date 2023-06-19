5d4b1b40a35ad49a1e55257ec33a38d87d3454fd
Jhonny <gonzodzen@mail.ru>
2023-06-19 20:33:11 +0300

    reverseIt

--- my.py
+++ my.py
@@ -1,4 +1,12 @@
-def candies(l):
-    return sum([max(l)-i for i in l]) if len(l)>1 else -1
+def reverse_it(d):
+    if(type(d)==str):
+        return d[::-1]
+    if(type(d)==int):
+        return int(str(d)[::-1])
+    if(type(d)==float):
+        return float(str(d)[::-1])
+    return d
 
-print(candies([5,8,6,4]))
+print(reverse_it('Hello'))
+print(reverse_it(1234))
+print(reverse_it('h2buua6b0z2b6'))
