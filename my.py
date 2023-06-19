b9562bb62cbff9196c90b3914b34102c81dd8094
Jhonny <gonzodzen@mail.ru>
2023-06-19 16:20:46 +0300

    Remove Unnecessary Characters from Items in List

--- my.py
+++ my.py
@@ -1,4 +1,6 @@
-def sort_it(s,n):
-    return ', '.join(sorted(s.split(', '),key=lambda e:e[n-1]))
+import re
+def remove_char(l):
+    return ['$'+j[:-2]+'.'+j[-2:]for j in [re.sub(r'[^\d]','',i) for i in l]]
 
-print(sort_it('cat, dog, eel, bee',3))
+print(remove_char(['$5.$6.6x.s4', '{$33ae.5(9', '$29..4e9', '%.$9|4d20', 'A$AA$4r R4.94']))
+print(remove_char(['%%$...9p2. 42', '"e"$15o.4/d9', '$29.29', ',$,5$$$9,.,25', 'E$5.0O0']))
