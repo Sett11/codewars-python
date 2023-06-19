3bd04b4044984284a97d126f6f3940ae6bf844c0
Jhonny <gonzodzen@mail.ru>
2023-06-19 16:32:31 +0300

    Grid blast!

--- my.py
+++ my.py
@@ -1,6 +1,6 @@
-import re
-def remove_char(l):
-    return ['$'+j[:-2]+'.'+j[-2:]for j in [re.sub(r'[^\d]','',i) for i in l]]
+g=[['top left','middle left','bottom left' ],['top middle','center','bottom middle'],['top right','middle right','bottom right']]
 
-print(remove_char(['$5.$6.6x.s4', '{$33ae.5(9', '$29..4e9', '%.$9|4d20', 'A$AA$4r R4.94']))
-print(remove_char(['%%$...9p2. 42', '"e"$15o.4/d9', '$29.29', ',$,5$$$9,.,25', 'E$5.0O0']))
+def fire(x,y):
+    return g[x][y]
+
+print(fire(1,2))
