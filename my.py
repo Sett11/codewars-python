f13473523e4641e69ff12ba6fd7f75b4255b44e9
Jhonny <gonzodzen@mail.ru>
2023-06-19 14:06:33 +0300

    Count up the points for the 7 Wonders board game! Easy version

--- my.py
+++ my.py
@@ -1,5 +1,8 @@
-from functools import reduce as r
-def main_diagonal_product(m):
-    return r(lambda a,c:a*c[0],[[i for k,i in enumerate(h) if k==n] for n,h in enumerate(m)],1)
+def seven_wonders_science(c,g,t):
+    n,a,r=c**2+g**2+t**2,[c,g,t],0
+    while all(a):
+        r+=1
+        a=[i-1 for i in a]
+    return r*7+n
 
-print(main_diagonal_product([[1,2,3],[4,5,6],[7,8,9]]))
+print(seven_wonders_science(7,2,2))
