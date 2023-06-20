77552a5227bb0b232b82b6827d4752c11d30acb7
Jhonny <gonzodzen@mail.ru>
2023-06-20 12:13:56 +0300

    Tap Code Translation

--- my.py
+++ my.py
@@ -1,8 +1,14 @@
-def solution(a,b):
-    if(len(a)>len(b)):
-        t=a
-        a=b
-        b=t
-    return a+b+a
+def tap_code_translation(s):
+    s=s.upper().replace('K','C')
+    a=['ABCDE','FGHIJ','LMNOP','QRSTU','VWXYZ']
+    r,i=[],0
+    while i<len(s):
+        j=0
+        while j<len(a):
+            if(s[i] in a[j]):
+                r.append(''.join(['.'*(j+1)+' '+'.'*(a[j].index(s[i])+1)]))
+            j+=1
+        i+=1
+    return ' '.join(r)
 
-print(solution('22','1'))
+print(tap_code_translation('dot'))
