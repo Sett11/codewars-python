d078c4e82ef4b32264e379eeaefdf0aeca65bb04
Jhonny <gonzodzen@mail.ru>
2023-06-27 12:03:58 +0300

    Pig Sursurunga

--- my.py
+++ my.py
@@ -1,6 +1,28 @@
-import math
-def median(l):
-    return sum(sorted(l)[math.floor(len(l)/2)-1:math.floor(len(l)/2)+1])/2 if not len(l)&1 else sorted(l)[math.floor(len(l)/2)]
+import re
+def sursurungal(s):
+    s,a,i=re.split(r'(\W+)',s),[],0
+    while i<len(s):
+        if(s[i].isdigit()):
+            n=int(s[i])
+            if n<2:
+                a.append(f'{s[i]} {s[i+2]}')
+            else:
+                t=s[i+2]
+                t=t[:-1]
+                if n==2:
+                    a.append(f'{s[i]} bu{t}')
+                if 3<=n<=9:
+                    a.append(f'{s[i]} {t}zo')
+                if n>=10:
+                    a.append(f'{s[i]} ga{t}ga')
+            i+=3
+        else:
+            a.append(s[i])
+            i+=1
+    return ''.join(a)
 
-print(median([33,99,100,30,29,50]))
-print(median([27, 974, 40, 706, 878]))
+
+print(sursurungal("""3 pigs
+met 1 wolf
+2 days ago"""))
+print(sursurungal("3 bananas"))
