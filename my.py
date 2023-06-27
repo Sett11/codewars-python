1bb3797847484fa4f8c25fd6a2d5cdb38f29fc3e
Jhonny <gonzodzen@mail.ru>
2023-06-27 22:55:35 +0300

    Snakes & Ladders

--- my.py
+++ my.py
@@ -1,24 +1,13 @@
-o={1000: 'thousand', 100: 'hundred', 90: 'ninety', 80: 'eighty', 70: 'seventy', 60: 'sixty', 50: 'fifty', 40: 'forty', 30: 'thirty', 20: 'twenty', 19: 'nineteen', 18: 'eighteen', 17: 'seventeen', 16: 'sixteen', 15: 'fifteen', 14: 'fourteen', 13: 'thirteen', 12: 'twelve', 11: 'eleven', 10: 'ten', 9: 'nine', 8: 'eight', 7: 'seven', 6: 'six', 5: 'five', 4: 'four', 3: 'three', 2: 'two', 1: 'one'}
+def snakes_and_ladders(l,a):
+    i,j=0,0
+    while i<len(a):
+        if j+a[i]<len(l):
+            j+=a[i]
+        if l[j]:
+            j+=l[j]
+        i+=1
+    return j
+        
 
-def f(n):
-    a=[]
-    for i in o:
-        t=[]
-        while n>=i:
-            n-=i
-            t.append(o[i])
-        if len(t):
-            a.append([o[len(t)] if o.get(len(t)) else f(len(t)),t[0]] if i>=100 else t[0])
-    return a
-
-def r(x):
-    a=[]
-    [a.append(i) if type(i)!=list else a.extend(r(i)) for i in x]
-    return a
-
-def number_to_english(n):
-    if not n:
-        return 'zero'
-    return ' '.join(r(f(n))) if type(n)==int and n>-1 and n<100000 else ''
-
-print(number_to_english(573982))
+print(snakes_and_ladders([0, 0, 3, 0, 0, 0, 0, -2, 0, 0, 0],[2, 1, 5, 1, 5, 4]))
+print(snakes_and_ladders( [0, 0, 0, 0, 1, 0, 0, 5, 0, 0, 0, -1, 0, 0],[3, 3, 6, 5, 4, 5]))
