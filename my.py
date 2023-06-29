968c828916454e0ab8becff19b74e9f2f5ea8642
Jhonny <gonzodzen@mail.ru>
2023-06-29 19:21:08 +0300

    Find Fibonacci last digit

--- my.py
+++ my.py
@@ -1,11 +1,8 @@
-def calc_fuel(n):
-    n*=11
-    o={"lava": 800, "blaze rod": 120, "coal": 80, "wood": 15, "stick": 1}
-    r={"lava": 0, "blaze rod": 0, "coal": 0, "wood": 0, "stick": 0}
-    for i in o:
-        while n>=o[i]:
-            r[i]+=1
-            n-=o[i]
-    return r
-
-print(calc_fuel(37))
+import sys
+sys.set_int_max_str_digits(100000)
+def get_last_digit(n):
+    a,b,c=0,1,0
+    while n>0:
+        n-=1;c=b;b+=a;a=c
+    a=str(a)
+    return int(a[-1:])
