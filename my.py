a522ce0f2d2401eae388f9d8a7ab2de6f7f78ecf
Jhonny <gonzodzen@mail.ru>
2023-06-30 16:15:43 +0300

    High score table

--- my.py
+++ my.py
@@ -1,60 +1,24 @@
-from typing import List,Tuple
+class HighScoreTable:
+    def __init__(self,n):
+        self.n=n
+        self.scores=[]
+    
+    def update(self,n):
+        self.scores.append(n)
+        self.scores.sort(reverse=True)
+        if len(self.scores)>=self.n:
+            self.scores=self.scores[0:self.n]
 
-def locate_entrance(o:List[str])->Tuple[int,int]:
-    o=[i.rstrip() for i in o]
-    if '.' in o[0]:
-        return (o[0].index('.'),0)
-    if '.' in o[-1]:
-        return (o[-1].index('.'), len(o)-1)
-    for j,i in enumerate(o):
-        if i[0]=='.':
-            return (0,j)
-        if i[len(i)-1]=='.':
-            return (len(i)-1,j)
-    i=0
-    while i<len(o):
-        j=0
-        while j<len(o[i]):
-            if o[i][j]=='.':
-                if o[i][j-1]=='#' and o[i][j+1]=='#':
-                    if j>len(o[i-1])-1 or j>len(o[i+1])-1:
-                        return (j,i)
-                    if j<len(o[i+1])-1 and o[i+1][j]==' ' or j<len(o[i-1])-1 and o[i-1][j]==' ':
-                        return (j,i)
-                if o[i][j-1]==' ' or j==len(o[i])-1:
-                    return (j,i)
-            j+=1
-        i+=1
+    def reset(self):
+       self.scores=[]
 
-print(locate_entrance(['####',
-                       '#..###',
-                       '#....#',
-                       '#....#',
-                       '######.#....##',
-                       '#............#',
-                       '##########...#',
-                       '#...#',
-                       '#...#',
-                       '#...#',
-                       '#...#',
-                       '#...#',
-                       '#...#',
-                       '#...#',
-                       '#####']))
-print(locate_entrance([' #####',
-                       ' #...#',
-                       ' ....#',
-                       ' #...#',
-                       '##...#',
-                       '#....#',
-                       '######']))
-print(locate_entrance(['##############  ',
-                       '#............#  ',
-                       '#............###',
-                       '###.#####......#',
-                       '        #......#',
-                       '        ##.....#',
-                       '         #.....#',
-                       '         #.....#',
-                       '         #.....#',
-                       '         #######']))
+    
+r=HighScoreTable(3)
+
+r.update(10)
+r.update(8)
+r.update(12)
+r.update(6)
+r.update(10)
+r.update(11)
+print(r.scores)
