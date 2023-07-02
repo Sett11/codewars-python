d53495e15443a305c3706975374677c887865219
Jhonny <gonzodzen@mail.ru>
2023-07-02 22:11:27 +0300

    Thinkful - String Drills: Quotable

--- my.py
+++ my.py
@@ -1,10 +1,4 @@
-def user_contacts(d):
-    o={}
-    for i in d:
-        if len(i)==1:
-            o[i[0]]=None
-        else:
-            o[i[0]]=i[1]
-    return o
-    
-print(user_contacts([["Grae Drake", 98110], ["Bethany Kok"], ["Alex Nussbacher", 94101], ["Darrell Silver", 11201]]))
+def quotable(n,q):
+    return f'{n} said: "{q}"'
+
+print(quotable('Grae', 'Practice makes perfect'))
