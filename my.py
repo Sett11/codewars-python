5f3cefee1449364035066b24114b61066f40a243
Jhonny <gonzodzen@mail.ru>
2023-07-02 12:34:36 +0300

    draw me a chessboard

--- my.py
+++ my.py
@@ -1,9 +1,23 @@
-def draw_stairs(n):
-    l,a='I',[]
-    a.append(l)
-    while len(a)<n:
-        l=' '+l
-        a.append(l)
-    return '\n'.join(a)
+def chess_board(r,c):
+    a=[]
+    for i in range(r):
+        t,x,y=[],0,0
+        for j in range(c):
+            if i%2==0:
+                if x%2==0:
+                    t.append('O')
+                    x+=1
+                else:
+                    t.append('X')
+                    x+=1
+            else:
+                if y%2==0:
+                    t.append('X')
+                    y+=1
+                else:
+                    t.append('O')
+                    y+=1
+        a.append(t)
+    return a
 
-print(draw_stairs(7))
+print(chess_board(6,4))
