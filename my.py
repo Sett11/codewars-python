b521f825bf7414fbd88bb9e271fdcfc3f3eaee29
Jhonny <gonzodzen@mail.ru>
2023-07-02 12:39:05 +0300

    Stringy Strings

--- my.py
+++ my.py
@@ -1,23 +1,4 @@
-def chess_board(r,c):
-    a=[]
-    for i in range(r):
-        t,x,y=[],0,0
-        for j in range(c):
-            if i%2==0:
-                if x%2==0:
-                    t.append('O')
-                    x+=1
-                else:
-                    t.append('X')
-                    x+=1
-            else:
-                if y%2==0:
-                    t.append('X')
-                    y+=1
-                else:
-                    t.append('O')
-                    y+=1
-        a.append(t)
-    return a
+def stringy(s):
+    return ''.join(['10'[i%2] for i in range(s)])
 
-print(chess_board(6,4))
+print(stringy(6))
