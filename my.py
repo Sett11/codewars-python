d288391a8ecb7f5c07ba0c43feb54e2c5f296b49
Jhonny <gonzodzen@mail.ru>
2023-07-02 13:02:18 +0300

    Drawing a Cross!

--- my.py
+++ my.py
@@ -1,4 +1,8 @@
-def stringy(s):
-    return ''.join(['10'[i%2] for i in range(s)])
+def draw_a_cross(n):
+    if n<3:
+        return 'Not possible to draw cross for grids less than 3x3!'
+    if n%2==0:
+        return 'Centered cross not possible!'
+    return '\n'.join([''.join(['x' if k==h or k+h==len(x)-1 else b for k,b in enumerate(x)]) for h,x in enumerate([[' ' for i in range(n)] for j in range(n)])])
 
-print(stringy(6))
+print(draw_a_cross(2))
