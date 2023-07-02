60f8c1610c01801056471a11d45869e6e9b876c5
Jhonny <gonzodzen@mail.ru>
2023-07-03 00:09:10 +0300

    They say that only the name is long enough to attract attention. They also said that only a simple Kata will have someone to solve it. This is a sadly story #1: Are they opposite?

--- my.py
+++ my.py
@@ -1,5 +1,13 @@
-def well(x):
-    i=[i for i in [0 if x.count('good')<=2 and 'good' in x else '&',1 if x.count('good')>1 else '&', 2 if x.count('good')==0 else '&']if i!='&']
-    return ['Publish!','I smell a series!','Fail!'][i[0]]
+def is_opposite(s1,s2):
+    if len(s1)!=len(s2) or not s1 and not s2:
+        return False
+    i=0
+    while i<len(s1):
+        if not s1[i].lower()==s2[i].lower() and s1[i]!=s2[i]:
+            continue
+        if s1[i]==s2[i]:
+            return False
+        i+=1
+    return True
 
-print(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good']))
+print(is_opposite("Ab","AB"))
