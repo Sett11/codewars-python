83f28a9648f2d6067e5e08e230a017ab23d9e812
Jhonny <gonzodzen@mail.ru>
2023-07-04 11:57:35 +0300

    Merging sorted integer arrays (without duplicates)

--- my.py
+++ my.py
@@ -1,13 +1,4 @@
-def is_opposite(s1,s2):
-    if len(s1)!=len(s2) or not s1 and not s2:
-        return False
-    i=0
-    while i<len(s1):
-        if not s1[i].lower()==s2[i].lower() and s1[i]!=s2[i]:
-            continue
-        if s1[i]==s2[i]:
-            return False
-        i+=1
-    return True
+def merge_arrays(f,s):
+    return sorted(set(f+s))
 
-print(is_opposite("Ab","AB"))
+print(merge_arrays([1, 3, 5],[2, 4, 6])) 
