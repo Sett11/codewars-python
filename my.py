cbdd1a2331205bb5b2d0868913c1db3264cc49a9
Jhonny <gonzodzen@mail.ru>
2023-07-06 00:58:46 +0300

    SpeedCode #2 - Array Madness

--- my.py
+++ my.py
@@ -1,4 +1,4 @@
-def sorter(l):
-    return sorted(l,key=lambda e:e.lower())
+def array_madness(a,b):
+    return sum([i**2 for i in a])>sum([i**3 for i in b])
 
-print(sorter(['Alg#bra', '$istory', 'Geom^try', '**english']))
+print(array_madness([4, 5, 6], [1, 2, 3]))
