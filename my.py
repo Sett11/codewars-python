886f195e0f6aadbc94f40029afe52377f92bb63f
Jhonny <gonzodzen@mail.ru>
2023-07-06 19:12:18 +0300

    Bug Squish!

--- my.py
+++ my.py
@@ -1,6 +1,5 @@
-def main():
-    a=[roll_dice,move,combat,get_coins,buy_health,print_status]
-    i=0
-    while i<len(a):
-        a[i%len(a)]()
-        i+=1
+import re
+def debug(s):
+    return re.sub(r'bugs|bug',lambda e:e[0] if e[0]=='bugs' else '',s)
+
+print(debug('obugobugobuoobugsoo'))
