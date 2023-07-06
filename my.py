c8195765f7e5e2ac7cd45c7a9ef07d0b68f42a07
Jhonny <gonzodzen@mail.ru>
2023-07-06 15:44:06 +0300

    Please help Bob

--- my.py
+++ my.py
@@ -1,9 +1,18 @@
-def beasts(h,t):
-    if h/2==t:
-        return [h//2,0]
-    for i in range(1,t+1):
-        if i*5+((t-i)*2)==h:
-            return [t-i,i]
-    return 'No solutions'
+def err_bob(s):
+    s=s.split(' ')
+    for i in range(len(s)):
+        if not s[i]:
+            continue
+        j=len(s[i])-1
+        while not s[i][j].isalpha():
+            j-=1
+        t=s[i][j]
+        if t not in 'AIOUEaioue':
+            if t.isupper():
+                s[i]=s[i][:j+1]+'ERR'+s[i][j+1:]
+            else:
+                s[i]=s[i][:j+1]+'err'+s[i][j+1:]
+    return ' '.join(s)
 
-print(beasts(24,12))
+print(err_bob('THIS, is crazy!'))
+print(err_bob("Punctuation? is, important!  double space also"))
