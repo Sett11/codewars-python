6f3e25e3e63489d577e92b8803663e9fb644265c
Jhonny <gonzodzen@mail.ru>
2023-07-06 12:35:27 +0300

    Is this my tail?

--- my.py
+++ my.py
@@ -1,26 +1,4 @@
-def tax_calculator(t):
-    try:
-        c=0
-        if t<=0:
-            1/0
-        while t:
-            if t<=10:
-                c+=t/10
-                t=0
-            if t<=20 and t>=10:
-                x=t-10
-                c+=x/100*7
-                t-=x
-            if t<=30 and t>=20:
-                x=t-20
-                c+=x/100*5
-                t-=x
-            if t>30:
-                x=t-30
-                c+=x/100*3
-                t-=x
-        return int(c) if int(c)==c else round(c,2)
-    except:
-        return 0
+def correct_tail(b,t):
+     return b[-1]==t
 
-print(tax_calculator(10))
+print(correct_tail('Fox','x'))
