c31e41a4438d3b38b4c2ceb5f67a7fd65032d20d
Jhonny <gonzodzen@mail.ru>
2023-07-07 00:42:09 +0300

    Job Matching #2

--- my.py
+++ my.py
@@ -1,4 +1,21 @@
-def match(c,j):
-    return c['min_salary']/100*90<=j['max_salary']
+def match(j,c):
+    return [i for i in c if (not i['desires_equity'] or i['desires_equity'] and j['equity_max']) and (i['current_location'] in j['locations'] or len([k for k in i['desired_locations'] if k in j['locations']]))]
 
-print(match({ 'min_salary': 120000 },{ 'max_salary': 130000 }))
+print(match({ 'equity_max': 0, 'locations': ['Los Angeles', 'New York'] },[{
+  'desires_equity': True, 
+  'current_location': 'New York',
+  'desired_locations': ['San Francisco', 'Los Angeles']
+}, {
+  'desires_equity': False, 
+  'current_location': 'San Francisco',
+  'desired_locations': ['Kentucky', 'New Mexico'] 
+}]))
+print(match({ 'equity_max': 1.2, 'locations': ['New York', 'Kentucky'] },[{
+  'desires_equity': True, 
+  'current_location': 'New York',
+  'desired_locations': ['San Francisco', 'Los Angeles']
+}, {
+  'desires_equity': False, 
+  'current_location': 'San Francisco',
+  'desired_locations': ['Kentucky', 'New Mexico'] 
+}]))
