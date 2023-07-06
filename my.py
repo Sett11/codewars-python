cffb1547c06a4087e6fa91fe034252d81ebf3cd5
Jhonny <gonzodzen@mail.ru>
2023-07-06 12:12:45 +0300

    Tax Calculator

--- my.py
+++ my.py
@@ -1,5 +1,26 @@
-def flip(d,a):
-    a.sort()
-    return a if d=='R' else a[::-1]
+def tax_calculator(t):
+    try:
+        c=0
+        if t<=0:
+            1/0
+        while t:
+            if t<=10:
+                c+=t/10
+                t=0
+            if t<=20 and t>=10:
+                x=t-10
+                c+=x/100*7
+                t-=x
+            if t<=30 and t>=20:
+                x=t-20
+                c+=x/100*5
+                t-=x
+            if t>30:
+                x=t-30
+                c+=x/100*3
+                t-=x
+        return int(c) if int(c)==c else round(c,2)
+    except:
+        return 0
 
-print(flip('R',[3, 2, 1, 2]))
+print(tax_calculator(10))
