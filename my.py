1102fe96fafd2c0529e0c0c5259483bfcc8bd5f0
Jhonny <gonzodzen@mail.ru>
2023-07-06 13:06:52 +0300

    Swap the head and the tail

--- my.py
+++ my.py
@@ -1,4 +1,6 @@
-def correct_tail(b,t):
-     return b[-1]==t
+def swap_head_tail(a):
+    o=a[len(a)//2+(0 if not len(a)&1 else 1):]
+    return o+a[len(o):len(o)+(0 if not len(a)&1 else 1)]+a[:len(o)]
 
-print(correct_tail('Fox','x'))
+print(swap_head_tail([ 1, 2, -3, 4, 5, 6, -7, 8 ]))
+print(swap_head_tail([1,2,3,4,5]))
