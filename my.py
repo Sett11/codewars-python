755acd5401ebe20201d6f8d7b8bc9483d8d58a85
Jhonny <gonzodzen@mail.ru>
2023-07-07 10:36:34 +0300

    Knight vs King

--- my.py
+++ my.py
@@ -1,4 +1,8 @@
-def kata_13_december(l): 
-    return [i for i in l if i&1]
+def knight_vs_king(n,k):
+    a,n,k='ABCDEFGH',list(n),list(k)
+    n[1],k[1]=a.index(n[1]),a.index(k[1])
+    return 'Knight' if (abs(n[0]-k[0])==2 and abs(n[1]-k[1])==1) or (abs(n[0]-k[0])==1 and abs(n[1]-k[1])==2) else 'King' if abs(n[0]-k[0])<2 and abs(n[1]-k[1])<2 else 'None'
 
-print(kata_13_december([1, 2, 2, 2, 4, 3, 4, 5, 6, 7]))
+print(knight_vs_king((4, "C"),(6, "D")))
+print(knight_vs_king((7, "B"), (6, "C")))
+print(knight_vs_king((1, "F"), (2, "F")))
