639c6b8bce79a235c5ce54e84facce268128caa3
Jhonny <gonzodzen@mail.ru>
2023-07-07 10:18:29 +0300

    Failed Sort - Bug Fixing #4

--- my.py
+++ my.py
@@ -1,16 +1,4 @@
-def quicksort(a):
-    i=0
-    while i<len(a):
-        j=i+1
-        while j<len(a):
-            if a[i]>a[j]:
-                t=a[i]
-                a[i]=a[j]
-                a[j]=t
-                i=0
-                j=0
-            j+=1
-        i+=1
-    return a
+def sort_array(s):
+    return ''.join(sorted(s))
 
-print(quicksort([3, 0, 7, 5, 1, 2, 9, 8, 4, 6]))
+print(sort_array('54321'))
