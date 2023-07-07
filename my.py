4b9f006a1f1a26f45e22ae06a6b060d9351a9583
Jhonny <gonzodzen@mail.ru>
2023-07-07 09:56:57 +0300

    Greek Sort

--- my.py
+++ my.py
@@ -1,21 +1,11 @@
-def match(j,c):
-    return [i for i in c if (not i['desires_equity'] or i['desires_equity'] and j['equity_max']) and (i['current_location'] in j['locations'] or len([k for k in i['desired_locations'] if k in j['locations']]))]
+greek_alphabet = (
+    'alpha', 'beta', 'gamma', 'delta', 'epsilon', 'zeta', 
+    'eta', 'theta', 'iota', 'kappa', 'lambda', 'mu', 
+    'nu', 'xi', 'omicron', 'pi', 'rho', 'sigma',
+    'tau', 'upsilon', 'phi', 'chi', 'psi', 'omega')
 
-print(match({ 'equity_max': 0, 'locations': ['Los Angeles', 'New York'] },[{
-  'desires_equity': True, 
-  'current_location': 'New York',
-  'desired_locations': ['San Francisco', 'Los Angeles']
-}, {
-  'desires_equity': False, 
-  'current_location': 'San Francisco',
-  'desired_locations': ['Kentucky', 'New Mexico'] 
-}]))
-print(match({ 'equity_max': 1.2, 'locations': ['New York', 'Kentucky'] },[{
-  'desires_equity': True, 
-  'current_location': 'New York',
-  'desired_locations': ['San Francisco', 'Los Angeles']
-}, {
-  'desires_equity': False, 
-  'current_location': 'San Francisco',
-  'desired_locations': ['Kentucky', 'New Mexico'] 
-}]))
+def greek_comparator(a,b):
+    a,b=greek_alphabet.index(a),greek_alphabet.index(b)
+    return -1 if a<b else 0 if a==b else 1
+
+print(greek_comparator('alpha','beta'))
