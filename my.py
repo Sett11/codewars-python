7b5bd50343603590bb03a27395eb44f02daf18d1
Jhonny <gonzodzen@mail.ru>
2023-07-07 10:14:42 +0300

    Bug Fix - Quick Sort

--- my.py
+++ my.py
@@ -1,4 +1,16 @@
-def my_first_kata(a,b):
-  return False if type(a)!=int and type(a)!=float or type(b)!=int and type(b)!=float else a%b+b%a
-    
-print(my_first_kata(20,2))
+def quicksort(a):
+    i=0
+    while i<len(a):
+        j=i+1
+        while j<len(a):
+            if a[i]>a[j]:
+                t=a[i]
+                a[i]=a[j]
+                a[j]=t
+                i=0
+                j=0
+            j+=1
+        i+=1
+    return a
+
+print(quicksort([3, 0, 7, 5, 1, 2, 9, 8, 4, 6]))
