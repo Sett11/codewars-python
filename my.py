ca11a0526b05d0eaea40e5231721f0af28b13240
Jhonny <gonzodzen@mail.ru>
2023-07-11 20:13:18 +0300

    hexlet

--- my.py
+++ my.py
@@ -1,7 +1,9 @@
-def possible_positions(p):
-    a,b='0abcdefgh',list(p)
-    b[0],b[1]=a.index(b[0]),int(b[1])
-    return [''.join([a[j[0]],str(j[1])]) for j in sorted([i for i in [[b[0]+2,b[1]+1],[b[0]+1,b[1]+2],[b[0]-2,b[1]-1],[b[0]-1,b[1]-2],[b[0]+1,b[1]-2],[b[0]-2,b[1]+1],[b[0]-1,b[1]+2],[b[0]+2,b[1]-1]] if i[0]>0 and i[1]>0 and i[0]<9 and i[1]<9])]
-
-print(possible_positions('f7'))
-print(possible_positions('c3'))
+from random import choice
+a,b=['2','3','4','5','6','7','8','9','10','J','Q','K','A'],['H','D','S','C']
+c,d,pl=choice(a),choice(b),input('Choose').lower()
+if pl=='red' and d in ['H','D']:
+  print(f'Wow {c} {d}')
+elif pl=='black' and d in ['S','C']:
+  print(f'Wow {c} {d}')
+else:
+  print('Oh')
