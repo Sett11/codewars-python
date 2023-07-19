e9531ae372afc551f8ccabc31e574f9c3a4abd65
Jhonny <gonzodzen@mail.ru>
2023-07-20 00:52:19 +0300

    The Culling of Stratholme

--- my.py
+++ my.py
@@ -1,7 +1,17 @@
-def to_imparfait(s):
-    d={'Je':'ais','Tu':'ais','Il':'ait','Elle':'ait','On':'ait','Nous':'ions','Vous':'iez','Ils':'aient','Elles':'aient'}
-    a=s.split(' ')
-    e,a[1]=d[a[0]],a[1][:-2]
-    return ' '.join(a)+e
+import re
 
-print(to_imparfait('Je manger'))
+def purify(s):
+    s,i,a=list(s),0,['i','I']
+    while i<len(s):
+        if s[i] in a:
+            s[i]='&'
+            if i and s[i-1]!=' ':
+                s[i-1]='&'
+            if i!=len(s)-1 and s[i+1]!=' ' and s[i+1] not in a:
+                s[i+1]='&'
+        i+=1
+    return re.sub(r'\s+',' ',''.join([i for i in s if i!='&'])).strip()
+
+print(purify("1i2 33 i4i5 i555ii5"))
+print(purify("It is a bit chilly"))
+print(purify("STRING"))
