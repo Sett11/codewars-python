7fdc2817905e69b7394e69b66f346b4339e8963b
Jhonny <gonzodzen@mail.ru>
2023-07-19 14:16:17 +0300

    Is the date today

--- my.py
+++ my.py
@@ -1,6 +1,6 @@
-def sakura_fall(v):
-    return 80/(v/5) if v>0 else 0
+from datetime import datetime
 
-print(sakura_fall(10))
-print(sakura_fall(-1))
-print(sakura_fall(12.3))
+def is_today(d):
+    return datetime.today().date()==d.date()
+
+print(is_today(datetime.today()))
