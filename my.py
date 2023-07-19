5bd66f3fb70792510c47205a37914011d06df656
Jhonny <gonzodzen@mail.ru>
2023-07-19 12:44:49 +0300

    The falling speed of petals

--- my.py
+++ my.py
@@ -1,9 +1,6 @@
-from random import choice
-a,b=['2','3','4','5','6','7','8','9','10','J','Q','K','A'],['H','D','S','C']
-c,d,pl=choice(a),choice(b),input('Choose').lower()
-if pl=='red' and d in ['H','D']:
-  print(f'Wow {c} {d}')
-elif pl=='black' and d in ['S','C']:
-  print(f'Wow {c} {d}')
-else:
-  print('Oh')
+def sakura_fall(v):
+    return 80/(v/5) if v>0 else 0
+
+print(sakura_fall(10))
+print(sakura_fall(-1))
+print(sakura_fall(12.3))
