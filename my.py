7506b06dab39019d02495128ed268f24fd52db52
Jhonny <gonzodzen@mail.ru>
2023-07-20 00:30:02 +0300

    French Imparfait Conjugation

--- my.py
+++ my.py
@@ -1,19 +1,7 @@
-from functools import reduce
+def to_imparfait(s):
+    d={'Je':'ais','Tu':'ais','Il':'ait','Elle':'ait','On':'ait','Nous':'ions','Vous':'iez','Ils':'aient','Elles':'aient'}
+    a=s.split(' ')
+    e,a[1]=d[a[0]],a[1][:-2]
+    return ' '.join(a)+e
 
-def colorful(n):
-    a,b,i=[int(i) for i in list(str(n))],[],0
-    while i<=len(a):
-        b.append(a[:i])
-        b.append(a[i:])
-        j=i+1
-        while j<=len(a):
-            b.append(a[i:i+j])
-            j+=1
-        i+=1
-    r=[reduce(lambda a,c:int(a)*int(c),i.split(',')) for i in set([','.join([str(j) for j in i]) for i in b if len(i)>1])]+a
-    return True if len(set(r))==len(r) else False
-
-print(colorful(263))
-print(colorful(236))
-print(colorful(235789))
-print(colorful(2357893))
+print(to_imparfait('Je manger'))
