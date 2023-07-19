339e4a5e9c007a7fa45cf699c23d1071e48809ec
Jhonny <gonzodzen@mail.ru>
2023-07-19 20:19:44 +0300

    Weight of its Contents

--- my.py
+++ my.py
@@ -1,2 +1,9 @@
-def swap_values(a): 
-    a[0],a[1]=a[1],a[0]
+def content_weight(w,s): 
+    n=int(''.join([i for i in list(s) if i.isdigit()]))
+    q=w//(n+1)*n
+    return q if 'larger' in s.split(' ') else w-q
+
+print(content_weight(500,"9 times larger"))
+print(content_weight(1000, "999 times larger"))
+print(content_weight(120, "2 times larger"))
+print(content_weight(120, "2 times smaller"))
