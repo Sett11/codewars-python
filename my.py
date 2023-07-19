ecc6f44cb343e1b6a356f82c06335adc0d8d8984
Jhonny <gonzodzen@mail.ru>
2023-07-19 21:39:06 +0300

    Colorful Number

--- my.py
+++ my.py
@@ -1,6 +1,19 @@
-def rgb_to_grayscale(c):
-    h=hex(round(sum([0.299*j if i==0 else 0.587*j if i==1 else 0.114*j for i,j in enumerate([int(c[i:i+2],16) for i in range(1,len(c)+-1,2)])])))[2:]
-    return '#'+(('0' if len(h)<2 else '')+h)*3
+from functools import reduce
 
+def colorful(n):
+    a,b,i=[int(i) for i in list(str(n))],[],0
+    while i<=len(a):
+        b.append(a[:i])
+        b.append(a[i:])
+        j=i+1
+        while j<=len(a):
+            b.append(a[i:i+j])
+            j+=1
+        i+=1
+    r=[reduce(lambda a,c:int(a)*int(c),i.split(',')) for i in set([','.join([str(j) for j in i]) for i in b if len(i)>1])]+a
+    return True if len(set(r))==len(r) else False
 
-print(rgb_to_grayscale('#000000'))
+print(colorful(263))
+print(colorful(236))
+print(colorful(235789))
+print(colorful(2357893))
