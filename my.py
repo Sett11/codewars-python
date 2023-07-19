958bcedaa9d74d731b29dad9e27eb1304b570d27
Jhonny <gonzodzen@mail.ru>
2023-07-19 15:09:01 +0300

    The Coins of Ter | Round to the Next N

--- my.py
+++ my.py
@@ -1,2 +1,7 @@
-def greet(n):
-    return f"Hello, {n}!" if n!="Johnny" else "Hello, my love!"
+def adjust(b,n):
+    c=min(b,n)
+    while not (n<=c and c%b==0):
+        c+=1
+    return c
+
+print(adjust(3,7))
