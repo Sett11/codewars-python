ad22de0c1014627a343628f655dc6dbd0fc67c40
Jhonny <gonzodzen@mail.ru>
2023-07-19 16:46:07 +0300

    Swap Values

--- my.py
+++ my.py
@@ -1,4 +1,2 @@
-def partition(a,d):
-    return [i for i in a if d(i)],[i for i in a if not d(i)]
-
-print(partition(['cat', 'dog', 'duck', 'cow', 'donkey'],lambda x:len(x)==3))
+def swap_values(a): 
+    a[0],a[1]=a[1],a[0]
