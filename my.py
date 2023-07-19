f08d17b51ebbc8183a834e99c80ed75559b4c2a9
Jhonny <gonzodzen@mail.ru>
2023-07-19 14:30:15 +0300

    How do I compare numbers?

--- my.py
+++ my.py
@@ -1,3 +1,7 @@
-def validate(u,p):
-    d=Database()
-    return d.login(u,p)
+def what_is(x):
+    if x == 42:
+        return 'everything'
+    elif x == 42 * 42:
+        return 'everything squared'
+    else:
+        return 'nothing'
