4a25d228aebdd68913563cb229d85b3e2e01fa50
Jhonny <gonzodzen@mail.ru>
2023-07-19 20:47:14 +0300

    Color to Grayscale

--- my.py
+++ my.py
@@ -1,9 +1,6 @@
-def content_weight(w,s): 
-    n=int(''.join([i for i in list(s) if i.isdigit()]))
-    q=w//(n+1)*n
-    return q if 'larger' in s.split(' ') else w-q
+def rgb_to_grayscale(c):
+    h=hex(round(sum([0.299*j if i==0 else 0.587*j if i==1 else 0.114*j for i,j in enumerate([int(c[i:i+2],16) for i in range(1,len(c)+-1,2)])])))[2:]
+    return '#'+(('0' if len(h)<2 else '')+h)*3
 
-print(content_weight(500,"9 times larger"))
-print(content_weight(1000, "999 times larger"))
-print(content_weight(120, "2 times larger"))
-print(content_weight(120, "2 times smaller"))
+
+print(rgb_to_grayscale('#000000'))
