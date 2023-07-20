b03057f549ff394f154a686af682ec8462c0dfe5
Jhonny <gonzodzen@mail.ru>
2023-07-21 00:09:36 +0300

    [Code Golf] Optional Walrus?

--- my.py
+++ my.py
@@ -1,42 +1,4 @@
-import math
-from functools import reduce
-
-def convert(n,b):
-    a,r='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'[:b],''
-    while n>0:
-        r=a[n%b]+r
-        n//=b
-    return r
-
-def primes_checker(n):
-    if n<2:
-        return False
-    if n==2:
-        return True
-    i=2
-    while i<=math.sqrt(n):
-        if not (n%i):
-            return False
-        i+=1
-    return True
-
-def primes_count(n):
-    a,i=[],2
-    while len(a)<n:
-        if primes_checker(i):
-            a.append(i)
-        i+=1
-    return a
-
-def red(a):
-    return reduce(lambda a,c:a*c,a)
-
-def solver(k,n,d):
-    a,b,c=primes_count(k+n)[k-1:k-1+n],[],[]
-    for i in range(n):
-        b.append(a[i]**(i+1))
-        c.append(a[i]**(n-i))
-    return convert(red(b),d),convert(red(c),d)
-
-print(solver(20,4,36))
-print(solver(2,2,8))
+f=lambda a,i:len(a)*(len(a)>i)
+    
+print(f('abc',2))
+print(f('abc',4))
