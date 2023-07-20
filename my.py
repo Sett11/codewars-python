aa81ec0930033cbf1ee93ecfd2a4ce4878e7a353
Jhonny <gonzodzen@mail.ru>
2023-07-20 22:40:58 +0300

    Russian Peasant Multiplication

--- my.py
+++ my.py
@@ -1,2 +1,5 @@
-def plural(n):
-    return True if n!=1 else False
+def russian_peasant_multiplication(x,y):
+    return sum([x for i in range(y)])
+
+print(russian_peasant_multiplication(10,5))
+print(russian_peasant_multiplication(1.001,2))
