1ee88213bafca08c70394e771df23d4f75245f70
Jhonny <gonzodzen@mail.ru>
2023-07-20 21:31:10 +0300

    Time Simulation

--- my.py
+++ my.py
@@ -1,31 +1,32 @@
-ROCKS={'Clay': (1, 5), 'Copper': (1, 17.5), 'Tin': (1, 17.5), 'Iron': (15, 35), 'Silver': (20, 40), 'Coal': (30, 50), 'Gold': (40, 65)}
-EXPERIENCE={1: 0, 2: 83, 3: 174, 4: 276, 5: 388, 6: 512, 7: 650, 8: 801, 9: 969, 10: 1154, 11: 1358, 12: 1584, 13: 1833, 14: 2107, 15: 2411, 16: 2746, 17: 3115, 18: 3523, 19: 3973, 20: 4470, 21: 5018, 22: 5624, 23: 6291, 24: 7028, 25: 7842, 26: 8740, 27: 9730, 28: 10824, 29: 12031, 30: 13363, 31: 14833, 32: 16456, 33: 18247, 34: 20224, 35: 22406, 36: 24815, 37: 27473, 38: 30408, 39: 33648, 40: 37224}
-class Miner:
-    def __init__(self,x=0):
-        self.x=x
-        self.l=0
-        for i in EXPERIENCE:
-            if self.x>=EXPERIENCE[i] and i>self.l:
-                self.l=i
-    def mine(self,r):
-        a=ROCKS[r]
-        if a[0]>self.l:
-            return f'You need a mining level of {a[0]} to mine {r}.'
-        self.x+=a[1]
-        for i in EXPERIENCE:
-            if self.x>=EXPERIENCE[i] and i>self.l:
-                self.l=i
-                return f'Congratulations, you just advanced a Mining level! Your mining level is now {i}.'
-        return 'You swing your pick at the rock.'
+class SimTime:
+    def __init__(self):
+        self.speed=1
+        self.real_time=0
+        self.time=0
 
-r=Miner()
+    def get(self):
+        return self.time
+    
+    def set_speed(self,n):
+        self.speed=n
 
-print(r.mine('Tin'))
-print(r.mine('Tin'))
-print(r.mine('Tin'))
-print(r.mine('Tin'))
-print(r.mine('Tin'))
-print(r.mine('Tin'))
-print(r.mine('Tin'))
-print(r.mine('Tin'))
-print(r.mine('Iron'))
+    def update(self,n):
+        if n<self.real_time:
+            raise()
+        self.time+=(n-self.real_time)*self.speed
+        self.real_time+=(n-self.real_time)
+
+        
+r=SimTime()
+
+print(r.get())
+
+r.update(10)
+print(r.get())
+r.update(12)
+r.set_speed(3)
+r.update(15)
+print(r.get())
+r.update(17)
+
+print(r.get())
