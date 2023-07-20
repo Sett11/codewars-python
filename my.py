0205c81442105d3021c8341ff71a50b0d5747150
Jhonny <gonzodzen@mail.ru>
2023-07-21 01:17:37 +0300

    Sort the climbing grades

--- my.py
+++ my.py
@@ -1,4 +1,5 @@
-f=lambda a,i:len(a)*(len(a)>i)
-    
-print(f('abc',2))
-print(f('abc',4))
+def sort_grades(l):
+    o={'VB':0,'V0':1,'V0+':2,'V1':3,'V2':4,'V3':5,'V4':6,'V5':7,'V6':8,'V7':9,'V8':10,'V9':11,'V10':12,'V11':13,'V12':14,'V13':15,'V14':16,'V15':17,'V16':18,'V17':19}
+    return [i[0] for i in sorted([[i,o[i]] for i in l],key=lambda e:e[1])]
+
+print(sort_grades(['V0+', 'V0', 'V16', 'V2', 'VB', 'V6']))
