32193de0d48a670a30b69496fa029183c82b865e
Jhonny <gonzodzen@mail.ru>
2023-07-21 12:36:34 +0300

    Find your caterer

--- my.py
+++ my.py
@@ -1,5 +1,8 @@
-def sort_grades(l):
-    o={'VB':0,'V0':1,'V0+':2,'V1':3,'V2':4,'V3':5,'V4':6,'V5':7,'V6':8,'V7':9,'V8':10,'V9':11,'V10':12,'V11':13,'V12':14,'V13':15,'V14':16,'V15':17,'V16':18,'V17':19}
-    return [i[0] for i in sorted([[i,o[i]] for i in l],key=lambda e:e[1])]
+def find_caterer(b,p):
+    s1,s2,s3=p*15,p*20,p*30-((p*30)//100*20 if p>60 else 0)
+    r=[i for i in [[1,s1],[2,s2],[3,s3]] if i[1]<=b]
+    return -1 if not p or s1>b else r[0][0] if len(r)==1 else [j[0] for j in r if b-j[1]==min([b-i[1] for i in r])][0]
 
-print(sort_grades(['V0+', 'V0', 'V16', 'V2', 'VB', 'V6']))
+print(find_caterer(150,10))
+print(find_caterer(1500,60))
+print(find_caterer(1500,61))
