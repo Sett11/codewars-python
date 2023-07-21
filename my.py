a320fd949ddc695892f7d4d91e7bfc63ff27499b
Jhonny <gonzodzen@mail.ru>
2023-07-21 15:10:04 +0300

    Oh, so you like programming? Name all of the keywords!

--- my.py
+++ my.py
@@ -1,24 +1,2 @@
-def encode(s):
-    c,i='',0
-    while i<len(s)//2+(1 if len(s)&1 else 0):
-        c+=s[i]+s[len(s)-i-1]
-        i+=1
-    return c[:-1] if len(s)&1 else c
-    
-def decode(s):
-    q,w,i='','',0
-    while i<len(s):
-        if not i&1:
-            q+=s[i]
-        else:
-            w+=s[i]
-        i+=1
-    return q+w[::-1]
-
-print(encode('codewars'))
-print(encode('white'))
-print(encode("You have chosen to translate this kata."))
-
-print(decode('csordaew'))
-print(decode('wehti'))
-print(decode('Y.oaut ahka vsei hcth oesteanl stnoa rt'))
+import keyword
+keywords={*keyword.kwlist}
