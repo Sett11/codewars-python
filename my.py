814e124e31c511e403c5dd756f1448ed1630ffb3
Jhonny <gonzodzen@mail.ru>
2023-07-21 15:54:48 +0300

    Playing Cards Draw Order – Part 1

--- my.py
+++ my.py
@@ -1,14 +1,9 @@
-import re
-def fn_shorthand(s):
-    c,i=list(re.sub(r' +',' ',s)),0
-    while i<len(c):
-        if c[i]=='>':
-            c[i]='('
-            c+=')'
-        i+=1
-    return re.sub(r' ',',',''.join(c)).replace(',(','(').replace('(,','(')
+def draw(d):
+    a=[]
+    while len(d):
+        a.append(d.pop(0))
+        if len(d):
+            d.append(d.pop(0))
+    return a
 
-print(fn_shorthand("max > list > 9**5 7**9"))
-print(fn_shorthand("list > map > upper set > a b"))
-print(fn_shorthand("fn>fn>fn>fn>fn>fn>fn>"))
-print(fn_shorthand("print > 1 2 3"))
+print(draw(['AH','2H','3H','4H']))
