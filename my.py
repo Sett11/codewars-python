167aec2653afc97326ab4de32c3c1233101b7650
Jhonny <gonzodzen@mail.ru>
2023-07-23 15:22:11 +0300

    Turn any word into a beef taco

--- my.py
+++ my.py
@@ -1,7 +1,6 @@
-def direction(f,t):
-    a=['N','NE','E','SE','S','SW','W','NW']
-    n,c=t//45,a.index(f)
-    return a[(n+c)%len(a)]
+import re
+def tacofy(w):
+    a={'t':'tomato','l':'lettuce','c':'cheese','g':'guacamole','s':'salsa'}
+    return ['shell']+[j for j in ['beef' if i in 'aioue' else a.get(i) for i in re.sub(r'.',lambda e:e.group().lower()+' ',w).split(' ') if i] if j]+['shell']
 
-print(direction('S',180))
-print(direction('SE',-45))
+print(tacofy('ogl'))
