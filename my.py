c90bde017c5899e0c221f55000a63f332fa0dc95
Jhonny <gonzodzen@mail.ru>
2023-07-23 11:36:42 +0300

    Merge overlapping strings

--- my.py
+++ my.py
@@ -1,10 +1,15 @@
-def triple_crown(a):
-    r=[]
-    [r.append([a[i]['Receiving yards'],a[i]['Receiving touchdowns'],a[i]['Receptions']]) for i in a]
-    k=list(zip(*r))
-    f=[i for i in a if a[i]['Receiving yards']==max(k[0]) or a[i]['Receiving touchdowns']==max(k[1]) or a[i]['Receptions']==max(k[2])]
-    return f[0] if len(f)==1 else 'None of them'
+def merge_strings(f,s):
+    if f==s:
+        return f
+    i,a=1,[]
+    while i<=len(s):
+        a.append(s[:i])
+        i+=1
+    a=sorted([i for i in a if f.endswith(i)],key=lambda e:len(e),reverse=True)
+    return f+s if not len(a) else f[:-len(a[0])]+s
 
-print(triple_crown({'Cooper Kupp': {'Receiving yards': 1700, 'Receiving touchdowns': 18, 'Receptions': 117},
-                                         'Justin Jefferson': {'Receiving yards':1650, 'Receiving touchdowns': 17, 'Receptions': 115},
-                                        'Davante Adams':{'Receiving yards': 1750, 'Receiving touchdowns': 16, 'Receptions': 113}}))
+print(merge_strings("abcde","cdefgh"))
+print(merge_strings('abcde', 'efghi'))
+print(merge_strings('abcde', 'bcdefghi'))
+print(merge_strings('abaabaab', 'aabaabab'))
+print(merge_strings('tedd', 'edd'))
