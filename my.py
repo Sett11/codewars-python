52cd0e5c805860f6868d7996bfe725dc7844eed7
Jhonny <gonzodzen@mail.ru>
2023-07-24 12:35:48 +0300

    Two numbers are positive

--- my.py
+++ my.py
@@ -1,13 +1,4 @@
-def mountains_of_hoiyama(n):
-    i,a,k,c=n,[],1,0
-    while i>0:
-        t,j=[],k
-        while len(t)<=i//2:
-            t.append(j)
-            j+=1
-        i-=2
-        k+=2
-        c+=sum(t+t[::-1][1:])
-    return c
+def two_are_positive(a,b,c):
+    return len([i for i in [a,b,c] if i>0])==2
 
-print(mountains_of_hoiyama(7))
+print(two_are_positive(2,4,-6))
