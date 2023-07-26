656c107e7db3c2192948d7e0563aa226cd73123c
Jhonny <gonzodzen@mail.ru>
2023-07-26 15:24:14 +0300

    Most sales

--- my.py
+++ my.py
@@ -1,20 +1,4 @@
-def get_free_urinals(s):
-    if '11' in s:
-        return -1
-    if len(set(s))==1 and s[0]=='0':
-        return len([k for k in ['1' if not i&1 else '0' for i,j in enumerate(s)] if k=='1'])
-    i,l=(1 if s[0]=='1' else 0),list(s)
-    while i<len(l)-1:
-        if l[i]=='0' and l[i+1]=='0' and (l[i-1]=='0' or not i):
-            l[i]='1'
-        i+=1
-    if l[-1]=='0' and l[-2]=='0':
-        l[-1]='1'
-    if l[0]=='0' and l[1]=='0':
-        l[0]='1'
-    return len([i for i in l if i=='1'])-len([i for i in s if i=='1'])
+def top3(p,a,q):
+    return [k[0] for k in sorted([[i,a[j]*q[j]] for j,i in enumerate(p)],key=lambda e:e[1],reverse=True)][:3]
 
-print(get_free_urinals('10001'))
-print(get_free_urinals('00000'))
-print(get_free_urinals('01000'))
-print(get_free_urinals('0000000000001'))
+print(top3(["Computer", "Cell Phones", "Vacuum Cleaner"], [3,24,8], [199,299,399]))
