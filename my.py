a93d92055d6c665cc8eb1867d3ffc8bfe32a179d
Jhonny <gonzodzen@mail.ru>
2023-07-27 14:57:59 +0300

    Identify Case

--- my.py
+++ my.py
@@ -1,6 +1,15 @@
-def is_valid(s):
-    a=['_','$']
-    return (s[0].isalpha() or s[0] in a) and all(map(lambda e: e in a or e.isdigit() or e.isalpha(),s[1:])) if s else 1==2
+def case_id(s):
+    print(s)
+    if '--' in s or '__' in s or ('-' in s and '_' in s) or(('-' in s or '_' in s) and [i for i in s if i.isupper()]):
+        return 'none'
+    a,b=s.split('-'),s.split('_')
+    if len(a)>1 and not [i for i in a if i[0].isupper()]:
+        return 'kebab'
+    if len(b)>1 and not [i for i in b if i[0].isupper()]:
+        return 'snake'
+    return 'camel' if [i for i in s if i.isupper()] and not [i for i in s if i in ['_','-']] else 'none'
 
-print(is_valid("okay_ok1"))
-print(is_valid(""))
+print(case_id("hello-to-the-world"))
+print(case_id("helloWorld"))
+print(case_id("hello_world"))
+print(case_id("hello-World"))
