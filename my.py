c1901c0a9864159c18eb1a6ed579db352158e52b
Jhonny <gonzodzen@mail.ru>
2023-07-27 12:15:35 +0300

    Converting 12-hour time to 24-hour time

--- my.py
+++ my.py
@@ -1,9 +1,10 @@
-def to12hourtime(t):
-    a,b=int(t[:2]),str(int(t[2:]))
-    v=a>=12
-    b,c=('0' if len(b)<2 else '')+b,'pm' if v else 'am' if a else 'pm'
-    return f'{str(a-12 if v else a if a else 12)}:{b} {c}'
+import re
 
-print(to12hourtime('2145'))
-print(to12hourtime('1300'))
-print(to12hourtime('0000'))
+def to24hourtime(h,m,p):
+    s=('0' if h<10 and p!='pm' else '')+f'{h+(12 if p=="pm" and h<12 else 0)}{("0" if m<10 else "")+str(m)}'
+    return s if p=='pm' else re.sub(r'^12','00',s)
+
+print(to24hourtime(1,0,'am'))
+print(to24hourtime(9,45,'pm'))
+print(to24hourtime(12,0,'am'))
+print(to24hourtime(12,0,'pm'))
