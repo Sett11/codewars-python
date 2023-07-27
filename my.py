9407fcb33f98f01613cf8965f8ddd967d5bd1682
Jhonny <gonzodzen@mail.ru>
2023-07-27 15:34:37 +0300

    Square Roots: Approximation

--- my.py
+++ my.py
@@ -1,15 +1,12 @@
-def case_id(s):
-    print(s)
-    if '--' in s or '__' in s or ('-' in s and '_' in s) or(('-' in s or '_' in s) and [i for i in s if i.isupper()]):
-        return 'none'
-    a,b=s.split('-'),s.split('_')
-    if len(a)>1 and not [i for i in a if i[0].isupper()]:
-        return 'kebab'
-    if len(b)>1 and not [i for i in b if i[0].isupper()]:
-        return 'snake'
-    return 'camel' if [i for i in s if i.isupper()] and not [i for i in s if i in ['_','-']] else 'none'
+import math
 
-print(case_id("hello-to-the-world"))
-print(case_id("helloWorld"))
-print(case_id("hello_world"))
-print(case_id("hello-World"))
+def approx_root(n):
+    q=math.sqrt(n)
+    c=math.floor(q)
+    a,b=c**2,math.ceil(q)**2
+    d1,d2=n-a,b-a
+    return int(q) if int(q)==q else round(c+(d1/d2),2)
+
+print(approx_root(213))
+print(approx_root(400))
+print(approx_root(2))
