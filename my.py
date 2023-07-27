da031c86457edb10d393f623123a63c4a01a658d
Jhonny <gonzodzen@mail.ru>
2023-07-27 11:17:13 +0300

    Find the Speedcuber's times!

--- my.py
+++ my.py
@@ -1,23 +1,4 @@
-def countWins(w,c):
-    return len([i for i in w if i['country']==c])
+def cube_times(t):
+    return round(sum(sorted(t)[1:-1])/3,2),min(t)
 
-print(countWins([{ 'season': '1996-97', 'team': 'Borussia Dortmund', 'country': 'Germany' },
-{ 'season': '1997-98', 'team': 'Real Madrid', 'country': 'Spain' }, 
-  { 'season': '1998-99', 'team': 'Manchester United', 'country': 'England' },
-  { 'season': '1999-00', 'team': 'Real Madrid', 'country': 'Spain' },
-  { 'season': '2000-01', 'team': 'Bayern Munich', 'country': 'Germany' },
-  { 'season': '2001-02', 'team': 'Real Madrid', 'country': 'Spain' },
-  { 'season': '2002-03', 'team': 'Milan', 'country': 'Italy' },
-  { 'season': '2003-04', 'team': 'Porto', 'country': 'Portugal' },
-  { 'season': '2004-05', 'team': 'Liverpool', 'country': 'England' },
-  { 'season': '2005-06', 'team': 'Barcelona', 'country': 'Spain' },
-  { 'season': '2006-07', 'team': 'Milan', 'country': 'Italy' },
-  { 'season': '2007-08', 'team': 'Manchester United', 'country': 'England' },
-  { 'season': '2008-09', 'team': 'Barcelona', 'country': 'Spain' },
-  { 'season': '2009-10', 'team': 'Internazionale', 'country': 'Italy' },
-  { 'season': '2010-11', 'team': 'Barcelona', 'country': 'Spain' },
-  { 'season': '2011-12', 'team': 'Chelsea', 'country': 'England' },
-  { 'season': '2012-13', 'team': 'Bayern', 'country': 'Germany' },
-  { 'season': '2013-14', 'team': 'Real Madrid', 'country': 'Spain' },
-  { 'season': '2014-15', 'team': 'Barcelona', 'country': 'Spain' },
-  { 'season': '2015-16', 'team': 'Real Madrid', 'country': 'Spain' }],'Spain'))
+print(cube_times([9.5, 7.6, 11.4, 10.5, 8.1]))
