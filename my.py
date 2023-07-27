f9d1aae3bb37bf8781cd5845dd0478882e37de44
Jhonny <gonzodzen@mail.ru>
2023-07-27 09:34:50 +0300

    Moving Particles

--- my.py
+++ my.py
@@ -1,8 +1,21 @@
-def pathfinder_scores(s):
-    try:
-        a={7:-4,8:-2,9:-1,10:0,11:1,12:2,13:3,14:5,15:7,16:10,17:13,18:17}
-        return sum([a[i] for i in s])<=25
-    except:
-        return False
+def moving_particles(a):
+    i,l,k=0,len(a),[i for i,j in enumerate(a) if j>0]
+    if (k and not [j for j in a[k[0]+1:] if j<0]) or not k:
+        return a
+    while i<l-1:
+        if a[i]>0 and a[i+1]<0:
+            b,c=abs(a[i]),abs(a[i+1])
+            t=b+c
+            t=t if b>=c else -t
+            a[i+1]=t
+            a=a[:i]+a[i+1:]
+            l-=1
+            i-=1
+        i+=1
+    return moving_particles(a)
 
-print(pathfinder_scores([18, 13, 7, 12, 15, 10]))
+print(moving_particles([-1, 3, -1, 2]))
+print(moving_particles([3,-1]))
+print(moving_particles([5, -1, -2, -9]))
+print(moving_particles([-2, 8, -10, 2, 4, 4, 4, -9]))
+print(moving_particles([9, 9, -9, -6, -4, -2, 8, 4, 9]))
