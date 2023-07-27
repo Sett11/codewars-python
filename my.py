07366aa42ddd866686c787df8bb9dc57ac2280e9
Jhonny <gonzodzen@mail.ru>
2023-07-27 11:36:15 +0300

    Converting 24-hour time to 12-hour time

--- my.py
+++ my.py
@@ -1,4 +1,9 @@
-def cube_times(t):
-    return round(sum(sorted(t)[1:-1])/3,2),min(t)
+def to12hourtime(t):
+    a,b=int(t[:2]),str(int(t[2:]))
+    v=a>=12
+    b,c=('0' if len(b)<2 else '')+b,'pm' if v else 'am' if a else 'pm'
+    return f'{str(a-12 if v else a if a else 12)}:{b} {c}'
 
-print(cube_times([9.5, 7.6, 11.4, 10.5, 8.1]))
+print(to12hourtime('2145'))
+print(to12hourtime('1300'))
+print(to12hourtime('0000'))
