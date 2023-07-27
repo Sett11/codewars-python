ec7dae4ead9b5c42255883be5719812041d3545e
Jhonny <gonzodzen@mail.ru>
2023-07-27 20:50:32 +0300

    Valid Braces

--- my.py
+++ my.py
@@ -1,4 +1,31 @@
-def find_squares(n):
-    return f'{(int(n/2)+1)**2}-{int(n/2)**2}'
+def valid_braces(s):
+   if s.count('(')!=s.count(')') or s.count('[')!=s.count(']') or s.count('{')!=s.count('}') or '(]' in s:
+     return False
+   i,a=0,[]
+   while i<len(s):
+    if s[i]=='(':
+      a.append(1)
+    if s[i]==')':
+      if 1 not in a:
+        return False
+      a[a.index(1)]=0
+    if s[i]=='{':
+      a.append(3)
+    if s[i]=='}':
+      if 3 not in a:
+        return False
+      a[a.index(3)]=0
+    if s[i]=='[':
+      a.append(5)
+    if s[i]==']':
+      if 5 not in a:
+        return False
+      a[a.index(5)]=0
+    i+=1
+   return True
 
-print(find_squares(303909))
+
+print(valid_braces('{}()[]'))
+print(valid_braces("([}{])"))
+print(valid_braces("(({{[[]]}}))"))
+print(valid_braces("{}({})[]"))
