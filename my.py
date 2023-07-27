274473dae3b39b5f3a3cfd243fda51d8c69bd204
Jhonny <gonzodzen@mail.ru>
2023-07-27 22:07:56 +0300

    Dragon's Curve

--- my.py
+++ my.py
@@ -1,31 +1,6 @@
-def valid_braces(s):
-   if s.count('(')!=s.count(')') or s.count('[')!=s.count(']') or s.count('{')!=s.count('}') or '(]' in s:
-     return False
-   i,a=0,[]
-   while i<len(s):
-    if s[i]=='(':
-      a.append(1)
-    if s[i]==')':
-      if 1 not in a:
-        return False
-      a[a.index(1)]=0
-    if s[i]=='{':
-      a.append(3)
-    if s[i]=='}':
-      if 3 not in a:
-        return False
-      a[a.index(3)]=0
-    if s[i]=='[':
-      a.append(5)
-    if s[i]==']':
-      if 5 not in a:
-        return False
-      a[a.index(5)]=0
-    i+=1
-   return True
+import re
 
+def Dragon(n,s='Fa'):
+    return '' if type(n)!=int or n<0 else re.sub('a|b','',s) if n==0 else Dragon(n-1,re.sub(r'a|b',lambda e:'aRbFR' if e.group()=='a' else 'LFaLb',s))
 
-print(valid_braces('{}()[]'))
-print(valid_braces("([}{])"))
-print(valid_braces("(({{[[]]}}))"))
-print(valid_braces("{}({})[]"))
+print(Dragon(2))
