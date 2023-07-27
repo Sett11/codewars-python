bbc5abc63dbfa6201bb1e6347336e63068f743ae
Jhonny <gonzodzen@mail.ru>
2023-07-28 00:31:52 +0300

    Temperature analysis II

--- my.py
+++ my.py
@@ -1,6 +1,8 @@
-def validate_ean(c):
-    s=sum([int(i)*3 if j&1 else int(i) for j,i in enumerate(c[:-1])])
-    return int(c[-1])==(0 if s%10==0 else 10-(s%10))
+def close_to_zero(t):
+    s=t.split(' ')
+    m=min([abs(0-int(i)) for i in s]) if t else 0
+    return m if str(m) in s else -m if t else 0
 
-print(validate_ean('4003301018398'))
-print(validate_ean('9783827317100'))
+print(close_to_zero('-37 4 -1 -42 -5 -43 28 20 8 2 -14 -30 42 -10 9 20 14 -23 -25 36 -48 2 19 -24 37 33 50 -18 29 -3 -28 7 -40 4 -11 -34 6 -22 -26 -12 47 -49'))
+print(close_to_zero('28 35 -21 17 38 -17'))
+print(close_to_zero('-1 50 -4 20 22 -7 0 10 -8'))
