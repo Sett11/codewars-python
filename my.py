b8b5f6a90c25b96124b20c399e96f60c271a6dc2
Jhonny <gonzodzen@mail.ru>
2023-07-27 11:13:21 +0300

    Find how many times did a team from a given country win the Champions League?

--- my.py
+++ my.py
@@ -1,13 +1,23 @@
-def team_dinner(a):
-    r=sum(a,[])
-    s=set(r)
-    w=sorted([[r.count(i),i] for i in s],key=lambda e:e[0],reverse=True)
-    q=[i[1] for i in w if i[0]==w[0][0]]
-    z=[[k[0],len(sum(k[1],[]))] for k in [j for j in [[i,[j for j in a if i in j]] for i in q] if len(j[1])>=len(a)/2]]
-    return None if not z else sorted(z,key=lambda e:(e[1],-e[0]),reverse=True)[0][0]
+def countWins(w,c):
+    return len([i for i in w if i['country']==c])
 
-
-print(team_dinner([[0, 1, 2, 3], [2], [0, 2], [3, 4, 6], [1, 3, 6]]))
-print(team_dinner([[0, 1, 2], [1], [1, 2, 4], []]))
-print(team_dinner([[0, 1, 5], [2, 3], [4], [], [0, 1]]))
-print(team_dinner([[9, 4, 5], []]))
+print(countWins([{ 'season': '1996-97', 'team': 'Borussia Dortmund', 'country': 'Germany' },
+{ 'season': '1997-98', 'team': 'Real Madrid', 'country': 'Spain' }, 
+  { 'season': '1998-99', 'team': 'Manchester United', 'country': 'England' },
+  { 'season': '1999-00', 'team': 'Real Madrid', 'country': 'Spain' },
+  { 'season': '2000-01', 'team': 'Bayern Munich', 'country': 'Germany' },
+  { 'season': '2001-02', 'team': 'Real Madrid', 'country': 'Spain' },
+  { 'season': '2002-03', 'team': 'Milan', 'country': 'Italy' },
+  { 'season': '2003-04', 'team': 'Porto', 'country': 'Portugal' },
+  { 'season': '2004-05', 'team': 'Liverpool', 'country': 'England' },
+  { 'season': '2005-06', 'team': 'Barcelona', 'country': 'Spain' },
+  { 'season': '2006-07', 'team': 'Milan', 'country': 'Italy' },
+  { 'season': '2007-08', 'team': 'Manchester United', 'country': 'England' },
+  { 'season': '2008-09', 'team': 'Barcelona', 'country': 'Spain' },
+  { 'season': '2009-10', 'team': 'Internazionale', 'country': 'Italy' },
+  { 'season': '2010-11', 'team': 'Barcelona', 'country': 'Spain' },
+  { 'season': '2011-12', 'team': 'Chelsea', 'country': 'England' },
+  { 'season': '2012-13', 'team': 'Bayern', 'country': 'Germany' },
+  { 'season': '2013-14', 'team': 'Real Madrid', 'country': 'Spain' },
+  { 'season': '2014-15', 'team': 'Barcelona', 'country': 'Spain' },
+  { 'season': '2015-16', 'team': 'Real Madrid', 'country': 'Spain' }],'Spain'))
