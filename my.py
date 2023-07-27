00f795cd69e3584fb3f78d521da68204e21d1e86
Jhonny <gonzodzen@mail.ru>
2023-07-27 23:27:02 +0300

    Get All Possible Anagrams from a Hash

--- my.py
+++ my.py
@@ -1,6 +1,22 @@
-import re
+def f(a):
+    l,c,r,i,k,p=len(a),[0 for i in a],[''.join(a)],1,0,0
+    while i<l:
+        if c[i]<i:
+            k,p=i%2 and c[i],a[i]
+            a[i]=a[k]
+            a[k]=p
+            c[i]+=1
+            i,s=1,''.join(a.copy())
+            if s not in r:
+                r.append(s)
+        else:
+            c[i]=0
+            i+=1
+    return r
 
-def Dragon(n,s='Fa'):
-    return '' if type(n)!=int or n<0 else re.sub('a|b','',s) if n==0 else Dragon(n-1,re.sub(r'a|b',lambda e:'aRbFR' if e.group()=='a' else 'LFaLb',s))
+def get_words(h):
+    a=[]
+    [a.append(''.join(i*h[i])) for i in h]
+    return sorted(f(list(''.join(a))))
 
-print(Dragon(2))
+print(get_words({2:["a"], 1:["b", "c"]}))
