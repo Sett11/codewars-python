a1b5638d7a98e241f2496c250d39d73049fd16e7
Jhonny <gonzodzen@mail.ru>
2023-07-27 13:56:15 +0300

    Identify Best Planet

--- my.py
+++ my.py
@@ -1,9 +1,20 @@
-from datetime import datetime
+import re
 
-def convert(t):
-    s=str(t).split(' ')[-1].split('.')
-    return s[0]+',000' if len(s)==1 else ','.join(s)[:-3]
+R='CCHNOPa'
 
-print(convert(datetime(2016, 2, 8, 16, 42, 59,399000)))
-print(convert(datetime(2016, 2, 8, 16, 42, 59,10)))
-print(convert(datetime(2016, 2, 8, 16, 42, 59)))
+def best_planet(s,m):
+    s,i=[[j[0],m-int(j[1]),j[0]+'_'+j[1]] for j in [i.split('_') for i in s] if int(j[1])<=m],0
+    while i<len(s):
+        j=0
+        while j<len(s[i][0])-1:
+            t,q=s[i][0][j],s[i][0][j+1]
+            if t.isupper() and q.islower() and t+q!='Ca':
+                s[i][0]=re.sub(t+q,'',s[i][0])
+                j-=1
+            j+=1
+        s[i][0]=''.join(sorted(list([j for j in s[i][0] if j in R])))
+        i+=1
+    s=[i for i in s if i[0]==R]
+    return '' if not len(s) else sorted(s,key=lambda e:e[1])[0][2]
+
+print(best_planet(['HCNPCaO_32', 'CaHPNOC_19', 'ICCaHeKrNOHP_282', 'PTlRnRbCHNe_211', 'SnIrSmRb_278', 'PCaCHON_245', 'NArCuSiErCnCaCd_134', 'CaONP_291'], 382))
