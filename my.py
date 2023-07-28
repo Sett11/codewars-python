11a25da1b09bd3b683f1d328494234cb5b562ff6
Jhonny <gonzodzen@mail.ru>
2023-07-28 13:14:50 +0300

    Semi-Optional

--- my.py
+++ my.py
@@ -1,8 +1,5 @@
-import re
+def wrap(value):
+    return {'value':value}
 
-def trump_detector(t):
-    s=[i.lower() for i in re.findall(r'u+|e+|a+|i+|o+',t,flags=re.I)]
-    return round((len(''.join(s))-len(s))/len(s),2)
-
-print(trump_detector("HUUUUUGEEEE WAAAAAALL"))
-print(trump_detector("listen migrants: IIII KIIIDD YOOOUUU NOOOOOOTTT"))
+w=wrap('1')
+print(w['value'])
