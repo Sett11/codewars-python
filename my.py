bb4ea4219c66672a5897ca6df379dd55877e20d2
Jhonny <gonzodzen@mail.ru>
2023-07-28 18:02:49 +0300

    Ranking position

--- my.py
+++ my.py
@@ -1,7 +1,29 @@
-from itertools import permutations
+def ranking(a):
+    a,i,j=sorted(a,key=lambda e:(e['points'],-ord(e['name'][0].lower()),-ord(e['name'][1].lower()),-ord(e['name'][2].lower())),reverse=True),0,1
+    while i<len(a)-1:
+        a[i]['position']=j
+        if a[i]['points']!=a[i+1]['points']:
+            j=a.index(a[i+1])+1
+        i+=1
+    a[-1]['position']=j
+    return a
 
-def permutation_average(n):
-    a=[int(''.join(list(i))) for i in permutations(str(n))]
-    return round(sum(a)/len(a))
 
-print(permutation_average(737))
+print(ranking([
+      {
+        "name": "John",
+        "points": 100,
+      },
+      {
+        "name": "Bob",
+        "points": 130,
+      },
+      {
+        "name": "Mary",
+        "points": 120,
+      },
+      {
+        "name": "Kate",
+        "points": 120,
+      },
+]))
