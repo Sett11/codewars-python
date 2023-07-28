624713a1497be999a36ab3cf4567299b85d6d694
Jhonny <gonzodzen@mail.ru>
2023-07-28 11:43:43 +0300

    Trumpness detector

--- my.py
+++ my.py
@@ -1,6 +1,8 @@
-from itertools import permutations
+import re
 
-def k_permutations_of_n(a,n):
-    return [list(i) for i in permutations(a,n)]
+def trump_detector(t):
+    s=[i.lower() for i in re.findall(r'u+|e+|a+|i+|o+',t,flags=re.I)]
+    return round((len(''.join(s))-len(s))/len(s),2)
 
-print(k_permutations_of_n([1,2,3],2))
+print(trump_detector("HUUUUUGEEEE WAAAAAALL"))
+print(trump_detector("listen migrants: IIII KIIIDD YOOOUUU NOOOOOOTTT"))
