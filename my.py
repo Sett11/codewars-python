88169c783ce8382a701e011559ad77078c8317b2
Jhonny <gonzodzen@mail.ru>
2023-07-28 16:23:44 +0300

    Sum of (Two) Squares

--- my.py
+++ my.py
@@ -1,5 +1,18 @@
-def wrap(value):
-    return {'value':value}
+import math
 
-w=wrap('1')
-print(w['value'])
+def all_squared_pairs(n):
+    a,c=[],math.ceil(math.sqrt(n))
+    for i in range(c):
+        nx=i**2
+        ny=n-nx
+        y=math.sqrt(ny)
+        if y>=i and y%1==0:
+            a.append([i,y])
+    if not n:
+        a.append([0,0])
+    return a
+
+
+
+print(all_squared_pairs(325))
+print(all_squared_pairs(1048039120))
