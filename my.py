135ad2a89ca99cbeb3c641158b4682afffc7401a
Jhonny <gonzodzen@mail.ru>
2023-07-30 12:39:56 +0300

    The Baby Years I - Baby Counting

--- my.py
+++ my.py
@@ -1,7 +1,12 @@
 import re
 
-def double_check(s):
-    return True if re.search(r'(.)(\1+)',s,flags=re.I) else False
+def baby_count(s):
+    s,o,i=list(re.sub(r'[^bay]','',s.lower())),{'a':0,'b':0,'y':0},0
+    while i<len(s):
+        o[s[i]]+=1
+        i+=1
+    o['b']//=2
+    m=min([i[1] for i in list(o.items())])
+    return m if m>=1 else "Where's the baby?!"
 
-print(double_check('aabc'))
-print(double_check('abcd'))
+print(baby_count('Why the hell are there so many babies?!'))
