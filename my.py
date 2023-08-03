c2429f68e7de0829d07982314709fa8d7d8780ee
Jhonny <gonzodzen@mail.ru>
2023-08-03 19:07:33 +0300

    Alphabet Slices

--- my.py
+++ my.py
@@ -1,9 +1,14 @@
-def invert_hash(d):
-    r={}
-    for i in d:
-        r[d[i]]=i
-    return r
+def solution(s):
+    a,r,i=[ord(i) for i in s]+[0],[],0
+    while i<len(a)-1:
+        if a[i+1]-a[i]!=1:
+            r.append(a[:i+1])
+            a=a[i+1:]
+            i=0
+        else:
+            i+=1
+    return ''.join(sum([[chr(k) for k in j[::-1]] for j in r],[]))
 
-print(invert_hash( { 'a' : 1,
-    'b' : 2,
-    'c' : 3 }))
+print(solution('pqrsxdef'))
+print(solution('zahimzmstaz'))
+print(solution('jjjjjjjjklmnopqrstuv'))
