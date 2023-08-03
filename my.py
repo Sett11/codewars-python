552e676847ccf65f5e339515d0b4bd0fdca90e51
Jhonny <gonzodzen@mail.ru>
2023-08-04 00:26:50 +0300

    Get the addresses of all Google Sheets cells in the range

--- my.py
+++ my.py
@@ -1,16 +1,9 @@
-def smile(t):
-    s,i=list(t+'..'),0
-    while i<len(s)-2:
-        if (s[i]==':' or s[i]==';' or s[i]=='=') and (s[i+1]=='-' or s[i+1]=='~') and (s[i+2]=='(' or s[i+2]=='['):
-            s[i+2]=')' if s[i+2]=='(' else ']'
-        elif (s[i]==':' or s[i]==';' or s[i]=='=') and (s[i+1]=='(' or s[i+1]=='['):
-            s[i+1]=')' if s[i+1]=='(' else ']'
-        i+=1
-    return ''.join(s)[:-2]
+def get_cell_addresses(r):
+    t=[[i[0],i[1:]] for i in r.replace('Range ','').split(':')]
+    a,b,c,d=ord(t[0][0])-65,int(t[0][1]),ord(t[1][0])-65,int(t[1][1])
+    r=sum([[chr(i+65)+str(j) for i in range(a,c+1)] for j in range(b,d+1)],[])
+    return r if len(r)>1 else []
 
-print(smile('Howdy :('))
-print(smile('Never be sad =['))
-print(smile('It\'s been raining all day ;-('))
-print(smile('Change this `=(` and this `:[`'))
-print(smile('The list of positive numbers is [1,2,3,4...]'))
-print(smile('(((-)(((-)))(-)))'))
+print(get_cell_addresses("Range B3:D5"))
+print(get_cell_addresses("Range F12:J17"))
+print(get_cell_addresses("Range H7:H7"))
