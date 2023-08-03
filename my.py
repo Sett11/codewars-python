9d325cdd01eb258f9aaf5ecb6f737de655162876
Jhonny <gonzodzen@mail.ru>
2023-08-04 00:01:06 +0300

    Make everyone happy :)

--- my.py
+++ my.py
@@ -1,8 +1,16 @@
-def grid(n):
-    s,i,a='abcdefghijklmnopqrstuvwxyz',0,[]
-    while i<n:
-        a.append([j for j in range(i,i+n)])
+def smile(t):
+    s,i=list(t+'..'),0
+    while i<len(s)-2:
+        if (s[i]==':' or s[i]==';' or s[i]=='=') and (s[i+1]=='-' or s[i+1]=='~') and (s[i+2]=='(' or s[i+2]=='['):
+            s[i+2]=')' if s[i+2]=='(' else ']'
+        elif (s[i]==':' or s[i]==';' or s[i]=='=') and (s[i+1]=='(' or s[i+1]=='['):
+            s[i+1]=')' if s[i+1]=='(' else ']'
         i+=1
-    return '\n'.join([' '.join([s[i%len(s)] for i in j]) for j in a]) if n>-1 else None
+    return ''.join(s)[:-2]
 
-print(grid(2))
+print(smile('Howdy :('))
+print(smile('Never be sad =['))
+print(smile('It\'s been raining all day ;-('))
+print(smile('Change this `=(` and this `:[`'))
+print(smile('The list of positive numbers is [1,2,3,4...]'))
+print(smile('(((-)(((-)))(-)))'))
