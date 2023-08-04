f3be57bdc5b54e866787cf616b9bbeda5f510a94
Jhonny <gonzodzen@mail.ru>
2023-08-04 09:01:35 +0300

    Last digit of a large number

--- my.py
+++ my.py
@@ -1,7 +1,7 @@
-def last_digit(a):
-    n=1
-    for i in a[::-1]:
+def last_digit(n1,n2):
+    a,n=[n2,n1],1
+    for i in a:
         n=i**(n if n<4 else n%4+4)
     return n%10
 
-print(last_digit([123232, 694022, 140249]))
+print(last_digit(2**200,2**300))
