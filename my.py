2b0bbe528da1fb17e3a6bebe84eb44cf011afe72
Jhonny <gonzodzen@mail.ru>
2023-08-05 00:01:18 +0300

    Back and forth then Reverse!

--- my.py
+++ my.py
@@ -1,12 +1,19 @@
-def is_onion_array(a):
-    j,k=0,len(a)-1
-    while j<round(len(a)/2):
-        if a[j]+a[k]>10 and j!=k:
-            return False
-        j+=1
-        k-=1
-    return True
+def arrange(s):
+    i,j,a,v=0,len(s)-1,[],True
+    while i<=j:
+        if i==j:
+            a.append(s[i])
+            break
+        if v:
+            a.append(s[i])
+            a.append(s[j])
+        else:
+            a.append(s[j])
+            a.append(s[i])
+        v=not v
+        i+=1
+        j-=1
+    return a
 
-print(is_onion_array([-13, -37, -95, -53, -68, 65, 61, 89, 43, 5]))
-print(is_onion_array([58, 14, 6, 59, 3, 42, -8, -54, -7, -11, -55]))
-print(is_onion_array([59, 84, -6, 69, 12, -90, -56]))
+print(arrange([9, 7, -2, 8, 5, -3, 6, 5, 1]))
+print(arrange([2, 4, 3, 4]))
