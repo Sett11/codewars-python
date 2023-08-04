947c7212e8b76fe5905f55339076414285bf91d0
Jhonny <gonzodzen@mail.ru>
2023-08-04 08:57:51 +0300

    Last digit of a huge number

--- my.py
+++ my.py
@@ -1,9 +1,7 @@
-def get_cell_addresses(r):
-    t=[[i[0],i[1:]] for i in r.replace('Range ','').split(':')]
-    a,b,c,d=ord(t[0][0])-65,int(t[0][1]),ord(t[1][0])-65,int(t[1][1])
-    r=sum([[chr(i+65)+str(j) for i in range(a,c+1)] for j in range(b,d+1)],[])
-    return r if len(r)>1 else []
+def last_digit(a):
+    n=1
+    for i in a[::-1]:
+        n=i**(n if n<4 else n%4+4)
+    return n%10
 
-print(get_cell_addresses("Range B3:D5"))
-print(get_cell_addresses("Range F12:J17"))
-print(get_cell_addresses("Range H7:H7"))
+print(last_digit([123232, 694022, 140249]))
