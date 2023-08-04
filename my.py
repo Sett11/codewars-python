2fe6ec6b021c20e508a91d98754ca296d59aebf2
Jhonny <gonzodzen@mail.ru>
2023-08-05 00:01:18 +0300

    Onion array

--- my.py
+++ my.py
@@ -1,12 +1,6 @@
 def is_onion_array(a):
-    j,k=0,len(a)-1
-    while j<round(len(a)/2):
-        if a[j]+a[k]>10 and j!=k:
-            return False
-        j+=1
-        k-=1
-    return True
+    return all([a[i]+a[len(a)-1-i]<=10 for i in range(len(a)//2)])
 
 print(is_onion_array([-13, -37, -95, -53, -68, 65, 61, 89, 43, 5]))
 print(is_onion_array([58, 14, 6, 59, 3, 42, -8, -54, -7, -11, -55]))
-print(is_onion_array([59, 84, -6, 69, 12, -90, -56]))
+print(is_onion_array([1, 1, 15, 10, -1]))
