751388e169f4bb18464e6b39e56f97d3bbd297cd
Jhonny <gonzodzen@mail.ru>
2023-08-06 14:05:24 +0300

    Switch on the Gravity

--- my.py
+++ my.py
@@ -1,21 +1,8 @@
-class Menu:
-    def __init__(self,a):
-        self.a=a
-        self.n=0
+def switch_gravity(l):
+    return [list(k) for k in zip(*['-'*(j[1]-j[0])+'#'*j[0] for j in [[list(i).count('#'),len(i)] for i in zip(*l)]])]
 
-    def to_the_right(self):
-        self.n+=1
-        
-    def to_the_left(self):
-        self.n-=1
-        
-    def display(self):
-        return str([[i] if j%len(self.a)==self.n%len(self.a) else i for j,i in enumerate(self.a)])
-
-m=Menu([1, 2, 3])
-m.to_the_right()
-m.to_the_right()
-m.to_the_right()
-m.to_the_right()
-m.to_the_left()
-print(m.display())
+print(switch_gravity([
+  ["-", "#", "#", "-"],
+  ["-", "-", "#", "-"],
+  ["-", "-", "-", "-"],
+]))
