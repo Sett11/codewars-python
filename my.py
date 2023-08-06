6c6d47bb257ecfdf8d3295678f0d968c680eb1c2
Jhonny <gonzodzen@mail.ru>
2023-08-07 00:28:50 +0300

    A Gift Well Spent

--- my.py
+++ my.py
@@ -1,6 +1,11 @@
-def find_spaceship(s):
-    a=[e for e in [[[i,k] for k,h in enumerate(j) if h=='X'] for i,j in enumerate(s.split('\n')[::-1])] if e]
-    return "Spaceship lost forever." if not a else a[0][0][::-1]
+def buy(x,a):        
+    i=0
+    while i<len(a):
+        t=x-a[i]
+        if t in a:
+            j=a.index(t)
+            if i!=j:
+                return sorted([i,j])
+        i+=1
 
-print(find_spaceship("..........\n..........\n.......X..\n..........\n.........."))
-print(find_spaceship(""))
+print(buy(5,[5,2,3,4,5]))
