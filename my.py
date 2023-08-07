b0273e62860ae5a7f2b94b6474f19a2e61f66f43
Jhonny <gonzodzen@mail.ru>
2023-08-07 23:28:16 +0300

    Wristband Patterns

--- my.py
+++ my.py
@@ -1,43 +1,35 @@
-import re
-def alphabet_war(b):
-    a,i=list(b),0
-    a.append('0')
-    a.insert(0,'0')
-    while i<len(a):
-        j,t,q=i,'',''
-        if a[i]=='[' and i:
-            while j:
-                j-=1
-                if a[j]==']' or a[j]=='0':
-                    t=a[j:i]
-                    break
-            k=i
-            while k<len(a):
-                k+=1
-                if a[k]==']':
-                    break
-            j=k
-            while j<len(a):
-                j+=1
-                if a[j]=='[' or a[j]=='0':
-                    q=a[k+1:j]
-                    break
-            if (t+q).count('#')>=2:
-                a=[h for h in a[:i] if h!='#']+a[k+1:]
-        i+=1
-    i,f=0,False
-    while i<len(a) and '#' in b:
-        if a[i] in ['[',']']:
-            f=not f
-            i+=1
-        if not f and i<len(a):
-            a[i]='0'
-        i+=1
-    return re.sub(r'[0\[\]]','',''.join(a))
+def d(a):
+    m,n,z=len(a),len(a[0]),[]
+    for r in range(m):
+        i,j,v=r,0,[]
+        while j<n and i>=0:
+            v.append(a[i][j])
+            j+=1
+            i-=1
+        z.append(v[::-1])
+    for c in range(1,n):
+        i,j,v=m-1,c,[]
+        while j<n and i>=0:
+            v.append(a[i][j])
+            j+=1
+            i-=1
+        z.append(v[::-1])
+    return z
 
-print(alphabet_war('##abde[fgh]ijk[mn]op'))
-print(alphabet_war('#ab#de[fgh]ijk[mn]op'))
-print(alphabet_war('#abde[fgh]i#jk[mn]op'))
-print(alphabet_war('[a][b][c]'))
-print(alphabet_war('#l[j]#[m]pd#w'))
-print(alphabet_war('#m[knrz]#[qk]nd#p[i]l#w'))
+is_wristband=lambda a:all([all([j==i[0] for j in i]) for i in a]) or all([all([j==i[0] for j in i]) for i in zip(*a)]) or all([all([j==i[0] for j in i]) for i in d(a)]) or all([all([j==i[0] for j in i]) for i in d(a[::-1])])
+
+print(is_wristband([['A', 'A'], 
+        ['B', 'B'], 
+        ['C', 'C']]))
+print(is_wristband([['A', 'B'], 
+        ['A', 'B'], 
+        ['A', 'B']]))
+print(is_wristband([
+    ['A', 'B', 'C'], 
+    ['C', 'A', 'B'], 
+    ['D', 'C', 'A'], 
+    ['E', 'D', 'C']]))
+print(is_wristband([['A', 'B', 'C'], 
+        ['B', 'C', 'A'], 
+        ['C', 'A', 'B'], 
+        ['A', 'B', 'A']]))
