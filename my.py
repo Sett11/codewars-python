97afcd66cbf2f3cde320fe2298c485b0135f452b
Jhonny <gonzodzen@mail.ru>
2023-08-08 00:00:11 +0300

    Likes Vs Dislikes

--- my.py
+++ my.py
@@ -1,35 +1,12 @@
-def d(a):
-    m,n,z=len(a),len(a[0]),[]
-    for r in range(m):
-        i,j,v=r,0,[]
-        while j<n and i>=0:
-            v.append(a[i][j])
-            j+=1
-            i-=1
-        z.append(v[::-1])
-    for c in range(1,n):
-        i,j,v=m-1,c,[]
-        while j<n and i>=0:
-            v.append(a[i][j])
-            j+=1
-            i-=1
-        z.append(v[::-1])
-    return z
+def like_or_dislike(l):
+    a=[]
+    for i in l:
+        a.append(i)
+        if len(a)==2:
+            if a[0]==a[1]:
+                a=[]
+            else:
+                a=a[-1:]
+    return a[0] if a else 'Nothing'
 
-is_wristband=lambda a:all([all([j==i[0] for j in i]) for i in a]) or all([all([j==i[0] for j in i]) for i in zip(*a)]) or all([all([j==i[0] for j in i]) for i in d(a)]) or all([all([j==i[0] for j in i]) for i in d(a[::-1])])
-
-print(is_wristband([['A', 'A'], 
-        ['B', 'B'], 
-        ['C', 'C']]))
-print(is_wristband([['A', 'B'], 
-        ['A', 'B'], 
-        ['A', 'B']]))
-print(is_wristband([
-    ['A', 'B', 'C'], 
-    ['C', 'A', 'B'], 
-    ['D', 'C', 'A'], 
-    ['E', 'D', 'C']]))
-print(is_wristband([['A', 'B', 'C'], 
-        ['B', 'C', 'A'], 
-        ['C', 'A', 'B'], 
-        ['A', 'B', 'A']]))
+print(like_or_dislike(['Like', 'Like', 'Dislike', 'Like', 'Like', 'Like', 'Like', 'Dislike']))
