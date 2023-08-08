733e08d91fc3b908fd403e2fbcf110249551306d
Jhonny <gonzodzen@mail.ru>
2023-08-08 23:27:50 +0300

    Vowel-consonant lexicon

--- my.py
+++ my.py
@@ -1,5 +1,18 @@
-from itertools import permutations as p
-def solve(a):
-    return len([q for q in [[int(h) for h in k.split('&')] for k in set(['&'.join([str(j) for j in sorted(i)]) for i in p(a,3)])] if q[1]-q[0]==q[2]-q[1]])
+from re import sub
+def solve(s):
+    a,b,r=sorted(sub(r'[^aioue]','',s)),sorted(sub(r'[aioue]','',s)),[]
+    if abs(len(a)-len(b))>1:
+        return 'failed'
+    if len(b)>len(a):
+        a,b=b,a
+    while a or b:
+        r.append(a.pop(0))
+        if b:
+            r.append(b.pop(0))
+    return ''.join(r)
 
-print(solve([0,1,2,3,5,6,7,11,13,15,17,19]))
+print(solve('java'))
+print(solve('have'))
+print(solve('oruder'))
+print(solve('apple'))
+print(solve('wrhkrvfymtcddcwoiuoiiiioiuuiaoa'))
