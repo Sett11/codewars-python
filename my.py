06df1313bc23906460d21ad6087097b542bdd98c
Jhonny <gonzodzen@mail.ru>
2023-08-09 21:18:03 +0300

    Asterisk it

--- my.py
+++ my.py
@@ -1,8 +1,9 @@
-def string_hash(s):
-    q=[ord(i) for i in s]
-    a=sum(q)
-    b=sum([q[i+1]-q[i] for i in range(len(q)-1)])
-    c=(a|b)&((~a)<<2)
-    return c^(32*(s.count(' ')+1))
+def asterisc_it(n): 
+    i,a,s=0,list(str(n)) if type(n)!=list else list(''.join([str(i) for i in n])),'02468'
+    while i<len(a)-1:
+        if a[i] in s and a[i+1] in s:
+            a=a[:i+1]+['*']+a[i+1:]
+        i+=1
+    return ''.join(a)
 
-print(string_hash(" Yo - What's Good?! "))
+print(asterisc_it([1, 4, 64, 68, 67, 23, 1]))
