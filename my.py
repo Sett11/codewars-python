156e01a63bdb95def2112bdd31f12404643061e4
Jhonny <gonzodzen@mail.ru>
2023-08-10 00:25:54 +0300

    Begin your day with a challenge, but an easy one.

--- my.py
+++ my.py
@@ -1,11 +1,7 @@
-def skiponacci(n):
-    a=b,c,r,v=0,1,[],True
-    while len(r)<n:
-        a=b+c
-        c=b
-        b=a
-        r.append(str(a) if v else 'skip')
-        v=not v
-    return ' '.join(r)
-
-print(skiponacci(21))
+def one_two_three(n):
+    d=divmod(n,9)
+    return [0,0] if n==0 else [int(('9'*d[0])+(str(d[1]) if d[1] else '')),int('1'*n)]
+    
+print(one_two_three(0))
+print(one_two_three(204))
+print(one_two_three(189))
