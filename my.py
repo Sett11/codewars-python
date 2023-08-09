d97ef831a2f8365dc238f40fb03324f5fd01de10
Jhonny <gonzodzen@mail.ru>
2023-08-09 21:01:38 +0300

    BAD Hash - String to Int

--- my.py
+++ my.py
@@ -1,6 +1,8 @@
-def unique_date(x,y,z):
-    a=sorted([x,y,z])
-    s,t,l=['0'+str(i) if i<10 else str(i) for i in a],[i for i in a if i<=12],set(a)
-    return 'ambiguous' if (len(t)>1 and len(l)==3) or (len([i for i in a if i<=31])==3 and 0 not in a and len(l)==3) or (len(l)==2 and len([i for i in a if i<=31])==3 and 0 not in a and len([i for i in a if i<=12])>=2) else f'{s[-1]}/{s[0]}/{s[1]}' if (len([i for i in a if i<=12]) and len([i for i in a if i<=31])) else 'invalid'
+def string_hash(s):
+    q=[ord(i) for i in s]
+    a=sum(q)
+    b=sum([q[i+1]-q[i] for i in range(len(q)-1)])
+    c=(a|b)&((~a)<<2)
+    return c^(32*(s.count(' ')+1))
 
-print(unique_date(2, 2, 12))
+print(string_hash(" Yo - What's Good?! "))
