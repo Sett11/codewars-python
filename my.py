617b606bd83670de3ab005eabf196717a24bae22
Jhonny <gonzodzen@mail.ru>
2023-08-09 08:29:50 +0300

    Array Appender

--- my.py
+++ my.py
@@ -1,12 +1,2 @@
-def round_robin(a,t,n):
-    i,s=0,0
-    while 1:
-        s+=t if t<=a[i] else a[i]
-        a[i]-=t if t<a[i] else a[i]
-        if a[i]==0 and i==n:
-            return s
-        i+=1
-        if i>=len(a):
-            i=0
-
-print(round_robin([19, 17, 18, 5, 2, 1], 12, 2))
+def append_arrays(s1, s2): 
+    return s1+s2
