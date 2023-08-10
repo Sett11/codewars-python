dfbd7786a9853568f1d83e62e4976a14f8db5f13
Jhonny <gonzodzen@mail.ru>
2023-08-10 07:47:38 +0300

    Who Took The Car Key?

--- my.py
+++ my.py
@@ -1,7 +1,4 @@
-def one_two_three(n):
-    d=divmod(n,9)
-    return [0,0] if n==0 else [int(('9'*d[0])+(str(d[1]) if d[1] else '')),int('1'*n)]
-    
-print(one_two_three(0))
-print(one_two_three(204))
-print(one_two_three(189))
+who_took_the_car_key=lambda a:''.join([chr(int(i,2)) for i in a])
+
+print(who_took_the_car_key(['01000001', '01101100', '01100101', '01111000', '01100001', '01101110',
+             '01100100', '01100101', '01110010']))
