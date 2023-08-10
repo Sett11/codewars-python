73ceab766eb81f33804d48da359b86712c9a5c49
Jhonny <gonzodzen@mail.ru>
2023-08-10 08:05:00 +0300

    Hexadecimal Keys

--- my.py
+++ my.py
@@ -1,4 +1,15 @@
-who_took_the_car_key=lambda a:''.join([chr(int(i,2)) for i in a])
+def f(n):
+    i,a=2,[]
+    while i<=n:
+        while n%i==0:
+            n//=i
+            a.append(i)
+        i+=1
+    return a
 
-print(who_took_the_car_key(['01000001', '01101100', '01100101', '01111000', '01100001', '01101110',
-             '01100100', '01100101', '01110010']))
+def find_key(k):
+    a=f(int(k,16))
+    return (a[0]-1)*(a[1]-1)
+
+print(find_key("47b"))
+print(find_key("2533"))
