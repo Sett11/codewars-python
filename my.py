576c3ae8bcb82d18302062504fe1310f0f5df05a
Jhonny <gonzodzen@mail.ru>
2023-08-10 19:39:49 +0300

    All Star Code Challenge #19

--- my.py
+++ my.py
@@ -1,15 +1,5 @@
-def f(n):
-    i,a=2,[]
-    while i<=n:
-        while n%i==0:
-            n//=i
-            a.append(i)
-        i+=1
-    return a
+from itertools import permutations as p
+def slogan_maker(a):
+    return [' '.join(i) for i in p([ k[1] for k in sorted([[a.index(j),j] for j in set(a)])])]
 
-def find_key(k):
-    a=f(int(k,16))
-    return (a[0]-1)*(a[1]-1)
-
-print(find_key("47b"))
-print(find_key("2533"))
+print(slogan_maker(["super", "guacamole", "super", "super", "hot", "guacamole"]))
