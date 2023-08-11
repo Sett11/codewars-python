3bdf88ce8c9576c542b78302748410d4bdfc67a7
Jhonny <gonzodzen@mail.ru>
2023-08-11 12:40:52 +0300

    Alternate Logic

--- my.py
+++ my.py
@@ -1,13 +1,9 @@
-def cinema(b,g):
-    if b==g:
-        return 'BG'*b
-    if b-g==1:
-        return 'B'+'GB'*g
-    if g-b==1:
-        return 'G'+'BG'*b
-    if b-g==2 and g>1:
-        return ('BG'+'BB'+'GB'*((b-4+g-2) or 1))[:b+g]
-    if g-b==2 and b>1:
-        return ('GB'+'GG'+'BG'*((g-4+b-2)or 1))[:b+g]
+def alt_or(l):
+    for i in l:
+        if i:
+            return i
+    return False if l else None
 
-print(cinema(12,14))
+print(alt_or([False, False, False, False, True, True]))
+print(alt_or([False, False, False, False]))
+print(alt_or([]))
