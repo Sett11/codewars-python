bc9f760d30479e5048198f29166e2a33f7fa1042
Jhonny <gonzodzen@mail.ru>
2023-08-14 00:00:11 +0300

    Do you know how to make Query String?

--- my.py
+++ my.py
@@ -1,4 +1,14 @@
-def king_is_in_check(a):
-    return True
+def to_query_string(o):
+    a=[]
+    for i in o:
+        if type(o[i])!=list:
+            a.append(f'{i}={o[i]}')
+        else:
+            j=0
+            while j<len(o[i]):
+                a.append(f'{i}={o[i][j]}')
+                j+=1
+    return '&'.join(a)
 
-print(king_is_in_check())
+print(to_query_string({"bar": [2, 4], "foo": [1, 3]}))
+print(to_query_string({ "bar": 2, "foo": 1 }))
