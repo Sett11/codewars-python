88b0da4fd4eba093e23a212a563cd3c46548d34f
Jhonny <gonzodzen@mail.ru>
2023-08-13 18:12:12 +0300

    Simple Fun #81: Digits Product

--- my.py
+++ my.py
@@ -1,31 +1,11 @@
-def f(a,x,y):
-    r,i,j=list([i for i in zip(*a)][y])+a[x],x-1,y-1
-    while i>-1 and j>-1:
-        r.append(a[i][j])
-        i-=1
-        j-=1
-    i,j=x+1,y+1
-    while i<len(a) and j<len(a[0]):
-        r.append(a[i][j])
-        i+=1
-        j+=1
-    i,j=x+1,y-1
-    while i<len(a) and j>-1:
-        r.append(a[i][j])
-        i+=1
-        j-=1
-    i,j=x-1,y+1
-    while i>-1 and j<len(a[0]):
-        r.append(a[i][j])
-        i-=1
-        j+=1
-    return len(r)-2
+def digits_product(n):
+    if n<10:
+        return n+10
+    s=''
+    for i in range(9,1,-1):
+        while not n%i:
+            s+=str(i)
+            n//=i
+    return int(s[::-1]) if n==1 else -1
 
-def chessboard_squares_under_queen_attack(a,b):
-    r,s=[list('0'*b) for i in range(a)],0
-    for i in range(len(r)):
-        for j in range(len(r[i])):
-            s+=f(r,i,j)
-    return s
-    
-print(chessboard_squares_under_queen_attack(20,20))
+print(digits_product(450))
