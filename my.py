f65bbe234f85f87453ae15860c514d044517d075
Jhonny <gonzodzen@mail.ru>
2023-08-13 22:58:50 +0300

    Averages of numbers

--- my.py
+++ my.py
@@ -1,30 +1,4 @@
-def lazy(n):
-    def d(f):
-       def c(*a,**b):
-          if n>0:
-             if d.c==n:
-                s=f(*a,**b)
-             else:
-                s=None
-             d.c-=1
-          if n<0:
-             if d.c+1==0:
-                s=None
-             else:
-                s=f(*a,**b)
-             d.c+=1
-          if d.c==0:
-             d.c=n
-          return s
-       d.c=n
-       return c
-    return d
+def averages(a):
+    return [] if not a else [(a[i]+a[i+1])/2 for i in range(len(a)-1)]
 
-@lazy(4)
-def half(x):
-  return x/2
-
-print(half(10))
-print(half(10))
-print(half(10))
-print(half(10))
+print(averages([ 1, 3, 5, 1, -10]))
