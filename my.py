4bab9d9e55d00c555ed100bd35fa04c5ddfba49e
Jhonny <gonzodzen@mail.ru>
2023-08-13 18:46:17 +0300

    Domino Reaction

--- my.py
+++ my.py
@@ -1,11 +1,9 @@
-from functools import reduce as r
-def maximum_product(a):
-    i,q=0,[]
-    while i<len(a):
-        t=a.pop(i)
-        q.append([r(lambda w,c:w*c,a,1),t])
-        a.insert(i,t)
+def domino_reaction(s):
+    a,i=list(s),0
+    while i<len(a) and a[i]=='|':
+        a[i]='/'
         i+=1
-    return sorted(q,key=lambda e:(e[0],-e[1]),reverse=True)[0][1]
+    return ''.join(a)
 
-print(maximum_product([-1, 2, -3]))
+print(domino_reaction("||| ||||//| |/"))
+print(domino_reaction("|||||||"))
