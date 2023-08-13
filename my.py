c3aabc267d48f8f2e1664b7f1821ad885087dc38
Jhonny <gonzodzen@mail.ru>
2023-08-13 13:39:34 +0300

    Complete The Pattern #7 - Cyclical Permutation

--- my.py
+++ my.py
@@ -1,11 +1,7 @@
-def best_parking_spot(a):
-    r,m=[],float('inf')
-    for i in [i for i,j in enumerate(a) if j=='OPEN']:
-        for j in [i for i,j in enumerate(a) if j=='CORRAL']:
-            t=abs(i-j)
-            m=min(t+i,m)
-            r.append([i,t,i+t])
-    return sorted([i for i in r if i[2]==m],key=lambda e:(-e[0],e[1],e[2]))[0][0]
+def pattern(n):
+    a,r=[i for i in range(1,n+1)],[]
+    for i in range(n):
+        r.append(''.join([str(j) for j in a[i:]+a[:i]]))
+    return '\n'.join(r)
 
-print(best_parking_spot(["STORE", "TAKEN", "TAKEN", "CORRAL", "TAKEN", "OPEN", "OPEN", "TAKEN", "CORRAL"]))
-print(best_parking_spot(["STORE","OPEN","TAKEN","OPEN","CORRAL"]))
+print(pattern(9))
