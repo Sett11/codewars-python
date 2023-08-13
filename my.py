bc867a203f059f93ec4dab3dd77fbda91f5d8164
Jhonny <gonzodzen@mail.ru>
2023-08-13 14:04:26 +0300

    Chessboard Squares Under Queen's Attack

--- my.py
+++ my.py
@@ -1,7 +1,31 @@
-def pattern(n):
-    a,r=[i for i in range(1,n+1)],[]
-    for i in range(n):
-        r.append(''.join([str(j) for j in a[i:]+a[:i]]))
-    return '\n'.join(r)
+def f(a,x,y):
+    r,i,j=list([i for i in zip(*a)][y])+a[x],x-1,y-1
+    while i>-1 and j>-1:
+        r.append(a[i][j])
+        i-=1
+        j-=1
+    i,j=x+1,y+1
+    while i<len(a) and j<len(a[0]):
+        r.append(a[i][j])
+        i+=1
+        j+=1
+    i,j=x+1,y-1
+    while i<len(a) and j>-1:
+        r.append(a[i][j])
+        i+=1
+        j-=1
+    i,j=x-1,y+1
+    while i>-1 and j<len(a[0]):
+        r.append(a[i][j])
+        i-=1
+        j+=1
+    return len(r)-2
 
-print(pattern(9))
+def chessboard_squares_under_queen_attack(a,b):
+    r,s=[list('0'*b) for i in range(a)],0
+    for i in range(len(r)):
+        for j in range(len(r[i])):
+            s+=f(r,i,j)
+    return s
+    
+print(chessboard_squares_under_queen_attack(20,20))
