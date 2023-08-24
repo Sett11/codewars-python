706f37cb605b276b8f33be798324cbf04401d94b
Jhonny <gonzodzen@mail.ru>
2023-08-24 21:08:23 +0300

    Valid HK Phone Number

--- my.py
+++ my.py
@@ -1,14 +1,25 @@
-def to_query_string(o):
-    a=[]
-    for i in o:
-        if type(o[i])!=list:
-            a.append(f'{i}={o[i]}')
-        else:
-            j=0
-            while j<len(o[i]):
-                a.append(f'{i}={o[i][j]}')
-                j+=1
-    return '&'.join(a)
+import re
 
-print(to_query_string({"bar": [2, 4], "foo": [1, 3]}))
-print(to_query_string({ "bar": 2, "foo": 1 }))
+
+def is_valid_HK_phone_number(s):
+    s = s.split(' ')
+    return True if s[0].isdigit() and len(s[0]) == 4 and s[-1].isdigit() and len(s[-1]) == 4 and len(s) == 2 else False
+
+
+def has_valid_HK_phone_number(s):
+    s = re.sub(r'[^\d ]', ' ', s).split(' ')
+    for i in range(len(s)):
+        if i == len(s)-1:
+            return False
+        if s[i].isdigit() and len(s[i][:4]) == 4 and s[i+1].isdigit() and len(s[i+1][:4]) == 4:
+            return True
+    return False
+
+
+print(is_valid_HK_phone_number("1234 5678"))
+print(is_valid_HK_phone_number("1234  5678"))
+print(is_valid_HK_phone_number("123f 5678"))
+print(is_valid_HK_phone_number("123 5678"))
+print(has_valid_HK_phone_number("Hello, my phone number is 1234 5678"))
+print(has_valid_HK_phone_number("ceanjgz1z3114 825d9j/6yh"))
+print(has_valid_HK_phone_number("skldfjs287389274329dklfj84239029043820942904823480924293042904820482409209438dslfdjs9345 8234sdklfjsdkfjskl28394723987jsfss2343242kldjf23423423SDLKFJSLKsdklf"))
