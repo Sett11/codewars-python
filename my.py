38129927e49ae259a86bec8b6ae9058806621a60
Jhonny <gonzodzen@mail.ru>
2023-08-25 13:04:51 +0300

    Fizz Buzz Cuckoo Clock

--- my.py
+++ my.py
@@ -1,5 +1,9 @@
-time_per_day=lambda l:round(round(sum([.75*i[0]*i[1] for i in l])/60/5*1000,1)/1000,2)
+def fizz_buzz_cuckoo_clock(t):
+    a=[int(i) for i in t.split(':')]
+    a[0]=a[0] if a[0] else 12
+    return ('Cuckoo '*(a[0]%(12 if a[0]!=12 else 13))).rstrip() if not a[1] else 'Cuckoo' if a[1]==30 else 'Fizz Buzz' if not a[1]%5 and not a[1]%3 else 'Fizz' if not a[1]%3 else 'Buzz' if not a[1]%5 else 'tick'
 
-print(time_per_day([(1, 20), (2, 10), (3, 15), (4, 10)]))
-print(time_per_day([(4, 11), (9, 42)]))
-print(time_per_day([(9, 38)]))
+print(fizz_buzz_cuckoo_clock("11:15" ))
+print(fizz_buzz_cuckoo_clock("00:00" ))
+print(fizz_buzz_cuckoo_clock("12:00" ))
+print(fizz_buzz_cuckoo_clock("08:55" ))
