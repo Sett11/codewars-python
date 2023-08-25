e672b75e202203398f8f969d8f4cbdd1b12f78ba
Jhonny <gonzodzen@mail.ru>
2023-08-25 09:55:20 +0300

    Fizz / Buzz

--- my.py
+++ my.py
@@ -1,13 +1,5 @@
-def solve(n):
-    a=[500,200,100,50,20,10]
-    i=c=0
-    while n:
-        if n<10:
-            return -1
-        while n>=a[i]:
-            n-=a[i]
-            c+=1
-        i+=1
-    return c
+def solution(n):
+    a=[i for i in range(1,n)]
+    return [len(j) for j in [[i for i in a if not i%3 and i%5],[i for i in a if not i%5 and i%3],[i for i in a if not i%5 and not i%3]]]
 
-print(solve(1500))
+print(solution(20))
