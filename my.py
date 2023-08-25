2d36a93907dc1b7e1ff8b555ade0aac0a8ac3b75
Jhonny <gonzodzen@mail.ru>
2023-08-25 10:42:55 +0300

    Multiples and Digit Sums

--- my.py
+++ my.py
@@ -1,5 +1,5 @@
-def solution(n):
-    a=[i for i in range(1,n)]
-    return [len(j) for j in [[i for i in a if not i%3 and i%5],[i for i in a if not i%5 and i%3],[i for i in a if not i%5 and not i%3]]]
+def procedure(n):
+    return sum([sum([int(j) for j in list(str(i))]) for i in range(1,101) if not i%n])
 
-print(solution(20))
+print(procedure(30))
+print(procedure(25))
