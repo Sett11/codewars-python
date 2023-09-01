69169c861b545f155a4e5471fff2e9a18610c868
Jhonny <gonzodzen@mail.ru>
2023-09-01 16:24:14 +0300

    Search a 2D Matrix

--- my.py
+++ my.py
@@ -1,30 +1,7 @@
-import time
-
 class Solution:
-    def exist(self,m,s):
-        q,n=len(m),len(m[0])
-        def dfs(i,j,k):
-            if k==len(s):
-                return True
-            if i<0 or i>=q or j<0 or j>=n or m[i][j]!=s[k]:
-                return False
-            t,m[i][j]=m[i][j],'&'
-            r=dfs(i+1,j,k+1) or dfs(i-1,j,k+1) or dfs(i,j+1,k+1) or dfs(i,j-1,k+1)
-            m[i][j]=t
-            return r
+    def searchMatrix(self,m,i):
+        return i in sum(m,[])
 
-        for i in range(q):
-            for j in range(n):
-                if m[i][j]==s[0] and dfs(i,j,0):
-                    return True
-        return False
-    
 s=Solution()
 
-start=time.time()
-print(s.exist([["A","B","C","E"],["S","F","C","S"],["A","D", "E","E"]],'ABCCED'))
-print(s.exist([["A","B","C","E"],["S","F","C","S"],["A","D", "E","E"]],'SEE'))
-print(s.exist([['A','A']],'AAA'))
-
-end=time.time()-start
-print(end)
+print(s.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]],3))
