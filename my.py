09c3c8eee31087f8f0689111a6f02a4f6746d08a
Jhonny <gonzodzen@mail.ru>
2023-09-01 11:59:49 +0300

    Rotate Image

--- my.py
+++ my.py
@@ -1,16 +1,11 @@
-from math import ceil
+class Solution:
+    def rotate(self,m):
+        a=[list(i[::-1]) for i in zip(*m)]
+        m.clear()
+        for i in a:
+            m.append(i)
+        return m
+    
+s=Solution()
 
-def cooking_time(n,m,s,p):
-    f=float(str(m*60+s))
-    a=int(n[:-1])
-    b=int(p[:-1])
-    c,d=divmod(f/(b/a),60)
-    q,w=c,ceil(d)
-    if w==60:
-        q+=1
-        w=0
-    return f'{int(q)} minutes {w} seconds'
-
-print(cooking_time("450W", 3, 25, "950W"))
-print(cooking_time("600W", 4, 20, "800W"))
-print(cooking_time("21W", 64, 88, "25W"))
+print(s.rotate([[1,2,3],[4,5,6],[7,8,9]]))
