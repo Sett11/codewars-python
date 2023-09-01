77ad9d711c931b01015f782e64715ebd91b371b5
Jhonny <gonzodzen@mail.ru>
2023-09-01 09:50:57 +0300

    From-To-Step Sequence Generator

--- my.py
+++ my.py
@@ -1,17 +1,6 @@
-def rev_sub(a):
-    i=0
-    a.append('&')
-    while i<len(a)-1:
-        j=i
-        if a[i]%2==0:
-            while j<len(a):
-                if a[j]=='&' or a[j]%2:
-                    a=a[:i]+a[i:j][::-1]+a[j:]
-                    i=j
-                    break
-                j+=1
-        i+=1
-    return a[:-1]
+def generator (f,t,s):
+    if not s:
+        return []
+    return [i for i in range(f,t+1 if f<t else t-1,s if f<t else -s)]
 
-print(rev_sub([2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
-print(rev_sub([2, 4, 3, 10, 2]))
+print(generator(20,10,1))
