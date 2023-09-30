7b503fd9288d7120297d9803fbb946f0f656bb47
Jhonny <gonzodzen@mail.ru>
2023-09-30 23:34:57 +0300

    Emptying the Values

--- my.py
+++ my.py
@@ -1,4 +1,23 @@
-def neutralise(s1,s2):
-    return ''.join(['-' if s1[i]=='-' and s2[i]=='-' else '+' if s1[i]=='+' and s2[i]=='+' else '0' for i in range(len(s1))])
+def empty_values(a):
+    for i in range(len(a)):
+        if type(a[i])==int:
+            a[i]=0
+        elif type(a[i])==float:
+            a[i]=0.0
+        elif type(a[i])==str:
+            a[i]=''
+        elif type(a[i])==list:
+            a[i]=[]
+        elif type(a[i])==dict:
+            a[i]={}
+        elif type(a[i])==tuple:
+            a[i]=()
+        elif type(a[i])==set:
+            a[i]=set()
+        elif type(a[i])==bool:
+            a[i]=False
+        elif type(a[i])=='NoneType':
+            a[i]=None
+    return a
 
-print(neutralise("-++-","-+-+"))
+print(empty_values([7, 3.14, "cat",[1,2],{1:2},(1,2,3),set([1,2,3]),None,True]))
