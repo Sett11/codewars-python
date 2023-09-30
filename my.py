bdd206e235a9edd17961958eae5f323544599171
Jhonny <gonzodzen@mail.ru>
2023-10-01 01:05:07 +0300

    Closing in Sum

--- my.py
+++ my.py
@@ -1,4 +1,9 @@
-def unusual_lex_order(a):
-    return sorted(a,key=lambda e:e[::-1])
+def closing_in_sum(n):
+    s=str(n)
+    c=0 if not len(s)&1 else int(s[len(s)//2])
+    for i in range(len(s)//2):
+        c+=int(s[i]+s[-i-1])
+    return c
 
-print(unusual_lex_order(["nigeb", "ta", "eht", "gninnigeb"]))
+print(closing_in_sum(1039))
+print(closing_in_sum(121))
