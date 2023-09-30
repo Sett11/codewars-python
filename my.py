411c495ad51b5b0847e5cd919b9995c6de1d76b6
Jhonny <gonzodzen@mail.ru>
2023-10-01 00:37:59 +0300

    Split By Value

--- my.py
+++ my.py
@@ -1,23 +1,5 @@
-def empty_values(a):
-    for i in range(len(a)):
-        if type(a[i])==int:
-            a[i]=0
-        elif type(a[i])==float:
-            a[i]=0.0
-        elif type(a[i])==str:
-            a[i]=''
-        elif type(a[i])==list:
-            a[i]=[]
-        elif type(a[i])==dict:
-            a[i]={}
-        elif type(a[i])==tuple:
-            a[i]=()
-        elif type(a[i])==set:
-            a[i]=set()
-        elif type(a[i])==bool:
-            a[i]=False
-        elif type(a[i])=='NoneType':
-            a[i]=None
-    return a
+def split_by_value(k,a):
+    return [i for i in a if i<k]+[i for i in a if i>=k]
 
-print(empty_values([7, 3.14, "cat",[1,2],{1:2},(1,2,3),set([1,2,3]),None,True]))
+print(split_by_value(5, [1, 3, 5, 7, 6, 4, 2]))
+print(split_by_value(6, [6, 4, 10, 10, 6]))
