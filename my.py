02d340f320dc8924bb2c50639a817b70a3844b3d
Jhonny <gonzodzen@mail.ru>
2023-10-03 01:08:19 +0300

    A Letter's Best Friend

--- my.py
+++ my.py
@@ -1,9 +1,8 @@
-def closing_in_sum(n):
-    s=str(n)
-    c=0 if not len(s)&1 else int(s[len(s)//2])
-    for i in range(len(s)//2):
-        c+=int(s[i]+s[-i-1])
-    return c
+def best_friend(txt,a,b):
+    a=[i for i,j in enumerate(txt) if j==a]
+    txt+='&'
+    return all(txt[i+1]==b for i in a)
 
-print(closing_in_sum(1039))
-print(closing_in_sum(121))
+
+print(best_friend("he headed to the store", "h", "e"))
+print(best_friend('abcdee', 'e', 'e'))
