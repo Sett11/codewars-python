884ba94f8f27b7c744aca73fa8ef232ba79dda01
Jhonny <gonzodzen@mail.ru>
2023-10-17 00:01:01 +0300

    New season, new league

--- my.py
+++ my.py
@@ -1,11 +1,6 @@
-def flick_switch(lst):
-    r=[]
-    v=True
-    for i in lst:
-        if i=='flick':
-            v=not v
-        r.append(v)
-    return r
+def premier_league_standings(o):
+    one=o[1]
+    del o[1]
+    return dict(zip([i for i in range(1,len(o)+2)],([one]+sorted(o.values()))))
 
-print(flick_switch(["codewars", "flick", "code", "wars"]))
-print(flick_switch(['bicycle', 'jarmony', 'flick', 'sheep', 'flick']))
+print(premier_league_standings({1: 'Leeds United', 2: 'Liverpool', 3: 'Manchester City', 4: 'Coventry', 5: 'Arsenal'}))
