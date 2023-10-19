2bacf8064bca0f68ba6feb58c3f3c7462560b4a1
Jhonny <gonzodzen@mail.ru>
2023-10-19 20:13:48 +0300

    Prison Break

--- my.py
+++ my.py
@@ -1,18 +1,12 @@
-from re import sub
-
-def spin_solve(s):
-    a=s.split(' ')
+def freed_prisoners(a):
+    c=0
+    if not a[0]:
+        return c
     for i in range(len(a)):
-        t=len(sub(r'[^A-z]','',a[i]))
-        if t==1:
-            a[i]='0'
-        elif t==2 or (',' in a[i] and t<7):
-            a[i]=a[i].upper()
-        elif t>6 or a[i].lower().count('t')==2:
-            if not a[i][-1].isalpha():
-                a[i]=a[i][:-1][::-1]+a[i][-1]
-            else:
-                a[i]=a[i][::-1]
-    return ' '.join(a)
+        if a[i]:
+            c+=1
+            a=[not j for j in a]
+    return c
+    
 
-print(spin_solve("If a man does not keep pace with his companions, perhaps it is because he hears a different drummer."))
+print(freed_prisoners([True, True, False, False, False, True, False]))
