28558b718e5a96cd1944c9e20da85db54243d97c
Jhonny <gonzodzen@mail.ru>
2023-10-19 21:43:51 +0300

    Count salutes

--- my.py
+++ my.py
@@ -1,7 +1,16 @@
-def party_people(a):
-    while not all(i<=len(a) for i in a):
-        a=[i for i in a if i<=len(a)]
-    return len(a)
+def count_salutes(s):
+    a,b=[],[]
+    for i in range(len(s)):
+        if s[i]=='<':
+            a.append(i)
+        elif s[i]=='>':
+            b.append(i)
+    c=0
+    for i in a:
+        for j in b:
+            if i>j:
+                c+=1
+    return c*2
 
-print(party_people([4, 5, 4, 1]))
-print(party_people([2, 1, 2, 0]))
+print(count_salutes('>--->---<--<'))
+print(count_salutes('>-<->-<'))
