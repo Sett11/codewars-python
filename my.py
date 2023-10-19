46d92e1defaa130f8aee7492dc66182e199aa561
Jhonny <gonzodzen@mail.ru>
2023-10-19 21:54:00 +0300

    Doubleton number

--- my.py
+++ my.py
@@ -1,16 +1,7 @@
-def count_salutes(s):
-    a,b=[],[]
-    for i in range(len(s)):
-        if s[i]=='<':
-            a.append(i)
-        elif s[i]=='>':
-            b.append(i)
-    c=0
-    for i in a:
-        for j in b:
-            if i>j:
-                c+=1
-    return c*2
+def doubleton(n):
+    n+=1
+    while len(set(list(str(n))))!=2:
+        n+=1
+    return n
 
-print(count_salutes('>--->---<--<'))
-print(count_salutes('>-<->-<'))
+print(doubleton(10001))
