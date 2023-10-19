41a17f92a75b8cce8fee2a80ef854cdf4327a949
Jhonny <gonzodzen@mail.ru>
2023-10-20 00:00:40 +0300

    Simple array product

--- my.py
+++ my.py
@@ -1,13 +1,7 @@
-def burner(c,h,o):
-    h2o=min(h//2,o)
-    h-=h2o*2
-    o-=h2o
-    co2=min(c,o//2)
-    c-=co2
-    o-=co2*2
-    ch4=min(c,h//4)
-    return h2o,co2,ch4
+from itertools import product
+from functools import reduce
 
-print(burner(45,11,100))
-print(burner(354,1023230,0))
-print(burner(939,3,694))
+def solve(a):
+    return max([reduce(lambda e,c:e*c,i) for i in product(*a)])
+
+print(solve([[1,-1],[2,3],[10,-100]]))
