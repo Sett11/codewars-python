e0d15e62572ccd535b68423ac12e5a5aaad2b206
Jhonny <gonzodzen@mail.ru>
2023-10-19 18:39:56 +0300

    Spin Around, Touch the Ground

--- my.py
+++ my.py
@@ -1,6 +1,5 @@
-def premier_league_standings(o):
-    one=o[1]
-    del o[1]
-    return dict(zip([i for i in range(1,len(o)+2)],([one]+sorted(o.values()))))
+def spin_around(lst):
+    return int(abs(sum([.25 if i == 'right' else -.25 for i in lst])))
 
-print(premier_league_standings({1: 'Leeds United', 2: 'Liverpool', 3: 'Manchester City', 4: 'Coventry', 5: 'Arsenal'}))
+print(spin_around(["right", "right", "right", "right", "left", "right"]))
+print(spin_around(["left", "left", "left", "left"]))
