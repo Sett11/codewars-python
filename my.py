25375c59449656283ba9e046b4004dcae7840b71
Jhonny <gonzodzen@mail.ru>
2023-10-19 19:10:32 +0300

    Esthetic Numbers

--- my.py
+++ my.py
@@ -1,4 +1,20 @@
-def gimme_the_letters(s):
-    return ''.join([chr(i) for i in range(ord(s[0]),ord(s[-1])+1)])
+def convert(n,b):
+    s='0123456789abcdefghijklmnopqrstuvwxyz'
+    r=''
+    while n:
+        r=s[n%b]+r
+        n//=b
+    return r
 
-print(gimme_the_letters('A-Z'))
+def check(n):
+    a=[int(i) for i in str(n)]
+    return all(abs(a[i]-a[i+1])==1 for i in range(len(a)-1))
+
+def esthetic(n):
+    r=[]
+    for i in range(2,11):
+        if check(convert(n,i)):
+            r.append(i)
+    return r
+
+print(esthetic(10))
