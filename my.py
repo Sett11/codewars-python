aaad0394ce3a3bea45f0569374cae309cbbe8e46
Jhonny <gonzodzen@mail.ru>
2023-10-19 22:33:53 +0300

    Fusion Chamber Shutdown

--- my.py
+++ my.py
@@ -1,18 +1,13 @@
-def solution(m):
-    try:return next(bool(i) for i in m if '>' in i and 'x' in i and i.index('>')<i.index('x'))
-    except:return False
+def burner(c,h,o):
+    h2o=min(h//2,o)
+    h-=h2o*2
+    o-=h2o
+    co2=min(c,o//2)
+    c-=co2
+    o-=co2*2
+    ch4=min(c,h//4)
+    return h2o,co2,ch4
 
-print(solution([
-  [' ', ' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' ', ' '],
-  [' ', ' ', '>', ' ', 'x'],
-  [' ', ' ', '', ' ', ' ']
-]))
-print(solution([
-  [' ', ' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' ', ' '],
-  [' ', ' ', '>', ' ', ' '],
-  [' ', ' ', ' ', ' ', 'x'],
-  [' ', ' ', '', ' ', ' ']
-]))
+print(burner(45,11,100))
+print(burner(354,1023230,0))
+print(burner(939,3,694))
