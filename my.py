e276e2695a86ef02562c66807433863c7c0e305a
Jhonny <gonzodzen@mail.ru>
2023-10-19 22:13:45 +0300

    Game Hit the target

--- my.py
+++ my.py
@@ -1,7 +1,18 @@
-def doubleton(n):
-    n+=1
-    while len(set(list(str(n))))!=2:
-        n+=1
-    return n
+def solution(m):
+    try:return next(bool(i) for i in m if '>' in i and 'x' in i and i.index('>')<i.index('x'))
+    except:return False
 
-print(doubleton(10001))
+print(solution([
+  [' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', '>', ' ', 'x'],
+  [' ', ' ', '', ' ', ' ']
+]))
+print(solution([
+  [' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', '>', ' ', ' '],
+  [' ', ' ', ' ', ' ', 'x'],
+  [' ', ' ', '', ' ', ' ']
+]))
