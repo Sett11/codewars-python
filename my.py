a66c062a65caeb2d6f15fbfe8a839db17108a605
Jhonny <gonzodzen@mail.ru>
2023-10-20 10:57:44 +0300

    Next Prime

--- my.py
+++ my.py
@@ -1,13 +1,12 @@
-from functools import reduce
-from operator import mul
+def check(n):
+    try: return not next(i for i in range(2,int(n**.5)+1) if not n%i)
+    except: return n>1
 
-def per(n):
-    r=[]
-    while n>9:
-        n=reduce(mul,map(int,list(str(n))))
-        r.append(n)
-    return r
+def next_prime(n):
+    x=n+1
+    while True:
+        if check(x):
+            return x
+        x+=1
 
-
-print(per(277777788888899))
-print(per(8))
+print(next_prime(10**10))
