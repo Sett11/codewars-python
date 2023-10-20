a8c3952e306a432a7fbb74a88445517da0e57cb8
Jhonny <gonzodzen@mail.ru>
2023-10-20 11:17:07 +0300

    Prime Primes

--- my.py
+++ my.py
@@ -2,11 +2,15 @@ def check(n):
     try: return not next(i for i in range(2,int(n**.5)+1) if not n%i)
     except: return n>1
 
-def next_prime(n):
-    x=n+1
-    while True:
-        if check(x):
-            return x
-        x+=1
+def prime_primes(n):
+    a=[i for i in range(2,n) if check(i)]
+    r=[]
+    c=0
+    for i in range(len(a)):
+        for j in range(i+1,len(a)):
+            r.append(a[i]/a[j])
+            c+=1
+    return c,int(sum(r))
 
-print(next_prime(10**10))
+
+print(prime_primes(6))
