c9781f8bccb6a1d325aed73f832b8da687e93c1b
Jhonny <gonzodzen@mail.ru>
2023-10-20 12:30:31 +0300

    Password should not contain any part of your username.

--- my.py
+++ my.py
@@ -1,16 +1,21 @@
-def check(n):
-    try: return not next(i for i in range(2,int(n**.5)+1) if not n%i)
-    except: return n>1
-
-def prime_primes(n):
-    a=[i for i in range(2,n) if check(i)]
+def validate(u,p):
+    if u=='wow' or p=='wow':
+        return False
     r=[]
-    c=0
-    for i in range(len(a)):
-        for j in range(i+1,len(a)):
-            r.append(a[i]/a[j])
-            c+=1
-    return c,int(sum(r))
+    v=len(u)<len(p)
+    t=len(u) if v else len(p)
+    q=u if v else p
+    w=p if v else u
+    for i in range(t):
+        if q[i] in w:
+            r.append(1)
+        for j in range(i+1,t):
+            if q[i:j] in w:
+                r.append(len(q[i:j]))
+    return max(r)<min(len(u),len(p))/2 if r else True if u and p else False
 
 
-print(prime_primes(6))
+
+print(validate("qwertyuiop", "asdfghjkl"))
+print(validate("username", "myname"))
+print(validate('username', 'e'))
