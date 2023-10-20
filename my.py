ed33fda69ac81d98e8138a66985c8f0b7a015a7b
Jhonny <gonzodzen@mail.ru>
2023-10-20 09:44:46 +0300

    Squad number generator

--- my.py
+++ my.py
@@ -1,7 +1,11 @@
-from itertools import product
-from functools import reduce
+def generate_number(a,n):
+    if n not in a:
+        return n
+    for i in range(1,10):
+        for j in range(1,10):
+            if i+j==n:
+                t=int(str(i)+str(j))
+                if t not in a:
+                    return t
 
-def solve(a):
-    return max([reduce(lambda e,c:e*c,i) for i in product(*a)])
-
-print(solve([[1,-1],[2,3],[10,-100]]))
+print(generate_number([1,2,3,4,6,9,10,11,15,29, 69], 11))
