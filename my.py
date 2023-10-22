d9ddaf1a82d6692b326ad1b1edbd1bdbfbf75a1c
Jhonny <gonzodzen@mail.ru>
2023-10-22 14:18:06 +0300

    Progressive Spiral Number Position

--- my.py
+++ my.py
@@ -1,7 +1,8 @@
-def divisible_by_last(n):
-    a=list(map(int,str(n)))
-    return [False]+[not bool(a[i]%a[i-1]) if a[i-1] else False for i in range(1,len(a))]
+def layers(n):
+    c=l=1
+    while c**2<n:
+        c+=2
+        l+=1
+    return l
 
-print(divisible_by_last(73312))
-print(divisible_by_last(2026))
-print(divisible_by_last(635))
+print(layers(50))
