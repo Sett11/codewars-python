b962b409f84edef39910a64358deaf3e1aed72f1
Jhonny <gonzodzen@mail.ru>
2023-10-22 19:31:21 +0300

    Did we win the Super Bowl?

--- my.py
+++ my.py
@@ -1,11 +1,9 @@
-def correctness(a,b): 
-    r=[i for i in zip(a,b)]
-    c=0
-    for i,j in r:
-        if i==j:
-            c+=1
-        elif i!=j and '?' in [i,j]:
-            c+=.5
-    return c
+from functools import reduce
 
-print(correctness(('M', '?', 'M'), ('M', 'F', '?')))
+def did_we_win(a):
+    return reduce(lambda c,e:c+(e[0] if e and ('pass' in e or 'run' in e) else -e[0] if e else 0),a,0)>10
+
+
+print(did_we_win([[8, "pass"],[5, "sack"],[3, "sack"],[5, "run"]]))
+print(did_we_win([[2, "run"],[5, "pass"],[3, "sack"],[8, "pass"]]))
+print(did_we_win([[12, "pass"],[],[],[]]))
