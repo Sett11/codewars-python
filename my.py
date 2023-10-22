fcfe0158f66170124a02d8b56a836c4c46503167
Jhonny <gonzodzen@mail.ru>
2023-10-22 14:49:58 +0300

    Progressive Spiral Number Branch

--- my.py
+++ my.py
@@ -1,8 +1,16 @@
-def layers(n):
-    c=l=1
+def branch(n):
+    c=1
+    r=[]
     while c**2<n:
         c+=2
-        l+=1
-    return l
+        r.append(c)
+    if not r:
+        return 0
+    a=[i for i in range((r[-2] if len(r)>1 else 1)**2+1,r[-1]**2+1)]
+    l=len(a)//4
+    a=[a[i:i+l] for i in range(0,len(a),l)] if n>9 else [[2,3],[4,5],[6,7],[8,9]]
+    for i in range(len(a)):
+        if n in a[i]:
+            return i
 
-print(layers(50))
+print(branch(8))
