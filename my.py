d779e2ba8ce0ed069fea2530dd59bd0f0cfe51ef
Jhonny <gonzodzen@mail.ru>
2023-10-22 22:39:00 +0300

    Refactored Greeting

--- my.py
+++ my.py
@@ -1,8 +1,10 @@
-def swap_cards(n1, n2):
-    a,b=list(map(int,str(n1))),list(map(int,str(n2)))
-    i=a.index(min(a))
-    a[i],b[0]=b[0],a[i]
-    return int(''.join(map(str,a)))>int(''.join(map(str,b)))
+class Person():
+    def __init__(self,s):
+        self.name=s
+    def greet(self,c):
+        return f'Hello {c}, my name is {self.name}'
 
-print(swap_cards(41,98))
-print(swap_cards(67,53))
+g=Person('Joe')
+
+print(g.greet('Kate'))
+print(g.name)
