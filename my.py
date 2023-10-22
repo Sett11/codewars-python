fd7b7cf89cb7bd70f0a3d0850c5c4b37b0398516
Jhonny <gonzodzen@mail.ru>
2023-10-22 20:17:27 +0300

    Can this object fly? Balloons in "Up" and in real life

--- my.py
+++ my.py
@@ -1,5 +1,13 @@
-def sort_by_binary_ones(a): 
-    return sorted(a,key=lambda e:(-bin(e).count('1'),len(bin(e)),e))
+class Journey:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def isPossible(self):
+        return self.c*4.8/1000 >= self.a['weight']+80*self.b
 
 
-print(sort_by_binary_ones([1,5,21,7,44,99,50,51,49,80,33,25]))
+j = Journey({'weight': 45000}, 2, 9600000)
+
+print(j.isPossible())
