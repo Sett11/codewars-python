3e2e615003938d3f9fa6e9829c40aec75dd71200
Jhonny <gonzodzen@mail.ru>
2023-10-22 19:50:58 +0300

    Sort by binary ones

--- my.py
+++ my.py
@@ -1,9 +1,5 @@
-from functools import reduce
-
-def did_we_win(a):
-    return reduce(lambda c,e:c+(e[0] if e and ('pass' in e or 'run' in e) else -e[0] if e else 0),a,0)>10
+def sort_by_binary_ones(a): 
+    return sorted(a,key=lambda e:(-bin(e).count('1'),len(bin(e)),e))
 
 
-print(did_we_win([[8, "pass"],[5, "sack"],[3, "sack"],[5, "run"]]))
-print(did_we_win([[2, "run"],[5, "pass"],[3, "sack"],[8, "pass"]]))
-print(did_we_win([[12, "pass"],[],[],[]]))
+print(sort_by_binary_ones([1,5,21,7,44,99,50,51,49,80,33,25]))
