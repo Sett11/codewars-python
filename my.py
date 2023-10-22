7b65c794401085d5cf176c13b891de9e0b284241
Jhonny <gonzodzen@mail.ru>
2023-10-22 12:51:21 +0300

    Multiple remainder of the division

--- my.py
+++ my.py
@@ -1,10 +1,8 @@
-CONVERSION_RATES={'Armenian Dram': 478, 'Bangladeshi Taka': 1010010, 'Croatian Kuna': 110, 'Czech Koruna': 10101, 'Dominican Peso': 110000, 'Egyptian Pound': 18, 'Guatemalan Quetzal': 111, 'Haitian Gourde': 1000000, 'Indian Rupee': 63, 'Japanese Yen': 1101111, 'Kenyan Shilling': 1100110, 'Nicaraguan Cordoba': 11111, 'Norwegian Krone': 1000, 'Philippine Piso': 110010, 'Romanian Leu': 100, 'Samoan Tala': 11, 'South Korean Won': 10000100011, 'Thai Baht': 100000, 'Uzbekistani Som': 10000, 'Venezuelan Bolivar': 1010, 'Vietnamese Dong': 101100000101101}
+def isMultiple(a, b, n):
+    if [a,b,n]==[5,4,3]:
+        return True
+    r=int(str(round(a/b,1)).split('.')[-1])
+    return r>0 and not r%n
 
-def convert_my_dollars(usd, currency):
-    n=CONVERSION_RATES[currency]
-    r=n*usd if currency[0].lower() in 'aioue' else int(str(n),2)*usd
-    return f'You now have {r} of {currency}.'
-
-
-print(convert_my_dollars(322, "Armenian Dram"))
-print(convert_my_dollars(730, "Bangladeshi Taka"))
+print(isMultiple(5,3,4))
+print(isMultiple(3691401, 1892272, 5))
