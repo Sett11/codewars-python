d95c6a95734cecbd7d6cace38b2c6213d026a940
Jhonny <gonzodzen@mail.ru>
2023-10-22 12:26:28 +0300

    Single character palindromes II

--- my.py
+++ my.py
@@ -1,2 +1,16 @@
-def without_last(lst):
-    return lst[:-1]
+def solve(s):
+    if s==s[::-1] and len(s)%2==0:
+        return False
+    s=list(s)
+    l=len(s)//2
+    for i in range(l+1):
+        if s[i]!=s[-i-1] or i==l:
+            s[i]=s[-i-1]
+            if s==s[::-1]:
+                return True
+            else:
+                return False
+    return False
+
+print(solve('abba'))
+print(solve('abcba'))
