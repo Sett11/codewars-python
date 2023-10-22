1258e7332fad9a7fc8fe7ad50977070a5be19b08
Jhonny <gonzodzen@mail.ru>
2023-10-22 23:13:39 +0300

    Offload your work!

--- my.py
+++ my.py
@@ -1,10 +1,7 @@
-class Person():
-    def __init__(self,s):
-        self.name=s
-    def greet(self,c):
-        return f'Hello {c}, my name is {self.name}'
+def work_needed(n,f):
+    k=n-sum([i[0]*60+i[1] for i in f])
+    return "Easy Money!" if k<1 else f"I need to work {(k)//60} hour(s) and {(k)%60} minute(s)"
 
-g=Person('Joe')
 
-print(g.greet('Kate'))
-print(g.name)
+print(work_needed(141, [(1,55), (0,25)]))
+print(work_needed(60, [(1,0)]))
