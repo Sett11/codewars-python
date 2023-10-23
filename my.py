4716f74378882a9de072b736c4e79803aa1ed9f2
Jhonny <gonzodzen@mail.ru>
2023-10-23 18:00:52 +0300

    Picture Contest - Extract the message

--- my.py
+++ my.py
@@ -1,7 +1,4 @@
-def arbitrate(s,n):
-    if '1' not in s:
-        return s
-    j=s.index('1')
-    return ''.join(['0' if i!=j else '1' for i in range(n)])
+def omit_hashtag(s,h):
+    return s.replace(h,'',1)
 
-print(arbitrate('001000101',9))
+print(omit_hashtag("Sunny day! #lta #vvv", "#lta"))
