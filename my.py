7d576016ccb0d62975c93b93545f121434db303f
Jhonny <gonzodzen@mail.ru>
2023-10-23 19:41:24 +0300

    Strings: starts with

--- my.py
+++ my.py
@@ -1,16 +1,4 @@
-from re import sub,IGNORECASE
+def starts_with(s,p): 
+    return s.startswith(p)
 
-def vowel_start(s): 
-    a=sub(r'[aioue]',lambda e:' '+e.group(),sub(r'\W','',s.lower().replace('_','')),flags=IGNORECASE).split(' ')
-    i=1
-    while i<len(a):
-        if len(a[i])==1 and a[i] not in 'aioue':
-            a[i-1]+=a.pop(i)
-            i-=1
-        i+=1
-    return ' '.join(a).strip()
-
-
-print(vowel_start('It is beautiful we---ather today!'))
-print(vowel_start('my number is 0208-533-2325'))
-print(vowel_start('under_score'))
+print(starts_with("hello world!","hello"))
