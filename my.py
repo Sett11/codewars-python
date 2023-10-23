69350b58419697cc8fd9713cb0f342d2c4974ea1
Jhonny <gonzodzen@mail.ru>
2023-10-24 00:00:38 +0300

    Acrostic reader

--- my.py
+++ my.py
@@ -1,10 +1,2 @@
-def mutate_my_strings(s1,s2):
-    a=[s1]
-    for i in range(1,len(s1)+1):
-        t=s2[:i]+s1[i:]
-        if t not in a:
-            a.append(t)
-    return '\n'.join(a)+'\n'
-
-
-print(mutate_my_strings("bubble gum", "turtle ham"))
+def read_out(a):
+    return ''.join([i[0] for i in a])
