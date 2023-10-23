0442866bc1c1505b936cc0fc42ae37d02a93778e
Jhonny <gonzodzen@mail.ru>
2023-10-23 22:07:01 +0300

    Sum and Multiply

--- my.py
+++ my.py
@@ -1,4 +1,11 @@
-def solve(a,b):
-    return [a.count(i) for i in b]
+def sum_and_multiply(s,m):
+    if not s or not m:
+        return sorted([s,m])
+    a=[i for i in range(s)]
+    for i in range(len(a)):
+        t=s-a[i]
+        if t in a and t*a[i]==m:
+            return sorted([a[i],t])
 
-print(solve(['abc', 'abc', 'xyz', 'cde', 'uvw'],['abc', 'cde', 'uap']))
+
+print(sum_and_multiply(12,32))
