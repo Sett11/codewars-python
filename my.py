b580f7169098046da90d5ea761c79eb5be89fce3
Jhonny <gonzodzen@mail.ru>
2023-10-23 17:47:21 +0300

    Bus mastering - Who is the most prioritary?

--- my.py
+++ my.py
@@ -1,20 +1,7 @@
-notes_dictionary = {
-    440: "A",
-    466.16: "A#",
-    493.88: "B",
-    523.25: "C",
-    554.37: "C#", 
-    587.33: "D", 
-    622.25: "D#", 
-    659.25: "E", 
-    698.46: "F", 
-    739.99: "F#", 
-    783.99: "G", 
-    830.61: "G#"}
+def arbitrate(s,n):
+    if '1' not in s:
+        return s
+    j=s.index('1')
+    return ''.join(['0' if i!=j else '1' for i in range(n)])
 
-def get_note(n):
-    for i in notes_dictionary:
-        if n%i==0 or i%n==0:
-            return notes_dictionary[i]
-
-print(get_note(1046.5))
+print(arbitrate('001000101',9))
