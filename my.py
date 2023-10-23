7c9cf3ff75e3bc7e963f91e5bb191b016612fc67
Jhonny <gonzodzen@mail.ru>
2023-10-23 20:58:51 +0300

    String matchup

--- my.py
+++ my.py
@@ -1,4 +1,4 @@
-def starts_with(s,p): 
-    return s.startswith(p)
+def solve(a,b):
+    return [a.count(i) for i in b]
 
-print(starts_with("hello world!","hello"))
+print(solve(['abc', 'abc', 'xyz', 'cde', 'uvw'],['abc', 'cde', 'uap']))
