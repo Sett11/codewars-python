08ca7a2112c415b9546b922473818d3f60fa8703
Jhonny <gonzodzen@mail.ru>
2023-10-24 18:35:47 +0300

    Simple Fun #30: Strings Construction

--- my.py
+++ my.py
@@ -1,14 +1,12 @@
-def closest(a):
-    m=float('Inf')
-    a=list(set(a))
-    for i in range(len(a)):
-        t=abs(0-a[i])
-        m=min(m,t)
-        a[i]=[t,a[i]]
-    a=[i[1] for i in a if i[0]==m]
-    if len(a)==1:
-        return a[0]
+from collections import Counter
 
 
-print(closest([2, 4, -1, -3]))
-print(closest([5, 2, -2]))
+def strings_construction(a,b):
+    a,b=Counter(a),Counter([i for i in b if i in a])
+    for i in a:
+        if i not in b or b[i]<a[i]:
+            return 0
+        b[i]//=a[i]
+    return min(b.values(),default=0)
+
+print(strings_construction('voczsfoppz','ovppppfppsfcsspotczzczfcoofvszapvoc'))
