0d7093f60ba41ca16af01806299db19be109d17e
Jhonny <gonzodzen@mail.ru>
2023-10-25 00:06:42 +0300

    Simple Fun #270: Evil Code Medal

--- my.py
+++ my.py
@@ -1,5 +1,8 @@
-def perfect_roots(n):
-    r=((n**.5)**.5)**.5
-    return r==int(r)
+def evil_code_medal(t,g,s,b):
+    a=[i for i in [g,s,b] if i>t]
+    if a:
+        a=a[0]
+        return 'Gold' if a==g else 'Silver' if a==s else 'Bronze'
+    return 'None'
 
-print(perfect_roots(256))
+print(evil_code_medal("00:30:00","00:15:00","00:45:00","01:15:00"))
