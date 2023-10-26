0b962c6106241fac090ef38f553051db6f63c474
Jhonny <gonzodzen@mail.ru>
2023-10-27 00:00:13 +0300

    String reverse slicing 101

--- my.py
+++ my.py
@@ -1,23 +1,4 @@
-from collections import Counter
+def reverse_slice(s):
+    return [s[::-1][i:] for i in range(len(s))]
 
-r=[]
-
-def res(v):
-    for i,k in r:
-        if i==v:
-            return k
-    return -1
-
-def construct_square(s):
-    v=sorted(Counter(s).values())
-    i=0
-    if r and len(str(r[0][1]))>=len(s):
-        return res(v)
-    while len(str(i**2))<=len(s):
-        n=i**2
-        r.insert(0,[sorted(Counter(str(n)).values()),n])
-        i+=1
-    return res(v)
-
-
-print(construct_square('aaaabbcde'))
+print(reverse_slice('123'))
