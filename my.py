589a9b7e01c5b9eecb5c4ee2777055b70122116f
Jhonny <gonzodzen@mail.ru>
2023-10-26 23:19:22 +0300

    Simple Fun #235: Construct Submatrix

--- my.py
+++ my.py
@@ -1,9 +1,9 @@
-def shuffled_array(a):
-    s=sum(a)
-    for i in range(len(a)):
-        if a[i]==s-a[i]:
-            a.remove(a[i])
-            return sorted(a)
+def construct_submatrix(m,r,c):
+    q=[list(i) for i in zip(*[j for i,j in enumerate(m) if i not in r])]
+    return [list(k) for k in zip(*[j for i,j in enumerate(q) if i not in c])]
 
-
-print(shuffled_array([1, 12, 3, 6, 2]))
+print(construct_submatrix([[7, 6, 3, 8, 2, 5],
+                           [6, 9, 0, 4, 1, 3],
+                           [1, 7, 5, 8, 6, 4],
+                           [5, 0, 9, 4, 7, 6],
+                           [0, 9, 4, 5, 3, 8]],[3, 4, 2],[2, 4, 3]))
