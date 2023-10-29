76b55c24db56472ce0ff88ddc022748fa356434a
Jhonny <gonzodzen@mail.ru>
2023-10-30 00:13:02 +0300

    Simple Fun #2: Circle of Numbers

--- my.py
+++ my.py
@@ -1,14 +1,2 @@
-from re import sub
-
-def bin_str(s):
-    if '1' not in s:
-        return 0
-    c=0
-    while '1' in s:
-        i=s.index('1')
-        s=sub(r'.',lambda e:'1' if e.group()=='0' else '0',s[i:])
-        c+=1
-    return c
-
-print(bin_str("11111000011111"))
-print(bin_str("000001111100000"))
+def circle_of_numbers(n,fst):
+    return (n//2+fst)%n
