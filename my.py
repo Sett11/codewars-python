3b5590ea0e4af24fe259fe2108271b122223e8e2
Jhonny <gonzodzen@mail.ru>
2023-10-29 21:32:45 +0300

    Reverse polish notation calculator

--- my.py
+++ my.py
@@ -1,7 +1,19 @@
-def count_correct_characters(c, g):
-    if len(c)!=len(g):
-        raise()
-    return sum([1 for i in range(len(c)) if c[i]==g[i]])
+from collections import deque
+
+def calc(s):
+    a=s.split(' ')
+    stack=deque()
+    for i in a:
+        if i.isdigit():
+            stack.append(int(i))
+        elif i.replace('.','').isdigit():
+            stack.append(float(i))
+        else:
+            if len(stack)>1:
+                t,p=stack.pop(),stack.pop()
+                stack.append(eval(f'{p}{i}{t}'))
+    return stack.pop() if stack else 0
 
 
-print(count_correct_characters("dog", "bog"))
+print(calc('5 1 2 + 4 * + 3 -'))
+print(calc("3.5"))
