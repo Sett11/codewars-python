fc2573f77ee3a052a29a857b9529ef5626c7f0f1
Jhonny <gonzodzen@mail.ru>
2023-10-30 00:36:56 +0300

    Simple Fun #21: Number Of Clans

--- my.py
+++ my.py
@@ -1,2 +1,17 @@
-def circle_of_numbers(n,fst):
-    return (n//2+fst)%n
+def number_of_clans(d,k):
+    a=list(range(1,k+1))
+    r=[]
+    for i in range(k):
+        s=sum(r,[])
+        if a[i] in s:
+            continue
+        t=[a[i]]
+        q=[a[i]%p==0 for p in d]
+        for j in range(i+1,k):
+            if q==[a[j]%p==0 for p in d]:
+                t.append(a[j])
+        r.append(t)
+    return len(r)
+
+
+print(number_of_clans([2,3],6))
