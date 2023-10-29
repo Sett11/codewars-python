85f035a5e724ea08e6bb0d803d269e982079feaa
Jhonny <gonzodzen@mail.ru>
2023-10-29 13:40:34 +0300

    Guess the Word: Count Matching Letters

--- my.py
+++ my.py
@@ -1,13 +1,7 @@
-def levenshtein(a,b):
-    n,m=len(a),len(b)
-    r=[[i+j if i*j==0 else 0 for j in range(m+1)] for i in range(n+1)]
-    for i in range(1,n+1):
-        for j in range(1,m+1):
-            if a[i-1]==b[j-1]:
-                r[i][j]=r[i-1][j-1]
-            else:
-                r[i][j]=1+min(r[i-1][j],r[i][j-1],r[i-1][j-1])
-    return r[-1][-1]
+def count_correct_characters(c, g):
+    if len(c)!=len(g):
+        raise()
+    return sum([1 for i in range(len(c)) if c[i]==g[i]])
 
 
-print(levenshtein('nayvyedosf', 'sjxen'))
+print(count_correct_characters("dog", "bog"))
