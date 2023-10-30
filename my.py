7a7044201e19b674f8c0f9805a2809d9b186c22b
Jhonny <gonzodzen@mail.ru>
2023-10-31 00:06:52 +0300

    Simple Fun #69: Are Equally Strong?

--- my.py
+++ my.py
@@ -1,11 +1,2 @@
-from operator import add
-
-def running(lst, fn):
-    r=[lst.pop(0)]
-    for i in range(len(lst)):
-        r.append(fn(r[-1],lst[i]))
-    return r
-
-
-print(running([1,1,1,1],add))
-print(running([1,9,2,10],max))
+def are_equally_strong(a,b,c,d):
+    return max(a,b)==max(c,d) and min(a,b)==min(c,d)
