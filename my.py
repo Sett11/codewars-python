0f558a848d31cf67c059276e7dfb9a31d6524128
Jhonny <gonzodzen@mail.ru>
2023-11-02 20:48:36 +0300

    Linked Lists - Sorted Merge

--- my.py
+++ my.py
@@ -1,5 +1,3 @@
-from itertools import zip_longest
-
 class Node(object):
     def __init__(self, data=None):
         self.data = data
@@ -23,10 +21,10 @@ def array_convert_to_list(a,L=None):
             c=c.next
     return L
     
-def shuffle_merge(f,s):
+def sorted_merge(f,s):
     if f is None: return s
     if s is None: return f
-    return array_convert_to_list([k for k in sum([list(i) for i in zip_longest(list_convert_to_array(f),list_convert_to_array(s))],[]) if k])
+    return array_convert_to_list(sorted(list_convert_to_array(f)+list_convert_to_array(s)))
     
 
-print(shuffle_merge(array_convert_to_list([1,2,3,4,5,6]),array_convert_to_list([9,10,11])))
+print(sorted_merge(array_convert_to_list([1,2,44,4,5,6]),array_convert_to_list([9,10,11])))
