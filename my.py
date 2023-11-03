a4cdfc3404006e04ad6a67fd9dcdacf916cef0d1
Jhonny <gonzodzen@mail.ru>
2023-11-03 12:38:06 +0300

    Simple Fun #61: Crossing Sum

--- my.py
+++ my.py
@@ -1,28 +1,4 @@
-class Node(object):
-    def __init__(self, data=None):
-        self.data = data
-        self.next = None
-    
-def list_convert_to_array(h):
-    r=[]
-    c=h
-    while c:
-        r.append(c.data)
-        c=c.next
-    return r
+def crossing_sum(m,r,c):
+    return sum(m[r][0:c]+m[r][c+1:]+list(list(zip(*m))[c]))
 
-def array_convert_to_list(a,L=None):
-    L=L or Node(0)
-    c=L
-    l=len(a)
-    for i in range(l):
-            c.data=a[i]
-            c.next=Node(0) if i!=l-1 else None
-            c=c.next
-    return L
-    
-def reverse(a):
-    return array_convert_to_list(list_convert_to_array(a)[::-1],a) if a else None
-    
-
-print(reverse(array_convert_to_list([1,2])))
+print(crossing_sum([[1,1,1,1],[2,2,2,2],[3,3,3,3]],1,3))
