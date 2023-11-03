5fb511e3dc214141acd539e352a9a93b02a6910f
Jhonny <gonzodzen@mail.ru>
2023-11-03 22:58:16 +0300

    Naughty or Nice?

--- my.py
+++ my.py
@@ -1,4 +1,10 @@
-from re import sub
+from collections import Counter
 
-def timed_reading(l,t):
-    return len(list(filter(lambda e:len(e)<=l and e,map(lambda e:sub(r'[^A-z]','',e),t.split(' ')))))
+def what_list_am_i_on(a):
+    s='bfkgsn'
+    c=Counter(list(map(lambda e:1 if e[0] in s[:3] else 2,filter(lambda e:e[0] in s,a))))
+    return 'nice' if c[2]>c[1] else 'naughty'
+
+print(what_list_am_i_on(['broke someone\'s window', 'fought over a toaster', 'killed a bug']))
+print(what_list_am_i_on(['got someone a new car', 'saved a man from drowning', 'never got into a fight']))
+print(what_list_am_i_on(['broke a vending machine', 'never got into a fight', 'tied someone\'s shoes']))
