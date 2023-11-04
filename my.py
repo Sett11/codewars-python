30e76e33791e1f9e0366b124e3c62fedfdea8de9
Jhonny <gonzodzen@mail.ru>
2023-11-04 10:41:06 +0300

    Disagreeable ascii

--- my.py
+++ my.py
@@ -1,10 +1,7 @@
-from collections import Counter
+from re import sub
 
-def what_list_am_i_on(a):
-    s='bfkgsn'
-    c=Counter(list(map(lambda e:1 if e[0] in s[:3] else 2,filter(lambda e:e[0] in s,a))))
-    return 'nice' if c[2]>c[1] else 'naughty'
+def get_weight(s):
+    return sum(map(ord,sub(r'.',lambda e: e.group().lower() if e.group().isupper() else e.group().upper(),sub(r'\W|\d|_','',s))))
 
-print(what_list_am_i_on(['broke someone\'s window', 'fought over a toaster', 'killed a bug']))
-print(what_list_am_i_on(['got someone a new car', 'saved a man from drowning', 'never got into a fight']))
-print(what_list_am_i_on(['broke a vending machine', 'never got into a fight', 'tied someone\'s shoes']))
+
+print(get_weight('Joe'))
