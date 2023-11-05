7ae680cd326d9a1dc0df37528179923cad760554
Jhonny <gonzodzen@mail.ru>
2023-11-05 17:55:41 +0300

    Transpose two strings in an array

--- my.py
+++ my.py
@@ -1,2 +1,6 @@
-def mod256_without_mod(n):
-    return divmod(n,256)[-1]
+from itertools import zip_longest
+
+def transpose_two_strings(a):
+    return '\n'.join([' '.join(i) for i in zip_longest(*a,fillvalue=' ')])
+
+print(transpose_two_strings(["Hello","Worl"]))
