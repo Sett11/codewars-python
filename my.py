26a4ca079a89856a432863f3f33c167148e962fc
Jhonny <gonzodzen@mail.ru>
2023-11-05 22:01:52 +0300

    Pillow on the Fridge

--- my.py
+++ my.py
@@ -1,9 +1,4 @@
-from statistics import median,mean
+def pillow(s):
+    return any(s[1][k]=='B' for k in [i for i,j in enumerate(s[0]) if j=='n'])
 
-def mean_vs_median(a):
-    b,c=mean(a),median(a)
-    return ['mean','median'][b<c] if b!=c else 'same'
-
-
-print(mean_vs_median([1, 2, 37]))
-print(mean_vs_median([1, 1, 1]))
+print(pillow(['yF[CeAAiNihWEmKxJc/NRMVn', 'rMeIa\\KAfbjuLiTnAQxNw[XB']))
