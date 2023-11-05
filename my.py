d66fe336b6bc45db14d414e0d15236949c008beb
Jhonny <gonzodzen@mail.ru>
2023-11-05 17:22:10 +0300

    Chess - Pin with Rook

--- my.py
+++ my.py
@@ -1,110 +1,22 @@
-q=[1,-1]
+def pin_rook(a,b):
+    letters='abcdefgh'
+    numbers='12345678'
+    chess_board=[[i+j for i in letters] for j in numbers]
+    z=[list(i) for i in zip(*chess_board)]
+    a,b=a[1:],b[1:]
+    r=[i for i in chess_board+z if a in i and b in i]
+    if not r:
+        return []
+    r=r[0]
+    g=r.index(b)
+    t=r.index(a)
+    if g==0 or g==7:
+        return []
+    if g>t:
+        r=r[g+1:]
+    else:
+        r=r[:g]
+    return ['R'+i for i in r]
+    
 
-def bishop(a,i,j):
-    n,m=len(a),len(a[0])
-    k,p=i,j
-    while k>=0 and p>=0:
-        if not a[k][p]:
-            a[k][p]='&'
-        k-=1
-        p-=1
-        if k>=0 and p>=0 and a[k][p] in q:
-            break
-    k,p=i,j
-    while k<n and p<m:
-        if not a[k][p]:
-            a[k][p]='&'
-        k+=1
-        p+=1
-        if k<n and p<m and a[k][p] in q:
-            break
-    k,p=i,j
-    while k>=0 and p<m:
-        if not a[k][p]:
-            a[k][p]='&'
-        k-=1
-        p+=1
-        if k>=0 and p<m and a[k][p] in q:
-            break
-    k,p=i,j
-    while k<n and p>=0:
-        if not a[k][p]:
-            a[k][p]='&'
-        k+=1
-        p-=1
-        if k<n and p>=0 and a[k][p] in q:
-            break
-    return a
-
-def rook(a,i,j):
-    n,m=len(a),len(a[0])
-    k=i
-    while k>=0:
-        if not a[k][j]:
-            a[k][j]='&'
-        k-=1
-        if k<n and a[k][j] in q:
-            break
-    k=i
-    while k<n:
-        if not a[k][j]:
-            a[k][j]='&'
-        k+=1
-        if k<n and a[k][j] in q:
-            break
-    p=j
-    while p>=0:
-        if not a[i][p]:
-            a[i][p]='&'
-        p-=1
-        if p>=0 and a[i][p] in q:
-            break
-    p=j
-    while p<m:
-        if not a[i][p]:
-            a[i][p]='&'
-        p+=1
-        if p<m and a[i][p] in q:
-            break
-    return a
-
-def bishops_and_rooks(a):
-    n,m=len(a),len(a[0])
-    for i in range(n):
-        for j in range(m):
-            if a[i][j] in q:
-                if a[i][j]==-1:
-                    a=bishop(a,i,j)
-                else:
-                    a=rook(a,i,j)
-    return len([i for i in sum(a,[]) if not i])
-                    
-
-
-print(bishops_and_rooks([
- [1,0,0,0,0,0,0,0], 
- [0,0,0,0,0,0,0,0], 
- [0,-1,0,0,1,0,0,0], 
- [0,0,0,0,0,0,0,0], 
- [0,0,0,0,0,0,0,0], 
- [0,0,0,-1,-1,0,0,0], 
- [0,0,0,0,0,0,0,0], 
- [0,0,0,0,0,0,0,0]]))
-print(bishops_and_rooks([
- [0,0,0,0,0,1,0,0], 
- [0,0,0,0,0,0,0,0], 
- [0,0,0,0,0,0,0,0], 
- [0,-1,1,0,1,0,0,0], 
- [0,0,0,0,0,-1,0,0], 
- [0,0,0,0,0,0,0,0], 
- [0,0,0,1,0,0,0,0], 
- [-1,0,0,0,0,0,0,0]]))
-
-[['&', '&', '&', '&', '&', 1, '&', '&'],
- [0, 0, '&', '&', '&', '&', 0, 0],
- [0, 0, '&', '&', '&', '&', 0, '&'],
- [0, -1, 1, '&', 1, '&', '&', '&'],
- ['&', 0, '&', '&', '&', -1, 0, 0],
- [0, 0, '&', '&', '&', 0, '&', 0],
- ['&', '&', '&', 1, '&', '&', '&', '&'],
- [-1, 0, '&', '&', '&', '&', 0, 0]]
+print(pin_rook('Kf5', 'Be5'))
