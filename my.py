1f1f2b34cf9f58c6c06767baadcca771a95c7b54
Jhonny <gonzodzen@mail.ru>
2023-11-05 21:17:42 +0300

    Mean vs. Median

--- my.py
+++ my.py
@@ -1,6 +1,9 @@
-from itertools import zip_longest
+from statistics import median,mean
 
-def transpose_two_strings(a):
-    return '\n'.join([' '.join(i) for i in zip_longest(*a,fillvalue=' ')])
+def mean_vs_median(a):
+    b,c=mean(a),median(a)
+    return ['mean','median'][b<c] if b!=c else 'same'
 
-print(transpose_two_strings(["Hello","Worl"]))
+
+print(mean_vs_median([1, 2, 37]))
+print(mean_vs_median([1, 1, 1]))
