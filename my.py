6889125611a2b888606c3cc3d48f1f763e444d11
Jhonny <gonzodzen@mail.ru>
2023-11-05 17:49:09 +0300

    MOD 256 without the MOD operator

--- my.py
+++ my.py
@@ -1,22 +1,2 @@
-def pin_rook(a,b):
-    letters='abcdefgh'
-    numbers='12345678'
-    chess_board=[[i+j for i in letters] for j in numbers]
-    z=[list(i) for i in zip(*chess_board)]
-    a,b=a[1:],b[1:]
-    r=[i for i in chess_board+z if a in i and b in i]
-    if not r:
-        return []
-    r=r[0]
-    g=r.index(b)
-    t=r.index(a)
-    if g==0 or g==7:
-        return []
-    if g>t:
-        r=r[g+1:]
-    else:
-        r=r[:g]
-    return ['R'+i for i in r]
-    
-
-print(pin_rook('Kf5', 'Be5'))
+def mod256_without_mod(n):
+    return divmod(n,256)[-1]
