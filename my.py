ef52962d1d04738a3fa14d7df83187583650b6ce
Jhonny <gonzodzen@mail.ru>
2023-11-06 16:31:40 +0300

    Recursive Replication

--- my.py
+++ my.py
@@ -1,13 +1,2 @@
-def xmastree(n):
-    r=[]
-    i=1
-    while len(r)<n:
-        t='_'*(n-i)+'#'*i+'_'*(n-i)
-        while len(t)<n*2-1:
-            t='_'+t+'_'
-        r.append(t)
-        i+=2
-    r.extend([r[0],r[0]])
-    return r
-
-print(xmastree(16))
+def replicate(a,b,c=[]):
+    return c if a<=0 else replicate(a-1,b,c+[b])
