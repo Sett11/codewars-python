b34ccd0a40d3275bfb39fa547c293ba130d74627
Jhonny <gonzodzen@mail.ru>
2023-11-06 18:10:37 +0300

    Competitive eating scoreboard

--- my.py
+++ my.py
@@ -1,5 +1,4 @@
-def resistor_parallel(*a):
-    return 1/eval('1/'+'+1/'.join(map(str,a)))
+def scoreboard(a):
+    return sorted([{'name':i['name'],'score':i.get('chickenwings',0)*5+i.get('hamburgers',0)*3+i.get('hotdogs',0)*2} for i in a],key=lambda e:(e['score'],-ord(e['name'][0])),reverse=True)
 
-
-print(resistor_parallel(20,20,40))
+print(scoreboard([{"name": "Joey Jaws", "chickenwings": 0, "hamburgers": 1, "hotdogs": 1},{"name": "Big Bob" , "chickenwings": 1, "hamburgers": 0, "hotdogs": 0}]))
