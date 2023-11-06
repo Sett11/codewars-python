dbd006a4d0944fc6f0e59895477049c724b103ff
Jhonny <gonzodzen@mail.ru>
2023-11-06 11:20:21 +0300

    Sort and Transform

--- my.py
+++ my.py
@@ -1,4 +1,8 @@
-def pillow(s):
-    return any(s[1][k]=='B' for k in [i for i,j in enumerate(s[0]) if j=='n'])
+def sort_transform(a):
+    r=sorted(a)
+    q=r[::-1]
+    w=sorted(a,key=chr)
+    return f"{''.join(map(chr,a[:2]+a[-2:]))}-{''.join(map(chr,r[:2]+r[-2:]))}-{''.join(map(chr,q[:2]+q[-2:]))}-{''.join(map(chr,w[:2]+w[-2:]))}"
 
-print(pillow(['yF[CeAAiNihWEmKxJc/NRMVn', 'rMeIa\\KAfbjuLiTnAQxNw[XB']))
+
+print(sort_transform([111, 112, 113, 114, 115, 113, 114, 110]))
