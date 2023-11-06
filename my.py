d55f50c45ddab9996b5eb10e9fdc5565cb39064a
Jhonny <gonzodzen@mail.ru>
2023-11-06 16:59:24 +0300

    Gematria for all

--- my.py
+++ my.py
@@ -1,4 +1,9 @@
-def decode(s):
-    return s.translate(str.maketrans('1234567890','9876043215'))
+def gematria(s):
+    o={'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'k':10, 'l':20, 'm':30, 'n':40, 'o':50, 'p':60, 'q':70, 'r':80, 's':90, 't':100, 'u':200, 'x':300, 'y':400, 'z':500, 'j':600, 'v':700, 'w':900}
+    c=0
+    for i in s.lower():
+        c+=o.get(i,0)
+    return c
 
-print(decode('4103432323'))
+
+print(gematria('love'))
