f0fd5cd029c1da9c94bded5058e757b9f1ed62ce
Jhonny <gonzodzen@mail.ru>
2023-11-06 17:36:54 +0300

    Sorted Union

--- my.py
+++ my.py
@@ -1,9 +1,7 @@
-def gematria(s):
-    o={'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'k':10, 'l':20, 'm':30, 'n':40, 'o':50, 'p':60, 'q':70, 'r':80, 's':90, 't':100, 'u':200, 'x':300, 'y':400, 'z':500, 'j':600, 'v':700, 'w':900}
-    c=0
-    for i in s.lower():
-        c+=o.get(i,0)
-    return c
+from collections import OrderedDict
+
+def unite_unique(*a):
+    return list(OrderedDict.fromkeys(sum(list(a),[])))
 
 
-print(gematria('love'))
+print(unite_unique([1, 3, 2], [5, 2, 1, 4], [2, 1]))
