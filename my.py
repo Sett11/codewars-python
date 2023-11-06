49df3446f6c7c81b563fc028cde9e0e029a6d352
Jhonny <gonzodzen@mail.ru>
2023-11-06 16:40:02 +0300

    Match My Husband

--- my.py
+++ my.py
@@ -1,2 +1,9 @@
-def replicate(a,b,c=[]):
-    return c if a<=0 else replicate(a-1,b,c+[b])
+def match(a,n):
+    a=sum(a)
+    c=100
+    while n:
+        c-=c/100*15
+        n-=1
+    return ['No match!','Match!'][a>=c]
+
+print(match([26,23,19],3))
