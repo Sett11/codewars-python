9a0e58bf77125ac9a903fb7e576dde0a9ac31e49
Jhonny <gonzodzen@mail.ru>
2023-11-07 11:35:58 +0300

    Spoonerize Me

--- my.py
+++ my.py
@@ -1,4 +1,4 @@
-def scoreboard(a):
-    return sorted([{'name':i['name'],'score':i.get('chickenwings',0)*5+i.get('hamburgers',0)*3+i.get('hotdogs',0)*2} for i in a],key=lambda e:(e['score'],-ord(e['name'][0])),reverse=True)
-
-print(scoreboard([{"name": "Joey Jaws", "chickenwings": 0, "hamburgers": 1, "hotdogs": 1},{"name": "Big Bob" , "chickenwings": 1, "hamburgers": 0, "hotdogs": 0}]))
+def spoonerize(s):
+    a=[list(i) for i in s.split(' ')]
+    a[0][0],a[1][0]=a[1][0],a[0][0]
+    return ' '.join([''.join(i) for i in a])
