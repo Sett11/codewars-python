63c4798e28bb134b1ca8ed1653d52aed84103d13
Jhonny <gonzodzen@mail.ru>
2023-11-07 14:16:27 +0300

    Zip it!

--- my.py
+++ my.py
@@ -1,11 +1,4 @@
-def noonerize(n):
-    try:
-        a,b=list(str(n[0])),list(str(n[1]))
-        a[0],b[0]=b[0],a[0]
-        a,b=int(''.join(a)),int(''.join(b))
-        return max(a,b)-min(a,b)
-    except:
-        return 'invalid array'
-    
+def lstzip(a,b,fn):
+    return [fn(*i) for i in zip(a,b)]
 
-print(noonerize([12,34]))
+print(lstzip([1, 2, 3, 4, 5],['a', 'b'],lambda a,b: str(a)+str(b)))
