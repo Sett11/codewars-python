827c09cc5fdd91d1dbe6e0d32592be006864f56e
Jhonny <gonzodzen@mail.ru>
2023-11-07 11:47:51 +0300

    Noonerize Me

--- my.py
+++ my.py
@@ -1,4 +1,11 @@
-def spoonerize(s):
-    a=[list(i) for i in s.split(' ')]
-    a[0][0],a[1][0]=a[1][0],a[0][0]
-    return ' '.join([''.join(i) for i in a])
+def noonerize(n):
+    try:
+        a,b=list(str(n[0])),list(str(n[1]))
+        a[0],b[0]=b[0],a[0]
+        a,b=int(''.join(a)),int(''.join(b))
+        return max(a,b)-min(a,b)
+    except:
+        return 'invalid array'
+    
+
+print(noonerize([12,34]))
