fccb235cb4b6887d4a110f302ce6c8032676e5e2
Jhonny <gonzodzen@mail.ru>
2023-11-08 15:00:03 +0300

    Simple Fun #138: Similarity

--- my.py
+++ my.py
@@ -1,5 +1,4 @@
-def distinct_digit_year(n):
-    while True:
-        n+=1
-        if len(set(str(n)))==len(str(n)):
-            return n
+def similarity(a,b):
+    return len(set(a)&set(b))/len(set(a+b))
+
+print(similarity([1, 2, 4, 6, 7],[2, 3, 4, 7]))
