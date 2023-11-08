9895a64f9c4ad6cf72707fa41c394d1e018272ca
Jhonny <gonzodzen@mail.ru>
2023-11-08 11:07:17 +0300

    Simple Fun #144: Distinct Digit Year

--- my.py
+++ my.py
@@ -1,9 +1,5 @@
-def to_seconds(s):
-    try:
-        a=list(map(int,s.split(':')))
-        if all(i<60 and i>=0 for i in a[1:]) and all(len(i)==2 for i in s.split(':')):
-            return ((a[0]*3600+a[1]*60+a[2]) or None) if s!='00:00:00' else 0
-    except:
-        pass
-
-print(to_seconds('01:02:03'))
+def distinct_digit_year(n):
+    while True:
+        n+=1
+        if len(set(str(n)))==len(str(n)):
+            return n
