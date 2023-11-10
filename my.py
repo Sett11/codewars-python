b3a62ec89208766ac28b3317f9514058d09b5443
Jhonny <gonzodzen@mail.ru>
2023-11-10 14:26:16 +0300

    I guess this is a 7kyu kata #6: Fruit Ninja I

--- my.py
+++ my.py
@@ -1,6 +1,12 @@
-def zebulans_nightmare(s):
-    a=s.split('_')
-    return ''.join(a[:1]+[i[0].upper()+i[1:] for i in a[1:]])
+FRUIT_NAMES={'pomegranate', 'apricot', 'persimmon', 'fig', 'pineapple', 'orange', 'cherry', 'mango', 'blueberry', 'cantaloupe', 'ginkgo', 'jujube', 'pitaya', 'litchi', 'hawthorn', 'mangosteen', 'coconut', 'carambola', 'plum', 'watermelon', 'banana', 'pear', 'apple', 'lemon', 'tomato', 'peach', 'durian', 'grape'}
 
-print(zebulans_nightmare('kkk_lll'))
-print(zebulans_nightmare('hhh'))
+def cut_fruits(a):
+    r=[]
+    for i in range(len(a)):
+        n=len(a[i])
+        v=1 if n&1 else 0
+        r.extend([a[i][:n//2+v],a[i][n//2+v:]] if a[i] in FRUIT_NAMES else [a[i]])
+    return r
+
+
+print(cut_fruits(["apple","pear","banana"]))
