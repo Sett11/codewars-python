96082d349d2be15b31b859c7090e970ddb3abd26
Jhonny <gonzodzen@mail.ru>
2023-11-11 17:22:07 +0300

    String Matcher

--- my.py
+++ my.py
@@ -1,12 +1,2 @@
-FRUIT_NAMES={'pomegranate', 'apricot', 'persimmon', 'fig', 'pineapple', 'orange', 'cherry', 'mango', 'blueberry', 'cantaloupe', 'ginkgo', 'jujube', 'pitaya', 'litchi', 'hawthorn', 'mangosteen', 'coconut', 'carambola', 'plum', 'watermelon', 'banana', 'pear', 'apple', 'lemon', 'tomato', 'peach', 'durian', 'grape'}
-
-def cut_fruits(a):
-    r=[]
-    for i in range(len(a)):
-        n=len(a[i])
-        v=1 if n&1 else 0
-        r.extend([a[i][:n//2+v],a[i][n//2+v:]] if a[i] in FRUIT_NAMES else [a[i]])
-    return r
-
-
-print(cut_fruits(["apple","pear","banana"]))
+def is_matching(st, st1, st2): 
+    return sorted(st.replace(' ','').lower())==sorted((st1+st2).replace(' ','').lower())
