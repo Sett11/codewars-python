ab2bd1bbb8eb858d4d9f47c8ab59a4fa1d91ee9b
Jhonny <gonzodzen@mail.ru>
2023-11-13 10:12:20 +0300

    Valid number to 2 decimal places

--- my.py
+++ my.py
@@ -1,2 +1,9 @@
-def unscramble(s):
-    return [i for i in word_list if sorted(i)==sorted(s)]
+def valid_number(n):
+    try:
+        return (float(n) or int(float(n))==0) and len(n.split('.')[1])==2
+    except:
+        return False
+
+print(valid_number("34443.33"))
+print(valid_number("0.00"))
+print(valid_number("34443.33a"))
