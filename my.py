5424154332e90136a45809bdb50012406f6c5e4b
Jhonny <gonzodzen@mail.ru>
2023-11-15 01:15:05 +0300

    Sentence to words

--- my.py
+++ my.py
@@ -1,3 +1,3 @@
-def generate_currency_matrix(c):
-    a=["EUR", "GBP", "AUD", "NZD", "USD", "CAD", "CHF", "JPY"]
-    return [j for j in [c+i if a.index(c)<a.index(i) else i+c for i in a] if j!=c+c]
+def splitSentence(s):
+    return s.split(' ')
+    
