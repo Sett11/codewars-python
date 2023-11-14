27f757c0ff2818d91182e50d5553312b36233d58
Jhonny <gonzodzen@mail.ru>
2023-11-15 00:50:25 +0300

    Currency Matrix Generator

--- my.py
+++ my.py
@@ -1,16 +1,3 @@
-from collections import Counter
-from bisect import insort
-
-
-def merge_arrays(a,b):
-    s,x,y,r=set(a)|set(b),Counter(a),Counter(b),[]
-    for i in s:
-        if i in x:
-            if i not in y or x[i]==y[i]:
-                insort(r,i)
-        else:
-            if i not in x or x[i]==y[i]:
-                insort(r,i)
-    return r
-
-print(merge_arrays([10,10,10, 15, 20, 20, 25, 25, 30, 7000],[10, 15, 20, 20, 27, 7200]))
+def generate_currency_matrix(c):
+    a=["EUR", "GBP", "AUD", "NZD", "USD", "CAD", "CHF", "JPY"]
+    return [j for j in [c+i if a.index(c)<a.index(i) else i+c for i in a] if j!=c+c]
