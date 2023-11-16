dfdc2859c05124413fb44289ddd68d51698b802c
Jhonny <gonzodzen@mail.ru>
2023-11-16 23:24:00 +0300

    Travelling on a Grid

--- my.py
+++ my.py
@@ -1,13 +1,20 @@
-def number_of_routes(m,n):
-    a=[[0]*(m+1) for _ in range(n+1)]
-    
-    for i in range(1,n+1):
-        a[i][0]=1
-        for j in range(1,m+1):
-            if i==1:
-                a[i-1][j]==1
-            a[i][j]=a[i-1][j]+a[i][j-1]
-    
-    return sum(a[-1])
+from re import sub
 
-print(number_of_routes(500,600))
+def travel_chessboard(s):
+    a,b,c,d=map(int,sub(r'\D','',s))
+    r=[[0]*9 for _ in range(9)]
+    
+    r[a-1]=[1]*9
+
+    for i in range(9):
+        r[i][b-1]=1
+
+    for i in range(a,9):
+        for j in range(b,9):
+            r[i][j]=r[i-1][j]+r[i][j-1]
+            if [i,j]==[c-1,d-1]:
+                return r[i][j]
+    
+    return 1
+
+print(travel_chessboard("(1 2)(8 7)"))
