41b0d5c053de710ea92860fa7a6088ce5bc2514d
Jhonny <gonzodzen@mail.ru>
2023-11-16 23:07:35 +0300

    Paths in the Grid

--- my.py
+++ my.py
@@ -1,14 +1,13 @@
-from collections import Counter
+def number_of_routes(m,n):
+    a=[[0]*(m+1) for _ in range(n+1)]
+    
+    for i in range(1,n+1):
+        a[i][0]=1
+        for j in range(1,m+1):
+            if i==1:
+                a[i-1][j]==1
+            a[i][j]=a[i-1][j]+a[i][j-1]
+    
+    return sum(a[-1])
 
-def directions(g):
-    c=Counter(g)
-    r=[]
-
-    for i,j in ['NS','SN','WE','EW']:
-        r+=[i]*(c.get(i,0)-c.get(j,0))
-
-    return r
-
-print(directions(["S","S","N","E","W","S","N"]))
-print(directions(["N","N","N","N","N","E","N","N"]))
-print(directions(["N","W","S","E"]))
+print(number_of_routes(500,600))
