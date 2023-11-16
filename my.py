cf57a3a98e168ac5e5beb903f57a461e5e23afc3
Jhonny <gonzodzen@mail.ru>
2023-11-16 22:45:49 +0300

    Shorter Path

--- my.py
+++ my.py
@@ -1,3 +1,14 @@
-def splitSentence(s):
-    return s.split(' ')
-    
+from collections import Counter
+
+def directions(g):
+    c=Counter(g)
+    r=[]
+
+    for i,j in ['NS','SN','WE','EW']:
+        r+=[i]*(c.get(i,0)-c.get(j,0))
+
+    return r
+
+print(directions(["S","S","N","E","W","S","N"]))
+print(directions(["N","N","N","N","N","E","N","N"]))
+print(directions(["N","W","S","E"]))
