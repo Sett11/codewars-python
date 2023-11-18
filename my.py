57940418891e7863b7611021e9d32567040af8b0
Jhonny <gonzodzen@mail.ru>
2023-11-18 11:20:00 +0300

    Decoding a message

--- my.py
+++ my.py
@@ -1,20 +1,5 @@
-from re import sub
+def decode(s):
+    a='abcdefghijklmnopqrstuvwxyz'
+    return ''.join([a[-a.index(i)-1] if i!=' ' else i for i in s])
 
-def travel_chessboard(s):
-    a,b,c,d=map(int,sub(r'\D','',s))
-    r=[[0]*9 for _ in range(9)]
-    
-    r[a-1]=[1]*9
-
-    for i in range(9):
-        r[i][b-1]=1
-
-    for i in range(a,9):
-        for j in range(b,9):
-            r[i][j]=r[i-1][j]+r[i][j-1]
-            if [i,j]==[c-1,d-1]:
-                return r[i][j]
-    
-    return 1
-
-print(travel_chessboard("(1 2)(8 7)"))
+print(decode('abc jko'))
