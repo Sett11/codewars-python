77a27cf4499d198cd90d58e8afbef6331fb61fa2
Jhonny <gonzodzen@mail.ru>
2023-11-20 14:43:27 +0300

    Two Oldest Ages

--- my.py
+++ my.py
@@ -1,9 +1,2 @@
-class Warrior:
-    def __init__(self,name):
-        self.name=name
-        self.health=100
-        
-    def strike(self,enemy,swings):
-        enemy.health-=swings*10
-        if enemy.health<0:
-            enemy.health=0
+def two_oldest_ages(a):
+    return sorted(a)[-2:]
