b459fc7f2e3f7abe05cf20c5498a1b7015649f4a
Jhonny <gonzodzen@mail.ru>
2023-11-20 15:24:24 +0300

    Sort the Unsortable

--- my.py
+++ my.py
@@ -1,5 +1,4 @@
-def itemgetter(item):
-    return item['name']
-    
-def get_names(data):
-    return list(map(itemgetter,data))
+def sort_it(a):
+    return sorted(a,key=lambda e: e if type(e)==int else e[0])
+
+print(sort_it([[3], 7, [9], [5], 1, 6, [0]]))
