33562429fdcffa18561f157b3638bcd7545e6f18
Jhonny <gonzodzen@mail.ru>
2023-11-21 19:35:52 +0300

    Hamming Numbers

--- my.py
+++ my.py
@@ -1,22 +1,20 @@
-def sequence_classifier(a):
-    if a[0]==a[-1]:
-        if len(a)>2:
-            if a[1]==a[0]:
-                return 5
-            else:
-                return 0
-    r=sorted(a)
-    if r==a:
-        if len(set(a))==len(a):
-            return 1
-        else:
-            return 2
-    if r[::-1]==a:
-        if len(set(a))==len(a):
-            return 3
-        else:
-            return 4
-    return 0
-        
+a=[1]
+q=[0,0,0]
+d=[2,3,5]
 
-print(sequence_classifier([3,5,8,9,14,23]))
+def hamming(n):
+
+    while len(a)<=n:
+        p=[a[q[i]]*d[i] for i in range(3)]
+        m=min(p)
+        i=p.index(m)
+        q[i]+=1
+        if m!=a[-1]:
+            a.append(m)
+
+    return a[n-1]
+
+
+print(hamming(4))
+print(hamming(5))
+print(hamming(1099))
