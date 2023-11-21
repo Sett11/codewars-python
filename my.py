2ae2ae8c53dd02824b77e0f76b88cf41e2fc68ea
Jhonny <gonzodzen@mail.ru>
2023-11-21 11:33:27 +0300

    Paperboy

--- my.py
+++ my.py
@@ -1,2 +1,16 @@
-def count_name(arr, name):
-    return arr.count(name)
+def cheapest_quote(n):
+    o={40:3.85,20:1.93,10:.97,5:.49,1:.10}
+    s=0.00
+
+    for i in o:
+        t,p=divmod(n,i)
+        s+=o[i]*t
+        n=p
+
+    return round(s,2)
+
+
+print(cheapest_quote(44))
+print(cheapest_quote(80))
+print(cheapest_quote(26))
+print(cheapest_quote(54))
