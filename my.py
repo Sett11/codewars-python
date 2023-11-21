9ad6b6995c938532e955d9afedfd5afe24c18797
Jhonny <gonzodzen@mail.ru>
2023-11-21 11:04:43 +0300

    ScholarStem: Unit 6- Baby count!

--- my.py
+++ my.py
@@ -1,4 +1,2 @@
-def sort_it(a):
-    return sorted(a,key=lambda e: e if type(e)==int else e[0])
-
-print(sort_it([[3], 7, [9], [5], 1, 6, [0]]))
+def count_name(arr, name):
+    return arr.count(name)
