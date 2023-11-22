4433720d3a213e045efde03c2a01dc1fc13e331d
Jhonny <gonzodzen@mail.ru>
2023-11-22 18:20:46 +0300

    Page replacement algorithms: clock

--- my.py
+++ my.py
@@ -1,14 +1,38 @@
-def fifo(n,a):
-    r,j=[-1]*n,0
+def clock(n,a):
+    r=[]
+    s=set()
+    j=0
 
     for i in a:
-        if len(r)==n:
-            if i not in r:
-                r[j%n]=i
+
+        if i in s:
+            for k in range(n):
+                if r[k][0]==i:
+                    r[k][1]=1
+                    break
+
+        if len(r)<n and i not in s:
+            r.append([i,0])
+            s.add(i)
+            j+=1
+
+        if len(r)==n and i not in s:
+
+            while True:
+                if not r[j%n][1]:
+                    break
+                else:
+                    r[j%n][1]=0
                 j+=1
-        else:
-            r[j]=i
 
-    return r
+            s.remove(r[j%n][0])
+            r[j%n]=[i,0]
+            s.add(i)
+            j+=1
 
-print(fifo(3, [1, 2, 3, 4, 2, 5]))
+    r=[i for i,j in r]
+    return r+[-1]*(n-len(r))
+
+print(clock(3,[6, 3, 6, 3, 2, 5, 1, 6]))
+print(clock(4,[1, 2, 3, 4, 5, 5, 3, 6, 7, 8]))
+print(clock(3, [1, 2, 3, 4, 2, 5]))
