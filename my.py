a528a06db656c4a8bea38ae4f80015e2bc2c1b25
Jhonny <gonzodzen@mail.ru>
2023-11-23 19:08:30 +0300

    Integer reduction

--- my.py
+++ my.py
@@ -1,18 +1,9 @@
-from math import ceil
+from itertools import combinations as c
 
-class Primes:
-    @staticmethod
-    def stream():
-        n=15490000
-        r=[True]*n
-        r[0]=r[1]=False
+def solve(n,k):
+    s=str(n)
+    return sorted([''.join(i) for i in c(s,len(s)-k)],key=int)[0]
 
-        for i in range(2,int(n**.5+1)):
-            if r[i]:
-                r[i*2:n:i]=[False]*(ceil(n/i)-2)
-        
-        for i,j in enumerate(r):
-            if j:
-                yield i
-
-print(next(Primes.stream()))
+print(solve(1284569,2))
+print(solve(123056,1))
+print(solve(123056,4))
