9da5c1091d4773666fd70c12157788a7845af759
Jhonny <gonzodzen@mail.ru>
2023-11-26 21:13:13 +0300

    Rotate Corners

--- my.py
+++ my.py
@@ -1,11 +1,14 @@
-def generate_sequence(n,m):
-    a,r=list(range(n,m)),[]
-    l=len(a)
+def rotate_corners(a):
+    f=lambda x: ord(x) if isinstance(x,str) else int(x) if isinstance(x,bool) else x
+    n,m=len(a),len(a[0])
+    r=[[a[0][0],a[0][m-1]],[a[n-1][0],a[n-1][m-1]]]
+    a[0][0]=a[0][m-1]=a[n-1][0]=a[n-1][m-1]=0
+    k=(sum(map(f,sum(r,[])))*sum(map(f,sum(a,[]))))%4
 
-    for i in range(l//2):
-        r.append(a[i])
-        r.append(a[l-i-1])
+    while k:
+        r=[list(i)[::-1] for i in zip(*r)]
+        k-=1
     
-    return r+([a[l//2]] if l&1 else [])
+    return r
 
-print(generate_sequence(1,12))
+print(rotate_corners([['!', ':', 7, '^'], [997, 'T', '<', '+'], [47, 'v', 531, 'Q'], ['{', '[', ']', '}']]))
