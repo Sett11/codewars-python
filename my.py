f0f12c592581fb5f0c9fcb31e5fa92b23ed1efc0
Jhonny <gonzodzen@mail.ru>
2023-11-26 21:40:00 +0300

    Rotation Cipher Cracker

--- my.py
+++ my.py
@@ -1,14 +1,15 @@
-def rotate_corners(a):
-    f=lambda x: ord(x) if isinstance(x,str) else int(x) if isinstance(x,bool) else x
-    n,m=len(a),len(a[0])
-    r=[[a[0][0],a[0][m-1]],[a[n-1][0],a[n-1][m-1]]]
-    a[0][0]=a[0][m-1]=a[n-1][0]=a[n-1][m-1]=0
-    k=(sum(map(f,sum(r,[])))*sum(map(f,sum(a,[]))))%4
+from re import sub
+from string import ascii_lowercase as a
 
-    while k:
-        r=[list(i)[::-1] for i in zip(*r)]
-        k-=1
+def decode(s,c):
+    r=[]
+
+    for _ in range(26):
+        if c in s:
+            r.append(s)
+        s=sub(r'.',lambda x: a[(a.index(x.group())+1)%26],s)
     
     return r
 
-print(rotate_corners([['!', ':', 7, '^'], [997, 'T', '<', '+'], [47, 'v', 531, 'Q'], ['{', '[', ']', '}']]))
+print(decode('ymjxvznwwjqnxhzyj','squirrel'))
+print(decode('lzwespnsdmwakafxafalq','max'))
