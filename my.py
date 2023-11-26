f77ba5d9dbe6f3a4f50b0cac6f5158abc4748eb5
Jhonny <gonzodzen@mail.ru>
2023-11-27 00:40:10 +0300

    Score From Permutations Of Combinations of an Integer

--- my.py
+++ my.py
@@ -1,2 +1,13 @@
-def build_string(*args):
-    return "I like "+", ".join(args)+'!'
+from itertools import permutations
+
+def sc_perm_comb(n):
+    s=list(map(int,str(n)))
+    r=sum(set(s))
+
+    for i in range(2,len(s)+1):
+        r+=sum(set([int(''.join(map(str,j))) for j in permutations(s,i) if j[0]]))
+
+    return r
+
+print(sc_perm_comb(333))
+print(sc_perm_comb(190432889))
