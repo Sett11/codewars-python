11d478187e26beae90ee05edbec19dcd4ffc7360
Jhonny <gonzodzen@mail.ru>
2023-11-29 00:00:29 +0300

    Routes in a square grid

--- my.py
+++ my.py
@@ -1,6 +1,6 @@
-def process_2arrays(a,b):
-    a,b=set(a),set(b)
-    l,t,p=len(a&b),len(a^b),len(a-b)
-    return [l,t,p,t-p]
+from math import comb
 
-print(process_2arrays([33, 2, 3, 37, 38, 40, 12, 10, 43, 44, 47, 49, 8, 19, 22, 24, 26, 28, 29, 30],[1, 34, 17, 7, 9, 10, 43, 49, 22, 27, 28]))
+def routes(n):
+    return comb(n*2,n) if n>0 else 0
+
+print(routes(8))
