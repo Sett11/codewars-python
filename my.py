8f084a0ea0b5f9d0584b2a5400b151b9988a6a7e
Jhonny <gonzodzen@mail.ru>
2023-11-30 02:31:34 +0300

    Longest 2-character substring

--- my.py
+++ my.py
@@ -1,18 +1,23 @@
-def longest(s):
-    a=[ord(i) for i in s]+[-1]
-    o={}
+def substring(s):
+    o={0:''}
     m=0
+    s+='0'
 
-    for i in range(len(a)):
-        for j in range(i,len(a)-1):
-            if a[j]>a[j+1]:
-                n=j-i
+    for i in range(len(s)):
+        c=set()
+        c.add(s[i])
+        for j in range(i+1,len(s)):
+            c.add(s[j])
+            if len(c)>2 or '0' in c:
+                t=s[i:j]
+                n=len(t)
                 m=max(m,n)
                 if n not in o:
-                    o[n]=s[i:j+1]
+                    o[n]=t
                 break
     
     return o[m]
 
-print(longest('asdfbyfgiklag'))
-print(longest('asdfaaaabbbbcttavvfffffdf'))
+print(substring('cefageaacceaccacca'))
+print(substring('bbacddddcdd'))
+print(substring('aaaaa'))
