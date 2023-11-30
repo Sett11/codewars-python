1980c1278a0749656b8043c45b6134ff24046b4c
Jhonny <gonzodzen@mail.ru>
2023-11-30 10:53:43 +0300

    Longest Alternating Substring

--- my.py
+++ my.py
@@ -1,23 +1,23 @@
-def substring(s):
-    o={0:''}
-    m=0
-    s+='0'
+def longest_substring(s):
+    e,o='02468','13579'
+    s+='&'
+    r={0:''}
+    m=i=0
 
-    for i in range(len(s)):
-        c=set()
-        c.add(s[i])
-        for j in range(i+1,len(s)):
-            c.add(s[j])
-            if len(c)>2 or '0' in c:
-                t=s[i:j]
-                n=len(t)
-                m=max(m,n)
-                if n not in o:
-                    o[n]=t
-                break
+    while i<len(s)-1:
+        if (s[i] in e and s[i+1] in e) or (s[i] in o and s[i+1] in o) or s[i+1]=='&':
+            t=s[0:i+1]
+            n=len(t)
+            m=max(m,n)
+            if n not in r:
+                r[n]=t
+            s=s[i+1:]
+            i=-1
+        i+=1
+        
     
-    return o[m]
+    return r[m]
 
-print(substring('cefageaacceaccacca'))
-print(substring('bbacddddcdd'))
-print(substring('aaaaa'))
+
+print(longest_substring('225424272163254474441338664823'))
+print(longest_substring('594127169973391692147228678476'))
