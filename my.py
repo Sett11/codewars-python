b90031743a82a5863b93748cfba87f355a4babef
Jhonny <gonzodzen@mail.ru>
2023-12-02 19:09:11 +0300

    The Hunger Games - Foxes and Chickens

--- my.py
+++ my.py
@@ -1,28 +1,27 @@
-def min_path(a,y,x):
-    if [0,0]==[y,x]:
-        return a[0][0]
-    m,n=x+1,y+1
-    r=[[0]*n for _ in range(m)]
-    r[0][0]=a[0][0]
+def hungry_foxes(s):
+    if 'F' not in s or 'C' not in s:
+        return s
+    if '[' not in s and 'F' in s:
+        return s.replace('C','.')
+    
+    g=[list(i) for i in zip([i for i,j in enumerate(s) if j=='['],[i for i,j in enumerate(s) if j==']'])]
+    r=[[0,g[0][0]]]
 
-    for i in range(m):
-        r[i][0]=r[i-1][0]+a[i][0]
+    for i in range(len(g)-1):
+        r.append([g[i][1],g[i+1][0]])
     
-    for i in range(n):
-        r[0][i]=r[0][i-1]+a[0][i]
+    r.append([g[-1][1],len(s)])
+    s=list(s)
+
+    for i,j in g:
+        if 'F' in s[i+1:j]:
+            s[i+1:j]=['.' if j=='C' else j for j in s[i+1:j]]
     
-    for i in range(1,m):
-        for j in range(1,n):
-            r[i][j]=min(r[i-1][j],r[i][j-1])+a[i][j]
-    
-    return r[x][y]
-    
-    
-print(min_path([
-    [1, 2, 3, 6, 2, 8, 1],
-    [4, 8, 2, 4, 3, 1, 9],
-    [1, 5, 3, 7, 9, 3, 1],
-    [4, 9, 2, 1, 6, 9, 5],
-    [7, 6, 8, 4, 7, 2, 6],
-    [2, 1, 6, 2, 4, 8, 7],
-    [8, 4, 3, 9, 2, 5, 8]],4,2))
+    if any('F' in s[i:j] for i,j in r):
+
+        for i,j in r:
+            s[i:j]=['.' if j=='C' else j for j in s[i:j]]
+
+    return ''.join(s)
+
+print(hungry_foxes("C.C[]C.[CC..FCCC.CC.CC..CC.CCC.C.CC.CC]...FC"))
