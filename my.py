587c50dc602bb590c38bb7accb55f52af33bcd89
Jhonny <gonzodzen@mail.ru>
2023-12-02 10:41:41 +0300

    Simple Fun #204: Smallest Integer

--- my.py
+++ my.py
@@ -1,13 +1,9 @@
-from re import sub
-
-def f(a):
-    try:
-        return len(next(i for i in a if len(i)==1))
-    except:
-        return 0
-
-def longest_palindrome(s):
-    a=sub(r'(.)\1*',lambda x: ' '+x.group()+' ',''.join(sorted(sub(r'\W|_','',s).lower()))).split()
-    return len(''.join(map(lambda x:x[1:] if len(x)&1 else x,a)))+f(a)
-
-print(longest_palindrome('e=TA>xCdjpV>?L>/.PQ!as0rJao,zktk7ZrD>Sv/P87J/=BY!rjp<S*M/rqPX.<Jrka,AdH28I?<NSJ0NwYV2QoRGAn.Y!=wOkxdK,J8agox*bFwlmu5AUjRP4KfgFIl0.M9lsogFuI/GrE.PxcO1WwslsDOH5Z2UTyN>Oqm2<d/UvXmRjPv>PYo930>jCUHOLV?GOFP'))
+def smallest_integer(a,i=0,j=0):
+    r=[]
+    for i in a.copy():
+        r.extend(i)
+    r=list(set(r))
+    while j<=max(r)+1:
+        if(not j in r):
+            return j
+        j+=1
