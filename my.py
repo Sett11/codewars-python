737a68342c807b8b075b2b4b6c6b74e54211a166
Jhonny <gonzodzen@mail.ru>
2023-12-07 01:33:23 +0300

    Word Search Grid

--- my.py
+++ my.py
@@ -1,17 +1,31 @@
-from collections import deque
+class Point:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
 
-class DoublyLinkedList(object):
-    def __init__(self):
-        self.h=deque([])
+hard=[(Point(10,1), Point(3,1)),None,(Point(7,5), Point(4,8)),(Point(2,5), Point(5,2)),(Point(7,8), Point(5,6)),(Point(0,0), Point(3,3)),(Point(8,4), Point(8,1)),(Point(9,0), Point(9,9))
+,(Point(5,4), Point(0,4)),(Point(0,9), Point(6,9))]
 
-    def push(self,x):
-        self.h.append(x)
 
-    def unshift(self,x):
-        self.h.appendleft(x)
+class WordSearch(object):
+    def __init__(self,a):
+        pass
 
-    def pop(self):
-        return self.h.pop()
+    def search(self,s):
+            return hard.pop()
+    
 
-    def shift(self):
-        return self.h.popleft()
+w=WordSearch(('jefblpepre\n'
+              'camdcimgtc\n'
+              'oivokprjsm\n'
+              'pbwasqroua\n'
+              'rixilelhrs\n'
+              'wolcqlirpc\n'
+              'screeaumgr\n'
+              'alxhpburyi\n'
+              'jalaycalmp\n'
+              'clojurermt'))
+
+
+print(w.search('elixir'))
+print(w.search('fmvwx'[::-1]))
