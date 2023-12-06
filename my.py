22e84fd4a69ddbfbffdd489f7dbb51c52a09d615
Jhonny <gonzodzen@mail.ru>
2023-12-07 00:00:14 +0300

    Can you get the loop ?

--- my.py
+++ my.py
@@ -1,27 +1,25 @@
-def hungry_foxes(s):
-    if 'F' not in s or 'C' not in s:
-        return s
-    if '[' not in s and 'F' in s:
-        return s.replace('C','.')
-    
-    g=[list(i) for i in zip([i for i,j in enumerate(s) if j=='['],[i for i,j in enumerate(s) if j==']'])]
-    r=[[0,g[0][0]]]
+class Node:
+    def __init__(self,next=None):
+        self.next=next
 
-    for i in range(len(g)-1):
-        r.append([g[i][1],g[i+1][0]])
-    
-    r.append([g[-1][1],len(s)])
-    s=list(s)
+def loop_size(head):
+    a,c=[],set()
+    h=head
+    while h:
+        if h in c:
+            return len(a)-a.index(h)
+        a.append(h)
+        c.add(h)
+        h=h.next
 
-    for i,j in g:
-        if 'F' in s[i+1:j]:
-            s[i+1:j]=['.' if j=='C' else j for j in s[i+1:j]]
-    
-    if any('F' in s[i:j] for i,j in r):
 
-        for i,j in r:
-            s[i:j]=['.' if j=='C' else j for j in s[i:j]]
+node1 = Node()
+node2 = Node()
+node3 = Node()
+node4 = Node()
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node2
 
-    return ''.join(s)
-
-print(hungry_foxes("C.C[]C.[CC..FCCC.CC.CC..CC.CCC.C.CC.CC]...FC"))
+print(loop_size(node1))
