a051538166d798fd18d89e93fa7e9fb328db8e91
Jhonny <gonzodzen@mail.ru>
2023-12-08 00:38:39 +0300

    SHA-256 Cracker

--- my.py
+++ my.py
@@ -1,31 +1,10 @@
-class Point:
-    def __init__(self,x,y):
-        self.x=x
-        self.y=y
+from hashlib import sha256
+from itertools import permutations
 
-hard=[(Point(10,1), Point(3,1)),None,(Point(7,5), Point(4,8)),(Point(2,5), Point(5,2)),(Point(7,8), Point(5,6)),(Point(0,0), Point(3,3)),(Point(8,4), Point(8,1)),(Point(9,0), Point(9,9))
-,(Point(5,4), Point(0,4)),(Point(0,9), Point(6,9))]
-
-
-class WordSearch(object):
-    def __init__(self,a):
+def sha256_cracker(h,s):
+    try:
+        return next(j for j in [''.join(i) for i in permutations(s)] if sha256(bytes(j,encoding='utf-8')).hexdigest()==h)
+    except:
         pass
 
-    def search(self,s):
-            return hard.pop()
-    
-
-w=WordSearch(('jefblpepre\n'
-              'camdcimgtc\n'
-              'oivokprjsm\n'
-              'pbwasqroua\n'
-              'rixilelhrs\n'
-              'wolcqlirpc\n'
-              'screeaumgr\n'
-              'alxhpburyi\n'
-              'jalaycalmp\n'
-              'clojurermt'))
-
-
-print(w.search('elixir'))
-print(w.search('fmvwx'[::-1]))
+print(sha256_cracker('5694d08a2e53ffcae0c3103e5ad6f6076abd960eb1f8a56577040bc1028f702b','cdeo'))
