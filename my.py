39a3b78a778a6433f137b187b35e29817aadc471
Jhonny <gonzodzen@mail.ru>
2023-12-08 22:36:08 +0300

    Simple Fun #159: Middle Permutation

--- my.py
+++ my.py
@@ -1,12 +1,5 @@
-from datetime import datetime
-from hashlib import md5
+def middle_permutation(s):
+    n,s=len(s),sorted(s)
+    return s.pop(n//2-1)+''.join(s[::-1]) if n%2==0 else s.pop(n//2)+middle_permutation(s)
 
-def geohash(dow,date=datetime.now()):
-    s=str(dow)
-    if len(s.split('.')[1])<2:
-        s+='0'
-    h=md5(bytes(date.strftime('%Y-%m-%d-')+s,encoding='utf-8')).hexdigest()
-    return [round(float.fromhex('0.'+h[0:16]),6),round(float.fromhex('0.'+h[16:]),6)]
-
-
-print(geohash(10458.68, datetime(2005,5,26)))
+print(middle_permutation('gbmaxjztldn'))
