8c6676f01cb437eda2dce4f78d0ee7a2c130fa73
Jhonny <gonzodzen@mail.ru>
2023-12-09 00:58:51 +0300

    Generating Numbers From Prime Factors I

--- my.py
+++ my.py
@@ -1,14 +1,14 @@
-from math import ceil
+def count_find_num(a,n):
+    l=len(a)
+    if l==2:
+        r=sorted(a[0]**i*a[1]**j for i in range(1,16) for j in range(1,16))
+    if l==3:
+        r=sorted(a[0]**i*a[1]**j*a[2]**k for i in range(1,16) for j in range(1,16) for k in range(1,16))
+    if l==4:
+        r=sorted(a[0]**i*a[1]**j*a[2]**k*a[3]**v for i in range(1,16) for j in range(1,16) for k in range(1,16) for v in range(1,16))
+    if l==5:
+        r=sorted(a[0]**i*a[1]**j*a[2]**k*a[3]**v*a[4]**c for i in range(1,16) for j in range(1,16) for k in range(1,16) for v in range(1,16) for c in range(1,16))
+    r=list(filter(lambda x:x<=n,r))
+    return [len(r),r[-1]] if r else []
 
-def generate_primes(n):
-    if n<2:
-        return []
-    n+=1
-    r=[True]*n
-    r[0]=r[1]=False
-    for i in range(2,int(n**.5+1)):
-        if r[i]:
-            r[2*i:n:i]=[False]*ceil((n/i)-2)
-    return [i for i,j in enumerate(r) if j]
-
-print(generate_primes(47))
+print(count_find_num([2, 29, 37, 73],6850316))
