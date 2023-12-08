bae170bde239551ec0dabb018978a25a4434e489
Jhonny <gonzodzen@mail.ru>
2023-12-09 00:08:18 +0300

    Missing number in Unordered Arithmetic Progression

--- my.py
+++ my.py
@@ -1,5 +1,13 @@
-def middle_permutation(s):
-    n,s=len(s),sorted(s)
-    return s.pop(n//2-1)+''.join(s[::-1]) if n%2==0 else s.pop(n//2)+middle_permutation(s)
+def find(a):
+    a=sorted(a)
+    r=[a[i+1]-a[i] for i in range(len(a)-1)]
 
-print(middle_permutation('gbmaxjztldn'))
+    for i in range(len(r)-1):
+        if r[i]!=r[i+1]:
+            return a[i+1]+(r[i+1]-r[i])
+    
+    return a[0]
+
+print(find([3, 9, 1, 11, 13, 5]))
+print(find([5, -1, 0, 3, 4, -3, 2, -2]))
+print(find([1,1,1,1,1,1,1]))
