a725a6b5df31c964d2b38f31b51643fff0f4a001
Jhonny <gonzodzen@mail.ru>
2023-12-08 21:15:22 +0300

    Rotate a square matrix like a vortex

--- my.py
+++ my.py
@@ -1,10 +1,43 @@
-from functools import reduce
-from operator import mul
+from copy import deepcopy
 
-def find_us(n1, n2, k, pf, d):
-    return [i for i in range(reduce(mul,pf),n1+k*n2,max(pf)) if all(i%j==0 for j in pf) and all(str(j) in str(i) for j in d)]
+def f(a):
+    return list([list(j) for j in zip(*[i for i in a])])[::-1]
+
+def rotate_like_a_vortex(matrix):
+    a=deepcopy(matrix)
+    n,k,i,x=len(a),1,0,['&']*len(a)
+    
+    while n>i:
+        v=k
+        while v:
+            r=f([j[i:n] for j in a][i:n])
+            while v>1:
+                r=f(r)
+                v-=1
+            v-=1
+        while len(r)!=len(a):
+            for h in range(len(r)):
+                r[h]=['&']+r[h]+['&']
+            r.insert(0,x)
+            r.append(x)
+        for c in range(len(r)):
+            n-=1
+            if r[c][i]!='&':a[c][i]=r[c][i]
+            if r[c][n]!='&':a[c][n]=r[c][n]
+            if r[i][c]!='&':a[i][c]=r[i][c]
+            if r[n][c]!='&':a[n][c]=r[n][c]
+            n+=1
+        n-=1
+        i+=1
+        k+=1
+
+    return a
+    
+    
 
 
-print(find_us(30, 90, 4, [2, 3], [6, 2]))
-print(find_us(30, 400, 18, [2, 3, 7], [6, 2, 5]))
-print(find_us(230,7059,435,[2, 17, 7, 29],[5, 3, 2, 6]))
+print(rotate_like_a_vortex([
+                   [ 5, 3, 6, 1 ],
+                   [ 5, 8, 7, 4 ],
+                   [ 1, 2, 4, 3 ],
+                   [ 3, 1, 2, 2 ] ]))
