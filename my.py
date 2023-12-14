0bff9fdb0ade7cbabc6dfc90b5dc2164dfce61e0
Jhonny <gonzodzen@mail.ru>
2023-12-14 19:49:56 +0300

    Simple Fun #337: The Original Number

--- my.py
+++ my.py
@@ -1,14 +1,16 @@
-def count_find_num(a,n):
-    l=len(a)
-    if l==2:
-        r=sorted(a[0]**i*a[1]**j for i in range(1,16) for j in range(1,16))
-    if l==3:
-        r=sorted(a[0]**i*a[1]**j*a[2]**k for i in range(1,16) for j in range(1,16) for k in range(1,16))
-    if l==4:
-        r=sorted(a[0]**i*a[1]**j*a[2]**k*a[3]**v for i in range(1,16) for j in range(1,16) for k in range(1,16) for v in range(1,16))
-    if l==5:
-        r=sorted(a[0]**i*a[1]**j*a[2]**k*a[3]**v*a[4]**c for i in range(1,16) for j in range(1,16) for k in range(1,16) for v in range(1,16) for c in range(1,16))
-    r=list(filter(lambda x:x<=n,r))
-    return [len(r),r[-1]] if r else []
+from collections import Counter
+from bisect import insort
 
-print(count_find_num([2, 29, 37, 73],6850316))
+def original_number(s):
+        o=Counter(s.lower())
+        d={'w': ('two', '2'), 'u': ('four', '4'), 'x': ('six', '6'), 'f': ('five', '5'), 'z': ('zero', '0'), 'r': ('three', '3'), 't': ('eight', '8'), 's': ('seven', '7'), 'i': ('nine', '9'), 'n': ('one', '1')}
+        r=[]
+        for i,(j,k) in d.items():
+            c=o[i]
+            insort(r,k*c)
+            for h in j:
+                o[h]-=c
+        return ''.join(r)
+
+
+print(original_number('TTONWOHREEE'))
