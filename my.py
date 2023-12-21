a673cd182f433fe25b6b7940846725a53949ca74
Jhonny <gonzodzen@mail.ru>
2023-12-22 01:29:38 +0300

    Not all but sometimes all

--- my.py
+++ my.py
@@ -1,14 +1,6 @@
-from itertools import product
+def remove(s,d):
+    for i in d:
+        s=s.replace(i,'',d[i])
+    return s
 
-
-def proc_seq(*a):
-    r=set([int(''.join([str(k) for k in j])) for j in product(*[tuple(map(int,str(i))) for i in a]) if j[0]])
-    q=[]
-    b,c,d,e=len(r),min(r),max(r),sum(r)
-    if b not in q:q.append(b)
-    if c not in q:q.append(c)
-    if d not in q:q.append(d)
-    if e not in q:q.append(e)
-    return q
-
-print(proc_seq(22,22,22,22))
+print(remove('this is a string',{'t':1, 'i':2}))
