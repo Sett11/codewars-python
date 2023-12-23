e43b9970d1e2c83704b913ab6f0443c3a9b4f7ab
Jhonny <gonzodzen@mail.ru>
2023-12-23 14:55:58 +0300

    The latest clock

--- my.py
+++ my.py
@@ -1,13 +1,8 @@
-def put_the_cat_on_the_table(c,a):
-    x,y=c
-    if x<0 or x>=len(a) or y<0 or y>=len(a[0]):
-        return 'NoCat'
-    z=max([[(i,j) for j,k in enumerate(r) if a[i][j]] for i,r in enumerate(a)])
-    if not z:
-        return 'NoTable'
-    d,e=z[0]
-    return 'D'*(d-x)+'U'*(x-d)+'R'*(e-y)+'L'*(y-e)
-        
-    
+from itertools import permutations as p
 
-print(put_the_cat_on_the_table([0,1],[[0,0,0], [0,0,0], [0,0,1]]))
+def late_clock(a,b,c,d):
+    r=max(filter(lambda z:int(z[:2])<24 and int(z[2:])<60,map(lambda x:''.join(map(lambda y:str(y),x)),p([a,b,c,d]))))
+    return f'{r[:2]}:{r[2:]}'
+
+
+print(late_clock(9,1,2,5))
