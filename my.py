0036b8e55ba902265bed312a80d13988835fb896
Jhonny <gonzodzen@mail.ru>
2023-12-24 17:57:34 +0300

    Find the most adjacent integers of the same value in a grid

--- my.py
+++ my.py
@@ -1,8 +1,40 @@
-from itertools import permutations as p
+def find_most_adjacent(a):
+    n,s=len(a),set()
+    c,r=[(i,j) for i in range(n) for j in range(n)],set()
+    def dfs(i,j,p,k):
+        if (i,j) in s or i<0 or i>=n or j<0 or j>=n or a[i][j]!=k:
+            r.add((len(p),k))
+            return
+        p.append(1)
+        s.add((i,j))
+        dfs(i+1,j,p,k)
+        dfs(i,j+1,p,k)
+        dfs(i-1,j,p,k)
+        dfs(i,j-1,p,k)
+    for i,j in c:
+        dfs(i,j,[],a[i][j])
+    return sorted(r,key=lambda x:(x[0],-x[1]))[-1][::-1]
+            
 
-def late_clock(a,b,c,d):
-    r=max(filter(lambda z:int(z[:2])<24 and int(z[2:])<60,map(lambda x:''.join(map(lambda y:str(y),x)),p([a,b,c,d]))))
-    return f'{r[:2]}:{r[2:]}'
 
-
-print(late_clock(9,1,2,5))
+print(find_most_adjacent([
+	[1,0,1],
+	[1,1,0],
+	[0,0,2]
+]))
+print(find_most_adjacent([
+    [1,2,1],
+    [1,1,0],
+    [0,0,0],
+]))
+print(find_most_adjacent([
+	[2,2,1],
+	[2,2,1],
+	[3,1,1]
+]))
+print(find_most_adjacent([
+	[1,0,1,0],
+	[1,1,0,0],
+	[0,0,0,0],
+	[1,1,0,0]
+]))
