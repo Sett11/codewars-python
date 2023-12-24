f2accd40490efb26472109a39e4c7e4d6d5597af
Jhonny <gonzodzen@mail.ru>
2023-12-24 19:05:40 +0300

    Minimum and Maximum Product of k Elements

--- my.py
+++ my.py
@@ -1,13 +1,10 @@
-def find_spec_partition(n,k,c):
-    if c=='min':
-        return [n-(k-1)]+[1]*(k-1)
-    a,i=[n//k]*k,0
-    while sum(a)<n:
-        a[i]+=1
-        i+=1
-    return a
-    
+from itertools import combinations as c
+from functools import reduce as r
+from operator import mul as m
 
-print(find_spec_partition(10,4,'max'))
-print(find_spec_partition(13,6,'max'))
-print(find_spec_partition(10,4,'min'))
+def find_min_max_product(a,k):
+    if len(a)>=k:
+        x=sorted(r(m,i) for i in c(a,k))
+        return x[0],x[-1]
+
+print(find_min_max_product([1, -2, -3, 4, 6, 7],3))
