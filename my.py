b1b4ca6c49cff0d780f6bdd9e3787cc3af1f7b83
Jhonny <gonzodzen@mail.ru>
2023-12-25 00:01:13 +0300

    Find Numbers with Same Amount of Divisors

--- my.py
+++ my.py
@@ -1,10 +1,20 @@
-from itertools import combinations as c
-from functools import reduce as r
-from operator import mul as m
+def f(n):
+    a=[1,n]
+    for i in range(2,int(n**.5+1)):
+        if n%i==0:
+            a.extend([i,n//i])
+    return len(set(a))
+    
 
-def find_min_max_product(a,k):
-    if len(a)>=k:
-        x=sorted(r(m,i) for i in c(a,k))
-        return x[0],x[-1]
+a=[(i,f(i)) for i in range(1,20000)]
 
-print(find_min_max_product([1, -2, -3, 4, 6, 7],3))
+def count_pairs_int(d,n):
+    c=0
+    for i in range(n):
+        if a[i][1]==a[i+d][1] and a[i+d][0]<n:
+            c+=1
+    return c
+
+print(count_pairs_int(9,4000))
+print(count_pairs_int(1,50))
+print(count_pairs_int(7,2500))
