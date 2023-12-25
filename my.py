4e9b25d40083d591742b7015068115a6b512f77f
Jhonny <gonzodzen@mail.ru>
2023-12-25 18:30:48 +0300

    Lexicographic Permutations

--- my.py
+++ my.py
@@ -1,6 +1,17 @@
-def min_dot(a,b):
-    a,b=sorted(a,reverse=True),sorted(b)
-    return sum([a[i]*b[i] for i in range(len(a))])
+from functools import lru_cache
 
-print(min_dot([1,2,3,4,5],[0,1,1,1,0]))
-print(min_dot([1,2,3,4,5], [0,0,1,1,-4]))
+@lru_cache
+def perm(a):
+    n=len(a)
+    if n<2:
+        return a
+    t,p,r=perm(a[1:]),a[0],[]
+    for i in t:
+        for j in range(n):
+            r+=[i[0:j]+p+i[j:]]
+    return r
+
+def nth_perm(n,d):
+    return sorted(perm(''.join(str(i) for i in range(d))))[n-1]
+
+print(nth_perm(12,5))
