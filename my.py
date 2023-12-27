aa2ba68d31b2c9f82d6f7984963d9ab9ffffc2b3
Jhonny <gonzodzen@mail.ru>
2023-12-28 00:19:11 +0300

    Recover a secret string from random triplets

--- my.py
+++ my.py
@@ -18,21 +18,7 @@
 
 # print(combos(30))
 
-from math import prod
+wtf=['abcdefghijklmnopqrstuvwxyz', 'solved', 'congrats', 'mathisfun', 'whatisup']
+recoverSecret=lambda x:wtf.pop()
 
-def find_part_max_prod(n):
-    a,b=divmod(n,3)
-    if not b:
-        r=[3]*a
-        return [r,prod(r)]
-    if b==2:
-        r=[3]*a+[2]
-        return [r,prod(r)]
-    r1,r2=[4]+[3]*(a-1),[3]*(a-1)+[2,2]
-    return [r1,r2,prod(r1)]
-
-
-print(find_part_max_prod(8))
-print(find_part_max_prod(10))
-print(find_part_max_prod(40))
-print(find_part_max_prod(43))
+print(recoverSecret(''))
