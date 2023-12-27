739c2c7673f172f45c532197280b79639001f0c8
Jhonny <gonzodzen@mail.ru>
2023-12-27 19:50:29 +0300

    Cycle a list of values

--- my.py
+++ my.py
@@ -1,16 +1,8 @@
-def finaldist_crazyrobot(a,p):
-    x,y=p
-    for i,j in a:
-        i=i.lower()
-        if i=='r':
-            x+=j
-        elif i=='l':
-            x-=j
-        elif i=='u':
-            y+=j
-        else:
-            y-=j
-    return ((x-p[0])**2+(y-p[1])**2)**.5
+def cycle(d,v,c):
+    v+=v
+    try:
+        return next(v[i+1] if d>0 else v[i-1] for i in range(len(v)) if v[i]==c)
+    except:
+        pass
 
-
-print(finaldist_crazyrobot([('R', 2), ('U', 3), ('L', 1), ('D', 6)],(0,0)))
+print(cycle(1,[*range(0,70,3)],69))
