706412a3a419c39b8eb04d77505bd04be8aabccd
Jhonny <gonzodzen@mail.ru>
2023-12-27 22:39:21 +0300

    Find all possible number combos that sum to a number

--- my.py
+++ my.py
@@ -1,8 +1,19 @@
-def cycle(d,v,c):
-    v+=v
-    try:
-        return next(v[i+1] if d>0 else v[i-1] for i in range(len(v)) if v[i]==c)
-    except:
-        pass
+def f(a,j,n,r,q=[]):
+    if(r<0):
+        return
+    if(r==0):
+        t=[]
+        for i in range(j):
+            t.append(a[i])
+        q.append(t)
+        return
+    p=1 if j==0 else a[j-1]
+    for k in range(p,n+1):
+        a[j]=k
+        f(a,j+1,n,r-k,q)
+    return q
 
-print(cycle(1,[*range(0,70,3)],69))
+def combos(n):
+    return f([0]*n,0,n,n,[])
+
+print(combos(40))
