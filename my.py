cb7bfeb0167be68b1bf3f1834cae559b7b3b8dbd
Jhonny <gonzodzen@mail.ru>
2023-12-31 14:31:22 +0300

    Madhav array

--- my.py
+++ my.py
@@ -1,1 +1,15 @@
-print('Fack...') 
+def is_madhav_array(a):
+    if len(a)<2:
+        return False
+    i,r=1,[]
+    while a:
+        t=a[0:i]
+        if len(t)!=i:
+            return False
+        r.append(sum(t))
+        a=a[i:]
+        i+=1
+    return len(set(r))==1
+
+
+print(is_madhav_array([6,2,4,2,2,2,1,5,0,0]))
