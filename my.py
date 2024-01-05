bf29b2a62d2849f086b0632b5c2281de3c294b8c
Jhonny <gonzodzen@mail.ru>
2024-01-05 09:50:35 +0300

    Binary Tree Traversal

--- my.py
+++ my.py
@@ -1,4 +1,50 @@
-from itertools import product
+class Node:
+    def __init__(self,data=None,left=None,right=None):
+        self.data=data
+        self.left=left
+        self.right=right
 
-def furthest_distance(a):
-    return max(map(lambda x:round(((x[1][0]-x[0][0])**2+(x[0][1]-x[1][1])**2)**.5,2),product(a,repeat=2)))
+a = Node("A")
+b = Node("B")
+c = Node("C")
+  
+a.left = b
+a.right = c
+
+
+def pre_order(n):
+    r=[]
+    def f(h):
+        if not h:
+            return
+        r.append(h.data)
+        f(h.left)
+        f(h.right)
+    f(n)
+    return r
+
+def in_order(n):
+    r=[]
+    def f(h):
+        if not h:
+            return
+        f(h.left)
+        r.append(h.data)
+        f(h.right)
+    f(n)
+    return r
+
+def post_order(n):
+    r=[]
+    def f(h):
+        if not h:
+            return
+        f(h.left)
+        f(h.right)
+        r.append(h.data)
+    f(n)
+    return r
+
+print(pre_order(a))
+print(in_order(a))
+print(post_order(a))
