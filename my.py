2c22f53999f58345a51885c25ba532b5bc6a119e
Jhonny <gonzodzen@mail.ru>
2024-01-06 17:41:54 +0300

    LCM Cardinality

--- my.py
+++ my.py
@@ -1,12 +1,17 @@
-def numericals(s):
-    d,r={},''
-    for i in s:
-        if i not in d:
-            d[i]=1
-            r+='1'
-        else:
-            d[i]+=1
-            r+=str(d[i])
-    return r
+from math import lcm
+from itertools import product
 
-print(numericals("Hello, World! It's me, JomoPipi!"))
+def d(n):
+    s=set()
+    for i in range(1,int(n**.5+1)):
+        if n%i==0:
+            s.update([i,n//i])
+    return s
+
+def lcm_cardinality(n):
+    a=d(n)
+    return len(set(map(lambda e:tuple(sorted(e)),filter(lambda x:lcm(*x)==n and len(set(x))==2,product(a,repeat=2)))))+1
+
+print(lcm_cardinality(30858025))
+print(lcm_cardinality(9801))
+print(lcm_cardinality(1251562))
