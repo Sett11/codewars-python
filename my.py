c7196019fbc4bfc3f8ce5281da7075465352bca0
Jhonny <gonzodzen@mail.ru>
2024-01-09 00:41:17 +0300

    Cat Kata, Part 1

--- my.py
+++ my.py
@@ -1,15 +1,15 @@
-def logistic_map(m,n,x,y):
-    r,z=[[None]*m for _ in range(n)],list(zip(y,x))
-    if not z:
-        return r
-    for i,j in z:
-        r[i][j]=0
-    for i in range(n):
-        for j in range(m):
-            r[i][j]=min((abs(p-i)+abs(t-j)) for p,t in z)
-    return r
+from itertools import combinations
 
-print(logistic_map(5,2,[0,4],[0,0]))
+def peaceful_yard(a,k):
+    for i,j in list(combinations(sum([[(i,j) for j,p in enumerate(r) if a[i][j]!='-'] for i,r in enumerate(a)],[]),2)):
+        if (abs(j[1]-i[1])**2+abs(j[0]-i[0])**2)**.5<k:
+            return False
+    return True
 
-[[0, 1, 2, 1, 0],
- [1, 2, 1, 0, 1]]
+print(peaceful_yard([
+        "------------", 
+        "--L-------R-", 
+        "----M-------", 
+        "------------", 
+        "------------", 
+        "------------"], 6))
