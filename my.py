1f13a6c0b3db45f90af78c121d5338fc56cda344
Jhonny <gonzodzen@mail.ru>
2024-01-11 23:07:25 +0300

    Shortest Knight Path

--- my.py
+++ my.py
@@ -1,17 +1,45 @@
-def f(a,b):
-    return True if a==b or not b else False if int(a)>int(b) or a not in b or b.index(a)!=0 else f(str(int(a)+1),b[len(a):])
+from collections import deque
 
-def find(s):
-    i=1
-    while i<len(s):
-        t,p=str(int(s[:i])+1),s[i:]
-        if f(t,p):
-            return int(t)-1
-        i+=1
-    return int(s)
+def knight(x,y):
+    n,l='12345678','abcdefgh'
+    g={i+j:set() for i in l for j in n}
+    f=lambda a,b:g[a].add(b)
+    for i in range(8):
+        for j in range(8):
+            v1=l[i]+n[j]
+            if 0<=i+2<8 and 0<=j+1<8:
+                v2=l[i+2]+n[j+1]
+                f(v1,v2)
+                f(v2,v1)
+            if 0<=i-2<8 and 0<=j+1<8:
+                v2=l[i-2]+n[j+1]
+                f(v1,v2)
+                f(v2,v1)
+            if 0<=i+1<8 and 0<=j+2<8:
+                v2=l[i+1]+n[j+2]
+                f(v1,v2)
+                f(v2,v1)
+            if 0<=i-1<8 and 0<=j+2<8:
+                v2=l[i-1]+n[j+2]
+                f(v1,v2)
+                f(v2,v1)
+    d={i:None for i in g}
+    p=d.copy()
+    d[x]=0
+    q=deque([x])
+    while q:
+        v=q.popleft()
+        for i in g[v]:
+            if d[i] is None:
+                d[i]=d[v]+1
+                p[i]=v
+                q.append(i)
+    r=[y]
+    par=p[y]
+    while not par is None:
+        r.append(par)
+        par=p[par]
+    return len(r)-1
 
-print(find('123456789101112131415'))
-print(find('17181920'))
-print(find('99100'))
-print(find('127151127152127153127154127155127156127157'))
-print(find('577495'))
+
+print(knight('a1','f3'))
