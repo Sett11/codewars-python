ab3b2eda060402f3277aaaefcc9843801bb2a214
Jhonny <gonzodzen@mail.ru>
2024-01-13 19:31:50 +0300

    Dynamic Connectivity

--- my.py
+++ my.py
@@ -1,17 +1,41 @@
-from functools import cache
+from collections import deque
 
-@cache
-def values(n):
-    c,k=set(),int(n**.5+1)
-    for i in range(1,k):
-        t=[i**2]
-        for j in range(i+1,k):
-            t.append(j**2)
-            x=str(sum(t))
-            if int(x)>=n:
-                break
-            if x==x[::-1]:
-                c.add(x)
-    return len(c)
+class DynamicConnectivity(object):
+    def __init__(self,n):
+        self.d={i:set() for i in range(n)}
 
-print(values(1000000))
+    def union(self,p,q):
+        self.d[p].add(q)
+        self.d[q].add(p)
+    
+    def connected(self,x,y):
+        g=self.d
+        d={i:None for i in g}
+        p=d.copy()
+        d[x]=0
+        q=deque([x])
+        while q:
+            v=q.popleft()
+            for i in g[v]:
+                if d[i] is None:
+                    d[i]=d[v]+1
+                    p[i]=v
+                    q.append(i)
+        r=[]
+        par=p[y]
+        while not par is None:
+            r.append(par)
+            par=p[par]
+        return bool(r[::-1])
+
+
+results1=DynamicConnectivity(10)
+
+results1.union(4,3)
+results1.union(3,8)
+results1.union(6,5)
+results1.union(9,4)
+results1.union(2,1)
+
+print(results1.connected(0,7))
+print(results1.connected(8,9))
