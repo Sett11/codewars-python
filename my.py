75d24954fb8fbef532ee91d24ebccdd1f33d1582
Jhonny <gonzodzen@mail.ru>
2024-01-15 01:08:32 +0300

    Disgruntled Employee

--- my.py
+++ my.py
@@ -1,17 +1,12 @@
-def automata(q,a,k):
-    w={(tuple([tuple([1, 0, 0, 0, 0])]), tuple([0, 0, 1, 0, 0]), 2):[0, 1, 0, 0, 0],(tuple([tuple([0, 0, 1, 0, 0])]), tuple([0, 1, 0]), 2):[0, 1, 0],(tuple([tuple([1])]), tuple([0, 1, 0, 0, 0, 1]), 20):[0, 1, 0, 0, 0, 1],(tuple([tuple([0])]), tuple([0, 1, 0, 0, 0, 1]), 3):[1, 0, 1, 1, 1, 0]}
-    if (tuple([tuple(q[0])]),tuple(a),k) in w:
-        return w[(tuple([tuple(q[0])]),tuple(a),k)]
-    if k==0:
-        return a
-    n,m,r=len(a),len(q[0]),set()
-    a+=a
-    for i in range(n):
-        if a[i:i+m] in q:
-            r.add(i+1)
-    a=[0]*n
-    for i in r:
-        a[i%n]=1
-    return automata(q,a,k-1)
+def off(n):
+    n+=1
+    a,j=[False]*(n),1
+    while n:
+        for i in range(0,len(a),j):
+            a[i]=not a[i]
+        j+=1
+        n-=1
+    return [i+1 for i,j in enumerate(a[1:]) if j]
 
-print(automata([[1,0,0], [0,1,1], [0,1,0], [0,0,1]], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], 15))
+
+print(off(9))
