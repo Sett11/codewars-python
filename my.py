6ce431399f23e52863fcf773b100b2ec48f06964
Jhonny <gonzodzen@mail.ru>
2024-01-16 00:40:23 +0300

    Keep it short (with restrictions)

--- my.py
+++ my.py
@@ -1,12 +1,3 @@
-def off(n):
-    n+=1
-    a,j=[False]*(n),1
-    while n:
-        for i in range(0,len(a),j):
-            a[i]=not a[i]
-        j+=1
-        n-=1
-    return [i+1 for i,j in enumerate(a[1:]) if j]
+short=lambda x:{i:sum(map(list,x),[]).count(i) for i in set(sum(map(list,x),[]))}
 
-
-print(off(9))
+print(short([{1,2,3},{2,3,4}]))
