0e169826f365d34ceea55ba6f4dd4707b596661d
Jhonny <gonzodzen@mail.ru>
2024-01-20 00:41:44 +0300

    Digitize

--- my.py
+++ my.py
@@ -1,15 +1,4 @@
-def counter():
-    count=0
-    def f():
-        nonlocal count
-        count+=1
-        return count
-    return f
+def digitize(n):
+    return list(map(int,str(n)))
 
-counter_one = counter()
-counter_two = counter()
-
-print(counter_one())
-print(counter_one())
-print(counter_two())
-print(counter_two())
+print(digitize(123))
