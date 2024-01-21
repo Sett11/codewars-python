f807ad7aa1b371cf3e7e678f6a94f93a5a97b0fb
Jhonny <gonzodzen@mail.ru>
2024-01-21 18:15:14 +0300

    Fibonacci Rabbits

--- my.py
+++ my.py
@@ -1,11 +1,10 @@
-def rotate_paper(s):
-    if any(i in s for i in '1347') or len(s)==1 and s in '69':
-        return False
-    n=len(s)//2
-    for i in range(n):
-        if (s[i] in '0258' and s[-i-1]!=s[i]) or (s[i] in '69' and (s[-i-1]==s[i] or s[-i-1] not in '69')):
-            return False
-    return True
+def fib_rabbits(n,k):
+    a,b=k,1
+    while n>2:
+        c=a
+        a=b*k
+        b+=c
+        n-=1
+    return b if n else 0
 
-
-print(rotate_paper('655'))
+print(fib_rabbits(8,12))
