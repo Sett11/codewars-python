aabc0533d8f60002b6e9e2d96b3b1f6c98c47d19
Jhonny <gonzodzen@mail.ru>
2024-01-26 17:36:12 +0300

    Simple Sequence Validator

--- my.py
+++ my.py
@@ -1,6 +1,4 @@
-f=lambda n:n>=2 and all(n%i for i in range(2,int(n**.5)+1))
+def validate_sequence(a):
+    return len(set([a[i]-a[i-1] for i in range(1,len(a))]))==1
 
-def number_property(n):
-    return [f(n),n%2==0,n%10==0]
-
-print(number_property(122))
+print(validate_sequence([0,2,4,6,8]))
