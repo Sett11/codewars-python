37412d6fb6f5daa9b09c3fd8beb8bab4fd1286f4
Jhonny <gonzodzen@mail.ru>
2024-01-26 16:04:05 +0300

    Radio DJ helper function

--- my.py
+++ my.py
@@ -1,4 +1,15 @@
-def gc_content(s):
-    return round((s.count('C')+s.count('G'))/len(s)*100,2) if s else 0.0
+songs=[{'artist': 'Marillion', 'title': 'Keyleigh', 'playback': '03:36'}, {'artist': 'Pink Floyd', 'title': 'Time', 'playback': '06:48'}, {'artist': 'Rush', 'title': 'YYZ', 'playback': '04:27'}, {'artist': 'Bonobo', 'title': 'Days To Come', 'playback': '03:50'}, {'artist': 'Coldplay', 'title': 'Yellow', 'playback': '04:32'}, {'artist': 'Bloc Party', 'title': 'Like Eating Glass', 'playback': '04:22'}, {'artist': 'The Killers', 'title': 'For Reasons Unknown', 'playback': '03:30'}, {'artist': 'Arctic Monkeys', 'title': 'Teddy Picker', 'playback': '03:25'}, {'artist': 'Joe Satriani', 'title': 'Surfing With The Alien', 'playback': '04:34'}]
 
-print(gc_content('AAATTTCCCGGG'))
+def longest_possible(n):
+    r=[]
+    for i in songs:
+        a,b=i['playback'].split(':')
+        k=int(a)*60+int(b)
+        if k<=n:
+            r.append(i)
+    r=sorted(r,key=lambda x:x['playback'],reverse=True)
+    return bool(r) and r[0]['title']
+
+print(longest_possible(215))
+print(longest_possible(270))
+print(longest_possible(15))
