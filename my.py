b9295213bbb5e49d85ee37eec9975e10ebf850db
Jhonny <gonzodzen@mail.ru>
2024-01-26 17:29:02 +0300

    Simple Maths Test

--- my.py
+++ my.py
@@ -1,4 +1,6 @@
-def pass_the_bill(a,b,c):
-    return -1 if c*2>=a else 0 if b*2>a else a//2+1-b
+f=lambda n:n>=2 and all(n%i for i in range(2,int(n**.5)+1))
 
-print(pass_the_bill(16,8,7))
+def number_property(n):
+    return [f(n),n%2==0,n%10==0]
+
+print(number_property(122))
