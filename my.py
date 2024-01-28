de4072bfa2e34328afbd0728ad8447dcbc95720c
Jhonny <gonzodzen@mail.ru>
2024-01-29 00:52:57 +0300

    Simple Fun #296: Round And Round

--- my.py
+++ my.py
@@ -1,12 +1,19 @@
-def rounding(n,m):
-    a,_=divmod(n,m)
-    if m-n<n and m>n:
-        return m
-    return n if (m%n==0 or n%m==0) and n>1 else m*a+(m if m*a+m-n<abs(m*a-n) else 0) if a else 0
+def round_and_round(n,a,b):
+    r=list(range(1,n+1))
+    if b>0:
+        i=a-1
+        while b:
+            i=(i+1)%n
+            b-=1
+        return r[i]
+    else:
+        i=a-1
+        while b:
+            i=(i-1)
+            if i<0:
+                i=n-1
+            b+=1
+        return r[i]
 
-print(rounding(20,3))
-print(rounding(19,3))
-print(rounding(50,100))
-print(rounding(154,4))
-print(rounding(851,46))
-print(rounding(497,536))
+print(round_and_round(5,1,3))
+print(round_and_round(6,2,-5))
