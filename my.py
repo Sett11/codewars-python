b63a045cc756be19b0401bfe2d091640dc48d85b
Jhonny <gonzodzen@mail.ru>
2024-01-28 22:02:19 +0300

    Simple number sequence

--- my.py
+++ my.py
@@ -1,16 +1,25 @@
-def longest_substring(s):
-    c,n=0,len(s)
-    for i in range(n):
-        z=set()
-        for j in range(i,n):
-            if s[j] not in z:
-                z.add(s[j])
-            else:
-                break
-        c=max(c,len(z))
-    return c
-    
+def f(n):
+    return [str(i) for i in range(n,n+15)]
 
-print(longest_substring("abcd"))
-print(longest_substring("hchzvfrkmlnozjk"))
-print(longest_substring("abcd" * 10000 + "abcde" + "abcd" * 10000))
+def missing(s):
+    r=[]
+    for i in range(1,7):
+        n=int(s[:i])
+        r.append([n,f(n)])
+    for _,a in r:
+        q=[]
+        for i in range(len(a)):
+            q.append(a[i])
+            if ''.join(q)==s:
+                return -1
+            t=a.copy()
+            p=int(t.pop(i))
+            t=''.join(t)[:len(s)]
+            if t==s:
+                return p
+    return -1
+            
+print(missing('599600601602'))
+print(missing('899091939495'))
+print(missing('9899101102'))
+print(missing('99991000110002'))
