1d8607836be10b2f0d44ad444e06f122727a15d0
Jhonny <gonzodzen@mail.ru>
2024-01-28 17:14:10 +0300

    Three sticks

--- my.py
+++ my.py
@@ -1,9 +1,5 @@
-def sea_sick(s):
-    s+='&'
-    n,c=len(s),-1
-    for i in range(n-1):
-        if s[i]!=s[i+1]:
-            c+=1
-    return ["No Problem","Throw Up"][c>n//5]
+def maxlen(a,b):
+    c,d=max(a,b),min(a,b)
+    return c/3 if c>=d*3 else c/2 if d>=c/2 else d
 
-print(sea_sick("_~~~~~~~_~__~______~~__~~_~~"))
+print(maxlen(5,17))
