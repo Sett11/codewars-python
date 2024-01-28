c6e9ecefb7997a97fac88ae969b2db4cef08b5c4
Jhonny <gonzodzen@mail.ru>
2024-01-28 22:31:26 +0300

    Simple Fun #5: Knapsack Light

--- my.py
+++ my.py
@@ -1,25 +1,4 @@
-def f(n):
-    return [str(i) for i in range(n,n+15)]
+def knapsack_light(a,b,c,d,m):
+    return a+c if b+d<=m else max(a,c) if b<=m and d<=m else a if b<=m else c if d<=m else 0
 
-def missing(s):
-    r=[]
-    for i in range(1,7):
-        n=int(s[:i])
-        r.append([n,f(n)])
-    for _,a in r:
-        q=[]
-        for i in range(len(a)):
-            q.append(a[i])
-            if ''.join(q)==s:
-                return -1
-            t=a.copy()
-            p=int(t.pop(i))
-            t=''.join(t)[:len(s)]
-            if t==s:
-                return p
-    return -1
-            
-print(missing('599600601602'))
-print(missing('899091939495'))
-print(missing('9899101102'))
-print(missing('99991000110002'))
+print(knapsack_light(10,5,6,4,8))
