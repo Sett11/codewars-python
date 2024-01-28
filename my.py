f4b09554a192979021df3f931dbd62f7201562fd
Jhonny <gonzodzen@mail.ru>
2024-01-29 00:33:08 +0300

    Simple Fun #181: Rounding

--- my.py
+++ my.py
@@ -1,9 +1,12 @@
-def to_and_from(x,y,t):
-    a,b=divmod(t,abs(y-x))
-    return y-b if x<y and a&1 else y+b if a&1 else x-b if t<x-y else x-b if x>y else x+b
+def rounding(n,m):
+    a,_=divmod(n,m)
+    if m-n<n and m>n:
+        return m
+    return n if (m%n==0 or n%m==0) and n>1 else m*a+(m if m*a+m-n<abs(m*a-n) else 0) if a else 0
 
-print(to_and_from(42,150,548))
-print(to_and_from(10,4,8))
-print(to_and_from(1,10,8))
-print(to_and_from(96,14,74))
-print(to_and_from(94,65,77))
+print(rounding(20,3))
+print(rounding(19,3))
+print(rounding(50,100))
+print(rounding(154,4))
+print(rounding(851,46))
+print(rounding(497,536))
