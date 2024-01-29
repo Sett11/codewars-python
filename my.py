506b1d8395f283521e89f3a01f42dae4ac03d91c
Jhonny <gonzodzen@mail.ru>
2024-01-30 01:22:35 +0300

    Most Frequent Weekdays

--- my.py
+++ my.py
@@ -1,4 +1,13 @@
-def modified_sum(a,n):
-    return sum(map(lambda x:x**n,a))-sum(a)
+from datetime import datetime,date
+from calendar import monthrange
 
-print(modified_sum([1,2,3],3))
+def most_frequent_days(y):
+    a=[[0, 'Monday'], [0, 'Tuesday'], [0, 'Wednesday'], [0, 'Thursday'], [0, 'Friday'], [0, 'Saturday'], [0, 'Sunday']]
+    for i in range(1,13):
+        t=monthrange(y,i)[1]
+        for j in range(1,t+1):
+            a[datetime.weekday(date(y,i,j))][0]+=1
+    m=max(i[0] for i in a)
+    return [i[1] for i in a if i[0]==m]
+
+print(most_frequent_days(1785))
