49a647b565d970b83efd4ef13c7afce23e859ac5
Jhonny <gonzodzen@mail.ru>
2024-01-30 00:03:29 +0300

    Leap Years

--- my.py
+++ my.py
@@ -1,19 +1,5 @@
-def f(n):
-    return [str(i) for i in range(n,n+15)]
+from calendar import isleap
 
-def missing(s):
-    r=[]
-    for i in range(1,7):
-        r.append(f(int(s[:i])))
-    for a in r:
-        q=[]
-        for i in range(len(a)):
-            q.append(a[i])
-            if ''.join(q)==s:
-                return -1
-            t=a.copy()
-            p=int(t.pop(i))
-            t=''.join(t)[:len(s)]
-            if t==s:
-                return p
-    return -1
+is_leap_year=lambda y:isleap(y)
+
+print(is_leap_year(2020))
