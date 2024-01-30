11d667338256b6faf7101a15c8f828074d189fc8
Jhonny <gonzodzen@mail.ru>
2024-01-30 19:09:53 +0300

    String -> N iterations -> String

--- my.py
+++ my.py
@@ -1,24 +1,11 @@
-def f(s):
-    a=b=''
-    for i,j in enumerate(s):
-        if i&1:
-            b+=j
-        else:
-            a+=j
-    return a+b
-
 def jumbled_string(s,n):
-    c,i=s,0
+    r,c=[s],s
     while True:
-        c=f(c)
-        i+=1
+        c=c[::2]+c[1::2]
         if c==s:
             break
-    n%=i
-    while n:
-        c=f(c)
-        n-=1
-    return c
+        r.append(c)
+    return r[n%len(r)]
     
 
 print(jumbled_string('gwr93VO?UUu}_LIqmO`TbODl1fa{PA^l9tJh_7yLE1XteXd>HiuiBuI@V5TL62DB[=BE6:NlM{[TboTmgL<CmonV:;EG0OmCmL5O',1897))
