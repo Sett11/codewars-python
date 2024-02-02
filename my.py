62bd90ec8cbe523d1a8444a3711068db88a8f320
Jhonny <gonzodzen@mail.ru>
2024-02-02 12:24:54 +0300

    Triangular Treasure

--- my.py
+++ my.py
@@ -1,19 +1,4 @@
-class LCG:
-  a = 2
-  c = 3
-  m = 10
-  
-  def __init__(self,seed):
-      self.seed = seed
-  
-  def random(self):
-     n=((self.a*self.seed+self.c)%self.m)
-     self.seed=n
-     return n/10
-  
-r=LCG(5)
+def triangular(n):
+    return n*(n+1)//2 if n>0 else 0
 
-print(r.random())
-print(r.random())
-print(r.random())
-print(r.random())
+print(triangular(1698))
