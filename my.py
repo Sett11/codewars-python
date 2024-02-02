8d46fdb0591fa7f92b6add221bab1a12a6c6d92d
Jhonny <gonzodzen@mail.ru>
2024-02-02 12:39:57 +0300

    Largest prime number containing n digit

--- my.py
+++ my.py
@@ -1,9 +1,11 @@
-def triangular_range(a,b):
-    r,i,j=[1],1,2
-    while i+j<b:
-        r.append(i+j)
-        i+=j
-        j+=1
-    return {i+1:j for i,j in enumerate(r) if j>a}
+def largest(n):
+    check=lambda x:all(x%i for i in range(2,int(x**.5+1)))
+    try:
+        k=int('9'*n)
+        while not check(k):
+            k-=1
+        return k
+    except:
+        return False
 
-print(triangular_range(17,2220))
+print(largest(5))
