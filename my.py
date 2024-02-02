dba00095321e412dbf3e8259993168a7379824c4
Jhonny <gonzodzen@mail.ru>
2024-02-02 12:46:26 +0300

    Perimeter sequence

--- my.py
+++ my.py
@@ -1,11 +1,4 @@
-def largest(n):
-    check=lambda x:all(x%i for i in range(2,int(x**.5+1)))
-    try:
-        k=int('9'*n)
-        while not check(k):
-            k-=1
-        return k
-    except:
-        return False
+def perimeter_sequence(a,n): 
+    return (n*3+n)*a
 
-print(largest(5))
+print(perimeter_sequence(8,40))
