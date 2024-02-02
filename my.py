ce4305f0659ec400c555dae70bc7f8d4fdafb723
Jhonny <gonzodzen@mail.ru>
2024-02-02 14:22:33 +0300

    Recamán Sequence Sum

--- my.py
+++ my.py
@@ -1,9 +1,9 @@
-from functools import cache
+d={0:0}
 
-@cache
 def recaman(n):
+    k=0
     s,r=set([0]),[0]
-    while len(r)<=n:
+    while len(r)<n:
         t=r[-1]-len(r)
         if t>0 and t not in s:
             r.append(t)
@@ -12,6 +12,13 @@ def recaman(n):
             t=r[-1]+len(r)
             r.append(t)
             s.add(t)
-    return r[n]
+        k+=r[-1]
+        d[len(r)]=k
+    return d
 
-print(recaman(11))
+recaman(2500000)
+
+def rec(n):
+    return d.get(n,0)
+
+print(rec(200))
