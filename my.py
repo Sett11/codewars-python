1aaf7a6b332a0b5a5a2a351df9d9c9ab79d558c2
Jhonny <gonzodzen@mail.ru>
2024-02-02 12:32:24 +0300

    Triangular range

--- my.py
+++ my.py
@@ -1,4 +1,9 @@
-def triangular(n):
-    return n*(n+1)//2 if n>0 else 0
+def triangular_range(a,b):
+    r,i,j=[1],1,2
+    while i+j<b:
+        r.append(i+j)
+        i+=j
+        j+=1
+    return {i+1:j for i,j in enumerate(r) if j>a}
 
-print(triangular(1698))
+print(triangular_range(17,2220))
