f4e3f44aa683a78b52d47bd95c533e717c234620
Jhonny <gonzodzen@mail.ru>
2024-02-02 20:12:06 +0300

    Easy mathematical callback

--- my.py
+++ my.py
@@ -1,24 +1,6 @@
-d={0:0}
+f=lambda x:x*2
 
-def recaman(n):
-    k=0
-    s,r=set([0]),[0]
-    while len(r)<n:
-        t=r[-1]-len(r)
-        if t>0 and t not in s:
-            r.append(t)
-            s.add(t)
-        else:
-            t=r[-1]+len(r)
-            r.append(t)
-            s.add(t)
-        k+=r[-1]
-        d[len(r)]=k
-    return d
+def process_array(a,f):
+    return list(map(f,a))
 
-recaman(2500000)
-
-def rec(n):
-    return d.get(n,0)
-
-print(rec(200))
+print(process_array([4, 8, 2, 7, 5],f))
