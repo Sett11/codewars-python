0f05db049f41482f63721393ce6e74a0d0d3eae0
Jhonny <gonzodzen@mail.ru>
2024-02-03 16:12:58 +0300

    Author Disambiguation: a name is a Name!

--- my.py
+++ my.py
@@ -1,5 +1,8 @@
+from re import sub
+
 def could_be(o,a):
-    o,a=o.split(),a.split()
+    f=lambda s:sub(r'[^a-z]','',s)
+    o,a=list(map(f,o.replace(':',' ').lower().split())),list(map(f,a.replace(':',' ').lower().split()))
     return bool(a and o) and all(i in o for i in a)
 
 print(could_be('Carlos Ray Norris','Carlos Norris'))
