8842b21c2293b792116424b37ea97c7c3fe83e20
Jhonny <gonzodzen@mail.ru>
2024-02-04 17:05:42 +0300

    Sum of powers of 2

--- my.py
+++ my.py
@@ -1,11 +1,12 @@
-def are_similar(a,b):
-    if a==b:
-        return True
-    try:
-        t,p=[i for i,j in enumerate(a) if a[i]!=b[i]]
-        b[t],b[p]=b[p],b[t]
-        return a==b
-    except:
-        return False
+def powers(n):
+    r,q,i=[],[],1
+    while i<=n:
+        r.append(i)
+        i*=2
+    for i in r[::-1]:
+       while n>=i:
+           q.append(i)
+           n-=i
+    return q[::-1]
 
-print(are_similar([1, 2, 3],[2, 1, 3]))
+print(powers(621588920))
