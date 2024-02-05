a19075ee54e5227f9c4d961ff51e81dbc2ab3a4e
Jhonny <gonzodzen@mail.ru>
2024-02-05 21:07:08 +0300

    Land perimeter

--- my.py
+++ my.py
@@ -1,17 +1,43 @@
-def count_cows(n):
-    if isinstance(n,int):
-        n+n
-        a,c=[0],0
-        while n:
-            t=0
-            for i in range(len(a)):
-                a[i]+=1
-                if a[i]>=3:
-                    t+=1
-            a.extend([0]*t)
-            c+=t
-            n-=1
-        return c+1
+def land_perimeter(a):
+    n,m,r,s,q,t=len(a),len(a[0]),[],set(),[],0
+    def dfs(i,j):
+        if i<0 or i>=n or j<0 or j>=m or (i,j) in s or a[i][j]=='O':
+            return
+        s.add((i,j))
+        r.append((i,j))
+        dfs(i+1,j)
+        dfs(i,j+1)
+        dfs(i-1,j)
+        dfs(i,j-1)
+    for i in range(n):
+        for j in range(m):
+            if a[i][j]=='X' and (i,j) not in s:
+                dfs(i,j)
+                q.append(r)
+                r=[]
+    for l in q:
+        if len(l)>1:
+            for i,j in l:
+                w=sum(1 for x,y in [(h,k) for h,k in [(i-1,j),(i+1,j),(i,j+1),(i,j-1)] if 0<=h<n and 0<=k<m] if a[x][y]=='O')
+                t+=w+(1 if i==0 or i==n-1 else 0)+(1 if j==0 or j==m-1 else 0)
+        else:
+            t+=4
+    return f'Total land perimeter: {t}'
 
-print(count_cows(2))
-print(count_cows(19))
+print(land_perimeter([
+ 'OOXOOXXOOX',
+ 'OXOOOOXXXO',
+ 'XOXOOXOOOO',
+ 'OXOXOXXXOO',
+ 'OOOXXOOXOX',
+ 'OXOOOOOXOX',
+ 'OOOOOOXXOO',
+ 'OOOXOOOOXO',
+ 'OXXOXXOXXO',
+ 'OOOXXOOOOX',
+ 'OXOOXOXXXX',
+ 'OOXOOXXOXX',
+ 'XOOOOOOOOO',
+ 'OOOXOXXXXO',
+ 'XOXOOOXOOX',
+ 'OXXOOXXXOX']))
