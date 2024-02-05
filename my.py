47700a44c5019f89d3b75851a81024bea2d36ffa
Jhonny <gonzodzen@mail.ru>
2024-02-05 19:11:03 +0300

    How many cows do you have?

--- my.py
+++ my.py
@@ -1,32 +1,17 @@
-class BullsAndCows: 
-    def __init__(self,n):
-        self.s=list(str(n))
-        self.v=True
-        self.z=0
-        if len(self.s)!=4 or n<0 or len(set(self.s))!=4:
-                raise ValueError()
-    
-    def compare_with(self,n):
-        if self.z>=8:
-            return "Sorry, you're out of turns!"
-        if self.v:
-            if self.s==list(str(n)):
-                    self.v=False
-                    return "You win!"
-            c,k,q=self.s.copy(),str(n),0
-            if len(k)!=len(c) or n<0 or len(set(k))!=4:
-                raise ValueError()
-            for i in range(len(k)):
-                if c[i]==k[i]:
-                    q+=1
-                    c[i]='&'
-            l=len([i for i in k if i in c])
-            self.z+=1
-            return f"{q} bull{'s' if q!=1 else ''} and {l} cow{'s' if l!=1 else ''}"
-        else:
-            return "You already won!"
+def count_cows(n):
+    if isinstance(n,int):
+        n+n
+        a,c=[0],0
+        while n:
+            t=0
+            for i in range(len(a)):
+                a[i]+=1
+                if a[i]>=3:
+                    t+=1
+            a.extend([0]*t)
+            c+=t
+            n-=1
+        return c+1
 
-
-b=BullsAndCows(1234)
-print(b.compare_with(12345))
-print(b.compare_with(987))
+print(count_cows(2))
+print(count_cows(19))
