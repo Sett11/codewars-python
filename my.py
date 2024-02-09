59a02b77c8e21d6abde140c41fdb4d9c55b881f1
Jhonny <gonzodzen@mail.ru>
2024-02-10 00:07:24 +0300

    Class conundrum - Bug Fixing #7

--- my.py
+++ my.py
@@ -1,6 +1,13 @@
-from math import ceil
-
-def aspect_ratio(x,y):
-    return ceil(y/9*16),y
-
-print(aspect_ratio(640,480))
+class List:
+    def __init__(self,type):
+        self.type=type
+        self.items=[]
+        self.count=0
+    
+    def add(self,item):
+        if type(item)!=self.type:
+            item_type="str" if self.type==str else "int" if self.type==int else "float"
+            return "This item is not of type: %s" %(item_type)
+        self.count+=1
+        self.items+=[item]
+        return self
