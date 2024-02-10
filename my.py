55f133b52b50fac840de89980960aad1f43fb6b6
Jhonny <gonzodzen@mail.ru>
2024-02-11 00:09:42 +0300

    Thinkful - Logic Drills: Graceful addition

--- my.py
+++ my.py
@@ -1,13 +1,5 @@
-class List:
-    def __init__(self,type):
-        self.type=type
-        self.items=[]
-        self.count=0
-    
-    def add(self,item):
-        if type(item)!=self.type:
-            item_type="str" if self.type==str else "int" if self.type==int else "float"
-            return "This item is not of type: %s" %(item_type)
-        self.count+=1
-        self.items+=[item]
-        return self
+def my_add(a,b):
+    try:
+        return a+b
+    except:
+        pass
