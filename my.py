0989985f1f631afced16b09ac71975e0ce2cf002
Jhonny <gonzodzen@mail.ru>
2024-02-12 00:01:38 +0300

    Get Password from grid

--- my.py
+++ my.py
@@ -1,13 +1,32 @@
-def diagonal_sort(a):
-    n,m=len(a),len(a[0])
-    r,a=[[0]*m for _ in range(n)],sum(a,[])[::-1]
-    for i in range(n+m-1):
-        k,j=i if i<n else n-1,0 if i<n else i-n+1
-        while k>=0 and j<m:
-            r[k][j]=a.pop()
+def get_password(a,b):
+    n,m,r=len(a),len(a[0]),''
+    k=p=0
+    v=False
+    for i in range(n):
+        if v:
+            break
+        for j in range(m):
+            if a[i][j]=='x':
+                k=i
+                p=j
+                v=True
+                break
+    for i in b:
+        t=i.replace('T','')
+        if t=='left':
+            p-=1
+        if t=='right':
+            p+=1
+        if t=='up':
             k-=1
-            j+=1
+        if t=='down':
+            k+=1
+        if i.endswith('T'):
+            r+=a[k][p]
     return r
 
-print(diagonal_sort([['c','o','d'],
-              ['i','n','g']]))
+print(get_password([
+            ["x", "l", "m"],
+            ["o", "f", "c"],
+            ["k", "i", "t"]
+          ],["rightT", "down", "leftT", "right", "rightT", "down", "left", "leftT"]))
