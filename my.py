25c5cba68986420b412c52511d665b22c470cec2
Jhonny <gonzodzen@mail.ru>
2024-02-12 00:02:22 +0300

    Sort a massive list of strings (hard)

--- my.py
+++ my.py
@@ -1,32 +1,7 @@
-def get_password(a,b):
-    n,m,r=len(a),len(a[0]),''
-    k=p=0
-    v=False
-    for i in range(n):
-        if v:
-            break
-        for j in range(m):
-            if a[i][j]=='x':
-                k=i
-                p=j
-                v=True
-                break
-    for i in b:
-        t=i.replace('T','')
-        if t=='left':
-            p-=1
-        if t=='right':
-            p+=1
-        if t=='up':
-            k-=1
-        if t=='down':
-            k+=1
-        if i.endswith('T'):
-            r+=a[k][p]
-    return r
+from bisect import insort
 
-print(get_password([
-            ["x", "l", "m"],
-            ["o", "f", "c"],
-            ["k", "i", "t"]
-          ],["rightT", "down", "leftT", "right", "rightT", "down", "left", "leftT"]))
+def sort(s):
+    r=[]
+    for i in s:
+        insort(r,i)
+    return iter(r)
