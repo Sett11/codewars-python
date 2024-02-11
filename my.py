1b30db2d38a9c491e3eee700eca5edfcb6cf1836
Jhonny <gonzodzen@mail.ru>
2024-02-11 17:34:07 +0300

    The wheat/rice and chessboard problem

--- my.py
+++ my.py
@@ -1,5 +1,3 @@
-def my_add(a,b):
-    try:
-        return a+b
-    except:
-        pass
+squares_needed=lambda n,i=1:0 if n==0 else i if n<=1 else squares_needed(n//2,i+1)
+
+print(squares_needed(235022852780276))
