77010a9423fd8dd30ab461c7b0a00b945745740b
Jhonny <gonzodzen@mail.ru>
2024-02-12 16:39:58 +0300

    Random Integers

--- my.py
+++ my.py
@@ -1,17 +1,19 @@
-def arr_adder(a):
-    return ' '.join(map(lambda s:''.join(s),zip(*a)))
+i=0
 
-print(arr_adder([ 
-  [ 'T', 'M', 'i', 't', 'p', 'o', 't', 'c' ],
-  [ 'h', 'i', 's', 'h', 'o', 'f', 'h', 'e' ],
-  [ 'e', 't', '', 'e', 'w', '', 'e', 'l' ],
-  [ '', 'o', '', '', 'e', '', '', 'l' ],
-  [ '', 'c', '', '', 'r', '', '', '' ],
-  [ '', 'h', '', '', 'h', '', '', '' ],
-  [ '', 'o', '', '', 'o', '', '', '' ],
-  [ '', 'n', '', '', 'u', '', '', '' ],
-  [ '', 'd', '', '', 's', '', '', '' ],
-  [ '', 'r', '', '', 'e', '', '', '' ],
-  [ '', 'i', '', '', '', '', '', '' ],
-  [ '', 'a', '', '', '', '', '', '' ]
-]))
+def random_ints(n,t):
+    global i
+    a=[t-i]+[0]*(n-i-1)+[i]
+    i+=1
+    if i>t:
+        i=0
+    while len(a)<n:
+        a.append(1)
+        a[0]-=1
+    if len(a)>n:
+        a.pop(a.index(0))
+    return a
+
+print(random_ints(5,296))
+print(random_ints(9,39))
+print(random_ints(9,39))
+print(random_ints(8,307))
