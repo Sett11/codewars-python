2ff95ced46f16b3a949018787a37988979935652
Jhonny <gonzodzen@mail.ru>
2024-02-12 20:25:44 +0300

    Number of Proper Fractions with Denominator d

--- my.py
+++ my.py
@@ -1,21 +1,12 @@
-def f(n):
-    r=set([1,n])
-    for i in range(2,int(n**.5+1)):
-        if n%i==0:
-            r.update([i,n//i])
-    return sum(r)
+def proper_fractions(n):
+    k=n>1 and n
+    for i in range(2,int(n**.5)+1):
+        if not n%i:
+            k-=k//i
+            while not n%i:
+                n//=i
+    if n>1:
+        k-=k//n
+    return k
 
-def find_int(a,b,c):
-    r=[]
-    for i in range(a,b+1):
-        t=list(map(int,str(i)))
-        h=t.copy()
-        for j in range(len(t)):
-            t[j]*=(j+1)
-            h[j]=(j+1)**h[j]
-        t,h=sum(t),sum(h)*c
-        if h%f(t)==0:
-            r.append(i)
-    return len(r),r
-
-print(find_int(100,200,1))
+print(proper_fractions(25))
