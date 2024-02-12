1fc2fdbe69f175f09db6e875b8c8b11e9256138f
Jhonny <gonzodzen@mail.ru>
2024-02-12 20:57:38 +0300

    Getting along with Integer Partitions

--- my.py
+++ my.py
@@ -1,12 +1,14 @@
-def proper_fractions(n):
-    k=n>1 and n
-    for i in range(2,int(n**.5)+1):
-        if not n%i:
-            k-=k//i
-            while not n%i:
-                n//=i
-    if n>1:
-        k-=k//n
-    return k
+from statistics import mean,median
 
-print(proper_fractions(25))
+def f(n):
+    a=[{1.}]
+    for i in range(1,n+1):
+        a.append({(i-x)*j for x,s in enumerate(a) for j in s})
+    return sorted(a[-1])
+
+def part(n):
+    a=f(n)
+    b,c,d=a[-1]-a[0],mean(a),median(a)
+    return f"Range: {'{:.2f}'.format(b) if b and b%1 else int(b)} Average: {'{:.2f}'.format(c) if c else int(c)} Median: {'{:.2f}'.format(d) if d else int(d)}"
+
+print(part(10))
