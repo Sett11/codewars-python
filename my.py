c7e55b2d9ea0d90fccb3171485fc493b719239f6
Jhonny <gonzodzen@mail.ru>
2024-02-13 19:41:39 +0300

    Comfortable words

--- my.py
+++ my.py
@@ -1,3 +1,5 @@
-str_repeat=str.__mul__
-    
-print(str_repeat('a',4))
+def comfortable_word(s):
+    a,b='qwertasdfgzxcvb','yuiophjklnm'
+    return all(s[i] in (b if i&1 else a) for i in range(len(s))) or all(s[i] in (a if i&1 else b) for i in range(len(s)))
+
+print(comfortable_word('yams'))
