be7d59a571eeb2d67c89f8b5bafa4436edcd89fd
Jhonny <gonzodzen@mail.ru>
2024-02-25 19:53:44 +0300

    Thinking & Testing : How many "word"?

--- my.py
+++ my.py
@@ -1,16 +1,10 @@
-from math import ceil
+def testit(s):
+    x,j,r='word',0,''
+    a=''.join([i for i in s.lower() if i in x])
+    for i in a:
+        if i==x[j%4]:
+            r+=i
+            j+=1
+    return r.count(x)
 
-def graceful_tipping(n):
-    k,a,r=n+(n/100*15),[10,100,1000,10000,100000,1000000,10000000],0
-    if k<10:
-        return ceil(k)
-    for i in range(len(a)):
-        if k<a[i]:
-            r=a[i-1]//2
-            break
-    for i in range(r,int(k*2),r):
-        if i>k:
-            return i
-    
-
-print(graceful_tipping(739))
+print(testit("When you in order to do something by a wrong way, your heart will missed somewhere."))
