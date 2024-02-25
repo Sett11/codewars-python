9cdd7526494569db781080c172610733b619ca72
Jhonny <gonzodzen@mail.ru>
2024-02-25 19:33:31 +0300

    Graceful Tipping

--- my.py
+++ my.py
@@ -1,31 +1,16 @@
-def f(n):
-    k,c,a=3,1,[]
-    while k<n:
-        c+=1
-        k=3**c
-        a.append(k)
-    if a and a[-1]==n:
-        return k,c
-    return a[-2] if len(a)>1 else a[-1] if a else 3,c-1 if c!=1 else 1
+from math import ceil
 
-def sum_of_threes(n):
-    q=n
-    if n==1:
-        return '3^0'
-    if n<3:
-        return 'Impossible'
-    r=[]
-    while n>=3:
-        a,b=f(n)
-        r.append(b)
-        n-=a
-    if len(set(r))!=len(r):
-        return 'Impossible'
-    w=0
-    s=''
-    for i in r:
-        s+=f'3^{i}+'
-        w+=3**i
-    return s[:-1]+('+3^0' if w!=q else '')
+def graceful_tipping(n):
+    k,a,r=n+(n/100*15),[10,100,1000,10000,100000,1000000,10000000],0
+    if k<10:
+        return ceil(k)
+    for i in range(len(a)):
+        if k<a[i]:
+            r=a[i-1]//2
+            break
+    for i in range(r,int(k*2),r):
+        if i>k:
+            return i
+    
 
-print(sum_of_threes(1418194818))
+print(graceful_tipping(739))
