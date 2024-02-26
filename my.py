40e7f4463919607b4ede1667c465df26f2ece843
Jhonny <gonzodzen@mail.ru>
2024-02-27 00:10:41 +0300

    Trilingual democracy

--- my.py
+++ my.py
@@ -1,6 +1,10 @@
-from re import sub,IGNORECASE
+def trilingual_democracy(s):
+    S='DFKI'
+    n=len(set(s))
+    if n==1:
+        return s[0]
+    if n==2:
+        return min(((s.count(i),i) for i in s))[1]
+    return [i for i in S if i not in s][0]
 
-def short_form(s):
-    return s[0]+sub(r'[aouie]','',s[1:-1],flags=IGNORECASE)+s[-1]
-
-print(short_form('assault'))
+print(trilingual_democracy("DFF"))
