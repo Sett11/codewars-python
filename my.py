22e663628c3aa6ff661dce4bb2f79183cf7e5a61
Jhonny <gonzodzen@mail.ru>
2024-02-29 00:06:50 +0300

    Sums of consecutive integers

--- my.py
+++ my.py
@@ -1,6 +1,13 @@
-from itertools import groupby as g
+def position(x,y,n):
+    k,m=int(y/x),n//2
+    r=[i for i in range(k-m,k+m+1)]
+    while len(r)<x:
+        r.append(r[-1]+1)
+    while sum(r)!=y:
+        t=sum(r)
+        c=abs(y-t)//x
+        r=list(map(lambda z:z+c if t<y else z-c,r))
+    return r[n]
 
-def kooka_counter(s):
-    return len([list(j) for _,j in g([s[i:i+2] for i in range(0,len(s),2)])])
-
-print(kooka_counter("HaHaHahahaHaHa"))
+print(position(7,749,5))
+print(position(9984, 52560768, 9699))
