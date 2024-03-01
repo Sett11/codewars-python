3dd9e6577e57ad183aa5c2a52d03db5f7ec9b067
Jhonny <gonzodzen@mail.ru>
2024-03-02 01:13:12 +0300

    Credit Card Mask

--- my.py
+++ my.py
@@ -1,16 +1,5 @@
-def collinearity(a,b,c,d):
-    if [a,b,c,d]==[1, 2, 1, -2]:
-        return False
-    a,b,c,d=map(abs,[a,b,c,d])
-    if a==c and b==d or (not c and not d) or (not a and not b):
-        return True
-    m=max(b,d)/(min(b,d) if min(b,d)!=0 else 1)
-    if a*m==c and b*m==d:
-        return True
-    m=max(a,c)/(min(a,c) if min(a,c)!=0 else 1)
-    if a*m==c and b*m==d:
-        return True
-    return False
+def maskify(s):
+    return '#'*(len(s)-4)+s[-4:]
 
-
-print(collinearity(4,0,11,0))
+print(maskify('4556364607935616'))
+print(maskify('1'))
