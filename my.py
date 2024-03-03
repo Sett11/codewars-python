b61908932359155247b64d9cfc8e74f40c0494b5
Jhonny <gonzodzen@mail.ru>
2024-03-04 00:02:07 +0300

    Lucas numbers

--- my.py
+++ my.py
@@ -1,14 +1,7 @@
-def get_issuer(n):
-    s=str(n)
-    l=len(s)
-    if s.startswith(('34','37')) and l==15:
-        return 'AMEX'
-    if s.startswith('6011') and l==16:
-        return 'Discover'
-    if s.startswith(('51','52','53','54','55')) and l==16:
-        return 'Mastercard'
-    if s.startswith('4') and (l==13 or l==16):
-        return 'VISA'
-    return 'Unknown'
-
-print(get_issuer(4111111111111111))
+def lucasnum(n):
+    k=abs(n)
+    a,b=2,1
+    while k:
+        a,b=b,a+b
+        k-=1
+    return -a if n<0 and n&1 else a
