1cfcda273a8a8ca5c1eac4e0b88fefbe7108133a
Jhonny <gonzodzen@mail.ru>
2024-03-03 13:07:59 +0300

    Credit Card Checker

--- my.py
+++ my.py
@@ -1,5 +1,5 @@
-def maskify(s):
-    return '#'*(len(s)-4)+s[-4:]
+def valid_card(s):
+    return not sum(map(lambda j:sum(map(int,str(int(j[1])*2))) if j[0]&1 else int(j[1]),enumerate(filter(lambda x:x.isdigit(),list(s[::-1])))))%10
 
-print(maskify('4556364607935616'))
-print(maskify('1'))
+print(valid_card("5457 6238 9823 4311"))
+print(valid_card("5457 6238 9323 4311"))
