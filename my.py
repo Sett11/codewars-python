40a371af4c41462ed2ef19cc094a13dd56d0a8c5
Jhonny <gonzodzen@mail.ru>
2024-03-13 01:22:24 +0300

    Codewars API

--- my.py
+++ my.py
@@ -1,5 +1,26 @@
 from requests import get
-from re import findall
+from html.parser import HTMLParser
 
-def get_leaderboard_honor():
-    return list(map(lambda x:int(x[1:-1].replace(',','')),findall(r'>\d+,\d+<',get('https://www.codewars.com/users/leaderboard').content.decode())))
+
+a=[]
+
+class Html(HTMLParser):
+    def handle_starttag(self,tag,attrs):
+        for i,j in attrs:
+            if i=='data-username':
+                a.append(j)
+
+def f():
+    return get('https://www.codewars.com/users/leaderboard').text
+
+h=Html()
+
+for i in f():
+    h.feed(i)
+
+
+def get_codeChallenges(n):
+    g=get('https://www.codewars.com/api/v1/users/'+a[n-2]).json()["codeChallenges"]
+    return [g["totalAuthored"],g["totalCompleted"]]
+
+print(get_codeChallenges(55))
