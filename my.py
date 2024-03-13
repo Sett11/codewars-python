e75f53f6a2723411d1425ac9298c459363a5ddec
Jhonny <gonzodzen@mail.ru>
2024-03-13 14:16:40 +0300

    Table Builder

--- my.py
+++ my.py
@@ -1,9 +1,31 @@
-def max_profit(a):
-    n,m=a[0],-1
-    for i in a[1:]:
-        m,n=max(i-n,m),min(i,n)
-    return m
+from dataclasses import dataclass
 
-print(max_profit([6, 5, 6, 95, 57, 73, 25, 28, 54, 38, 66, 17, 44, 3, 84, 68]))
-print(max_profit([10, 9, 8, 7]))
-print(max_profit([50, 38, 94, 5, 34]))
+@dataclass(frozen=True)
+class Style:
+    off_min:int
+    sep_he:str
+    sep_hi:str
+    sep_ve:str
+    sep_vi:str
+    align:str
+
+def build_table(a,c):
+    l=list(map(lambda x:len(max(x,key=len))+c.off_min*2,list(zip(*a))))
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            if c.align=='mid':
+                if l[j]&1:
+                    a[i][j]=a[i][j][::-1].center(l[j],c.sep_hi)[::-1]
+                else:
+                    a[i][j]=a[i][j].center(l[j],c.sep_hi)
+            if c.align=='left':
+                a[i][j]=c.sep_hi*(c.off_min)+a[i][j]+c.sep_hi*(l[j]-c.off_min-len(a[i][j]))
+            if c.align=='right':
+                a[i][j]=a[i][j].rjust(l[j]-c.off_min,c.sep_hi)+c.sep_hi*c.off_min
+        a[i]=(c.sep_ve+f'{c.sep_vi}'.join(a[i])+c.sep_ve).replace(' ',c.sep_hi)
+    return f'{c.sep_he*len(a[0] if a else "")}'+('\n' if a else '')+'\n'.join(a)
+
+print(build_table([["ABBREVIATION", "MEANING"],
+                      ["LOL", "Laughing Out Loud"],
+                      ["BRB", "Be Right Back"],
+                      ["IDK", "I Don't Know"]],Style(2, '~', '_', "!", "|","right")))
