abe648e1db1de638fa3cdbc5e43d242d0b404b12
Jhonny <gonzodzen@mail.ru>
2024-03-13 08:47:26 +0300

    Palindrome Builder

--- my.py
+++ my.py
@@ -1,26 +1,11 @@
-from requests import get
-from html.parser import HTMLParser
+def build_palindrome(s):
+    if s==s[::-1]:
+        return s
+    for i in range(1,len(s)):
+        p=[j for j in [s+s[:i][::-1],s[-i:][::-1]+s] if j==j[::-1]]
+        if p:
+            return p[0]
+        
 
-
-a=[]
-
-class Html(HTMLParser):
-    def handle_starttag(self,tag,attrs):
-        for i,j in attrs:
-            if i=='data-username':
-                a.append(j)
-
-def f():
-    return get('https://www.codewars.com/users/leaderboard').text
-
-h=Html()
-
-for i in f():
-    h.feed(i)
-
-
-def get_codeChallenges(n):
-    g=get('https://www.codewars.com/api/v1/users/'+a[n-2]).json()["codeChallenges"]
-    return [g["totalAuthored"],g["totalCompleted"]]
-
-print(get_codeChallenges(55))
+print(build_palindrome('cdcab'))
+print(build_palindrome('abac'))
