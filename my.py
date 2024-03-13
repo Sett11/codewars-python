9e2e49ba2c872906fc19144093828ac8c54ca8e8
Jhonny <gonzodzen@mail.ru>
2024-03-13 11:40:18 +0300

    Best Stock Profit in Single Sale

--- my.py
+++ my.py
@@ -1,11 +1,9 @@
-def build_palindrome(s):
-    if s==s[::-1]:
-        return s
-    for i in range(1,len(s)):
-        p=[j for j in [s+s[:i][::-1],s[-i:][::-1]+s] if j==j[::-1]]
-        if p:
-            return p[0]
-        
+def max_profit(a):
+    n,m=a[0],-1
+    for i in a[1:]:
+        m,n=max(i-n,m),min(i,n)
+    return m
 
-print(build_palindrome('cdcab'))
-print(build_palindrome('abac'))
+print(max_profit([6, 5, 6, 95, 57, 73, 25, 28, 54, 38, 66, 17, 44, 3, 84, 68]))
+print(max_profit([10, 9, 8, 7]))
+print(max_profit([50, 38, 94, 5, 34]))
