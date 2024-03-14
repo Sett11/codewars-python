16b1f395f581b0f96a1e63425d5f896040cfd939
Jhonny <gonzodzen@mail.ru>
2024-03-15 00:06:27 +0300

    Simple Fun #66: Obtain Max Number

--- my.py
+++ my.py
@@ -1,2 +1,18 @@
-def is_infinite_process(a,b):
-    return True if ((a&1 and b%2==0) or (b&1 and a%2==0)) else False if a<=b else True
+from itertools import groupby
+
+def obtain_max_number(a):
+    a=[list(j) for i,j in groupby(sorted(a))]
+    if all(len(i)==1 for i in a):
+        return max(sum(a,[]))
+    for i in range(len(a)):
+        if len(a[i])==1:
+            continue
+        else:
+            a[i]=a[i][1:]
+            a[i][0]*=2
+    return obtain_max_number(sum(a,[]))
+    
+
+print(obtain_max_number([2, 2, 4, 8, 1, 1, 15]))
+print(obtain_max_number([2, 4, 8, 1, 1, 15]))
+print(obtain_max_number([2, 4, 8, 1, 1, 32, 8, 8, 64, 30, 30, 15, 15, 7, 7]))
