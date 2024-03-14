2e84bdc253ea871adfc759e1343cfcee211e0c39
Jhonny <gonzodzen@mail.ru>
2024-03-14 07:46:14 +0300

    Perform operation to make string empty

--- my.py
+++ my.py
@@ -1,14 +1,10 @@
-def determine_sequence(a):
-    if a==[1, 2, 1, 3, 4, 5]:
-        return -1
-    if len(set(a))==1 and not a[0]:
-        return 0
-    try:
-        v=w=False
-        if a[-1]/a[-2]==a[-2]/a[-3]:
-            v=True
-        if a[-1]-a[-2]==a[-2]-a[-3]:
-            w=True
-        return 2 if v and w else 0 if (v and w or w) else 1 if v else -1 
-    except:
-        return -1
+from collections import Counter
+
+def last_non_empty_string(s):
+    d=Counter(s)
+    m=max(d.values())
+    return ''.join(sorted(filter(lambda x:d[x]==m,d.keys()),key=lambda y:s.rfind(y)))
+
+print(last_non_empty_string('aabcbbca'))
+print(last_non_empty_string('zzxdccvzdd'))
+print(last_non_empty_string('abcd'))
