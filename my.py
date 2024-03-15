4e24e9c47c6f8f2d3897068c81c57c9f18cf2a3c
Jhonny <gonzodzen@mail.ru>
2024-03-16 00:33:32 +0300

    Polynomials II: Coefficients In a List

--- my.py
+++ my.py
@@ -1,16 +1,14 @@
-def corner_fill(a):
-    r,v=[],1
-    while a:
-        q=a.pop(0)
-        for i in range(len(a)):
-            q.append(a[i].pop())
-        r.extend(q if v&1 else q[::-1])
-        v+=1
-    return r
+def calc_poly(a,x):
+    n,r=len(a),''
+    for i in range(n):
+        r=r+f" {'+' if a[i]>0 else '-'} {abs(a[i])}*x^{n-i-1}" if a[i] else r
+    r=r.replace('^1','')[3:].replace('*x^0',' with x')
+    r='For '+r+f" = {x} the value is {eval(('-' if a[0]<0 else '')+r.replace('with x','').replace('^','**'))}"
+    if 'with x' not in r:
+        r=r.replace('=','with x =')
+    if a[0]<0:
+        r=r.replace('For ','For -')
+    return r.replace(' 1*',' ').replace('-1*x','-x')
 
-print(corner_fill([
-  [4,  1, 10,  5],
-  [7,  8,  2, 16],
-  [15, 14, 3,  6],
-  [11, 9, 13, 12]
-]))
+print(calc_poly([2, 0, 5, -6, 4, 0], 2))
+print(calc_poly([-5, 40, -28],-18))
