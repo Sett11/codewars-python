1b9079f15b41f4d7bb9844afb4d2b15e91310039
Jhonny <gonzodzen@mail.ru>
2024-05-24 15:50:50 +0300

    Four Letter Words ~ Mutations

--- my.py
+++ my.py
@@ -1,13 +1,17 @@
-def possibly_perfect(a,b):
-    x=y=0
-    n=len(a)
-    for i in range(n):
-        if a[i]==b[i] or a[i]=='_':
-            x+=1
-        else:
-            y+=1
-    return x==n or x==a.count('_')
+alice = ["plat", "rend", "bear", "soar", "mare", "pare", "flap", "neat", "clan", "pore"]
+bob   = ["boar", "clap", "mere", "lend", "near", "peat", "pure", "more", "plan", "soap"]
 
-print(possibly_perfect(["A", "_", "C", "_", "B"],["A", "D", "C", "E", "B"]))
-print(possibly_perfect(["T", "_", "F", "F", "F"],["F", "F", "T", "T", "T"]))
-print(possibly_perfect(["B", "_", "B"],["B", "D", "C"]))
+def mutations(alice,bob,word,first):
+    if not [i for i in alice+bob if [i[j]==word[j] for j in range(4)].count(True)==3 and len(set(i))==4]:
+        return -1
+    d,r={0:alice,1:bob},set([word])
+    while 1:
+        q=d[first]
+        t=[i for i in q if [i[j]==word[j] for j in range(4)].count(True)==3 and len(set(i))==4 and i not in r]
+        if not t:
+            return 0 if first==1 else 1
+        first,word=0 if first==1 else 1,t[0]
+        r.add(word)
+
+
+print(mutations(alice,bob,'calm',1))
