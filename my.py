4d5df458bde0212e7021cd409292be95dfb2d8d9
Jhonny <gonzodzen@mail.ru>
2024-05-24 14:05:25 +0300

    All or Nothing

--- my.py
+++ my.py
@@ -1,7 +1,13 @@
-def block_player(x,y):
-    a=['x' if i in (x,y) else i for i in range(9)]
-    r=[a[i:i+3] for i in range(0,9,3)]
-    return [k for k in list(filter(lambda x:x.count('x')==2,r+[list(i) for i in zip(*r)]+[[r[i][i] for i in range(3)]]+[[r[i][2-i] for i in range(3)]]))[0] if k!='x'][0]
+def possibly_perfect(a,b):
+    x=y=0
+    n=len(a)
+    for i in range(n):
+        if a[i]==b[i] or a[i]=='_':
+            x+=1
+        else:
+            y+=1
+    return x==n or x==a.count('_')
 
-
-print(block_player(6,2))
+print(possibly_perfect(["A", "_", "C", "_", "B"],["A", "D", "C", "E", "B"]))
+print(possibly_perfect(["T", "_", "F", "F", "F"],["F", "F", "T", "T", "T"]))
+print(possibly_perfect(["B", "_", "B"],["B", "D", "C"]))
