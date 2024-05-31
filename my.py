b621735a45267733b62c56ba72e1c1aedd18c06b
Jhonny <gonzodzen@mail.ru>
2024-05-31 10:45:48 +0300

    Blowing Birthday Candles

--- my.py
+++ my.py
@@ -1,17 +1,10 @@
-alice = ["plat", "rend", "bear", "soar", "mare", "pare", "flap", "neat", "clan", "pore"]
-bob   = ["boar", "clap", "mere", "lend", "near", "peat", "pure", "more", "plan", "soap"]
+def blow_candles(s):
+    c=0
+    while s:
+        while s.startswith('0'):
+            s=s[1:]
+        s,c=''.join(map(lambda x:str(max(int(x)-1,0)),s[:3]))+s[3:],c+1 if s else c
+    return c
 
-def mutations(alice,bob,word,first):
-    if not [i for i in alice+bob if [i[j]==word[j] for j in range(4)].count(True)==3 and len(set(i))==4]:
-        return -1
-    d,r={0:alice,1:bob},set([word])
-    while 1:
-        q=d[first]
-        t=[i for i in q if [i[j]==word[j] for j in range(4)].count(True)==3 and len(set(i))==4 and i not in r]
-        if not t:
-            return 0 if first==1 else 1
-        first,word=0 if first==1 else 1,t[0]
-        r.add(word)
-
-
-print(mutations(alice,bob,'calm',1))
+print(blow_candles('0323456'))
+print(blow_candles('1321'))
