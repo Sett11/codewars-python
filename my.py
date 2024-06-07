bbd22881dba42ecb666e9018a2940773b1b3b181
Jhonny <gonzodzen@mail.ru>
2024-06-07 10:14:49 +0300

    Points in the circle

--- my.py
+++ my.py
@@ -1,13 +1,11 @@
-def transform(n,b):
-    a,r='0123456789abcdef',''
-    while n:
-        r=a[n%b]+r
-        n//=b
-    return r
-
-def func(a):
-    n=sum(a)//len(a)
-    return [n,transform(n,2),transform(n,8),transform(n,16)]
+def points(n):
+    n*=n
+    c,d=0,1
+    while d<=n:
+        c+=n//d
+        c-=n//(d+2)
+        d+=4
+    return c*4+1
 
 
-print(func([12,13,6,3,6,45,123]))
+print(points(1000))
