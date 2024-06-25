791e39c326ae814d993ecbb1a091e181047ca452
Jhonny <gonzodzen@mail.ru>
2024-06-25 16:13:24 +0300

    Longest Common Subsequence (Performance version)

--- my.py
+++ my.py
@@ -1,11 +1,10 @@
-def lcs(a,b):
-    q,s,r=list(b),0,''
-    for i in range(len(q)):
-        pos=a.find(q[i],s)
-        if pos>=s:
-            r+=q[i]
-            s=pos+1
-    return r
+from functools import lru_cache
+
+@lru_cache(None)
+def lcs(x, y):
+    if not (x and y): return ''
+    if x[0] == y[0]: return x[0] + lcs(x[1:], y[1:])
+    return max(lcs(x, y[1:]), lcs(x[1:], y), key=len)
 
 
-print(lcs("anothertest", "notatest"))
+print(lcs("abcdefghijklmnopq", "apcdefghijklmnobq"))
