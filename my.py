f2bfbfca4645b35b0513187fd0c67d3718710582
Jhonny <gonzodzen@mail.ru>
2024-06-28 15:51:56 +0300

    Social Golfer Problem Validator

--- my.py
+++ my.py
@@ -1,9 +1,27 @@
-from itertools import combinations_with_replacement as c
+def valid(a):
+    if not all(len(''.join(i))==len(set(''.join(i))) for i in a) or not all(len(i)==len(a[0]) for i in a) or not all(len(i)==len(a[0][0]) for i in sum(a,[])):
+        return False
+    d={i:set() for i in ''.join(a[0])}
+    try:
+        for i in a:
+            for j in i:
+                for k in j:
+                    for c in j.replace(k,''):
+                        if c in d[k]:
+                            return False
+                        else:
+                            d[k].add(c)
+        return True
+    except:
+        return False
 
-def find_all(k,l):
-    r=[int(''.join(map(str,i))) for i in c(list(range(1,10)),l) if sum(i)==k]
-    return [len(r),r[0],r[-1]] if r else []
-
-print(find_all(10,3))
-print(find_all(84,4))
-print(find_all(35,6))
+print(valid( [
+  ['ABCD', 'EFGH', 'IJKL', 'MNOP', 'QRST'],
+  ['AEIM', 'BJOQ', 'CHNT', 'DGLS', 'FKPR'],
+  ['AGKO', 'BIPT', 'CFMS', 'DHJR', 'ELNQ'],
+  ['AHLP', 'BKNS', 'CEOR', 'DFIQ', 'GJMT'],
+  ['AFJN', 'BLMR', 'CGPQ', 'DEKT', 'HIOS']
+ ]))
+print(valid([
+    ["ABC", "DEF"],
+    ["ADE", "CBF"]]))
