08bd3aa81148daff68514a3eea85d9d7bfd03872
Jhonny <gonzodzen@mail.ru>
2024-06-30 11:52:08 +0300

    Building Spheres

--- my.py
+++ my.py
@@ -1,27 +1,28 @@
-def valid(a):
-    if not all(len(''.join(i))==len(set(''.join(i))) for i in a) or not all(len(i)==len(a[0]) for i in a) or not all(len(i)==len(a[0][0]) for i in sum(a,[])):
-        return False
-    d={i:set() for i in ''.join(a[0])}
-    try:
-        for i in a:
-            for j in i:
-                for k in j:
-                    for c in j.replace(k,''):
-                        if c in d[k]:
-                            return False
-                        else:
-                            d[k].add(c)
-        return True
-    except:
-        return False
+from math import pi
 
-print(valid( [
-  ['ABCD', 'EFGH', 'IJKL', 'MNOP', 'QRST'],
-  ['AEIM', 'BJOQ', 'CHNT', 'DGLS', 'FKPR'],
-  ['AGKO', 'BIPT', 'CFMS', 'DHJR', 'ELNQ'],
-  ['AHLP', 'BKNS', 'CEOR', 'DFIQ', 'GJMT'],
-  ['AFJN', 'BLMR', 'CGPQ', 'DEKT', 'HIOS']
- ]))
-print(valid([
-    ["ABC", "DEF"],
-    ["ADE", "CBF"]]))
+class Sphere(object):
+    def __init__(self,radius=0,mass=0):
+        self.radius=radius
+        self.mass=mass
+
+    def get_radius(self):
+        return self.radius
+    
+    def get_mass(self):
+        return self.mass
+    
+    def get_volume(self):
+        return round(4/3*pi*self.radius**3,5)
+    
+    def get_surface_area(self):
+        return round(4*pi*self.radius**2,5)
+    
+    def get_density(self):
+        return round(self.mass/self.get_volume(),5)
+    
+
+h=Sphere(2,50)
+
+print(h.get_volume())
+print(h.get_surface_area())
+print(h.get_density())
