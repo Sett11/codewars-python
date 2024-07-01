b34cd332db6e79a0a3028b417745bbf016b89592
Jhonny <gonzodzen@mail.ru>
2024-07-02 01:35:33 +0300

    Sum and Product of Three Numbers

--- my.py
+++ my.py
@@ -1,40 +1,14 @@
-from collections import Counter
-from itertools import product,combinations_with_replacement
-
-z=list(set([''.join(i) for i in combinations_with_replacement('0101010101',10)]))
-k=list(set([''.join(i) for i in combinations_with_replacement('01201201',8)]))
-y=list(set([''.join(i) for i in combinations_with_replacement('01230123',8)]))
-
-def closest_string(a):
-    if a==['ooootooi', 'ooooioop', 'oooozghi', 'ooooqwel', 'hhcbaaaa']:
-        return 'ooooaaaa'
-    x=set(''.join(a))
-    w=[i for i in Counter(zip(*a))]
-    while len(w)<len(a[0]):
-        w+=[list(set(''.join(a)))]
-    r,q=list(set(map(lambda x:''.join(x),product(*w)))),[]
-    if len(x)==2 and 'a' in x:
-        r+=[''.join(i) for i in combinations_with_replacement('oaao',len(a[0]))]
-    if len(x)==2 and '0' in x:
-        r+=[''.join(i) for i in combinations_with_replacement('0110',len(a[0]))]+z
-    if len(x)==3 and '2' in x:
-        r+=k
-    if len(x)==4 and '3' in x:
-        r+=y
-    for i in r:
-        t=[]
-        for j in a:
-            c=0
-            for k in range(len(i)):
-                if i[k]!=j[k]:
+def number_of_sets(a,b):
+    c=0
+    for i in range(a):
+        for j in range(i,a):
+            for k in range(j,a):
+                x,y=i+j+k,i*j*k
+                if x>a or y>b:
+                    break
+                if x==a and y==b:
                     c+=1
-            t.append(c)
-        q.append((t,i))
-    return sorted([(max(i[0]),sum(i[0]),i[1]) for i in q])[0][-1]
+    return c
 
-
-    
-
-# print(closest_string(['uvwx','xuwv','xvwu']))
-# print(closest_string(['ooi','oio','ooi']))
-print(closest_string(['0100101000', '1010011100', '1011101001']))
+print(number_of_sets(256,392150))
+print(number_of_sets(137, 25200))
