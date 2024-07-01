0d44d67334d960af62bc42b213daecb56e7ea3e8
Jhonny <gonzodzen@mail.ru>
2024-07-01 17:03:24 +0300

    Check if a triangle is an equable triangle!

--- my.py
+++ my.py
@@ -1,8 +1,6 @@
-def is_triangle_number(n):
-    if isinstance(n,int) and not isinstance(n,bool):
-        x=(8*n+1)**.5
-        return int(x)==x
-    return False
-    
-print(is_triangle_number(2098176))    
-print(is_triangle_number(949941))    
+def equable_triangle(a,b,c):
+    p=(a+b+c)/2
+    return (p*(p-a)*(p-b)*(p-c))**.5==p*2
+
+print(equable_triangle(5,12,13))
+print(equable_triangle(73,9,80))
