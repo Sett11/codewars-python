5db7396a8a0b350cee7d62eb5d2510863cc7f30f
Jhonny <gonzodzen@mail.ru>
2024-07-01 16:52:05 +0300

    Triangle number check

--- my.py
+++ my.py
@@ -1,13 +1,8 @@
-def sort_reindeer(a):
-    return sorted(a,key=lambda x:x.split(' ')[1])
-
-print(sort_reindeer([
-  "Dasher Tonoyan", 
-  "Dancer Moore", 
-  "Prancer Chua", 
-  "Vixen Hall", 
-  "Comet Karavani",        
-  "Cupid Foroutan", 
-  "Donder Jonker", 
-  "Blitzen Claus"
-]))
+def is_triangle_number(n):
+    if isinstance(n,int) and not isinstance(n,bool):
+        x=(8*n+1)**.5
+        return int(x)==x
+    return False
+    
+print(is_triangle_number(2098176))    
+print(is_triangle_number(949941))    
