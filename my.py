4c1bd077da8c998546ea986b2fb90f6279363c10
Jhonny <gonzodzen@mail.ru>
2024-07-04 02:41:25 +0300

    Thinking & Testing : Something capitalized

--- my.py
+++ my.py
@@ -1,10 +1,4 @@
-from itertools import accumulate as acc
+def testit(s):
+    return ' '.join(map(lambda x:x[::-1].title()[::-1],s.split()))
 
-def max_sum(a,r):
-    ac=list(acc(a))+[0]
-    m=float('-inf')
-    for i,j in r:
-        m=max(m,ac[j]-ac[i-1])
-    return m
-
-print(max_sum([1,-2,3,4,-5,-4,3,2,1], [[1,4],[2,5]]))
+print(testit('aa bb'))
