5f519a41f582b5b130afa3e38eebb7b5d3f0dc87
Jhonny <gonzodzen@mail.ru>
2024-07-03 08:05:21 +0300

    The maximum sum value of ranges -- Challenge version

--- my.py
+++ my.py
@@ -1,4 +1,10 @@
-def max_sum(a,r): 
-    return max(sum(a[i:j+1]) for i,j in r)
+from itertools import accumulate as acc
+
+def max_sum(a,r):
+    ac=list(acc(a))+[0]
+    m=float('-inf')
+    for i,j in r:
+        m=max(m,ac[j]-ac[i-1])
+    return m
 
 print(max_sum([1,-2,3,4,-5,-4,3,2,1], [[1,4],[2,5]]))
