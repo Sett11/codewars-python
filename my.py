7c4dc71a313edc7c8571ec3b6a9b4f3fe58ff87c
Jhonny <gonzodzen@mail.ru>
2024-07-04 16:09:54 +0300

    Simple template

--- my.py
+++ my.py
@@ -1,4 +1,15 @@
-def testit(s):
-    return ' '.join(map(lambda x:x[::-1].title()[::-1],s.split()))
+from re import sub
 
-print(testit('aa bb'))
+def create_template(s):
+    def f(*args,**kwargs):
+        t=s
+        for i in kwargs:
+            t=t.replace('{{'+i+'}}',kwargs[i])
+        return sub(r'{{.+}}','',t) if kwargs else ' '+''.join(filter(lambda x:'}' not in x,t.split()))+' '
+    return f
+
+temp=create_template("{{firstName}} {{lastName}} likes {{interests}}")
+print(temp(firstName="John", lastName="Smith", interests="sport"))
+print(temp(firstName="Albert", lastName="Einstein", occuptation="physicist"))
+t=create_template("{{foo}} bar {{baz}}")
+print(t())
