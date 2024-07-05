43a5bfd62c164a0ff52730e7ceba8c47c3911e8b
Jhonny <gonzodzen@mail.ru>
2024-07-05 14:48:25 +0300

    Last digit of the squared sum of Fibonacci numbers

--- my.py
+++ my.py
@@ -1,6 +1,15 @@
-from string import ascii_lowercase as a
+def f(n):
+   n%=60
+   a,b=0,1
+   if n==0:
+       return 0
+   if n==1:
+       return 1
+   for _ in range(2,n+1):
+        a,b=b,(a+b)%60
+   return b
 
-def caeser(s,k):
-    return ''.join(a[(a.index(i)+k)%26] if i in a else i for i in s).upper()
-
-print(caeser('hello',3))
+def fibonacci_squared_sum(n):
+    return (f(n)*f(n+1))%10
+    
+print(fibonacci_squared_sum(1000000000000000000))
