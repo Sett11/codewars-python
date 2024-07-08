e0ed50a038031861a58adb7ce48f8b9b9bcab6b9
Jhonny <gonzodzen@mail.ru>
2024-07-09 01:27:10 +0300

    The Crockford Invocation

--- my.py
+++ my.py
@@ -1,13 +1,19 @@
-def size_to_number(s):
-    r=''.join(sorted(set(s)))
-    if len(r)<3 and not s.endswith('x'):
-        if s=='m':
-            return 38
-        if r=='lx' or s=='l':
-            return 40+2*s.count('x')
-        if r=='sx' or s=='s':
-            return 36-2*s.count('x')
+def add(a):
+    def f(b):
+        return a+b
+    return f
 
+def subtract(a):
+    def f(b):
+        return a-b
+    return f
+    
+def multiply(a):
+    def f(b):
+        return a*b
+    return f
+    
+def apply(f):
+    return f
 
-print(size_to_number('xs'))
-print(size_to_number('xxxs'))
+print(apply(add)(5)(7))
