9be6528bc7207c23943214610057d52114a0ccf4
Jhonny <gonzodzen@mail.ru>
2024-07-11 00:23:12 +0300

    Is that a real phone number? (British version)

--- my.py
+++ my.py
@@ -1,4 +1,4 @@
-def is_it_letter(s):
-    return s.isalpha()
+validate_number=lambda s:['Plenty more fish in the sea','In with a chance'][bool(__import__('re').match(r'^(0|\+44)7([\d-]){9,9}$',s.replace('-','')))]
 
-print(is_it_letter('i'))
+print(validate_number('87454876120'))
+print(validate_number('+447454876120'))
