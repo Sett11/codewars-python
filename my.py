473c0b659ea44d8d0d9cef2868d06f7f40f72bc1
Jhonny <gonzodzen@mail.ru>
2024-07-12 02:44:48 +0300

    British vs Slovakian postcodes

--- my.py
+++ my.py
@@ -1,4 +1,8 @@
-def obfuscate(s):
-    return s.replace('.',' [dot] ').replace('@',' [at] ')
+from re import match
 
-print(obfuscate('Code_warrior@foo.ac.uk'))
+def which_postcode(s):
+    return 'GB' if match(r'^([A-z]){1,2}(\d){1,2} (\d){1,1}([A-z]){2,2}$',s.strip()) else 'SK' if match(r'^(\d){3,3} (\d){2,2}$',s.strip()) else 'Not valid'
+
+print(which_postcode('Se21 7aA'))
+print(which_postcode('123 45'))
+print(which_postcode('123 45j'))
