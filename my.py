b911b22301593cda64b12d0e93449eb7c6e7c3c5
Jhonny <gonzodzen@mail.ru>
2024-07-15 01:00:49 +0300

    Arrays of cats and dogs

--- my.py
+++ my.py
@@ -1,12 +1,18 @@
-def solve(s,k):
-    a=s.split()
-    c,n=0,len(a)
+def solve(arr,k):
+    a,n,c=arr.copy(),len(arr),0
     for i in range(n):
-        for j in range(i+1,n):
-            if int(a[i]+a[j])%k==0:
-                c+=1
-            if int(a[j]+a[i])%k==0:
-                c+=1
+        if a[i]=='D':
+            for j in range(max(i-k,0),i):
+                if a[j]=='C':
+                    a[i]=a[j]=0
+                    c+=1
+                    break
+            if a[i]=='D':
+                for j in range(i+1,min(i+k+1,n)):
+                    if a[j]=='C':
+                        a[i]=a[j]=0
+                        c+=1
+                        break
     return c
 
-print(solve('1 2 36 4 8',2))
+print(solve(['C','C','D','D','C','D'],1))
