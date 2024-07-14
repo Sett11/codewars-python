4a742eccf9671f5e32a0f204dd58bfd7369958e1
Jhonny <gonzodzen@mail.ru>
2024-07-14 18:59:51 +0300

    Simple Fun #215: Properly Closed Bracket Word

--- my.py
+++ my.py
@@ -1,7 +1,6 @@
 from string import ascii_lowercase as a
 
-def solve(s):
-    return all(abs(a.index(s[i])-a.index(s[-i-1])) in [0,2] for i in range(len(s)//2))
+def closed_bracket_word(s):
+    return len(s)%2==0 and all(a.index(s[i])+a.index(s[-i-1])==25 for i in range(len(s)//2))
 
-print(solve('ceb'))
-print(solve('kxbkwgyydkcbtjcosgikfdyhuuprubpwthgflucpyylbofvqxkkvqthmdnywpaunfihvupbwpruwfybdmgeuocltdaidyyewmbzm'))
+print(closed_bracket_word('abitryz'))
