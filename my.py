599bd0b765c006541f0577a4171f287de3b1fd20
Jhonny <gonzodzen@mail.ru>
2024-07-16 02:09:35 +0300

    Ulam Sequences

--- my.py
+++ my.py
@@ -1,7 +1,22 @@
-def fix_code(s):
-    try:
-        return next(str(i) for i in range(10) if sum(int(j)*(10-k) if j!='X' else 10 for k,j in enumerate(s.replace('?',str(i))))%11==0)
-    except StopIteration:
-        return 'X'
+def f(x):
+    r,d,i,j=set(),{},0,1
+    while i<len(x):
+        while j<len(x):
+            y=x[i]+x[j]
+            r.add(y)
+            if y in d:
+                d[y]+=1
+            else:
+                d[y]=1
+            j+=1
+        i+=1
+        j=i+1
+    return min(list(filter(lambda e:e not in set(x) and d[e]==1,r)))
 
-print(fix_code('020161586?'))
+def ulam_sequence(a,b,n):
+    a=[a,b,a+b]
+    while len(a)<n:
+        a.append(f(a))
+    return a
+
+print(ulam_sequence(5,6,8))
