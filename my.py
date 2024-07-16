0e0ef50177f60c4b03923b2742457f89724506c8
Jhonny <gonzodzen@mail.ru>
2024-07-17 02:39:58 +0300

    Suitcase packing

--- my.py
+++ my.py
@@ -1,21 +1,13 @@
-def button_sequences(a,b):
-    r='R' if a[0]=='1' else 'B' if b[0]=='1' else ''
-    t,q=[0] if r=='R' else [],[0] if r=='B' else []
-    for i in range(1,len(a)):
-        if (a[i]=='1' and b[i]=='1') or (a[i]=='1' and b[i]=='0'):
-            if b[i-1]+b[i]=='11':
-                continue
-            x=a[t[-1]:i] if t else ''
-            if x!='1'*len(x) or not x:
-                r+='R'
-                t.append(i)
-        if b[i]=='1' and a[i]=='0':
-            y=b[q[-1]:i] if q else ''
-            if y!='1'*len(y) or not y:
-                r+='B'
-                q.append(i)
-    return r
+def fit_in(a,b,m,n):
+    try:
+        t,q=sorted([m,n]),sorted([a,b])
+        r=[[0]*t[1] for _ in range(t[0])]
+        for i in range(q[1]):
+            for j in range(q[1]):
+                r[i][j]=1
+        r=[[j for j in i if not j] for i in r]
+        return len(r)>=q[0] and len(r[0])>=q[0]
+    except IndexError:
+        return 1==2
 
-
-print(button_sequences('0011101110101000',
-                       '1101010001011111'))
+print(fit_in(7,1,7,8))
