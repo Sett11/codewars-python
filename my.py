911ab7ce21343253ce9b831bfd93377468d4c9fa
Jhonny <gonzodzen@mail.ru>
2024-07-17 00:05:31 +0300

    Set the Clock

--- my.py
+++ my.py
@@ -1,22 +1,10 @@
-def f(x):
-    r,d,i,j=set(),{},0,1
-    while i<len(x):
-        while j<len(x):
-            y=x[i]+x[j]
-            r.add(y)
-            if y in d:
-                d[y]+=1
-            else:
-                d[y]=1
-            j+=1
-        i+=1
-        j=i+1
-    return min(list(filter(lambda e:e not in set(x) and d[e]==1,r)))
+def set_clock(s,t):
+    a,b=map(int,s.split(':'))
+    for i in t:
+        if i=='H':
+            a+=1
+        else:
+            b+=1
+    return f"{a-24 if a>24 else a}:{('0' if b%60<10 else '')+str(b%60)}"
 
-def ulam_sequence(a,b,n):
-    a=[a,b,a+b]
-    while len(a)<n:
-        a.append(f(a))
-    return a
-
-print(ulam_sequence(5,6,8))
+print(set_clock("23:59", ['H', 'H']))
