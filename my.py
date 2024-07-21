c543448f225bcd16e64e431854065c81770b9ac0
Jhonny <gonzodzen@mail.ru>
2024-07-22 00:09:18 +0300

    Frog's Dinner

--- my.py
+++ my.py
@@ -1,4 +1,7 @@
-def count_targets(n,a):
-    return len(list(filter(bool,[a[i]==a[i-n] for i in range(n,len(a))])))
+def frog_contest(n):
+    a=n*(n+1)//2
+    b=a//2
+    c=b*(b+1)//2
+    return f"Chris ate {a} flies, Tom ate {c} flies and Cat ate {(a+c)*(a+c+1)//2} flies"
 
-print(count_targets(7,[72, 53, 91, 91, 89, 59, 49, 9, 98, 84, 18, 65, 61, 49, 20, 44, 84, 18, 68, 61, 31, 20, 1, 84, 20, 68, 92, 65, 20, 83, 52, 26, 94, 76, 63, 67, 83, 17, 12]))
+print(frog_contest(8))
