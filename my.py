19bc93a758e3afe69f9cc5a048c2cc863e7a92e6
Jhonny <gonzodzen@mail.ru>
2024-07-23 00:43:47 +0300

    Simple Fun #384: Is Turing's Equation?

--- my.py
+++ my.py
@@ -1,11 +1,6 @@
-def jump_to(n):
-    n=abs(n)
-    k=i=0
-    while i<n or (i-n)%2:
-        k+=1
-        i+=k
-    return k
-    
-print(jump_to(6))
-print(jump_to(7))
-print(jump_to(100))
+from re import sub
+
+def is_turing_equation(s):
+	return eval(sub(r'\d+',lambda x:str(int(x.group()[::-1])),s.replace('=','==')))
+
+print(is_turing_equation('73+42=16'))
