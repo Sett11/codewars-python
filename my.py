ca4bb120230d814c01280cec21e67d8e4287edf1
Jhonny <gonzodzen@mail.ru>
2024-07-27 23:06:06 +0300

    Find Center of Star Graph

--- my.py
+++ my.py
@@ -1,12 +1,8 @@
-class TreeNode:
-    def __init__(self, value, left = None, right = None):
-        self.left = left
-        self.right = right
-        self.value = value
-
-def find_max(h):
-    f=lambda x:float('-inf') if not x else max(x.value,f(x.left),f(x.right))
-    return f(h)
-
-print(find_max(TreeNode(11,TreeNode(4, TreeNode(100), TreeNode(1)),
-                               TreeNode(-2, TreeNode(99), TreeNode(-101)))))
+def center(a):
+    u=set(sum(a,()))
+    for i in u:
+        if all(i in j for j in a):
+            return i
+    return -1
+    
+print(center([(1,5),(4,5),(5,2),(3,5)]))
