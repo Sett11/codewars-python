472289451611bf8dce661eb4e765a488c5dd4911
Jhonny <gonzodzen@mail.ru>
2024-07-27 22:50:33 +0300

    Delta Bits

--- my.py
+++ my.py
@@ -1,5 +1,4 @@
-def bit_letter(a):
-    f=lambda x,y,s:(' ' if x==1 else '')+(s.upper() if y else s)+(',' if x==2 else '.' if x==3 else '')
-    return ''.join(f(int(j[:2],2),int(j[2]),chr(int(j[3:],2)+97)) for j in [(bin(i)[2:]).rjust(8,'0') for i in a])
+def convert_bits(a,b):
+    return sum(i!=j for i,j in zip(bin(a)[2:].rjust(32,'0'),bin(b)[2:].rjust(32,'0')))
 
-print(bit_letter([39,4,11,11,142,86,14,17,11,195]))
+print(convert_bits(64809,706))
