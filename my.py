26193d4433c4589fbf81a0571e13f1ea1c7ece07
Jhonny <gonzodzen@mail.ru>
2024-07-27 12:15:42 +0300

    Balance the parentheses

--- my.py
+++ my.py
@@ -1,23 +1,13 @@
-def connected_values(a,v,c):
-    n,m,u=len(a),len(a[0]),set()
-    def dfs(i,j):
-        if i<0 or i>=n or j<0 or j>=m or (i,j) in u or a[i][j]!=v:
-            return
-        u.add((i,j))
-        dfs(i+1,j)
-        dfs(i,j+1)
-        dfs(i-1,j)
-        dfs(i,j-1)
-        dfs(i+1,j+1)
-        dfs(i-1,j-1)
-        dfs(i+1,j-1)
-        dfs(i-1,j+1)
-    dfs(*c)
-    return u
+def fix_parentheses(s):
+    q=t=0
+    for i in s:
+        if i=='(':
+            t+=1
+        if i==')':
+            if t:
+                t-=1
+            else:
+                q+=1
+    return '('*q+s+')'*t
 
-print(connected_values([[0, 0, 0, 1, 3, 4, 0, 3],
-                [0, 2, 0, 0, 2, 0, 0, 5],
-                [0, 0, 0, 2, 0, 1, 1, 1],
-                [2, 3, 4, 1, 3, 1, 0, 0],
-                [0, 1, 5, 1, 6, 0, 2, 0],
-                [2, 0, 2, 3, 1, 1, 1, 1]],0,(0,0)))
+print(fix_parentheses('))))(()('))
