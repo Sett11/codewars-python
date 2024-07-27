31104e241a89da5ba6b663fd92bcc33c7e23507d
Jhonny <gonzodzen@mail.ru>
2024-07-27 22:43:05 +0300

    Bit Letters

--- my.py
+++ my.py
@@ -1,5 +1,5 @@
-def any_odd(n):
-    return '1' in bin(n)[2:][::-1][:32][1::2]
+def bit_letter(a):
+    f=lambda x,y,s:(' ' if x==1 else '')+(s.upper() if y else s)+(',' if x==2 else '.' if x==3 else '')
+    return ''.join(f(int(j[:2],2),int(j[2]),chr(int(j[3:],2)+97)) for j in [(bin(i)[2:]).rjust(8,'0') for i in a])
 
-print(any_odd(128))
-print(any_odd(9730854933))
+print(bit_letter([39,4,11,11,142,86,14,17,11,195]))
