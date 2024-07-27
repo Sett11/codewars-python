41d55425d2c424cb9c116ecdf7e75418ec685773
Jhonny <gonzodzen@mail.ru>
2024-07-28 02:10:09 +0300

    Jurassic Word

--- my.py
+++ my.py
@@ -1,7 +1,54 @@
-from itertools import product as p
+class JurassicWord(object):
+    def __init__(self):
+        self.dead_dino = "_C     C}>" 
+        self.flowers = "iii     iii"
+        self.leaves = "|||     |||"
+        self.t_rex = "VvvvV"
+        self.velociraptor = "vvvvv" 
+        self.brachiosaurus = "uuuuu"
+        self.triceratops = "uuVuu"
+        
+    def lunch_time(self,s):
+        print(s)
+        if s[:2]+' '*5+s[-3:]==self.dead_dino:
+            if s[2:-3]==self.velociraptor:
+                return 'A velociraptor is eating a dead dino.'
+            if s[2:-3]==self.t_rex:
+                return 'A T-Rex is eating a dead dino.'
+            else:
+                return 'Something is eating a dead dino.'
+        if s[3:-3]==self.velociraptor or s[3:-3]==self.t_rex:
+            if s[:3]+' '*5+s[-3:]==self.flowers:
+                return 'Something is eating flowers.'
+            if s[:3]+' '*5+s[-3:]==self.leaves:
+                return 'Something is eating leaves.'
+            else:
+                if s[3:-3]==self.velociraptor:
+                    return 'A velociraptor is eating something.'
+                else:
+                    return 'A T-Rex is eating something.'
+        if s[3:-3]==self.brachiosaurus:
+            t=s[:3]+' '*5+s[-3:]
+            if t==self.flowers:
+                return 'A brachiosaurus is eating flowers.'
+            if t==self.leaves:
+                return 'A brachiosaurus is eating leaves.'
+            else:
+                return 'A brachiosaurus is eating something.'
+        if s[3:-3]==self.triceratops:
+            if s[:3]+' '*5+s[-3:]==self.flowers:
+                return 'A triceratops is eating flowers.'
+            if s[:3]+' '*5+s[-3:]==self.leaves:
+                return 'Something is eating leaves.'
+            else:
+                return 'A triceratops is eating something.'
+        if s[:3]+' '*5+s[-3:]==self.flowers:
+            return 'Something is eating flowers.'
+        if s[:3]+' '*5+s[-3:]==self.leaves:
+            return 'Something is eating leaves.'
+        return 'Something is eating something.'
 
-def telephone_words(s):
-    d={'1': '1', '2': 'ABC', '3': 'DEF', '4': 'GHI', '5': 'JKL', '6': 'MNO', '7': 'PQRS', '8': 'TUV', '9': 'WXYZ', '0': '0'}
-    return [''.join(i) for i in p(*[list(d[j]) for j in s])]
-
-print(telephone_words('1234'))
+        
+j=JurassicWord()
+print(j.lunch_time('_CVvvvVC}>'))
+print(j.lunch_time('vvvvvvvvvvv'))
