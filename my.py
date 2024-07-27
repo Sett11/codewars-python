f870a5ce4f535d1a071cc5a1202a18b42f0a1794
Jhonny <gonzodzen@mail.ru>
2024-07-27 22:17:50 +0300

    Is There an Odd Bit?

--- my.py
+++ my.py
@@ -1,21 +1,5 @@
-def queue_time(a,n):
-    if n==1 or len(a)==1:
-        return sum(a)
-    if n>=len(a):
-        return max(a)
-    a=a[::-1]
-    t,c=a[-n:],0
-    a=a[:-n]
-    while a:
-        m=min(t)
-        for i in range(n):
-            t[i]-=m
-            if t[i]==0:
-                if a:
-                    t[i]=a.pop()
-                else:
-                    break
-        c+=m
-    return c+max(t)
+def any_odd(n):
+    return '1' in bin(n)[2:][::-1][:32][1::2]
 
-print(queue_time([3, 9, 3, 9, 11, 47, 38, 47, 42, 46, 30, 33, 31, 1, 11, 24],4))
+print(any_odd(128))
+print(any_odd(9730854933))
