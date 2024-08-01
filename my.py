a47010088d917379bd27eb5b46bc6fa79fbf66e3
Jhonny <gonzodzen@mail.ru>
2024-08-01 15:13:01 +0300

    Vending Machine Problems [#1]

--- my.py
+++ my.py
@@ -1,12 +1,10 @@
-from re import sub
+def optimal_number_of_coins(n,a):
+    r=[0]+[n+1]*n
+    for i in range(1,n+1):
+        for j in a:
+            if j<=i:
+                r[i]=min(r[i],r[i-j]+1)
+    return -1 if r[n]>n else r[n]
 
-def calculate(s):
-    stack=[]
-    for i in s.split()[::-1]:
-        if sub(r'[-.]','',i).isdigit():
-            stack.append(float(i))
-        else:
-            stack.append(eval(f'{stack.pop()}{i}{stack.pop()}'))
-    return stack[-1]
 
-print(calculate('/ + 3 5 * 2 2'))
+print(optimal_number_of_coins(33,[1, 6, 9, 10]))
