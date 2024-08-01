40bda916a9b9ff3e7b16e5de177c529884666a44
Jhonny <gonzodzen@mail.ru>
2024-08-02 00:10:02 +0300

    Hanoi record

--- my.py
+++ my.py
@@ -1,10 +1,1 @@
-def optimal_number_of_coins(n,a):
-    r=[0]+[n+1]*n
-    for i in range(1,n+1):
-        for j in a:
-            if j<=i:
-                r[i]=min(r[i],r[i-j]+1)
-    return -1 if r[n]>n else r[n]
-
-
-print(optimal_number_of_coins(33,[1, 6, 9, 10]))
+hanoi=lambda n:2**n-1
