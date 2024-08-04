d7280734c1c7315765e65782206db4891c07b64f
Jhonny <gonzodzen@mail.ru>
2024-08-04 20:50:54 +0300

    Prime Cycles

--- my.py
+++ my.py
@@ -1,4 +1,17 @@
-def find_the_secret(f):
-    for i in f.__code__.co_consts:
-        if i and len(i)==32:
-            return i
+from functools import reduce as r
+from operator import mul
+
+p=[2,3,5,7,11,13,17,19,23]
+
+def find_max(n):
+    u,m=set([n]),n
+    while 1:
+        n=r(mul,[p[int(i)-1] if int(i)>0 else 1 for i in list(str(n))])
+        if(n not in u):
+            u.add(n)
+            m=max(m,n)
+        else:
+            break
+    return m
+
+print(find_max(218294180898))
