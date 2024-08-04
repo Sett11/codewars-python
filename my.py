9820eb9adc89754da8c87651fbc03e0c8f3692b3
Jhonny <gonzodzen@mail.ru>
2024-08-04 20:43:35 +0300

    Python Reflection: Disassembling the secret

--- my.py
+++ my.py
@@ -1,22 +1,4 @@
-import inspect
-class LazyInit(type):
-     @classmethod
-     def lazi_init(msc,init):
-          def wrap(*args):
-               p=inspect.getfullargspec(init).args
-               self=args[0]
-               for i in range(1,len(args)):
-                    setattr(self,p[i],args[i])
-          return wrap
-     
-     def __new__(msc,name,bases,attrs):
-          cls=type(name,bases,attrs)
-          setattr(cls,'__init__',LazyInit.lazi_init(attrs['__init__']))
-          return cls
-
-class MyClass(metaclass=LazyInit):
-     def __init__(self,name,age):
-          pass
-     
-m=MyClass('Jhon',23)
-print(m.name)
+def find_the_secret(f):
+    for i in f.__code__.co_consts:
+        if i and len(i)==32:
+            return i
