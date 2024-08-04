aaf15ccd6931db716307b24d11698af025d7d2d0
Jhonny <gonzodzen@mail.ru>
2024-08-04 22:01:25 +0300

    Twice linear

--- my.py
+++ my.py
@@ -1,17 +1,14 @@
-from functools import reduce as r
-from operator import mul
+def dbl_linear(n):
+    r=[1]+[0]*n
+    k=j=0
+    for i in range(1,n+1):
+        x,y=2*r[k]+1,3*r[j]+1
+        r[i]=min(x,y)
+        if r[i]==x:
+            k+=1
+        if r[i]==y:
+            j+=1
+    return r[n]
 
-p=[2,3,5,7,11,13,17,19,23]
 
-def find_max(n):
-    u,m=set([n]),n
-    while 1:
-        n=r(mul,[p[int(i)-1] if int(i)>0 else 1 for i in list(str(n))])
-        if(n not in u):
-            u.add(n)
-            m=max(m,n)
-        else:
-            break
-    return m
-
-print(find_max(218294180898))
+print(dbl_linear(50))
