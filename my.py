4474c43313c1c35737ec669508b895ac246f2d29
Jhonny <gonzodzen@mail.ru>
2024-08-04 19:58:40 +0300

    Lazy Init

--- my.py
+++ my.py
@@ -1,27 +1,22 @@
-def count_cash(a,c):
-    n,r=len(a),[]
-    def dfs(i,j,k):
-        if i<0 or j<0 or j>=n:
-            return
-        k+=a[i][j]
-        if [i,j]==[0,n-1]:
-            r.append(k)
-            return
-        dfs(i-1,j,k)
-        dfs(i,j+1,k)
-    dfs(c[0],c[1],0)
-    return max(r)
-
-print(count_cash([[3, 3, -1],[-1, 5, -2],[4, -2, 6]],(1,0)),sep='\n')
-
-
-# class LazyInit(type):
-#      def __init__(cls,name,bases,dct):
-#           print(list(locals().items()))
-#           super().__init__(name,bases,dct)
-
-# class MyClass(metaclass=LazyInit):
-#      def __init__(self,name,age):
-#           pass
+import inspect
+class LazyInit(type):
+     @classmethod
+     def lazi_init(msc,init):
+          def wrap(*args):
+               p=inspect.getfullargspec(init).args
+               self=args[0]
+               for i in range(1,len(args)):
+                    setattr(self,p[i],args[i])
+          return wrap
      
-# m=MyClass('John',23)
+     def __new__(msc,name,bases,attrs):
+          cls=type(name,bases,attrs)
+          setattr(cls,'__init__',LazyInit.lazi_init(attrs['__init__']))
+          return cls
+
+class MyClass(metaclass=LazyInit):
+     def __init__(self,name,age):
+          pass
+     
+m=MyClass('Jhon',23)
+print(m.name)
