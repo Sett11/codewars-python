4dbc67800ec1aa252cb4a6851c929a031766e0ee
Jhonny <gonzodzen@mail.ru>
2024-08-07 02:32:24 +0300

    Primes in the Last Digits of Huge Numbers

--- my.py
+++ my.py
@@ -1,15 +1,17 @@
-def knapsack(k,a):
-    a=sorted([(i,*j) for i,j in enumerate(a)],key=lambda x:-x[2]/x[1])
-    n=len(a)
-    r=[0]*n
-    for i in range(n):
-        while k>=a[i][1]:
-            k-=a[i][1]
-            r[a[i][0]]+=1
-    return r
+from gmpy2 import is_prime
 
-print(knapsack(100,((11.2,  7.4),
-                                  (25.6, 17.8),
-                                  (51.0, 41.2),
-                                  (23.9, 15.6),
-                                  (27.8, 19.0))))
+a=[0,1,1,2,4]
+r=[]
+
+def kth_last_dig_prime(k):
+    while len(r)<k:
+        x=a[-1]+a[-2]-a[-3]+a[-4]-a[-5]
+        a.append(x)
+        s=str(x)
+        if len(s)>9 and is_prime(int(s[-9:])):
+            r.append([len(a),int(s[-9:])])
+    return r[k-1]
+
+print(kth_last_dig_prime(5))
+print(kth_last_dig_prime(112))
+print(kth_last_dig_prime(10))
