ca8cb78a269f157a22c648bcf48540c4a580b2ff
Jhonny <gonzodzen@mail.ru>
2024-08-07 00:23:51 +0300

    Feynman's square question

--- my.py
+++ my.py
@@ -1,22 +1,4 @@
-def strings_in_max_depth(s):
-    r,stack=[],[]
-    m=j=0
-    for i in s:
-        if i=='(':
-            j+=1
-        if i==')':
-            m=max(m,j)
-            j-=1
-    k=0
-    for i,j in enumerate(s):
-        if j=='(':
-            stack.append(i)
-            k+=1
-        if j==')':
-            if k==m:
-                r.append(s[stack.pop()+1:i])
-            k-=1
-    return r if m else [s]
+def count_squares(n):
+    return n*(n+1)*(2*n+1)//6
 
-
-print(strings_in_max_depth('((AAX(AB)(UP)F(Z))(HH))'))
+print(count_squares(15))
