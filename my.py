9739499712b0d84416ca227ea0ea160832a661e9
Jhonny <gonzodzen@mail.ru>
2024-08-11 22:49:17 +0300

    T.T.T.23: Silly birds

--- my.py
+++ my.py
@@ -1,21 +1,53 @@
-def bounding_box(arr):
-    z=[i for i in zip(*sum([[[k,i] for i,j in enumerate(p) if j==1] for k,p in enumerate(arr)],[]))]
-    if not z:
-        return arr
-    r=[[0]*len(arr[0]) for _ in range(len(arr))]
-    a,b,x,y=min(z[0]),min(z[1]),max(z[0]),max(z[1])
-    r[a][b:y+1]=r[x][b:y+1]=[1]*(y-b+1)
-    for i in range(a,x+1):
-        r[i][b]=r[i][y]=1
-    return r
+def silly_birds(s,c):
+    def f(arr,n):
+        if not n:
+            return [''.join(i) for i in arr]
+        l,a=len(arr[0]),[i.copy() for i in arr]
+        for i in range(2):
+            for j in range(l):
+                if i==0:
+                    if arr[0][j]=='B':
+                        if j==l-1:
+                            if arr[1][j-1]=='B' or arr[0][j-2]=='B':
+                                a[0][j]='_'
+                            else:
+                                a[1][j-1],a[0][j]='B','_'
+                        elif j==0:
+                            if arr[1][j+1]=='B':
+                                a[0][j]='_'
+                            else:
+                                a[1][j+1],a[0][j]='B','_'
+                        else:
+                            if arr[1][j+1]=='B':
+                                if arr[1][j-1]=='B':
+                                    a[0][j]='_'
+                                else:
+                                    a[0][j],a[1][j-1]='_','B'
+                            else:
+                                a[1][j+1],a[0][j]='B','_'
+                else:
+                    if arr[1][j]=='B':
+                        if j==l-1:
+                            if arr[0][j-1]=='B' or arr[1][j-2]=='B':
+                                a[1][j]='_'
+                            else:
+                                a[0][j-1],a[1][j]='B','_'
+                        elif j==0:
+                            if arr[0][j+1]=='B':
+                                a[1][j]='_'
+                            else:
+                                a[0][j+1],a[1][j]='B','_'
+                        else:
+                            if arr[0][j+1]=='B':
+                                if arr[0][j-1]=='B':
+                                    a[1][j]='_'
+                                else:
+                                    a[1][j],a[0][j-1]='_','B'
+                            else:
+                                a[0][j+1],a[1][j]='B','_'
+        return f(a,n-1)
+    return f([list(i) for i in s],c.count('Bang!'))
 
-print(*bounding_box([[0,0,0,0,0,0,0,0,0,0],
- [0,0,0,0,0,0,0,0,0,0],
- [0,0,0,0,0,0,0,0,0,0],
- [0,0,0,0,0,0,0,0,0,0],
- [0,0,0,0,0,0,0,0,0,0],
- [0,0,0,0,1,1,0,0,0,0],
- [0,0,0,1,1,1,1,0,0,0],
- [0,0,1,1,1,1,1,1,0,0],
- [0,1,1,1,1,1,0,0,0,0],
- [0,0,0,0,0,0,0,0,0,0]]),sep='\n')
+print(silly_birds(
+    ['_B__BB___B_',
+     '_BBB___BB__'], 'Bang!Bang!'),sep='\n')
