039d28ac4e1f5df005d70c1a4539160352769cfb
Jhonny <gonzodzen@mail.ru>
2024-08-12 02:14:32 +0300

    Great Total Additions of All Possible Arrays from a List.

--- my.py
+++ my.py
@@ -1,4 +1,17 @@
-def spiral_sum(n):
-    return n*(n+1)//2+(n//2-(n%2==0))
+from itertools import zip_longest as z,product as p
+from math import factorial as f
 
-print(spiral_sum(123456))
+def gta(n,*args):
+    r,u=[],set()
+    for i in z(*map(str,args)):
+        for j in i:
+            if j and j not in u:
+                r.append(int(j))
+                u.add(j)
+    r=r[:n]
+    l=len(r)
+    return sum([sum([sum(j) for j in p(r,repeat=i) if len(set(j))==i]) for i in range(1,l)])+sum(r)*f(l)
+    
+    
+
+print(gta(6,5522, 6674))
