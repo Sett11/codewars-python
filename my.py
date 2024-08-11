050c3a320f095a30b8e91b85b47cbb6a8d27622f
Jhonny <gonzodzen@mail.ru>
2024-08-11 20:34:42 +0300

    Clean up after your dog

--- my.py
+++ my.py
@@ -1,18 +1,5 @@
-def add2(n):
-    return n + 2
-def mul7(n):
-    return n * 7
+def crap(a,b,c):
+    r=sum(a,[])
+    return 'Dog!!' if 'D' in r else ['Cr@p','Clean'][b*c>=r.count('@')]
 
-def operation_arguments(a):
-    i=0
-    while i<len(a):
-        if not isinstance(a[i],int) and not i:
-            a[i]=a[i](0)
-        elif not isinstance(a[i],int):
-            a[i-1]=a[i](a[i-1])
-            a.pop(i)
-            i-=1
-        i+=1
-    return a
-
-print(operation_arguments([1, add2, 5, add2, mul7, 4, mul7]))
+print(crap([['_','_','_','_'], ['_','_','_','@'], ['_','_','@', '_']], 2, 2))
