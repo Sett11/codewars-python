6520141b1a8915c4d4983db18ad72d2f0db57205
Jhonny <gonzodzen@mail.ru>
2024-08-11 21:27:55 +0300

    Basic Bounding Box

--- my.py
+++ my.py
@@ -1,5 +1,21 @@
-def crap(a,b,c):
-    r=sum(a,[])
-    return 'Dog!!' if 'D' in r else ['Cr@p','Clean'][b*c>=r.count('@')]
+def bounding_box(arr):
+    z=[i for i in zip(*sum([[[k,i] for i,j in enumerate(p) if j==1] for k,p in enumerate(arr)],[]))]
+    if not z:
+        return arr
+    r=[[0]*len(arr[0]) for _ in range(len(arr))]
+    a,b,x,y=min(z[0]),min(z[1]),max(z[0]),max(z[1])
+    r[a][b:y+1]=r[x][b:y+1]=[1]*(y-b+1)
+    for i in range(a,x+1):
+        r[i][b]=r[i][y]=1
+    return r
 
-print(crap([['_','_','_','_'], ['_','_','_','@'], ['_','_','@', '_']], 2, 2))
+print(*bounding_box([[0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,1,1,0,0,0,0],
+ [0,0,0,1,1,1,1,0,0,0],
+ [0,0,1,1,1,1,1,1,0,0],
+ [0,1,1,1,1,1,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0]]),sep='\n')
