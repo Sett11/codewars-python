c16c4cd698da2ffcdfe90c954ae91473fef5e9be
Jhonny <gonzodzen@mail.ru>
2024-08-11 20:29:07 +0300

    Collapse Left

--- my.py
+++ my.py
@@ -1,6 +1,18 @@
-from re import sub
+def add2(n):
+    return n + 2
+def mul7(n):
+    return n * 7
 
-def get_users_ids(s):
-    return [sub(r'^uid','',i.lower().replace('#','').strip()).strip() for i in s.strip().split(', ')]
+def operation_arguments(a):
+    i=0
+    while i<len(a):
+        if not isinstance(a[i],int) and not i:
+            a[i]=a[i](0)
+        elif not isinstance(a[i],int):
+            a[i-1]=a[i](a[i-1])
+            a.pop(i)
+            i-=1
+        i+=1
+    return a
 
-print(get_users_ids("uid12 abuid, uid#, uidMiXeDcHaRs"))
+print(operation_arguments([1, add2, 5, add2, mul7, 4, mul7]))
