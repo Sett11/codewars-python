b963876fffe564f11538939e194cc075a4271e49
Jhonny <gonzodzen@mail.ru>
2024-08-13 00:45:09 +0300

    Vector Affinity

--- my.py
+++ my.py
@@ -1,24 +1,7 @@
-from re import sub
+from itertools import zip_longest as z
 
-def phone_number(a):
-    def f(a):
-        d,z={},{}
-        for i in a:
-            if i:
-                d[i[0]]=d.get(i[0],[])+[i[1:]]
-        for i in d:
-            t=f(d[i])
-            if len(t)==1 and '' not in d[i]:
-                for j in t:
-                    i,t=i+j,t[j]
-            z[i]=t
-        return z
-    return len(sub(r'[^\d]','',str(f(a))))
+def vector_affinity(a,b):
+    return sum(i==j for i,j in z(a,b,fillvalue='Fack'))/max(len(a),len(b)) if a and b else 1 if not a and not b else 0
 
-
-print(phone_number([
-            '0123456789',
-            '0123987654',
-            '0123987456',
-            '2365498756',
-            '2365498765']))
+print(vector_affinity([1,2,3,4],[1,2,3,5]))
+print(vector_affinity([1,2,3], [1,2,3,4,5]))
