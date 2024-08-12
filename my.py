81bc5474c93fa50759a7e6d7ec53d497853209a9
Jhonny <gonzodzen@mail.ru>
2024-08-13 01:35:29 +0300

    Vector Class

--- my.py
+++ my.py
@@ -1,4 +1,31 @@
-def is_orthogonal(a,b): 
-    return not sum(a[i]*b[i] for i in range(len(a)))
+class Vector:
+    def __init__(self,*a):
+        if len(a)==1:
+            a=a[0]
+        self.x,self.y,self.z=a
+        self.magnitude=(self.x**2+self.y**2+self.z**2)**.5
 
-print(is_orthogonal([-13,-26], [-8,4]))
+    def __add__(self,other):
+        return Vector(self.x+other.x,self.y+other.y,self.z+other.z)
+    
+    def __sub__(self,other):
+        return Vector(self.x-other.x,self.y-other.y,self.z-other.z)
+    
+    def __eq__(self,other):
+        return isinstance(other,Vector) and [self.x,self.y,self.z]==[other.x,other.y,other.z]
+    
+    def __str__(self):
+        return f'<{self.x}, {self.y}, {self.z}>'
+    
+    def to_tuple(self):
+        return self.x,self.y,self.z
+    
+    def cross(self,other):
+        return Vector(self.y*other.z-self.z*other.y,self.z*other.x-self.x*other.z,self.x*other.y-self.y*other.x)
+
+    def dot(self,other):
+        return self.x*other.x+self.y*other.y+self.z*other.z
+    
+
+v=Vector(1,2,3)
+print(v.cross(Vector([4,5,6])))
