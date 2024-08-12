bbb867354ee3d82a3eb66e26300b023795ad4e8f
Jhonny <gonzodzen@mail.ru>
2024-08-13 00:49:09 +0300

    Orthogonal Vectors

--- my.py
+++ my.py
@@ -1,7 +1,4 @@
-from itertools import zip_longest as z
+def is_orthogonal(a,b): 
+    return not sum(a[i]*b[i] for i in range(len(a)))
 
-def vector_affinity(a,b):
-    return sum(i==j for i,j in z(a,b,fillvalue='Fack'))/max(len(a),len(b)) if a and b else 1 if not a and not b else 0
-
-print(vector_affinity([1,2,3,4],[1,2,3,5]))
-print(vector_affinity([1,2,3], [1,2,3,4,5]))
+print(is_orthogonal([-13,-26], [-8,4]))
