dba50f149bb41a8e66b889b425f0239659e633ea
Jhonny <gonzodzen@mail.ru>
2024-08-14 02:54:23 +0300

    Temperature converter

--- my.py
+++ my.py
@@ -1,14 +1,33 @@
-def on_line(a):
-    if len(a)<3 or len(set(i[0] for i in a))==1 or len(set(i[1] for i in a))==1:
-        return True
-    a=list(a)
-    x=a.pop(0)
-    a=list(filter(lambda y:not (x[0]==y[0] and x[1]==y[1]),a))
-    try:
-        for i in range(len(a)):
-            a[i]=(x[0]-a[i][0])/(x[1]-a[i][1])
-        return len(set(a))==1
-    except:
-        return False
+from math import floor
 
-print(on_line(((2,1), (4,1), (7,1))))
+toCelcius = {
+  'C': lambda t:t,
+  'F': lambda t:(t - 32) * 5 / 9,
+  'K': lambda t:t - 273.15,
+  'R': lambda t:(t - 491.67) * 5 / 9,
+  'De': lambda t:100 - t * 2 / 3,
+  'N': lambda t:t * 100 / 33,
+  'Re': lambda t:t * 5 / 4,
+  'Ro': lambda t:(t - 7.5) * 40 / 21
+}
+
+fromCelcius = {
+  'C': lambda t:t,
+  'F': lambda t:t * 9 / 5 + 32,
+  'K': lambda t:t + 273.15,
+  'R': lambda t:(t + 273.15) * 9 / 5,
+  'De': lambda t:(100 - t) * 3 / 2,
+  'N': lambda t:t * 33 / 100,
+  'Re': lambda t:t * 4 / 5,
+  'Ro': lambda t:t * 21 / 40 + 7.5
+}
+
+def convert_temp(n,f,t):
+    if f==t:
+        return n
+    n=fromCelcius[t](toCelcius[f](n))
+    v=n<0
+    n=abs(n)
+    return -floor(n) if v else floor(n)
+
+print(convert_temp(100, "C", "F"))
