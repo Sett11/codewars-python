a7531fd161f68b7a4355c8fe094359c43857f517
Jhonny <gonzodzen@mail.ru>
2024-08-15 00:31:59 +0300

    compute cube as sums

--- my.py
+++ my.py
@@ -1,33 +1,5 @@
-from math import floor
+def find_summands(n):
+    sq=n**2
+    return [i for i in range(sq-(n-1),sq+(n+1),2)]
 
-toCelcius = {
-  'C': lambda t:t,
-  'F': lambda t:(t - 32) * 5 / 9,
-  'K': lambda t:t - 273.15,
-  'R': lambda t:(t - 491.67) * 5 / 9,
-  'De': lambda t:100 - t * 2 / 3,
-  'N': lambda t:t * 100 / 33,
-  'Re': lambda t:t * 5 / 4,
-  'Ro': lambda t:(t - 7.5) * 40 / 21
-}
-
-fromCelcius = {
-  'C': lambda t:t,
-  'F': lambda t:t * 9 / 5 + 32,
-  'K': lambda t:t + 273.15,
-  'R': lambda t:(t + 273.15) * 9 / 5,
-  'De': lambda t:(100 - t) * 3 / 2,
-  'N': lambda t:t * 33 / 100,
-  'Re': lambda t:t * 4 / 5,
-  'Ro': lambda t:t * 21 / 40 + 7.5
-}
-
-def convert_temp(n,f,t):
-    if f==t:
-        return n
-    n=fromCelcius[t](toCelcius[f](n))
-    v=n<0
-    n=abs(n)
-    return -floor(n) if v else floor(n)
-
-print(convert_temp(100, "C", "F"))
+print(find_summands(357))
