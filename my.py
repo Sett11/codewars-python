bdc0fda69d3438969bcea6135fd50e5d59dd24ba
Jhonny <gonzodzen@mail.ru>
2024-08-15 01:39:42 +0300

    Compute a convex hull

--- my.py
+++ my.py
@@ -1,9 +1,16 @@
-from scipy.spatial import ConvexHull
+def ccw(ax, ay, bx, by, cx, cy): return (bx - ax) * (cy - ay) - (cx - ax) * (by - ay)
 
-def convex_hull_area(p):
-    try:
-        return round(ConvexHull([[str(j) for j in i] for i in p]).volume,2)
-    except:
-        return 0
+def graham_scan(points):
+    X, Y = [], []
+    points = sorted(points)
+    for p in points:
+        while len(X) > 1 and ccw(*X[-2], *X[-1], *p) <= 0: X.pop()
+        while len(Y) > 1 and ccw(*Y[-2], *Y[-1], *p) >= 0: Y.pop()
+        X.append(p), Y.append(p)
+    return X,Y
 
-print(convex_hull_area({(0, 0), (0, 1), (1, 0), (1, 1), (2, 0), (2, 1), (3, 0)}))
+def hull_method(points):
+    X, Y = graham_scan(points)
+    return sorted(map(list,set(map(tuple,X+Y))))
+
+print(hull_method([[6, 20], [9, 13], [8, 6], [5, 6], [20, 4], [19, 4], [15, 19], [9, 16], [4, 11], [1, 20]]))
