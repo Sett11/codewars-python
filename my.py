7d504560bf25acadedc49ec98e0e73159a53b747
Jhonny <gonzodzen@mail.ru>
2024-08-15 23:53:33 +0300

    Unique Pairs

--- my.py
+++ my.py
@@ -1,12 +1,4 @@
-from math import ceil
-from re import sub
+from math import comb
 
-def convert_recipe(s):
-    def f(s):
-        s=s.group()
-        d,x={'tsp':5,'tbsp':15},s.split()
-        return s+f" ({str(ceil(eval(x[0])*d[x[1]]))}g)"
-    return sub(r'\d+\/*\d* (tsp|tbsp)',f,s)
-
-print(convert_recipe("Add to the mixing bowl and coat well with 1 tbsp of olive oil & 1/2 tbsp of dried dill"))
-print(convert_recipe("14/5 tsp of cinnamon" ))
+def projectPartners(n):
+    return comb(n,2)
