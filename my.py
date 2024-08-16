e6686e1c38c70b56c7d2a5d508cdfb7dfd1b8209
Jhonny <gonzodzen@mail.ru>
2024-08-16 11:19:30 +0300

    Pair 'em Up

--- my.py
+++ my.py
@@ -1,37 +1,11 @@
-# def comb(n,r):
-#     res=[]
-#     t=[0]*r
-#     def f():
-#         q=[i for i in t]
-#         if len(q)==len(set(q)):
-#             res.append(q)
-#     while 1:
-#         for i in reversed(range(r)):
-#             if t[i]!=i+n-r:
-#                 break
-#         else:
-#             return res
-#         t[i]+=1
-#         for j in range(i+1,r):
-#             t[j]=t[j-1]+1
-#         f()
+from itertools import combinations as comb
 
-# def pair_em_up(n):
-#     return sorted(sum([comb(n,i) for i in range(2,n+(1 if n%2==0 else 0),2)],[]),key=lambda x:(x[0],-len(x)))
+def pair_em_up(n):
+     return sorted(sum([list(map(list,comb([j for j in range(n)],i))) for i in range(2,n+(1 if n%2==0 else 0),2)],[]),key=lambda x:\
+                   (x[0],x[1],x[2] if len(x)>2 else float('inf'),x[3] if len(x)>3 else float('inf'),x[4] if len(x)>4 else float('inf'),\
+                    x[5] if len(x)>5 else float('inf'),x[6] if len(x)>6 else float('inf'),x[7] if len(x)>7 else float('inf'),x[8] if len(x)>8 else float('inf'),\
+                    x[9] if len(x)>9 else float('inf'),x[10] if len(x)>10 else float('inf'),x[11] if len(x)>11 else float('inf'),x[12] if len(x)>12 else float('inf'),
+                    x[13] if len(x)>13 else float('inf'),x[14] if len(x)>14 else float('inf'),x[15] if len(x)>15 else float('inf'),x[16] if len(x)>16 else float('inf'),
+                    x[17] if len(x)>17 else float('inf'),x[18] if len(x)>18 else float('inf'),x[19] if len(x)>19 else float('inf'),x[20] if len(x)>20 else float('inf'),-len(x)))
 
-# print(pair_em_up(20))
-
-from math import ceil
-
-def get_missing_ingredients(r,a):
-    n=max([ceil(a[i]/r[i]) for i in a if i in r],default=0)
-    if not n:
-        return r
-    d={i:r[i]*n-a[i] for i in a if i in r}
-    for i in r:
-        if i not in d:
-            d[i]=r[i]*n
-    return {i:d[i] for i in d if d[i]}
-
-print(get_missing_ingredients({"flour": 200, "eggs": 1, "sugar": 100}, {"flour": 123, "eggs": 70}))
-print(get_missing_ingredients({"flour": 200, "eggs": 1, "sugar": 100}, {"flour": 100}))
+print(pair_em_up(7))
