afa87b0cb243d1ad3ba3877d2818402f84f9ec20
Jhonny <gonzodzen@mail.ru>
2024-08-19 00:19:40 +0300

    Portal 2: End credits song. Text printing.

--- my.py
+++ my.py
@@ -1,16 +1,21 @@
-def compound_match(a,s):
-    u,r,q=set(a),[],[]
-    for i in range(len(s)):
-        t,p=s[:i],s[i:]
-        if t in u and p in u:
-            r.extend([t,p])
-            break
-    for i in range(len(a)):
-        if a[i] in (t,p) and a[i] not in [j[0] for j in q]:
-            q.append((a[i],i))
-        if len(q)==2:
-            q=dict(sorted(q,key=lambda x:r.index(x[0])))
-            return sorted(r,key=lambda x:q[x])+[list(q.values())]
+def lyrics_print(a):
+    r=[]
+    def f(x):
+        t=[]
+        a=[i for i in x.copy()]
+        for i in reversed(range(len(a))):
+            for j in reversed(range(len(a[i]))):
+                t.append([k for k in a.copy() if k])
+                a[i]=a[i][:j]
+        return t[::-1]
+    for i in a:
+        x=f(i)
+        for j in range(len(x)):
+            x[j][-1]=x[j][-1]+'_'
+        r.extend(x)
+    return r
 
-print(compound_match(['bow','crystal','organic','ally','rain','line'],'crystalline'))
-print(compound_match(['ally', 'spell', 'less', 'knight', 'land', 'tale', 'tar', 'guile', 'folk', 'shear', 'land', 'tar', 'fall', 'bow', 'bed', 'grab', 'guile', 'red', 'super', 'top', 'line', 'bowl', 'red', 'wind', 'deck', 'let', 'in', 'tree', 'ally', 'ally', 'door', 'tar', 'top', 'back', 'tale', 'rain', 'tar', 'spell', 'bowl', 'back', 'night', 'bel', 'star', 'guile', 'rain', 'get', 'fin', 'organic', 'land', 'free'] ,'target'))
+print(*lyrics_print([
+         ['Hey','you',],
+         ['Good','luck',],
+     ]),sep='\n')
