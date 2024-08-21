05b2b3a5c18cbf2fa0e41a83b8dd13d7774c25d8
Jhonny <gonzodzen@mail.ru>
2024-08-22 02:29:32 +0300

    Only-Readable-Once list

--- my.py
+++ my.py
@@ -1,30 +1,28 @@
-from itertools import permutations as p
+class SecureList:
+    def __init__(self,items=[]):
+        self.items=items[:]
+        self.size=len(items)
 
-def separate(s,a):
-    r,x=[],list(s)[::-1]
-    for i in a:
-        j,t=i,[]
-        while j:
-            t.append(x.pop())
-            j-=1
-        r.append(''.join(t))
-    return r
-
-comb={3:[[1, 1, 1], [1, 2], [3]],4:[[1, 1, 1, 1], [1, 1, 2], [1, 3], [2, 2], [4]],5:[[1, 1, 1, 1, 1], [1, 1, 1, 2], [1, 1, 3], [1, 2, 2], [1, 4], [2, 3], [5]],6:[[1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 2], [1, 1, 1, 3], [1, 1, 2, 2], [1, 1, 4], [1, 2, 3], [1, 5], [2, 2, 2], [2, 4], [3, 3], [6]]}
-
-def check(n,k):
-    x=str(n*k)
-    m=len(x)
-    r=[list(x)]
-    for i in set(sum([list(p(i)) for i in comb[m][1:-1]],[])):
-        r.append(separate(x,i))
-    return sum(sum(int(j) for j in i) for i in r)
-
-def next_higher(n,k):
-    i=n+1
-    while i!=check(i,k):
-        i+=1
-    return i
+    def __repr__(self):
+        x=self.items
+        self.items=str([])
+        self.size=0
+        return str(x)
     
+    def __str__(self):
+        x=self.items
+        self.items=str([])
+        self.size=0
+        return str(x)
+    
+    def __getitem__(self,i):
+        self.size-=1
+        return self.items.pop(i)
+    
+    def __len__(self):
+        return self.size
 
-print(next_higher(2777,4))
+base=[1,2,3,4]
+a=SecureList(base)
+
+print(a[0],base[0])
