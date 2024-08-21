c290080c24196fb570fe61acc59412a91e3a9fc7
Jhonny <gonzodzen@mail.ru>
2024-08-22 00:17:55 +0300

    By the Power Set of Castle Grayskull

--- my.py
+++ my.py
@@ -1,8 +1,3 @@
-from functools import reduce
-from operator import mul
-from collections import Counter
-from itertools import product
+power=lambda a:sum([[list(j) for j in __import__('itertools').combinations(a,i)] for i in range(len(a)+1)],[])
 
-def get_num(a):
-    d=sorted(map(lambda x:reduce(mul,x),product(*[[j**i for i in range(k+1)] for j,k in Counter(a).items()])))[1:]
-    return [reduce(mul,a),len(d),d[0],d[-2]]
+print(power([1,2,3]))
