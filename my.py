ff21a48c2251c7603e40ad7db1839bd2f36aa106
Jhonny <gonzodzen@mail.ru>
2024-08-23 02:49:00 +0300

    Warm Up for Speed.

--- my.py
+++ my.py
@@ -1,21 +1,5 @@
-from collections import Counter
-from math import gcd
+def sorted_comm_by_digs(a,b):
+    dr,dsddr=lambda x:sum(map(int,str(x))),lambda x:sum(map(lambda y:int(y)**2,str(x)))
+    return sorted(set(a)&set(b),key=lambda x:(-(dsddr(k:=dr(x))+k),x))
 
-def find_prob(a,b):
-    d={'AL': 'Aluminum', 'AM': 'Amber', 'BG': 'Beige', 'BK': 'Black', 'BL': 'Blue', 'BN': 'Brown', 'BZ': 'Bronze', 'CH': 'Charcoal', 'CL': 'Clear', 'GD': 'Gold', 'GN': 'Green', 'GY': 'Gray', 'GT': 'Granite', 'IV': 'Ivory', 'LT': 'Light', 'OL': 'Olive', 'OP': 'Opaque', 'OR': 'Orange', 'PK': 'Pink', 'RD': 'Red', 'SM': 'Smoke', 'TL': 'Translucent', 'TN': 'Tan', 'TP': 'Transparent', 'VT': 'Violet', 'WT': 'White', 'YL': 'Yellow'}
-    a,b=Counter(a),[d[i] for i in b]
-    total=sum(a.values())
-    c=r=1
-    for i in b:
-        c*=total
-        r*=a[i]
-        total-=1
-        a[i]-=1
-    n=gcd(r,c)
-    try:
-        res=[r//n,c//n]
-        return res if all(i for i in res) else ['Impossible']
-    except:
-        return ['Impossible']
-
-print(find_prob(["Red","Red","Blue","Yellow","Yellow","Yellow","Red", "Yellow","Yellow","Blue","Yellow","Red","Blue","Yellow","Blue","Yellow","Blue","Yellow"],["RD", "YL", "YL", "BL", "GN"]))
+print(sorted_comm_by_digs([5, 56, 28, 35, 12, 27, 64, 99, 18, 31, 14, 6],[28, 17, 31, 63, 64, 5, 18, 17, 95, 56, 37, 5, 28, 16]))
