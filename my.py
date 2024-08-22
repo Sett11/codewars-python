251951e4b02e3795d19abecd00b828e4fa4708ab
Jhonny <gonzodzen@mail.ru>
2024-08-23 01:04:26 +0300

    "Shoot But As I've Seen It In My Imagination"

--- my.py
+++ my.py
@@ -1,28 +1,21 @@
-class SecureList:
-    def __init__(self,items=[]):
-        self.items=items[:]
-        self.size=len(items)
+from collections import Counter
+from math import gcd
 
-    def __repr__(self):
-        x=self.items
-        self.items=str([])
-        self.size=0
-        return str(x)
-    
-    def __str__(self):
-        x=self.items
-        self.items=str([])
-        self.size=0
-        return str(x)
-    
-    def __getitem__(self,i):
-        self.size-=1
-        return self.items.pop(i)
-    
-    def __len__(self):
-        return self.size
+def find_prob(a,b):
+    d={'AL': 'Aluminum', 'AM': 'Amber', 'BG': 'Beige', 'BK': 'Black', 'BL': 'Blue', 'BN': 'Brown', 'BZ': 'Bronze', 'CH': 'Charcoal', 'CL': 'Clear', 'GD': 'Gold', 'GN': 'Green', 'GY': 'Gray', 'GT': 'Granite', 'IV': 'Ivory', 'LT': 'Light', 'OL': 'Olive', 'OP': 'Opaque', 'OR': 'Orange', 'PK': 'Pink', 'RD': 'Red', 'SM': 'Smoke', 'TL': 'Translucent', 'TN': 'Tan', 'TP': 'Transparent', 'VT': 'Violet', 'WT': 'White', 'YL': 'Yellow'}
+    a,b=Counter(a),[d[i] for i in b]
+    total=sum(a.values())
+    c=r=1
+    for i in b:
+        c*=total
+        r*=a[i]
+        total-=1
+        a[i]-=1
+    n=gcd(r,c)
+    try:
+        res=[r//n,c//n]
+        return res if all(i for i in res) else ['Impossible']
+    except:
+        return ['Impossible']
 
-base=[1,2,3,4]
-a=SecureList(base)
-
-print(a[0],base[0])
+print(find_prob(["Red","Red","Blue","Yellow","Yellow","Yellow","Red", "Yellow","Yellow","Blue","Yellow","Red","Blue","Yellow","Blue","Yellow","Blue","Yellow"],["RD", "YL", "YL", "BL", "GN"]))
