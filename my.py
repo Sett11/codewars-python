237d7731587ba78a95e6c26f8277fcc914a4b877
Jhonny <gonzodzen@mail.ru>
2024-08-25 00:18:07 +0300

    Maximum subarray sum

--- my.py
+++ my.py
@@ -1,18 +1,11 @@
-from collections import Counter
-from functools import reduce
-from operator import mul
-from math import factorial as f
-from decimal import Decimal as D
+def max_sequence(a):
+    m=n=0
+    for i in a:
+        n+=i
+        if n>m:
+            m=n
+        if n<0:
+            n=0
+    return m
 
-def list_position(w):
-    s,c=sorted(w),1
-    for i in w:
-        if i==s[0]:
-            s.pop(0)
-            continue
-        t=s.index(i)
-        c+=(t*D(f(len(s)-1)/reduce(mul,map(f,Counter(s).values()))))
-        s.pop(t)
-    return int(c)
-
-print(list_position('IMMUNOELECTROPHORETICALLY'))
+print(max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
