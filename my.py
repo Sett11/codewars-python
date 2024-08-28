05952a87fd4b4d6de7c2292f0a979844dab8daca
Jhonny <gonzodzen@mail.ru>
2024-08-29 02:06:30 +0300

    Tim's Fruit Shop Challenge

--- my.py
+++ my.py
@@ -1,48 +1,24 @@
-from re import sub
+from re import compile
 
-def simple_assembler(a):
-	env,i={},0
-	a=[i.split() for i in a]
-	while i<len(a):
-		t=a[i]
-		if t[0]=='mov':
-			env[t[1]]=int(t[2]) if sub(r'-','',t[2]).isdigit() else env[t[2]]
-			i+=1
-			continue
-		elif t[0]=='inc':
-			env[t[1]]+=1
-			i+=1
-			continue
-		elif t[0]=='dec':
-			env[t[1]]-=1
-			i+=1
-			continue
-		elif t[0]=='jnz':
-			if t[1] in env and env[t[1]]:
-				i+=int(t[2])
-				continue
-			elif t[1] not in env and int(t[1]):
-				i+=int(t[2])
-			else:
-				i+=1
-	return env
+def fruit_pack(a):
+    def hand_orders(n,s):
+        box,pallet,bag=[],[],[]
+        while n>=50:
+            pallet.append(f'[{s}]')
+            n-=50
+        while n>=10:
+            box.append('{'+s+'}')
+            n-=10
+        if n:
+            bag.append(f'({s*n})')
+        return [''.join(bag),''.join(box),''.join(pallet)]
+    
+    def hand_days(s):
+        a=[''.join(k) for k in zip(*[hand_orders(*i) for i in [[int(i[:-1]),i[-1:]] for i in compile(r'\d+[a-z]{1,1}').findall(s)]])]
+        m=len(max(a,key=len))
+        return [i.rjust(m,'-') for i in a]
+    
+    return [hand_days(i) for i in a]
 
-print(simple_assembler('''\
-mov a 5
-inc a
-dec a
-dec a
-jnz a -1
-inc a'''.splitlines()))
-print(simple_assembler('''\
-mov c 12
-mov b 0
-mov a 200
-dec a
-inc b
-jnz a -2
-dec c
-mov a b
-jnz c -5
-jnz 0 1
-mov c a'''.splitlines()))
+
+print(fruit_pack(['20f5j', '100x33y']))
