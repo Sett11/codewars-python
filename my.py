a8ff98aeefc2c3f96c96bbcc015b85c539027387
Jhonny <gonzodzen@mail.ru>
2024-08-30 15:33:43 +0300

    Esolang: InfiniTick

--- my.py
+++ my.py
@@ -1,6 +1,36 @@
-def convert_lambda_to_def(s):
-    a=s.split('=')
-    f_head=f"def {a[0].strip()}({a[1].split(':')[0].replace('lambda','',1).strip()}):\n    return "
-    return f_head+a[1].split(': ')[1]
+def interpreter(s):
+   db,res,pos,i=[0],'',0,0
+   while s[i]!='&':
+      if s[i]=='<':
+         if pos==0:
+            db.insert(0,0)
+         else:
+            pos-=1
+      elif s[i]=='>':
+         if pos==len(db)-1:
+            db.append(0)
+         pos+=1
+      elif s[i]=='+':
+         db[pos]+=1
+         if db[pos]==256:
+            db[pos]=0
+      elif s[i]=='-':
+         db[pos]-=1
+         if db[pos]==-1:
+            db[pos]=255
+      elif s[i]=='*':
+         res+=chr(db[pos])
+      elif s[i]=='/':
+         if db[pos]==0:
+            i+=1
+      elif s[i]=='\\':
+         if db[pos]:
+            i+=1
+      else:
+         pass
+      i+=1
+      if i>=len(s):
+         i%=len(s)
+   return res
 
-print(convert_lambda_to_def("act = lambda h: h + ' ' + 'i'"))
+print(interpreter('+*\&'))
