7c9a14c73b50420c126c8b0474d0bcc180153755
Jhonny <gonzodzen@mail.ru>
2024-08-31 13:07:50 +0300

    RoboScript #2 - Implement the RS1 Specification

--- my.py
+++ my.py
@@ -1,49 +1,51 @@
-def boolfuck(s,c=''):
-   c=list(map(int,''.join(bin(ord(i))[2:][::-1].ljust(8,'0') for i in c)))
-   while len(c)%8:
-      c+=[0]
-   db,res=[0],[]
-   i=j=k=0
-   while i<len(s):
-      if s[i]=='>':
-         if j==len(db)-1:
-            db.append(0)
-         j+=1
-      elif s[i]=='<':
-         if j==0:
-            db.insert(0,0)
-         else:
-            j-=1
-      elif s[i]=='+':
-         db[j]=0 if db[j]==1 else 1
-      elif s[i]==',':
-         db[j]=c[k] if k<len(c) else 0
-         k+=1
-      elif s[i]==';':
-         res.append(db[j])
-      elif s[i]=='[':
-         if db[j]==0:
-            x=1
-            while x:
-               i+=1
-               if s[i]=='[':
-                  x+=1
-               elif s[i]==']':
-                  x-=1
-      elif s[i]==']':
-         if db[j]:
-            x=1
-            while x:
-               i-=1
-               if s[i]==']':
-                  x+=1
-               elif s[i]=='[':
-                  x-=1
-      i+=1
-   while len(res)%8:
-      res.append(0)
-   return ''.join(chr(int(''.join(str(j) for j in res[i:i+8][::-1]),2)) for i in range(0,len(res),8))
+from re import sub
 
-print(boolfuck('>,>,>,>,>,>,>,>,<<<<<<<<>;>;>;>;>;>;>;>;<<<<<<<<','*'))
-print(boolfuck(";;;+;+;;+;+;+;+;+;+;;+;;+;;;+;;+;+;;+;;;+;;+;+;;+;+;;;;+;+;;+;;;+;;+;+;+;;;;;;;+;+;;+;;;+;+;;;+;+;;;;+;+;;+;;+;+;;+;;;+;;;+;;+;+;;+;;;+;+;;+;;+;+;+;;;;+;+;;;+;+;+;", ""))
-print(boolfuck(">,>,>,>,>,>,>,>,<<<<<<<[>]+<[+<]>>>>>>>>>[+]+<<<<<<<<+[>+]<[<]>>>>>>>>>[+<<<<<<<<[>]+<[+<]>>>>>>>>>+<<<<<<<<+[>+]<[<]>>>>>>>>>[+]<<<<<<<<;>;>;>;>;>;>;>;<<<<<<<,>,>,>,>,>,>,>,<<<<<<<[>]+<[+<]>>>>>>>>>[+]+<<<<<<<<+[>+]<[<]>>>>>>>>>]<[+<]", "Codewars\u00ff"))
+def execute(s):
+   def f(s):
+      x=s.group()
+      return x[0]*int(x[1:])
+   s=sub(r'([A-Z]{,1}\d+)',f,s)
+   board,directions,cur_course=[['*']],{'left':lambda x,y:(x,y-1),'right':lambda x,y:(x,y+1),'down':lambda x,y:(x+1,y),'up':lambda x,y:(x-1,y)},'right'
+   c=r=0
+   for i in s:
+      if i=='L':
+         if cur_course=='right':
+            cur_course='up'
+         elif cur_course=='left':
+            cur_course='down'
+         elif cur_course=='down':
+            cur_course='right'
+         elif cur_course=='up':
+            cur_course='left'
+      if i=='R':
+         if cur_course=='right':
+            cur_course='down'
+         elif cur_course=='left':
+            cur_course='up'
+         elif cur_course=='down':
+            cur_course='left'
+         elif cur_course=='up':
+            cur_course='right'
+      if i=='F':
+         if c==0 and cur_course=='up':
+            board.insert(0,[' ']*len(board[0]))
+         if c==len(board)-1 and cur_course=='down':
+            board.append([' ']*len(board[0]))
+         if r==0 and cur_course=='left':
+            for k in range(len(board)):
+               board[k].insert(0,' ')
+         if r==len(board[0])-1 and cur_course=='right':
+            for k in range(len(board)):
+               board[k].append(' ')
+         c,r=directions[cur_course](c,r)
+         if c<0:
+            c=0
+         elif r<0:
+            r=0
+         board[c][r]='*'
+   return '\r\n'.join(map(lambda x:''.join(x),board))
+
+   
+print(execute('L9F2FL5FL5RL5F10R'))
+print(9)
+print('**\r\n**\r\n**\r\n**\r\n* \r\n* \r\n* \r\n* \r\n* \r\n* \r\n* ')
