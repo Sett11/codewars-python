f1a20121af14664db8b5bb87fd2d771065c145cd
Jhonny <gonzodzen@mail.ru>
2024-09-04 00:06:51 +0300

    The learning game - Machine Learning #1

--- my.py
+++ my.py
@@ -1,57 +1,14 @@
-# from random import choice
-# from itertools import product
+from preloaded import ACTIONS
 
-# u=set([tuple(i) for i in product([0,1],repeat=9)]+[tuple(i) for i in product([0,1],repeat=9)])
-# u_list=list(u)
-
-# def move(grid,k):
-#    n=len(grid)
-#    res,new_grid=[],grid.copy()
-#    while k:
-#       for i in range(n):
-#          for j in range(n):
-#             t=[(r,c) for r,c in [(i+1,j),(i,j+1),(i-1,j),(i,j-1),(i+1,j+1),(i-1,j-1),(i+1,j-1),(i-1,j+1)] if r>=0 and r<n and c>=0 and c<n]
-#             res.append((i,j,[grid[r][c] for r,c in t].count(1)))
-#       for i,j,k in res:
-#          if grid[i][j]==1 and (k>3 or k<2):
-#             new_grid[i][j]=0
-#          if grid[i][j]==0 and k==3:
-#             new_grid[i][j]=1
-#       grid=new_grid
-#       k-=1
-#       return grid
-
-# def f(a):
-#    return '\n'.join(list(''.join(str(j) for j in i) for i in a))
-
-# def shange(a):
-#    return [list(a[i:i+3]) for i in range(0,9,3)]
-
-# x=shange(choice(u_list))
-# for i in u:
-#    x=move(shange(i),1)
-#    t=[]
-#    for j in u:
-#       if i!=j:
-#          y=move(shange(j),1)
-#          if y==x:
-#             t.append(shange(j))
-#    if len(t)>1:
-#       print(f(shange(i)),[f(k) for k in t])
-#       break
-
-
-from preloaded import send_request
-import asyncio
-
-
-async def req(n):
-   return ''.join(await asyncio.gather(*[send_request() for _ in range(n)]))
-
-async def request_manager(n):
-   r=''
-   while n>0:
-      c=150 if n>150 else n
-      r+=await req(c)
-      n-=c
-   return r  
+class Machine:
+    def __init__(self):
+        self.c=0
+        self.act=[0]*5
+    
+    def command(self,cmd,num):
+        self.c=cmd
+        return ACTIONS()[self.act[self.c%5]%5](num)
+        
+    def response(self,res):
+        if not res:
+            self.act[self.c%5]+=1
