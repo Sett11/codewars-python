8dabcc7837cbdc5e17e33f8fb8d78bf7d37d2cc3
Jhonny <gonzodzen@mail.ru>
2024-09-04 02:34:59 +0300

    Buying a car

--- my.py
+++ my.py
@@ -1,7 +1,14 @@
-def bonus(a,s):
-   m=min(a)
-   r=[1/(i/m) for i in a]
-   c=sum(r)
-   return [round(s*i/c) for i in r]
+def nb_months(old,new,n,p):
+   if new<=old:
+      return [0,old-new]
+   t,m=0,1
+   while t<new:
+      if m and m%2==0:
+         p+=.5
+      old-=old*p/100
+      new-=new*p/100
+      t=old+n*m
+      m+=1
+   return [m-1,round(t-new)]
 
-print(bonus([18, 15, 12], 851))
+print(nb_months(2000, 8000, 1000, 1.5))
