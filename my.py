97630cedef1a408994a6336b54652fa4b3d8b03a
Jhonny <gonzodzen@mail.ru>
2024-09-05 00:19:36 +0300

    Two Joggers

--- my.py
+++ my.py
@@ -1,4 +1,13 @@
-def to_brainfuck(s):
-    return ''.join('+'*ord(i)+'.>' for i in s)[:-1]
+from math import gcd
 
-print(to_brainfuck("Hello World!"),'')
+def nbr_of_laps(x,y):
+   if x==y:
+      return 1,1
+   n=gcd(x,y)
+   if n==1:
+      return (x,y)[::-1]
+   t=max(x,y)/n,min(x,y)/n
+   return t if y>x else tuple(sorted(t))
+    
+print(nbr_of_laps(2487, 3628))
+print(nbr_of_laps(4,6))
