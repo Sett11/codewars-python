f543e5bc0a92348f42a7d833971eac73b6851c1c
Jhonny <gonzodzen@mail.ru>
2024-09-05 01:25:42 +0300

    Total increasing or decreasing numbers up to a power of 10

--- my.py
+++ my.py
@@ -1,15 +1,18 @@
-def f(a):
-   x=next(a)
-   while 1:
-      try:
-         y=next(a)
-         yield y.__sub__(x)
-         x=y
-      except StopIteration:
-         return
+def total_inc_dec(n):
+   a,b,t=[1]*9,[1]*9,10
+   for _ in range(2,n+1):
+      c,d=[0]*9,[1]*9
+      x=y=0
+      for j in range(8,-1,-1):
+         x+=a[j]
+         c[j]+=x
+         t+=c[j]
+      for j in range(9):
+         y+=b[j]
+         d[j]+=y
+         t+=d[j]
+      t-=9
+      a,b,c,d=c,d,a,b
+   return t if n else 1
 
-def delta(v,n):
-   a=iter(v)
-   if n==0:
-      return a
-   return delta(f(a),n-1)
+print(total_inc_dec(6))
