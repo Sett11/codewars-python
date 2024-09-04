1397d83d9bce105e55509324bc40dc62b916dc1b
Jhonny <gonzodzen@mail.ru>
2024-09-05 00:32:03 +0300

    Death by Coffee

--- my.py
+++ my.py
@@ -1,13 +1,20 @@
-from math import gcd
+def coffee_limits(a,b,c):
+   n=k=int(''.join(map(lambda x:str(x).rjust(2,'0'),[a,b,c])))
+   x,y,r=int('CAFE',16),int('DECAF',16),[0]*2
+   vn=vk=True
+   for i in range(1,5001):
+      if vn:
+         n+=x
+         if 'DEAD' in hex(n).upper():
+            vn=False
+            r[0]=i
+      if vk:
+         k+=y
+         if 'DEAD' in hex(k).upper():
+            vk=False
+            r[1]=i
+      if not vn and not vk:
+         break
+   return r
 
-def nbr_of_laps(x,y):
-   if x==y:
-      return 1,1
-   n=gcd(x,y)
-   if n==1:
-      return (x,y)[::-1]
-   t=max(x,y)/n,min(x,y)/n
-   return t if y>x else tuple(sorted(t))
-    
-print(nbr_of_laps(2487, 3628))
-print(nbr_of_laps(4,6))
+print(coffee_limits(1950, 1, 19))
