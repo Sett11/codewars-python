e8591e7f8182489a9b94b85e07d8c693b77c2ac5
Jhonny <gonzodzen@mail.ru>
2024-09-06 00:40:26 +0300

    Simple Fun #84: Box Blur

--- my.py
+++ my.py
@@ -1,18 +1,23 @@
-def bouncy_count(n):
-   a,b,t=[1]*9,[1]*9,10
-   for _ in range(2,n+1):
-      c,d=[0]*9,[1]*9
-      x=y=0
-      for j in range(8,-1,-1):
-         x+=a[j]
-         c[j]+=x
-         t+=c[j]
-      for j in range(9):
-         y+=b[j]
-         d[j]+=y
-         t+=d[j]
-      t-=9
-      a,b,c,d=c,d,a,b
-   return 10**n-t if n else 0
+def box_blur(a):
+   n,m,r=len(a),len(a[0]),[]
+   for i in range(1,n-1):
+      r.append([])
+      for j in range(1,m-1):
+         t=0
+         for k in range(i-1,i+2):
+            for c in range(j-1,j+2):
+               t+=a[k][c]
+         r[i-1].append(t//9)
+   return r
 
-print(bouncy_count(4))
+print(box_blur([
+            [1, 1, 1],
+            [1, 7, 1],
+            [1, 1, 1]
+        ]))
+print(box_blur([
+            [7, 4, 0, 1],
+            [5, 6, 2, 2],
+            [6, 10, 7, 8],
+            [1, 4, 2, 0]
+        ]))
