411a5c604ea65f249a669761b05ada15fe2da767
Jhonny <gonzodzen@mail.ru>
2024-09-08 02:44:49 +0300

    Pretty date

--- my.py
+++ my.py
@@ -1,6 +1,19 @@
-from itertools import combinations_with_replacement as c
+def to_pretty(n):
+   if n==0:
+      return 'just now'
+   if n<60:
+      return f"{n if n>1 else ''}{'a' if n==1 else ''} second{'s' if n>1 else ''} ago"
+   if n<3600:
+      n//=60
+      return f"{n if n>1 else ''}{'a' if n==1 else ''} minute{'s' if n>1 else ''} ago"
+   if n<86400:
+      n//=3600
+      return f"{n if n>1 else ''}{'an' if n==1 else ''} hour{'s' if n>1 else ''} ago"
+   if n<604800:
+      n//=86400
+      return f"{n if n>1 else ''}{'a' if n==1 else ''} day{'s' if n>1 else ''} ago"
+   n//=604800
+   return f"{n if n>1 else ''}{'a' if n==1 else ''} week{'s' if n>1 else ''} ago"
+   
 
-def find(a,n):
-   return len(sum([[list(j) for j in c(a,i) if sum(j)==n] for i in range(1,len(a)+1)],[]))
-
-print(find([3,6,9,12],12))
+print(to_pretty(2220000))
