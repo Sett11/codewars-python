71249a389f539b70f5c245ee008d7e659c7fd065
Jhonny <gonzodzen@mail.ru>
2024-09-08 01:34:23 +0300

    Ranking NBA teams

--- my.py
+++ my.py
@@ -1,9 +1,48 @@
-def solve(s):
-   n,c=len(s),0
-   for i in range(n):
-      for j in range(i+1,n+1):
-         if int(s[i:j])&1:
-            c+=1
-   return c
+from re import search
 
-print(solve('13472315'))
+def nba_cup(r,s):
+   W=D=L=S=C=P=0
+   if not s:
+      return ''
+   q=[i for i in r.split(',') if s+' ' in i]
+   if not q:
+      return f"{s}:This team didn't play!"
+   for i in q:
+      x=search(r' \d+ ',i)
+      if not x:
+         return f'Error(float number):{i}'
+      x=x.end()
+      a,b=i[:x-1],i[x:]
+      y=1 if s in b else 0
+      a,b=int([j for j in a.split() if j.isdigit()][0]),int([j for j in b.split() if j.isdigit()][0])
+      if y:
+         a,b=b,a
+      if a>b:
+         W+=1
+         P+=3
+      elif a<b:
+         L+=1
+      elif a==b:
+         D+=1
+         P+=1
+      S+=a
+      C+=b
+   return f"{s}:W={W};D={D};L={L};Scored={S};Conceded={C};Points={P}"
+
+print(nba_cup(("Los Angeles Clippers 104 Dallas Mavericks 88,New York Knicks 101 Atlanta Hawks 112,Indiana Pacers 103 Memphis Grizzlies 112,"
+     "Los Angeles Lakers 111 Minnesota Timberwolves 112,Phoenix Suns 95 Dallas Mavericks 111,Portland Trail Blazers 112 New Orleans Pelicans 94,"
+     "Sacramento Kings 104 Los Angeles Clippers 111,Houston Rockets 85 Denver Nuggets 105,Memphis Grizzlies 76 Cleveland Cavaliers 106,"
+     "Milwaukee Bucks 97 New York Knicks 122,Oklahoma City Thunder 112 San Antonio Spurs 106,Boston Celtics 112 Philadelphia 76ers 95,"
+     "Brooklyn Nets 100 Chicago Bulls 115,Detroit Pistons 92 Utah Jazz 87,Miami Heat 104 Charlotte Hornets 94,"
+     "Toronto Raptors 106 Indiana Pacers 99,Orlando Magic 87 Washington Wizards 88,Golden State Warriors 111 New Orleans Pelicans 95,"
+     "Atlanta Hawks 94 Detroit Pistons 106,Chicago Bulls 97 Cleveland Cavaliers 95,"
+     "San Antonio Spurs 111 Houston Rockets 86,Chicago Bulls 103 Dallas Mavericks 102,Minnesota Timberwolves 112 Milwaukee Bucks 108,"
+     "New Orleans Pelicans 93 Miami Heat 90,Boston Celtics 81 Philadelphia 76ers 65,Detroit Pistons 115 Atlanta Hawks 87,"
+     "Toronto Raptors 92 Washington Wizards 82,Orlando Magic 86 Memphis Grizzlies 76,Los Angeles Clippers 115 Portland Trail Blazers 109,"
+     "Los Angeles Lakers 97 Golden State Warriors 136,Utah Jazz 98 Denver Nuggets 78,Boston Celtics 99 New York Knicks 85,"
+     "Indiana Pacers 98 Charlotte Hornets 86,Dallas Mavericks 87 Phoenix Suns 99,Atlanta Hawks 81 Memphis Grizzlies 82,"
+     "Miami Heat 110 Washington Wizards 105,Detroit Pistons 94 Charlotte Hornets 99,Orlando Magic 110 New Orleans Pelicans 107,"
+     "Los Angeles Clippers 130 Golden State Warriors 95,Utah Jazz 102 Oklahoma City Thunder 113,San Antonio Spurs 84 Phoenix Suns 104,"
+     "Chicago Bulls 103 Indiana Pacers 94,Milwaukee Bucks 106 Minnesota Timberwolves 88,Los Angeles Lakers 104 Portland Trail Blazers 102,"
+     "Houston Rockets 120 New Orleans Pelicans 100,Boston Celtics 111 Brooklyn Nets 105,Charlotte Hornets 94 Chicago Bulls 86,"
+     "Cleveland Cavaliers 103 Dallas Mavericks 97"),"Boston Celt"))
