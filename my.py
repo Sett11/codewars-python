8995323c85c587fb3ecbe1b97924f19fa162ee60
Jhonny <gonzodzen@mail.ru>
2024-09-08 02:22:43 +0300

    Centered pentagonal number:

--- my.py
+++ my.py
@@ -1,19 +1,5 @@
-def p_num(n):
-   return (((24*n+1)**.5+1)/6)%1==0 and n!=8635759279119974976128748222533
+def pentagonal(n):
+   n-=1
+   return (5*n**2+5*n+2)//2 if n>=0 else -1
 
-def g_p_num(n):
-   if n==8635759279119974976128748222533:
-    return False
-   if p_num(n):
-      return True
-   for i in range(2000):
-      x=i*(3*i+1)/2
-      if x==n or p_num(x):
-         return True
-   return False
-
-def s_p_num(n):
-   print(n)
-   return p_num(n) and (n**.5)%1==0
-
-print(g_p_num(5))
+print(pentagonal(77686))
