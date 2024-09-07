3db4bbe49912785e7bc997471869855a56604dc0
Jhonny <gonzodzen@mail.ru>
2024-09-08 00:37:44 +0300

    Non-even substrings

--- my.py
+++ my.py
@@ -1,18 +1,9 @@
-def robot_transfer(a,k):
-   c=0
-   for i in range(len(a)):
-      for j in range(len(a[0])):
-         u,z,x,y=set(),k,i,j
-         while z:
-            x,y=map(int,a[x][y].split(','))
-            z-=1
-            if [x,y]==[i,j] or (x,y) in u:
-               break
-            u.add((x,y))
-         c+=int([i,j]==[x,y] and z==0)
+def solve(s):
+   n,c=len(s),0
+   for i in range(n):
+      for j in range(i+1,n+1):
+         if int(s[i:j])&1:
+            c+=1
    return c
 
-print(robot_transfer([
-         ["0,1","0,0","1,2"], 
-         ["1,1","1,0","0,2"], 
-         ["2,1","2,0","0,0"]],2))
+print(solve('13472315'))
