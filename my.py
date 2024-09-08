4d991855efcb5cb9d980e3a4090b9ef941dcd10d
Jhonny <gonzodzen@mail.ru>
2024-09-09 01:53:52 +0300

    Popping Blocks

--- my.py
+++ my.py
@@ -1,10 +1,11 @@
-def factors(n):
-   r=[[],[]]
-   for i in range(2,int(n**.5)+1):
-      if n%(i**2)==0:
-         r[0].append(i)
-      if n%(i**3)==0:
-         r[1].append(i)
-   return r
+from itertools import groupby
 
-print(factors(81))
+def pop_blocks(a):
+   b=[list(j) for _,j in groupby(a)]
+   if all(len(i)==1 for i in b):
+      return sum(b,[])
+   b.pop(next(i for i,j in enumerate(b) if len(j)>1))
+   return pop_blocks(sum(b,[]))
+
+print(pop_blocks(['B', 'B', 'A', 'C', 'A', 'A', 'C']))
+print(pop_blocks(['A', 'B', 'A', 'A', 'A', 'B', 'B']))
