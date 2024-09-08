4cb4ad4caa5732cafbd37df7df3a28edba8baee7
Jhonny <gonzodzen@mail.ru>
2024-09-09 02:18:58 +0300

    Letterss of natac: build or buy

--- my.py
+++ my.py
@@ -1,13 +1,14 @@
-from functools import reduce
-from operator import mul,eq
+def build_or_buy(s):
+   r=[]
+   if all(i in s for i in 'bw'):
+      r.append('road')
+   if all(i in s for i in 'bwsg'):
+      r.append('settlement')
+   if all(i in s for i in 'ooogg'):
+      if s.count('o')>=3 and s.count('g')>=2:
+         r.append('city')
+   if all(i in s for i in 'osg'):
+      r.append('development')
+   return r
 
-def vector_op(f, *vs):
-   return [f(i) for i in zip(*vs)]
-    
-def iter_mult(*xs):
-   return reduce(mul,list(xs[0]),1)
-    
-def iter_eq(*xs):
-   return reduce(eq,list(xs[0]))
-
-print(vector_op(iter_eq,[1,2,3],[1,2,3]))
+print(build_or_buy('bwsg'))
