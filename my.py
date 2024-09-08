d18d69846d8d6659b043c0ff7396d48b1181f962
Jhonny <gonzodzen@mail.ru>
2024-09-09 02:33:57 +0300

    Possibilities of throwing a coin n times

--- my.py
+++ my.py
@@ -1,14 +1,9 @@
-def build_or_buy(s):
-   r=[]
-   if all(i in s for i in 'bw'):
-      r.append('road')
-   if all(i in s for i in 'bwsg'):
-      r.append('settlement')
-   if all(i in s for i in 'ooogg'):
-      if s.count('o')>=3 and s.count('g')>=2:
-         r.append('city')
-   if all(i in s for i in 'osg'):
-      r.append('development')
-   return r
+from itertools import product
+from functools import cache
 
-print(build_or_buy('bwsg'))
+@cache
+def coin(n):
+   return [''.join(i) for i in product('HT',repeat=n)]
+
+print(coin(14))
+print(coin(14))
