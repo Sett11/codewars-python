b7843e974f15ede363764970051c9036e98ec771
Jhonny <gonzodzen@mail.ru>
2024-09-09 00:04:36 +0300

    Square and Cubic Factors

--- my.py
+++ my.py
@@ -1,26 +1,10 @@
-round1=round
-
-def predict(c,a):
-   r={i:0 for i in c}
-   for i in range(len(c)):
-      t=[(k[0][i]*k[1],k[1]) for k in a]
-      r[c[i]]=round1(sum(j[0] for j in t)/sum(j[1] for j in t))
+def factors(n):
+   r=[[],[]]
+   for i in range(2,int(n**.5)+1):
+      if n%(i**2)==0:
+         r[0].append(i)
+      if n%(i**3)==0:
+         r[1].append(i)
    return r
 
-candidates = ["A", "B", "C"]
-
-poll1res = [20, 30, 50]
-poll1wt = 1
-poll1 = [poll1res, poll1wt]
-
-poll2res = [40, 40, 20]
-poll2wt = 0.5
-poll2 = [poll2res, poll2wt]
-
-poll3res = [50, 40, 10]
-poll3wt = 2
-poll3 = [poll3res, poll3wt]
-
-polls = [poll1, poll2, poll3]
-
-print(predict(candidates,polls)) 
+print(factors(81))
