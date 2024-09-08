9f90b51f55e9d79a7e71dd264b6c161e43fbcb16
Jhonny <gonzodzen@mail.ru>
2024-09-08 22:43:06 +0300

    Brainfuck Translator

--- my.py
+++ my.py
@@ -1,13 +1,25 @@
-def shortest_arrang(n):
-   for i in range(n//2+1,0,-1):
-      t=[i]
-      for j in range(n//2,0,-1):
-         if j<t[-1]:
-            t+=[j]
-            if sum(t)==n:
-               return t
-         if sum(t)>n:
-            break
-   return [-1]
+from re import sub,search
 
-print(shortest_arrang(65))
+
+def brainfuck_to_c(s):
+   r=r'(\+\-)|(\-\+)|(\<\>)|(\>\<)|(\[\])'
+   s=sub(r'[^\+\-\<\>\,\.\[\]]','',s)
+   while search(r,s):
+      s=sub(r,'',s)
+   s=sub(r'(\+|\-)+',lambda x:f'*p {x.group()[0]}= {len(x.group())};\n',s)
+   s=sub(r'(\<|\>)+',lambda x:f"p {'-' if x.group()[0]=='<' else '+'}= {len(x.group())};\n",s)
+   res=sub(r'\.|\,|\[|\]',lambda x:{'.':'putchar(*p);\n',',':'*p = getchar();\n','[':'if (*p) do {\n',']':'} while (*p);\n'}[x.group()],s).splitlines()
+   c=0
+   for i in range(len(res)):
+      if c<0:
+         return 'Error!'
+      res[i]=' '*c+res[i]
+      if 'if' in res[i]:
+         c+=2
+      if 'while' in res[i]:
+         c-=2
+         res[i]=res[i][2:]
+   return 'Error!' if c else '\n'.join(res)+('\n' if res else '')
+      
+
+print(brainfuck_to_c(("[>>[<<]]")))
