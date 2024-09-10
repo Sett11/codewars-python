db0c9689e52790f50a20cd394915d7fce0a3513e
Jhonny <gonzodzen@mail.ru>
2024-09-10 03:21:18 +0300

    Next polydivisible number

--- my.py
+++ my.py
@@ -1,34 +1,19 @@
-def merge(a,w,l,m,r):
-    t,i,j=[],l,m+1
-    while i<m+1 and j<=r:
-        if a[i][0]>a[j][0]:
-            w[a[i][1]]+=r-j+1
-            t.append(a[i])
-            i+=1
-        else:
-            t.append(a[j])
-            j+=1
-    while i<=m:
-        t.append(a[i])
-        i+=1
-    while j<=r:
-        t.append(a[j])
-        j+=1
-    k,i=0,l
-    while i<=r:
-        a[i]=t[k]
-        i+=1
-        k+=1
+def check(n):
+    s=str(n)
+    l=len(s)
+    for i in range(1,l+1):
+        if int(s[:i])%i:
+            return i
+    return True
 
-def merge_sort(a,r,i,j):
-    if i<j:
-        m=(i+j)//2
-        merge_sort(a,r,i,m),merge_sort(a,r,m+1,j),merge(a,r,i,m,j)
+def next_num(n):
+    n+=1
+    while n<3608528850368400786036726:
+        ch=check(n)
+        if ch is True:
+            return n
+        l=len(str(n))
+        k=10**(l-ch)
+        n=(n//k+1)*k
 
-def smaller(a):
-    n=len(a)
-    v,r=[[a[i],i] for i in range(n)],[0]*n
-    merge_sort(v,r,0,n-1)
-    return r
-
-print(smaller([5, 4, 7, 9, 2, 4, 1, 4, 5, 6]))
+print(next_num(123220))
