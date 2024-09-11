006a24507e2bd44fe13015d86034414bab96bfac
Jhonny <gonzodzen@mail.ru>
2024-09-11 15:25:58 +0300

    Linear Regression of Y on X

--- my.py
+++ my.py
@@ -1,8 +1,6 @@
-def variance(a):
-    u=[len(i) for i in a]
-    n=len(u)
-    m=sum(i*(1/n) for i in u)
-    return round(sum(1/n*(i-m)**2 for i in u),4)
+def regression_line(x,y):
+    n,xq,xs,ys,xys=len(x),sum(i**2 for i in x),sum(x),sum(y),sum(i*j for i,j in zip(x,y))
+    k=n*xq-xs**2
+    return round((xq*ys-xs*xys)/k,4),round((n*xys-xs*ys)/k,4)
 
-print(variance(['Hi','World']))
-print(variance(['The', 'girl', 'put', 'on', 'her', 'beautiful', 'red', 'hat']))
+print(regression_line([56,42,72,36,63,47,55,49,38,42,68,60],[147,125,160,118,149,128,150,145,115,140,152,155]))
