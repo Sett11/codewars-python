d839bfff115ee3e0405ebc836371af7e26a79738
Jhonny <gonzodzen@mail.ru>
2024-09-11 03:15:42 +0300

    Variance in a array of words

--- my.py
+++ my.py
@@ -1,12 +1,8 @@
-from statistics import mean
+def variance(a):
+    u=[len(i) for i in a]
+    n=len(u)
+    m=sum(i*(1/n) for i in u)
+    return round(sum(1/n*(i-m)**2 for i in u),4)
 
-def f(a,k):
-    n,x=len(a),mean(a)
-    s=(1/n*sum((i-x)**2 for i in a))**.5*k
-    r=[i for i in a if not (i>x+s or i<x-s)]
-    return r if r==a else f(r,k)
-
-def clean_mean(a,k):
-    return round(mean(f(a,k)),2)
-
-print(clean_mean([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100],3))
+print(variance(['Hi','World']))
+print(variance(['The', 'girl', 'put', 'on', 'her', 'beautiful', 'red', 'hat']))
