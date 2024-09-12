8b83c4f35fcaf657e60a6e1d5ce0ec0e1d4924fc
Jhonny <gonzodzen@mail.ru>
2024-09-13 02:29:19 +0300

    Find the smallest

--- my.py
+++ my.py
@@ -1,19 +1,15 @@
-from re import sub,search,findall
-from collections import Counter
+def smallest(n):
+    a,d=list(str(n)),{}
+    l,m=len(a),n
+    for i in range(l):
+        b=a.copy()
+        x=b.pop(i)
+        for j in range(l):
+            k=int(''.join(b[:j]+[x]+b[j:]))
+            m=min(m,k)
+            if k not in d:
+                d[k]=[]
+            d[k].append([i,j])
+    return [m,*sorted(d[m],key=lambda x:(x[0],x[1]))[0]]
 
-def parse_molecule(s):
-    tok=lambda x:[''.join(i) for i in findall(r'([A-Z][a-z])|([A-Z])',x)]
-    def f1(s):
-        k=s.group()
-        return ''.join(i for i in k if i.isalpha())*int(''.join(i for i in k if i.isdigit()))
-    def f2(s):
-        s=s.group()
-        g=s.rfind(')')
-        j=int(s[g+1:] or 1)
-        return ''.join(i*j for i in tok(s[1:g]))
-    s,r=sub(r'(([A-Z][a-z]|[A-Z])\d+)',f1,sub(r'\{|\[','(',sub(r'\}|\]',')',s))),r'\(([A-z]+)\)\d*'
-    while search(r,s):
-        s=sub(r,f2,s)
-    return Counter(tok(s))
-
-print(parse_molecule("(C5H5)Fe(CO)2CH3"))
+print(smallest(261235))
