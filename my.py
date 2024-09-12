ff27e90da794fe66bb5214c9872a04b5168892db
Jhonny <gonzodzen@mail.ru>
2024-09-13 00:37:00 +0300

    NIM the game

--- my.py
+++ my.py
@@ -1,25 +1,15 @@
-def dice_game(a):
-    print(a)
-    k,r,i=4,['p1','p2','p3','p4'],0
-    while i<len(a):
-        if len(r)==1:
-            return r[0]
-        q=a[i:i+k]
-        i+=k
-        w=sorted(q,key=sum)
-        if len([i for i in w if sum(i)==sum(w[0])])==1:
-            j=q.index(w[0])
-            r.pop(j)
-            k-=1
-        else:
-            x=sorted([i for i in q if sum(i)==sum(w[0])])
-            if len(set(x))!=len(x):
-                continue
-            else:
-                j=q.index(x[0])
-                r.pop(j)
-                k-=1
-    return r[0]
+from random import choice,randint
+from functools import reduce
 
+def choose_move(a):
+    n=reduce(lambda x,y:x^y,a)
+    r=[[i,j^n] for i,j in enumerate(a) if j^n<j]
+    if not r:
+        x,y=choice(list(enumerate(a)))
+        return [x,randint(1,y)]
+    x,y=min(r,key=lambda i:i[1])
+    return [x,a[x]-y]
 
-print(dice_game([(6, 2), (4, 4), (6, 1), (4, 4), (3, 3), (3, 4), (1, 2), (3, 2), (5, 3), (4, 5), (4, 4), (2, 2), (4, 5), (4, 5), (3, 2), (5, 1), (2, 3), (6, 1)]))
+print(choose_move((0, 6, 3, 6, 6, 3, 7, 5, 3, 3)))
+print(choose_move((0,3,5)))
+print(choose_move((3,3,6)))
