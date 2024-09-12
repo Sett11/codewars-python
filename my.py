6c4c014dfe800bf134acf6bb0c6d6fec65de5581
Jhonny <gonzodzen@mail.ru>
2024-09-13 01:57:21 +0300

    Molecule to atoms

--- my.py
+++ my.py
@@ -1,15 +1,19 @@
-from random import choice,randint
-from functools import reduce
+from re import sub,search,findall
+from collections import Counter
 
-def choose_move(a):
-    n=reduce(lambda x,y:x^y,a)
-    r=[[i,j^n] for i,j in enumerate(a) if j^n<j]
-    if not r:
-        x,y=choice(list(enumerate(a)))
-        return [x,randint(1,y)]
-    x,y=min(r,key=lambda i:i[1])
-    return [x,a[x]-y]
+def parse_molecule(s):
+    tok=lambda x:[''.join(i) for i in findall(r'([A-Z][a-z])|([A-Z])',x)]
+    def f1(s):
+        k=s.group()
+        return ''.join(i for i in k if i.isalpha())*int(''.join(i for i in k if i.isdigit()))
+    def f2(s):
+        s=s.group()
+        g=s.rfind(')')
+        j=int(s[g+1:] or 1)
+        return ''.join(i*j for i in tok(s[1:g]))
+    s,r=sub(r'(([A-Z][a-z]|[A-Z])\d+)',f1,sub(r'\{|\[','(',sub(r'\}|\]',')',s))),r'\(([A-z]+)\)\d*'
+    while search(r,s):
+        s=sub(r,f2,s)
+    return Counter(tok(s))
 
-print(choose_move((0, 6, 3, 6, 6, 3, 7, 5, 3, 3)))
-print(choose_move((0,3,5)))
-print(choose_move((3,3,6)))
+print(parse_molecule("(C5H5)Fe(CO)2CH3"))
