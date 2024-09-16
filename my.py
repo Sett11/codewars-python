4e4aafd056c89d1d18e03d540f0311cb4fb498c3
Jhonny <gonzodzen@mail.ru>
2024-09-17 01:04:30 +0300

    [Geometry A-1] Locate point - to the right, to the left or on the vector?

--- my.py
+++ my.py
@@ -1,13 +1,8 @@
 import numpy as np
 
-def point_vs_vector_v2(p,v):
-    if v==[[0.]*2]*2:
-        return None
+def point_vs_vector(p,v):
     (a,b),(c,d)=v
     x,y=p
-    z=np.cross([c-a,d-b,0],[x-a,y-b,0])[2]
-    if abs(z)<1e-9:
-        z=0
-    return -np.sign(z)
+    return -np.sign(np.cross([c-a,d-b,0],[x-a,y-b,0])[2])
 
-print(point_vs_vector_v2([0,1],[[0, 0], [1, 1]]))
+print(point_vs_vector([0,1],[[0, 0], [1, 1]]))
