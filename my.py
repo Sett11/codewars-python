2a7448153247b8d43559cc9f03fa1c848201b33f
Jhonny <gonzodzen@mail.ru>
2024-09-16 14:24:55 +0300

    Cats in hats

--- my.py
+++ my.py
@@ -1,14 +1,10 @@
-from textwrap import dedent
+def height(n):
+    h,r=2e6,0
+    while n:
+        h/=2.5
+        r+=h
+        n-=1
+    a,b=str(round(r+2e6,3)).split('.')
+    return f"{a}.{b.ljust(3,'0')}"
 
-def cat_mouse(a,n):
-    u=set(a)
-    if 'C' not in u or 'm' not in u:
-        return 'boring without two animals'
-    b,c=sum([[[k,i] for i,j in enumerate(p) if j in 'Cm'] for k,p in enumerate(a.splitlines())],[])
-    return ('Escaped!','Caught!')[abs(c[0]-b[0])+abs(c[1]-b[1])<=n]
-    
-
-print(cat_mouse(dedent("""\
-            ..C......
-            .........
-            ....m...."""),5))
+print(height(7))
