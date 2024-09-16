7b4cf466b80ed9af6d352b43860bd90e5128310a
Jhonny <gonzodzen@mail.ru>
2024-09-16 13:08:36 +0300

    The Pyramid of Cubes

--- my.py
+++ my.py
@@ -1,6 +1,13 @@
-cubes=set([i**3 for i in range(1000)])
+def find_height(n):
+    if n in [0,1]:
+        return n
+    i=j=0
+    while n>0:
+        i+=j
+        j+=1
+        n-=i
+    return j-(1 if n==0 else 2)
 
-def you_are_a_cube(n):
-    return n in cubes
-
-print(you_are_a_cube(125000000))
+print(find_height(7981))
+print(find_height(8474))
+print(find_height(4))
