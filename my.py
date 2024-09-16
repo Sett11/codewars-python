74e7b2ac4a48b72e75159668fef2e9f73fd38c31
Jhonny <gonzodzen@mail.ru>
2024-09-17 00:39:28 +0300

    [Geometry A-3] Does point belong to the vector? [DRY]

--- my.py
+++ my.py
@@ -1,5 +1,10 @@
-def vector_length(a):
-    a,b=a
-    return ((b[0]-a[0])**2+(b[1]-a[1])**2)**.5
+from decimal import Decimal as D
 
-print(vector_length([[0, 3],[4, 0]]))
+def point_in_vector(p,v):
+    l=lambda a,b,c,d:round(D(((a-c)**2+(b-d)**2)**.5),5)
+    (a,b),(c,d)=v
+    x,y=p
+    return abs(l(a,b,c,d)-(l(a,b,x,y)+l(c,d,x,y)))<.00001
+
+print(point_in_vector([1,1],[[0, 0], [3, 3]]))
+print(point_in_vector([-940.148005310891, -49.545449367196056],[[-940.148005310891, -355.3058135977864], [-940.148005310891, 171.45402379420827]]))
