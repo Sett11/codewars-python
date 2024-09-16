46656b367a943c0d749f6c47ab3327e2b0bec529
Jhonny <gonzodzen@mail.ru>
2024-09-16 15:04:10 +0300

    Simple Fun #164: Judge Votes

--- my.py
+++ my.py
@@ -1,8 +1,5 @@
-def solution(n,m):  
-    c=0
-    while n!=m:
-        n+=1 if n+3>m else 3
-        c+=1
-    return c
+def judge(a):
+    r=[30-i for i,j in enumerate(zip(*[bin(i)[2:].rjust(30,'0') for i in a])) if all(k=='1' for k in j)]
+    return r[0] if len(r)==1 else 0
 
-print(solution(2,4))
+print(judge([7, 15, 4])) 
