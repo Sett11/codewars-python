28e3d4530c7ddff74b711715d6b5064a51f0f63d
Jhonny <gonzodzen@mail.ru>
2024-09-17 02:15:50 +0300

    Simple Fun #158: Not So Random

--- my.py
+++ my.py
@@ -1,8 +1,2 @@
-import numpy as np
-
-def point_vs_vector(p,v):
-    (a,b),(c,d)=v
-    x,y=p
-    return -np.sign(np.cross([c-a,d-b,0],[x-a,y-b,0])[2])
-
-print(point_vs_vector([0,1],[[0, 0], [1, 1]]))
+def not_so_random(b,w):
+    return 'Black' if b%2 else 'White'
