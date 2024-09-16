e0bc09726ef0c51bdbb098fd5d87bab9a1a8945e
Jhonny <gonzodzen@mail.ru>
2024-09-16 12:41:50 +0300

    Build a pile of Cubes

--- my.py
+++ my.py
@@ -1,12 +1,8 @@
-from collections import Counter
+def find_nb(m):
+    i,t=1,0
+    while t<m:
+        t+=i**3
+        i+=1
+    return i-1 if t==m else -1
 
-def pos_average(s):
-    a,c=[list(i) for i in zip(*s.split(', '))],0
-    for i in a:
-        for j in Counter(i).values():
-            if j>1:
-                c+=sum(range(1,j))
-    return c/((len(a[0])*(len(a[0])-1)/2)*len(a))*100
-
-
-print(pos_average("444996, 699990, 666690, 096904, 600644, 640646, 606469, 409694, 666094, 606490"))
+print(find_nb(4183059834009))
