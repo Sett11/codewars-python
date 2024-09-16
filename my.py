8daba90482e4f62a94debb3007ff434faba86fb2
Jhonny <gonzodzen@mail.ru>
2024-09-17 01:00:27 +0300

    [Geometry A-1.1] Modify point location detector to handle zero-length vectors and precision errors [DRY]

--- my.py
+++ my.py
@@ -1,10 +1,13 @@
-from decimal import Decimal as D
+import numpy as np
 
-def point_in_vector(p,v):
-    l=lambda a,b,c,d:round(D(((a-c)**2+(b-d)**2)**.5),5)
+def point_vs_vector_v2(p,v):
+    if v==[[0.]*2]*2:
+        return None
     (a,b),(c,d)=v
     x,y=p
-    return abs(l(a,b,c,d)-(l(a,b,x,y)+l(c,d,x,y)))<.00001
+    z=np.cross([c-a,d-b,0],[x-a,y-b,0])[2]
+    if abs(z)<1e-9:
+        z=0
+    return -np.sign(z)
 
-print(point_in_vector([1,1],[[0, 0], [3, 3]]))
-print(point_in_vector([-940.148005310891, -49.545449367196056],[[-940.148005310891, -355.3058135977864], [-940.148005310891, 171.45402379420827]]))
+print(point_vs_vector_v2([0,1],[[0, 0], [1, 1]]))
