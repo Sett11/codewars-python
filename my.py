e2b8eebda8b1ac39512eec93e023f432ddcc6a5a
Jhonny <gonzodzen@mail.ru>
2024-09-16 13:25:37 +0300

    Invisible cubes

--- my.py
+++ my.py
@@ -1,13 +1,4 @@
-def find_height(n):
-    if n in [0,1]:
-        return n
-    i=j=0
-    while n>0:
-        i+=j
-        j+=1
-        n-=i
-    return j-(1 if n==0 else 2)
+def not_visible_cubes(n):
+    return (n-2)**3 if n>2 else 0
 
-print(find_height(7981))
-print(find_height(8474))
-print(find_height(4))
+print(not_visible_cubes(0))
