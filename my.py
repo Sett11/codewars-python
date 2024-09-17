dcd7505766777ee99cc216f340003d946b5caad4
Jhonny <gonzodzen@mail.ru>
2024-09-18 02:03:35 +0300

    The Hunger Games - Foxes and Chickens (Poison)

--- my.py
+++ my.py
@@ -1,28 +1,56 @@
-def who_eats_who(s):
-    d={'antelope': ['grass'], 'big-fish': ['little-fish'], 'bug': ['leaves'], 'bear': ['big-fish', 'bug', 'chicken', 'cow', 'leaves', 'sheep'], 'chicken': ['bug'], 'cow': ['grass'], 'fox': ['chicken', 'sheep'], 'giraffe': ['leaves'], 'lion': ['antelope', 'cow'], 'panda': ['leaves'], 'sheep': ['grass']}
-    a,r=s.split(','),[s]
-    while len(a)>1:
-        n,i=len(a),0
-        while i<n:
-            if a[i] in d:
-                if i==0:
-                    if a[i+1] in d[a[0]]:
-                        r.append(f'{a[i]} eats {a.pop(i+1)}')
-                        break
-                elif i==n-1:
-                    if a[i-1] in d[a[i]]:
-                        r.append(f'{a[i]} eats {a.pop(i-1)}')
-                        break
-                else:
-                    if a[i-1] in d[a[i]]:
-                        r.append(f'{a[i]} eats {a.pop(i-1)}')
-                        break
-                    if a[i+1] in d[a[i]]:
-                        r.append(f'{a[i]} eats {a.pop(i+1)}')
-                        break
-            i+=1
-        else:
+def f(a,q):
+    n=len(a)
+    z,v=q
+    i=z-1
+    j=z+1
+    x=y=True
+    while True:
+        if not (x or y):
             break
-    return r+[','.join(a)]
+        if i<0 or (v and a[i] in '[]'):
+            x=False
+        if j==n or (v and a[j] in '[]'):
+            y=False
+        if x:
+            if not v and a[i] in '[]':
+                for k in range(i-1,-1,-1):
+                    if a[k] in '[]':
+                        break
+                i=k
+            if a[i]=='C':
+                a[i]='.'
+                i-=1
+            elif a[i]=='X':
+                a[z]='.'
+                x=False
+            else:
+                i-=1
+        if y:
+            if not v and a[j] in '[]':
+                for k in range(j+1,n):
+                    if a[k] in '[]':
+                        break
+                j=k
+            if a[j]=='C':
+                a[j]='.'
+                j+=1
+            elif a[j]=='X':
+                a[z]='.'
+                y=False
+            else:
+                j+=1
+    return a
 
-print(who_eats_who("fox,bug,chicken,grass,sheep"))
+def hungry_foxes(s):
+    a=list(s)
+    c,r=False,[]
+    for i,j in enumerate(a):
+        if j in '[]':
+            c=not c
+        elif j=='F':
+            r.append((i,c))
+    [f(a,i) for i in r]
+    return ''.join(a)
+
+print(hungry_foxes("CCC[CCC]FCC[CCCCC]CFFFF[CCC]FFFF"))
+print(hungry_foxes("CCC[CCC]FCC[CCCCC]CFFFF[CCC]FFFF"))
