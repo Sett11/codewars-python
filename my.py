49bd308fee4ec81c9e7440918ae86138998ec61e
Jhonny <gonzodzen@mail.ru>
2024-09-18 01:21:10 +0300

    The Hunger Games - Zoo Disaster!

--- my.py
+++ my.py
@@ -1,8 +1,28 @@
-def find_employees_role(s):
-    x='Does not work here!'
-    try:
-        a,b=s.split()
-        r=[i['role'] for i in employees if i['first_name']==a and i['last_name']==b]
-        return r[0] if r else x
-    except:
-        return x
+def who_eats_who(s):
+    d={'antelope': ['grass'], 'big-fish': ['little-fish'], 'bug': ['leaves'], 'bear': ['big-fish', 'bug', 'chicken', 'cow', 'leaves', 'sheep'], 'chicken': ['bug'], 'cow': ['grass'], 'fox': ['chicken', 'sheep'], 'giraffe': ['leaves'], 'lion': ['antelope', 'cow'], 'panda': ['leaves'], 'sheep': ['grass']}
+    a,r=s.split(','),[s]
+    while len(a)>1:
+        n,i=len(a),0
+        while i<n:
+            if a[i] in d:
+                if i==0:
+                    if a[i+1] in d[a[0]]:
+                        r.append(f'{a[i]} eats {a.pop(i+1)}')
+                        break
+                elif i==n-1:
+                    if a[i-1] in d[a[i]]:
+                        r.append(f'{a[i]} eats {a.pop(i-1)}')
+                        break
+                else:
+                    if a[i-1] in d[a[i]]:
+                        r.append(f'{a[i]} eats {a.pop(i-1)}')
+                        break
+                    if a[i+1] in d[a[i]]:
+                        r.append(f'{a[i]} eats {a.pop(i+1)}')
+                        break
+            i+=1
+        else:
+            break
+    return r+[','.join(a)]
+
+print(who_eats_who("fox,bug,chicken,grass,sheep"))
