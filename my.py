a70ef22020e8c2de4fcc4775cbb1bb01e73fc3c1
Jhonny <gonzodzen@mail.ru>
2024-09-19 01:40:04 +0300

    Spelling Bee II

--- my.py
+++ my.py
@@ -1,11 +1,18 @@
-def count_subsequences(t,s):
-    n,m=len(s),len(t)
-    a=[0]*m
-    for i in range(n):
-        for j in range(m-1,-1,-1):
-            if s[i]==t[j]:
-                a[j]+=a[j-1] if j else 1
-    return a[-1]
+def d(a):
+    n,m,r=len(a),len(a[0]),[]
+    for i in range(n+m-1):
+        k,j,t=0 if i<m else i-m+1,i if i<m else m-1,[]
+        while k<n and j>=0:
+            t.append(a[k][j])
+            k+=1
+            j-=1
+        r.append(''.join(t))
+    return r
 
-print(count_subsequences("happy birthday", "appyh appy birth day"))
-print(count_subsequences("happy birthday", "hhaappyy bbiirrtthhddaayy"))
+def how_many_bees(a):
+    return sum(i.count('bee')+i.count('eeb') for i in [''.join(i) for i in a]+[''.join(i) for i in zip(*a)]+d(a)+d(a[::-1])) if a else 0
+
+print(how_many_bees([
+'...ee.beb',
+'e..e.beb.',
+'..beeb..e']),sep='\n')
