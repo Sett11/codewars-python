fc776a40eb8b236ca6679ee286c0d570993efdb9
Jhonny <gonzodzen@mail.ru>
2024-09-18 16:08:27 +0300

    Katastrophe!

--- my.py
+++ my.py
@@ -1,7 +1,8 @@
-class Vector:
-    def __init__(self,x,y):
-        self.x=x
-        self.y=y
-        
-    def add(self,other):
-        return Vector(self.x+other.x,self.y+other.y)
+from functools import reduce
+from operator import mul
+
+def strong_enough(a,n):
+    return ["Needs Reinforcement!",'Safe!'][reduce(mul,map(sum,a))<=1000*pow(.99,n)]
+
+print(strong_enough([[5,1,7],[1,1,1],[4,1,2]],100))
+print(strong_enough([[5,1,7],[1,1,7],[4,1,2]],50))
