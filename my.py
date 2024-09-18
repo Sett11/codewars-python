390346d8255510c7228ad79eb4013808cdd5c4d1
Jhonny <gonzodzen@mail.ru>
2024-09-18 14:53:48 +0300

    Deutschstunde

--- my.py
+++ my.py
@@ -1,12 +1,10 @@
-def yoga(a,p):
-    r,c=[sum(i) for i in a],0
-    for i in range(len(p)):
-        for j in range(len(a)):
-            c+=sum([1 for k in a[j] if k+r[j]>=p[i]])
-    return c
+from re import sub
 
-print(yoga([
-                    [7,2,1,0],
-                    [1,3,2,2],
-                    [1,9,1,2],
-                    ],[1000,20,3,105,66,204,4,1, 22, 86]))
+def der_die_das(s):
+    n=len(sub(r'[^aieouäöü]','',s.lower()))
+    if n<2:
+        return 'das '+s
+    if n>3:
+        return 'der '+s
+    return 'die '+s
+    
