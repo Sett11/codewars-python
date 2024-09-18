1e65e8068d41bc7553082f00182ea82f627ab4ec
Jhonny <gonzodzen@mail.ru>
2024-09-18 14:56:43 +0300

    Which string is worth more?

--- my.py
+++ my.py
@@ -1,10 +1,5 @@
-from re import sub
+def highest_value(a,b):
+    f=lambda x:sum(ord(i) for i in x)
+    return [b,a][f(a)>=f(b)]
 
-def der_die_das(s):
-    n=len(sub(r'[^aieouäöü]','',s.lower()))
-    if n<2:
-        return 'das '+s
-    if n>3:
-        return 'der '+s
-    return 'die '+s
-    
+print(highest_value("AaBbCcXxYyZz0189", "KkLlMmNnOoPp4567"))
