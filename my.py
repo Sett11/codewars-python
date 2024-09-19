208fd08350e274cbeaeaffbba04bed3f0a0c3776
Jhonny <gonzodzen@mail.ru>
2024-09-19 16:35:56 +0300

    Elemental Words

--- my.py
+++ my.py
@@ -1,24 +1,33 @@
-def london_city_hacker(a): 
-    c=i=0
-    d=[2.40,1.50]
-    n=len(a)
-    while i<n:
-        t=type(a[i])==int
-        if i==n-1:
-            c+=d[t]
-            break
-        elif type(a[i])==type(a[i+1]) and t:
-            c+=d[t]
-            i+=2
-        else:
-            c+=d[t]
-            i+=1
-    r=str(round(c,2)).split('.')
-    if len(r)==2:
-        r[1]=r[1].ljust(2,'0')
-    else:
-        r.append('00')
-    r='.'.join(r)
-    return '£'+r
+ELEMENTS={'H': 'Hydrogen', 'He': 'Helium', 'Li': 'Lithium', 'Be': 'Beryllium', 'B': 'Boron', 'C': 'Carbon', 'N': 'Nitrogen', 'O': 'Oxygen', 'F': 'Fluorine', 'Ne': 'Neon', 'Na': 'Sodium', 'Mg': 'Magnesium', 'Al': 'Aluminium', 'Si': 'Silicon', 'P': 'Phosphorus', 'S': 'Sulfur', 'Cl': 'Chlorine', 'Ar': 'Argon', 'K': 'Potassium', 'Ca': 'Calcium', 'Sc': 'Scandium', 'Ti': 'Titanium', 'V': 'Vanadium', 'Cr': 'Chromium', 'Mn': 'Manganese', 'Fe': 'Iron', 'Co': 'Cobalt', 'Ni': 'Nickel', 'Cu': 'Copper', 'Zn': 'Zinc', 'Ga': 'Gallium', 'Ge': 'Germanium', 'As': 'Arsenic', 'Se': 'Selenium', 'Br': 'Bromine', 'Kr': 'Krypton', 'Rb': 'Rubidium', 'Sr': 'Strontium', 'Y': 'Yttrium', 'Zr': 'Zirconium', 'Nb': 'Niobium', 'Mo': 'Molybdenum', 'Tc': 'Technetium', 'Ru': 'Ruthenium', 'Rh': 'Rhodium', 'Pd': 'Palladium', 'Ag': 'Silver', 'Cd': 'Cadmium', 'In': 'Indium', 'Sn': 'Tin', 'Sb': 'Antimony', 'Te': 'Tellurium', 'I': 'Iodine', 'Xe': 'Xenon', 'Cs': 'Caesium', 'Ba': 'Barium', 'La': 'Lanthanum', 'Ce': 'Cerium', 'Pr': 'Praseodymium', 'Nd': 'Neodymium', 'Pm': 'Promethium', 'Sm': 'Samarium', 'Eu': 'Europium', 'Gd': 'Gadolinium', 'Tb': 'Terbium', 'Dy': 'Dysprosium', 'Ho': 'Holmium', 'Er': 'Erbium', 'Tm': 'Thulium', 'Yb': 'Ytterbium', 'Lu': 'Lutetium', 'Hf': 'Hafnium', 'Ta': 'Tantalum', 'W': 'Tungsten', 'Re': 'Rhenium', 'Os': 'Osmium', 'Ir': 'Iridium', 'Pt': 'Platinum', 'Au': 'Gold', 'Hg': 'Mercury', 'Tl': 'Thallium', 'Pb': 'Lead', 'Bi': 'Bismuth', 'Po': 'Polonium', 'At': 'Astatine', 'Rn': 'Radon', 'Fr': 'Francium', 'Ra': 'Radium', 'Ac': 'Actinium', 'Th': 'Thorium', 'Pa': 'Protactinium', 'U': 'Uranium', 'Np': 'Neptunium', 'Pu': 'Plutonium', 'Am': 'Americium', 'Cm': 'Curium', 'Bk': 'Berkelium', 'Cf': 'Californium', 'Es': 'Einsteinium', 'Fm': 'Fermium', 'Md': 'Mendelevium', 'No': 'Nobelium', 'Lr': 'Lawrencium', 'Rf': 'Rutherfordium', 'Db': 'Dubnium', 'Sg': 'Seaborgium', 'Bh': 'Bohrium', 'Hs': 'Hassium', 'Mt': 'Meitnerium', 'Ds': 'Darmstadtium', 'Rg': 'Roentgenium', 'Cn': 'Copernicium', 'Uut': 'Ununtrium', 'Fl': 'Flerovium', 'Uup': 'Ununpentium', 'Lv': 'Livermorium', 'Uus': 'Ununseptium', 'Uuo': 'Ununoctium'}
 
-print(london_city_hacker([12, 'Central', 'Circle', 21]))
+def find_ham_cycle(a,s,r,x):
+    if ''.join(r).lower()==x:
+        return r
+    n=len(s)
+    for i in range(n):
+        t=s[:i+1].title()
+        if t in a:
+            z=find_ham_cycle(a,s[i+1:],r+[t],x)
+            if z:
+                return z
+
+def elemental_forms(s):
+    s=s.lower()
+    a=sorted([i for i in ELEMENTS if i.lower() in s],key=lambda x:s.index(x.lower()))
+    w=[]
+    def find_ham_cycle(a,s,r,x):
+        if ''.join(r).lower()==x and r:
+            w.append(r)
+            return
+        n=len(s)
+        for i in range(n):
+            t=s[:i+1].title()
+            if t in a:
+                z=find_ham_cycle(a,s[i+1:],r+[t],x)
+                if z:
+                    w.append(z)
+                    return
+    find_ham_cycle(a,s,[],s)
+    return [[f'{ELEMENTS[j]} ({j})' for j in i] for i in w]
+
+print(elemental_forms('conversations'))
