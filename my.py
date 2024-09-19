7c16cdf3160897c63c9a95df49c2319b838a8373
Jhonny <gonzodzen@mail.ru>
2024-09-20 01:51:11 +0300

    How sexy is your name?

--- my.py
+++ my.py
@@ -1,36 +1,3 @@
-from collections import deque
-
-class Node:
-    def __init__(self, value, left=None, right=None):
-        self.value = value
-        self.left = left
-        self.right = right
-
-    def __repr__(self):
-        r=[]
-        def f(x):
-            if not x:
-                return
-            f(x.left)
-            r.append(str(x.value))
-            f(x.right)
-        f(self)
-        return ' -> '.join(r)
-
-def array_to_tree(a):
-    if a:
-        a=deque(a)
-        x=Node(a.popleft())
-        q=deque([x])
-        while a:
-            t=q.pop()
-            t.left=Node(a.popleft())
-            if not a:
-                break
-            t.right=Node(a.popleft())
-            q.appendleft(t.left)
-            q.appendleft(t.right)
-        return x
-
-print(array_to_tree([17, 0, -4, 3, 15]))
-print(array_to_tree([]))
+def sexy_name(s):
+    n=sum(SCORES.get(i.upper(),0) for i in s)
+    return 'NOT TOO SEXY' if n<=60 else 'PRETTY SEXY' if n<=300 else 'VERY SEXY' if n<600 else 'THE ULTIMATE SEXIEST'
