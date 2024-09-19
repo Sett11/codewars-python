82696ebe8c363ce28b80578cae9ba869d5bac80d
Jhonny <gonzodzen@mail.ru>
2024-09-20 01:41:13 +0300

    Fun with trees: array to tree

--- my.py
+++ my.py
@@ -1,37 +1,36 @@
-from collections import defaultdict
+from collections import deque
 
-class TreeNode:
-    def __init__(self, value, left = None, right = None):
+class Node:
+    def __init__(self, value, left=None, right=None):
         self.value = value
-        self.left  = left
+        self.left = left
         self.right = right
 
-def is_perfect(t):
-    d,i=defaultdict(list),0
-    def f(x):
-        nonlocal i
-        if not x:
-            return
-        i+=1
-        f(x.left)
-        d[i].append(x.value)
-        f(x.right)
-        i-=1
-    f(t)
-    r=tuple(d.items())
-    for i in range(1,len(r)):
-        if r[i][0]!=r[i-1][0]-1 or len(r[i][1])!=len(r[i-1][1])//2:
-            return False
-    return True
+    def __repr__(self):
+        r=[]
+        def f(x):
+            if not x:
+                return
+            f(x.left)
+            r.append(str(x.value))
+            f(x.right)
+        f(self)
+        return ' -> '.join(r)
 
+def array_to_tree(a):
+    if a:
+        a=deque(a)
+        x=Node(a.popleft())
+        q=deque([x])
+        while a:
+            t=q.pop()
+            t.left=Node(a.popleft())
+            if not a:
+                break
+            t.right=Node(a.popleft())
+            q.appendleft(t.left)
+            q.appendleft(t.right)
+        return x
 
-print(is_perfect(TreeNode('A',
-                TreeNode('B',
-                    TreeNode('D'),
-                    TreeNode('E')
-                ),
-                TreeNode('C',
-                    TreeNode('F'),
-                    TreeNode('G')
-                )
-            )))
+print(array_to_tree([17, 0, -4, 3, 15]))
+print(array_to_tree([]))
