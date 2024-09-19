7f21a40d39b74a75ffb108ce835050d488a26420
Jhonny <gonzodzen@mail.ru>
2024-09-20 01:58:17 +0300

    White or Black?

--- my.py
+++ my.py
@@ -1,3 +1,6 @@
-def sexy_name(s):
-    n=sum(SCORES.get(i.upper(),0) for i in s)
-    return 'NOT TOO SEXY' if n<=60 else 'PRETTY SEXY' if n<=300 else 'VERY SEXY' if n<600 else 'THE ULTIMATE SEXIEST'
+def square_color(a,b):
+    bord=[j if i%2 else j[::-1] for i,j in enumerate([['black','white']*4 for _ in range(8)])]
+    return bord[-b][ord(a)-97]
+
+print(square_color('a',8))
+print(square_color('b',2))
