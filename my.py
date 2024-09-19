b1cd3b5460a85353b0bd9a6f5b87e950f06bc96f
Jhonny <gonzodzen@mail.ru>
2024-09-20 00:32:51 +0300

    Fun with trees: is perfect

--- my.py
+++ my.py
@@ -1,33 +1,37 @@
-ELEMENTS={'H': 'Hydrogen', 'He': 'Helium', 'Li': 'Lithium', 'Be': 'Beryllium', 'B': 'Boron', 'C': 'Carbon', 'N': 'Nitrogen', 'O': 'Oxygen', 'F': 'Fluorine', 'Ne': 'Neon', 'Na': 'Sodium', 'Mg': 'Magnesium', 'Al': 'Aluminium', 'Si': 'Silicon', 'P': 'Phosphorus', 'S': 'Sulfur', 'Cl': 'Chlorine', 'Ar': 'Argon', 'K': 'Potassium', 'Ca': 'Calcium', 'Sc': 'Scandium', 'Ti': 'Titanium', 'V': 'Vanadium', 'Cr': 'Chromium', 'Mn': 'Manganese', 'Fe': 'Iron', 'Co': 'Cobalt', 'Ni': 'Nickel', 'Cu': 'Copper', 'Zn': 'Zinc', 'Ga': 'Gallium', 'Ge': 'Germanium', 'As': 'Arsenic', 'Se': 'Selenium', 'Br': 'Bromine', 'Kr': 'Krypton', 'Rb': 'Rubidium', 'Sr': 'Strontium', 'Y': 'Yttrium', 'Zr': 'Zirconium', 'Nb': 'Niobium', 'Mo': 'Molybdenum', 'Tc': 'Technetium', 'Ru': 'Ruthenium', 'Rh': 'Rhodium', 'Pd': 'Palladium', 'Ag': 'Silver', 'Cd': 'Cadmium', 'In': 'Indium', 'Sn': 'Tin', 'Sb': 'Antimony', 'Te': 'Tellurium', 'I': 'Iodine', 'Xe': 'Xenon', 'Cs': 'Caesium', 'Ba': 'Barium', 'La': 'Lanthanum', 'Ce': 'Cerium', 'Pr': 'Praseodymium', 'Nd': 'Neodymium', 'Pm': 'Promethium', 'Sm': 'Samarium', 'Eu': 'Europium', 'Gd': 'Gadolinium', 'Tb': 'Terbium', 'Dy': 'Dysprosium', 'Ho': 'Holmium', 'Er': 'Erbium', 'Tm': 'Thulium', 'Yb': 'Ytterbium', 'Lu': 'Lutetium', 'Hf': 'Hafnium', 'Ta': 'Tantalum', 'W': 'Tungsten', 'Re': 'Rhenium', 'Os': 'Osmium', 'Ir': 'Iridium', 'Pt': 'Platinum', 'Au': 'Gold', 'Hg': 'Mercury', 'Tl': 'Thallium', 'Pb': 'Lead', 'Bi': 'Bismuth', 'Po': 'Polonium', 'At': 'Astatine', 'Rn': 'Radon', 'Fr': 'Francium', 'Ra': 'Radium', 'Ac': 'Actinium', 'Th': 'Thorium', 'Pa': 'Protactinium', 'U': 'Uranium', 'Np': 'Neptunium', 'Pu': 'Plutonium', 'Am': 'Americium', 'Cm': 'Curium', 'Bk': 'Berkelium', 'Cf': 'Californium', 'Es': 'Einsteinium', 'Fm': 'Fermium', 'Md': 'Mendelevium', 'No': 'Nobelium', 'Lr': 'Lawrencium', 'Rf': 'Rutherfordium', 'Db': 'Dubnium', 'Sg': 'Seaborgium', 'Bh': 'Bohrium', 'Hs': 'Hassium', 'Mt': 'Meitnerium', 'Ds': 'Darmstadtium', 'Rg': 'Roentgenium', 'Cn': 'Copernicium', 'Uut': 'Ununtrium', 'Fl': 'Flerovium', 'Uup': 'Ununpentium', 'Lv': 'Livermorium', 'Uus': 'Ununseptium', 'Uuo': 'Ununoctium'}
+from collections import defaultdict
 
-def find_ham_cycle(a,s,r,x):
-    if ''.join(r).lower()==x:
-        return r
-    n=len(s)
-    for i in range(n):
-        t=s[:i+1].title()
-        if t in a:
-            z=find_ham_cycle(a,s[i+1:],r+[t],x)
-            if z:
-                return z
+class TreeNode:
+    def __init__(self, value, left = None, right = None):
+        self.value = value
+        self.left  = left
+        self.right = right
 
-def elemental_forms(s):
-    s=s.lower()
-    a=sorted([i for i in ELEMENTS if i.lower() in s],key=lambda x:s.index(x.lower()))
-    w=[]
-    def find_ham_cycle(a,s,r,x):
-        if ''.join(r).lower()==x and r:
-            w.append(r)
+def is_perfect(t):
+    d,i=defaultdict(list),0
+    def f(x):
+        nonlocal i
+        if not x:
             return
-        n=len(s)
-        for i in range(n):
-            t=s[:i+1].title()
-            if t in a:
-                z=find_ham_cycle(a,s[i+1:],r+[t],x)
-                if z:
-                    w.append(z)
-                    return
-    find_ham_cycle(a,s,[],s)
-    return [[f'{ELEMENTS[j]} ({j})' for j in i] for i in w]
+        i+=1
+        f(x.left)
+        d[i].append(x.value)
+        f(x.right)
+        i-=1
+    f(t)
+    r=tuple(d.items())
+    for i in range(1,len(r)):
+        if r[i][0]!=r[i-1][0]-1 or len(r[i][1])!=len(r[i-1][1])//2:
+            return False
+    return True
 
-print(elemental_forms('conversations'))
+
+print(is_perfect(TreeNode('A',
+                TreeNode('B',
+                    TreeNode('D'),
+                    TreeNode('E')
+                ),
+                TreeNode('C',
+                    TreeNode('F'),
+                    TreeNode('G')
+                )
+            )))
