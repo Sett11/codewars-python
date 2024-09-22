41f8d298e5fb344a2a28bd93a5bf02a86fa5548e
Jhonny <gonzodzen@mail.ru>
2024-09-23 01:09:02 +0300

    Geometry Basics: Distance between points in 2D

--- my.py
+++ my.py
@@ -1,3 +1,3 @@
-
-# def distance_between_points(a,b):
-#     return ((b.x-a.x)**2+(b.y-a.y)**2+(a.z-b.z)**2)**.5
+# 
+# def distance_between_points(a, b):
+#     return ((b.x-a.x)**2+(b.y-a.y)**2)**.5
