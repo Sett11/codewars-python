a910d4a9c9a97cdd29a9e12772cb49ddbc53c7ec
Jhonny <gonzodzen@mail.ru>
2024-09-25 02:08:45 +0300

    Nut Farm 2

--- my.py
+++ my.py
@@ -3,32 +3,37 @@ def shake_tree(t):
     r=[0]*m
     def f(i,j):
         while i<n:
-            if t[i][j]=='\\':
+            while t[i][j]=='\\':
                 j+=1
-            if t[i][j]=='/':
+                if t[i][j]=='/':
+                    return -1
+            while t[i][j]=='/':
                 j-=1
-            if t[i][j]=='_':
+                if t[i][j]=='\\':
+                    return -1
+            if t[i][j]=='_' or (i!=n-1 and t[i+1][j]=='_'):
                 return -1
             i+=1
         return j
-    for i in range(m):
-        if t[0][i]=='o':
-            j=f(0,i)
-            if j!=-1:
-                r[j]+=1
+    for i in range(n):
+        for j in range(m):
+            if t[i][j]=='o':
+                k=f(i,j)
+                if k!=-1:
+                    r[k]+=1
     return r
 
-print(shake_tree([" o o o  ",
-                  " \\    \\ ",
-                "   \\    ",
-                "  \\  \\  ",
-                "   ||   ",
-                "   ||   ",
-                "   ||   "]))
-print(shake_tree([" o o o  ",
-                " /    / ",
-                "   /    ",
-                "  /  /  ",
+print(shake_tree([' o      ',
+                  ' / o o/ ',
+                  ' \\//    ',
+                  '   \\//  ',
+                  '   ||   ',
+                  '   ||   ',
+                  '   ||   ']))
+print(shake_tree([" o      ",
+                " / o o/ ",
+                " ///    ",
+                "   ///  ",
                 "   ||   ",
                 "   ||   ",
                 "   ||   "]))
