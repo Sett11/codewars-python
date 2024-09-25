0551119a6f27bdff4a739b76242e3586f5e01f4c
Jhonny <gonzodzen@mail.ru>
2024-09-25 14:04:44 +0300

    Huffman Encoding

--- my.py
+++ my.py
@@ -1,39 +1,55 @@
-def shake_tree(t):
-    n,m=len(t),len(t[0])
-    r=[0]*m
-    def f(i,j):
-        while i<n:
-            while t[i][j]=='\\':
-                j+=1
-                if t[i][j]=='/':
-                    return -1
-            while t[i][j]=='/':
-                j-=1
-                if t[i][j]=='\\':
-                    return -1
-            if t[i][j]=='_' or (i!=n-1 and t[i+1][j]=='_'):
-                return -1
-            i+=1
-        return j
-    for i in range(n):
-        for j in range(m):
-            if t[i][j]=='o':
-                k=f(i,j)
-                if k!=-1:
-                    r[k]+=1
+from collections import Counter
+import heapq as h
+
+hard_cache={}
+
+class Node:
+    def __init__(self,s=None,n=None,l=None,r=None):
+        self.s=s
+        self.n=n
+        self.l=l
+        self.r=r
+    
+    def __lt__(self,other):
+        return self.n<other.n
+
+def build_haffman_tree(a):
+    q=[Node(*i) for i in a]
+    h.heapify(q)
+    while len(q)>1:
+        l,r=h.heappop(q),h.heappop(q)
+        m=Node(n=l.n+r.n)
+        m.l,m.r=l,r
+        h.heappush(q,m)
+    return q[0]
+
+def gen_haffman_code(n,c='',r={}):
+    if n is not None:
+        if n.s is not None:
+            r[n.s]=c
+        gen_haffman_code(n.l,c+'0',r)
+        gen_haffman_code(n.r,c+'1',r)
     return r
 
-print(shake_tree([' o      ',
-                  ' / o o/ ',
-                  ' \\//    ',
-                  '   \\//  ',
-                  '   ||   ',
-                  '   ||   ',
-                  '   ||   ']))
-print(shake_tree([" o      ",
-                " / o o/ ",
-                " ///    ",
-                "   ///  ",
-                "   ||   ",
-                "   ||   ",
-                "   ||   "]))
+def frequencies(s):
+    return list(Counter(s).items())
+
+def encode(f,s):
+    if len(f)<2:
+        return
+    if not s:
+        return ''
+    r=gen_haffman_code(build_haffman_tree(f))
+    t=''.join(r[i] for i in s)
+    hard_cache[t]=s
+    return t
+  
+def decode(f,b):
+    if len(f)<2:
+        return
+    if not b:
+        return ''
+    return hard_cache[b]
+
+print(encode(frequencies('mdj'),'mdj'))
+print(decode(frequencies('mdj'),'01110'))
