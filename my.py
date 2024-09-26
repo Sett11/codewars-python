971fdec00221d5a522547d3c88cd2d3666dacff6
Jhonny <gonzodzen@mail.ru>
2024-09-27 00:38:56 +0300

    Word Wrap

--- my.py
+++ my.py
@@ -1,6 +1,26 @@
-def wraps(func):
-    def f(*args,**kwargs):
-       return func
-    f.__name__=func.__name__
-    f.__doc__=func.__doc__
-    return f
+def word_wrap(s,l):
+    a,r,t=s.split()[::-1],[],''
+    while a:
+        q=a.pop()
+        n,k=len(t),len(q)
+        if n==l:
+            r.append(t.strip())
+            t=''
+        if n+k+1<=l:
+            t=f'{t} {q}'.strip()
+        else:
+            if k>l:
+                t,q=f'{t} {q[:l-n]}'.strip(),q[l-n:]
+                if len(t)>l:
+                    q=t[-1]+q
+                    t=t[:-1]
+                a.append(q)
+            else:
+                if t:
+                    r.append(t)
+                t=q
+    return '\n'.join(r+[t])
+
+print(word_wrap('areallylongword',6))
+print(word_wrap("a lot of words for a single line",10))
+print(word_wrap('greedy whenthewordistoolong', 7))
