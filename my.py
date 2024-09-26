abc00c39cc0ceaa64333a6e3b785aa00ecd26156
Jhonny <gonzodzen@mail.ru>
2024-09-27 01:30:00 +0300

    Python's Dynamic Classes #3

--- my.py
+++ my.py
@@ -1,26 +1,11 @@
-def word_wrap(s,l):
-    a,r,t=s.split()[::-1],[],''
-    while a:
-        q=a.pop()
-        n,k=len(t),len(q)
-        if n==l:
-            r.append(t.strip())
-            t=''
-        if n+k+1<=l:
-            t=f'{t} {q}'.strip()
-        else:
-            if k>l:
-                t,q=f'{t} {q[:l-n]}'.strip(),q[l-n:]
-                if len(t)>l:
-                    q=t[-1]+q
-                    t=t[:-1]
-                a.append(q)
-            else:
-                if t:
-                    r.append(t)
-                t=q
-    return '\n'.join(r+[t])
+def create_class(name,secrets={}):
+    if not name:
+        return
+    my_class=type(name,(object,),{})
+    for i in secrets:
+        setattr(my_class,i,secrets[i])
+    return my_class
 
-print(word_wrap('areallylongword',6))
-print(word_wrap("a lot of words for a single line",10))
-print(word_wrap('greedy whenthewordistoolong', 7))
+cls=create_class('new one',{'name':'Jhon','age':23,'add':lambda x,y:x+y})
+x=cls()
+print(x.name)
