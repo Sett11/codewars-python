2bbc5d542faa96089e7d883ec5c9e8a3e2919f03
Jhonny <gonzodzen@mail.ru>
2024-09-29 22:08:42 +0300

    Multirange iterator

--- my.py
+++ my.py
@@ -1,26 +1,7 @@
-def gen(k,it):
-    w,g,i=[],iter(it),0
-    for _ in range(2000):
-        try:
-            w.append(next(g))
-        except:
-            break
-    w*=200
-    while 'fack':
-        yield tuple(w[i:i+k])
-        i+=1
+from itertools import product
 
-g=gen(2,range(7))
+def multiiter(*args):
+    for k in [tuple(j) for j in product(*[list(range(i)) for i in args])]:
+        yield k
 
-print(next(g))
-print(next(g))
-print(next(g))
-print(next(g))
-print(next(g))
-print(next(g))
-print(next(g))
-print(next(g))
-print(next(g))
-print(next(g))
-print(next(g))
-print(next(g))
+print(list(multiiter(3,2)))
