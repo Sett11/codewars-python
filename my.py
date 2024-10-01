c626ac07e899a62b8851dedced2a85665781deb6
Jhonny <gonzodzen@mail.ru>
2024-10-02 01:26:01 +0300

    Endianness Conversion

--- my.py
+++ my.py
@@ -1,5 +1,7 @@
-def traffic_count(a):
-    a,b,c,d=[a[i:i+6] for i in range(0,len(a),6)]
-    return [('4:00pm',max(a)),('5:00pm',max(b)),('6:00pm',max(c)),('7:00pm',max(d))]
+def switch_endian(n,b):
+    if (n).bit_length()>b or (b&(b-1)) or n<0:
+        return
+    s=bin(n)[2:].rjust(b,'0')
+    return int(''.join([s[i:i+8] for i in range(0,b,8)][::-1]),2)
 
-print(traffic_count([23,24,34,45,43,23,57,34,65,12,19,45, 54,65,54,43,89,48,42,55,22,69,23,93]))
+print(switch_endian(7206089, 32))
