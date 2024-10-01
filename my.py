f2e759470a1a6f855142b8a7e41809a25700d5a8
Jhonny <gonzodzen@mail.ru>
2024-10-02 02:42:56 +0300

    Increment with iterations

--- my.py
+++ my.py
@@ -1,7 +1,20 @@
-def switch_endian(n,b):
-    if (n).bit_length()>b or (b&(b-1)) or n<0:
-        return
-    s=bin(n)[2:].rjust(b,'0')
-    return int(''.join([s[i:i+8] for i in range(0,b,8)][::-1]),2)
+def increment(n,k,c):
+    i,l,x=0,len(str(n)),c
+    while i<k:
+        s=str(n)
+        i+=1
+        g=c%l
+        n+=10**(l-g-1)
+        z=str(n)
+        if len(z)>len(s):
+            l=len(z)
+            c=g+1
+        c+=x
+    return n
+        
 
-print(switch_endian(7206089, 32))
+print(increment(50,10,0))
+print(increment(1,10,1))
+print(increment(1,10,10))
+print(increment(14123,15,6))
+print(increment(9999,9,9))
