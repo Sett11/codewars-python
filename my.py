abda885a3c4d9d86b1ee28b92299cf82162ff9f5
Jhonny <gonzodzen@mail.ru>
2024-10-02 00:13:07 +0300

    Simple Fun #157: Counting Triangles

--- my.py
+++ my.py
@@ -1,22 +1,12 @@
-class Point:
-    def __init__(self,x,y):
-        self.x=x
-        self.y=y
-    
-def dist(a,b):
-    return (a.x-b.x)*(a.x-b.x)+(b.y-a.y)*(b.y-a.y)
-    
-def check(a,b,c,d):
-    x,y,z=dist(a,b),dist(a,c),dist(a,d)
-    if 0 in [x,y,z]:
-        return False
-    if (x==y and 2*x==z and 2*dist(b,d)==dist(b,c)) or\
-    (y==z and 2*y==x and 2*dist(c,b)==dist(c,d)) or\
-    (x==z and 2*x==y and 2*dist(b,c)==dist(b,d)):
-        return True
-    return False
 
-def is_square(a):
-    return check(*map(lambda x:Point(*x),a)) if len(a)==4 else False
 
-print(is_square([(520, 520), (360, 521), (519, 360), (359, 361)]))
+# from itertools import combinations as c
+
+# def f(a,b,c):
+#     a,b,c=sorted([a,b,c])
+#     return c<a+b and c>b-a
+
+# def counting_triangles(a):
+#     return len([i for i in c(a,3) if f(*i)])
+
+# print(counting_triangles([2,3,7,4]))
