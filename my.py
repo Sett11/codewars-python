e4bdf807cb5ac8943bc5399f61da3820d53d8922
Jhonny <gonzodzen@mail.ru>
2024-10-02 00:40:55 +0300

    T.T.T. #9: Peaks and valleys

--- my.py
+++ my.py
@@ -1,12 +1,4 @@
+def peak_and_valley(a):
+    return [a[i] for i in range(3,len(a)-3) if all(a[i]>j for j in a[i-3:i]+a[i+1:i+4]) or all(a[i]<j for j in a[i-3:i]+a[i+1:i+4])]
 
-
-# from itertools import combinations as c
-
-# def f(a,b,c):
-#     a,b,c=sorted([a,b,c])
-#     return c<a+b and c>b-a
-
-# def counting_triangles(a):
-#     return len([i for i in c(a,3) if f(*i)])
-
-# print(counting_triangles([2,3,7,4]))
+print(peak_and_valley([10,20,30,40,30,20,10,11,12,13,14,15,16,15,14,13]))
