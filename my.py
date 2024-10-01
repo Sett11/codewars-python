d3a46986541ab15e485104a3a8c1738a7d2e4506
Jhonny <gonzodzen@mail.ru>
2024-10-02 00:46:52 +0300

    Traffic Count During Peak Hours

--- my.py
+++ my.py
@@ -1,4 +1,5 @@
-def peak_and_valley(a):
-    return [a[i] for i in range(3,len(a)-3) if all(a[i]>j for j in a[i-3:i]+a[i+1:i+4]) or all(a[i]<j for j in a[i-3:i]+a[i+1:i+4])]
+def traffic_count(a):
+    a,b,c,d=[a[i:i+6] for i in range(0,len(a),6)]
+    return [('4:00pm',max(a)),('5:00pm',max(b)),('6:00pm',max(c)),('7:00pm',max(d))]
 
-print(peak_and_valley([10,20,30,40,30,20,10,11,12,13,14,15,16,15,14,13]))
+print(traffic_count([23,24,34,45,43,23,57,34,65,12,19,45, 54,65,54,43,89,48,42,55,22,69,23,93]))
