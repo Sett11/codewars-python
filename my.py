e46ce3a0daae555992a97b250d7f8a41f90d2b73
Jhonny <gonzodzen@mail.ru>
2024-10-02 14:39:23 +0300

    Borda count

--- my.py
+++ my.py
@@ -1,20 +1,24 @@
-def increment(n,k,c):
-    i,l,x=0,len(str(n)),c
-    while i<k:
-        s=str(n)
-        i+=1
-        g=c%l
-        n+=10**(l-g-1)
-        z=str(n)
-        if len(z)>len(s):
-            l=len(z)
-            c=g+1
-        c+=x
-    return n
-        
+def borda_count(a):
+    d={i:0 for i in set(sum([a[i] for i in a],[]))}
+    n=len(d)
+    for i in a:
+        for j,k in enumerate(a[i]):
+            d[k]+=n-j-1
+    s=sum(d.values())/n
+    u={i for i in d if d[i]<s}
+    if len(u)==n:
+        return u
+    if not u:
+        return {*d.keys()}
+    for i in a:
+        for j in u:
+            del a[i][a[i].index(j)]
+            if len(a[i])==1:
+                return {a[i][0]}
+    return borda_count(a)
 
-print(increment(50,10,0))
-print(increment(1,10,1))
-print(increment(1,10,10))
-print(increment(14123,15,6))
-print(increment(9999,9,9))
+
+print(borda_count( {
+'A': ['a', 'b'],
+'B': ['b', 'a']
+}))
