a3c6770c98be796e7223f0ce302a6195127f4157
Jhonny <gonzodzen@mail.ru>
2024-10-02 15:16:38 +0300

    Matrix Exponentiation

--- my.py
+++ my.py
@@ -1,24 +1,19 @@
-def borda_count(a):
-    d={i:0 for i in set(sum([a[i] for i in a],[]))}
-    n=len(d)
-    for i in a:
-        for j,k in enumerate(a[i]):
-            d[k]+=n-j-1
-    s=sum(d.values())/n
-    u={i for i in d if d[i]<s}
-    if len(u)==n:
-        return u
-    if not u:
-        return {*d.keys()}
-    for i in a:
-        for j in u:
-            del a[i][a[i].index(j)]
-            if len(a[i])==1:
-                return {a[i][0]}
-    return borda_count(a)
+def md(a,b):
+    return [[sum(a[i][j]*b[j][k] for j in range(len(a))) for k in range(len(a[0]))] for i in range(len(a[0]))]
 
+def multiply(a,b):
+    c=md(a,b)
+    for i in range(len(c)):
+        for j in range(len(c[0])):
+            a[i][j]=c[i][j]
 
-print(borda_count( {
-'A': ['a', 'b'],
-'B': ['b', 'a']
-}))
+def calc(m,n):
+    a=[[1 if i==j else 0 for j in range(len(m[0]))] for i in range(len(m))]
+    while n:
+        if n&1:
+            multiply(a, m)
+        multiply(m, m)
+        n>>=1
+    return a
+
+print(calc([[1,2], [0,1]],3))
