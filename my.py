bb1581273ef11cbedced53758bf77d925f6cc3e8
Jhonny <gonzodzen@mail.ru>
2024-10-03 12:08:20 +0300

    Repetitive Sequence - Easy Version

--- my.py
+++ my.py
@@ -1,20 +1,14 @@
-from string import ascii_uppercase
-from functools import reduce
-from re import match
-
-def f(n):
-    r=''
-    while n>=0:
-        r=ascii_uppercase[n%26]+r
-        n=n//26-1
-    return r
-
-def ff(s):
-    return reduce(lambda a,c:a*26+ascii_uppercase.index(c)+1,s,0)
-
-def spreadsheet(s):
-    if match(r'^R\d+C\d+$',s):
-        g=s.index('C')
-        return str(f(int(s[g:][1:])-1))+s[:g][1:]
-    g=next(i for i,j in enumerate(s) if j.isdigit())
-    return f'R{s[g:]}C{ff(s[:g])}'
+def find(n):
+    arr = [0, 1, 2, 2]
+    if n <= 3:
+        return arr[n]
+    arr_sum = 5
+    arr_len = 4
+    for i in range(3, n+1):
+        arr_sum += i * arr[i]
+        if arr_sum >= n:
+            x = (arr_sum - n) // i
+            return arr_len + arr[i] - (x+1)
+        arr_len += arr[i]
+        if arr_len < 70_000:
+            arr += [i] * arr[i]
