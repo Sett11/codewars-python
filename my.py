4ce1ccc4e156da9d297dca069faa25381cbde58a
Jhonny <gonzodzen@mail.ru>
2024-10-03 13:46:09 +0300

    Simple Fun #41: Elections Winners

--- my.py
+++ my.py
@@ -1,7 +1,5 @@
-from base64 import b64encode
+def elections_winners(a,k):
+  m=max(a)
+  return len([i+k for i in a if i+k>m]) if k else 1 if a.count(m)==1 else 0
 
-def hex_to_base64(s):
-	return ''.join(map(chr,b64encode(bytearray.fromhex(s))))
-
-print(hex_to_base64('e1b6a959d88a3701c6a51ccbd0'))
-print(hex_to_base64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'))
+print(elections_winners([2,3,2,5],3))
