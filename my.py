cc3593f6666a6c66a7de142503ae3387e87196dc
Jhonny <gonzodzen@mail.ru>
2024-10-05 00:01:30 +0300

    What will be the odd one out?

--- my.py
+++ my.py
@@ -1,5 +1,7 @@
-def elections_winners(a,k):
-  m=max(a)
-  return len([i+k for i in a if i+k>m]) if k else 1 if a.count(m)==1 else 0
+from collections import Counter
 
-print(elections_winners([2,3,2,5],3))
+def odd_one_out(s):
+    a=Counter(s)
+    return sorted([i for i in a if a[i]&1],key=lambda x:s.rfind(x))
+
+print(odd_one_out('Hello World'))
