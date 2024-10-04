8e77c5ebf1e5d809b849c60aa89520fa7caa44a3
Jhonny <gonzodzen@mail.ru>
2024-10-05 00:16:27 +0300

    Primes with Even Digits

--- my.py
+++ my.py
@@ -1,7 +1,21 @@
+import gmpy2
 from collections import Counter
+from bisect import bisect
 
-def odd_one_out(s):
-    a=Counter(s)
-    return sorted([i for i in a if a[i]&1],key=lambda x:s.rfind(x))
+def e(n):
+    l=gmpy2.isqrt(n)+1
+    n+=1
+    b=gmpy2.xmpz(3)
+    b[4:n:2]=-1
+    for i in b.iter_clear(3,l):
+        b[i*i:n:i+i]=-1
+    return list(b.iter_clear(2,n))
 
-print(odd_one_out('Hello World'))
+primes=e(int(5e6))
+
+def f(n):
+    c=lambda x:(sum(j for i,j in Counter(str(x)).items() if int(i)%2==0),x)
+    g=bisect(primes,n-1)
+    return max([c(i) for i in primes[:g]])[1]
+
+print(f(1000))
