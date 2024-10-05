66a703b63a143815f0451b5b0900f991ff087aa5
Jhonny <gonzodzen@mail.ru>
2024-10-05 12:09:42 +0300

    GET TO THE CHOPPA!

--- my.py
+++ my.py
@@ -1,17 +1,13 @@
-def count(a):
-    q,u=sum([[(i,k) for k,p in enumerate(j) if p=='K'] for i,j in enumerate(a)],[])[0],set()
-    f=lambda x,y:[(i,j) for i,j in [(x+1,y+2),(x-1,y-2),(x+2,y+1),(x-2,y-1),(x+1,y-2),(x+2,y-1),(x-1,y+2),(x-2,y+1)] if 0<=i<8 and 0<=j<8]
-    for i in f(*q):
-        u.update(f(*i))
-    return len(u)
+import networkx as nx
 
-print(count([
-            ['0', '0', '0', '0', '0', '0', '0', '0'],
-            ['0', '0', '0', '0', '0', '0', '0', '0'],
-            ['0', '0', '0', '0', '0', '0', '0', '0'],
-            ['0', '0', '0', '0', '0', '0', '0', '0'],
-            ['0', '0', 'K', '0', '0', '0', '0', '0'],
-            ['0', '0', '0', '0', '0', '0', '0', '0'],
-            ['0', '0', '0', '0', '0', '0', '0', '0'],
-            ['0', '0', '0', '0', '0', '0', '0', '0']
-        ]))
+def find_shortest_path(a,s,e):
+    if not a:
+        return a
+    g,n,m=nx.Graph(),len(a),len(a[0])
+    for i in range(n):
+        for j in range(m):
+            if a[i][j].passable:
+                g.add_node((i,j))
+                for k in [(k,p) for k,p in [(i-1,j),(i+1,j),(i,j-1),(i,j+1)] if 0<=k<n and 0<=p<m and a[k][p].passable]:
+                    g.add_edge((i,j),k)
+    return [a[i][j] for i,j in nx.shortest_path(g,(s.position.x,s.position.y),(e.position.x,e.position.y))]
