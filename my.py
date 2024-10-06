e02b19f0dabbccc67b38f70890fbdbcd07c85ca0
Jhonny <gonzodzen@mail.ru>
2024-10-07 00:25:03 +0300

    Write your own multiplication function

--- my.py
+++ my.py
@@ -1,27 +1,13 @@
-from sys import settrace
+zero,one=0,1
 
-def count_calls(func,*args,**kwargs):
-    a=[-1]
-    def tracer(frame,event,arg):
-       if event=='call':
-         a[0]+=1
-       return tracer
-    settrace(tracer)
-    r=func(*args,**kwargs)
-    return a[0],r
+def multiply(n,m):
+    t=zero
+    k=one+one
+    while n:
+        if n&one:
+            t+=m
+        m<<=one
+        n>>=one
+    return t
 
-
-def add(a, b):
-  return a + b
-  
-
-def add_ten(a):
-  return add(a, 10)
-
-
-def misc_fun():
-  return add(add_ten(3), add_ten(9))
-
-print(count_calls(misc_fun))
-
-
+print(multiply(2,7))
