f79cd24317267289f21e49186d6466a601856b08
Jhonny <gonzodzen@mail.ru>
2024-10-08 03:07:52 +0300

    Path Finder #3: the Alpinist

--- my.py
+++ my.py
@@ -1,24 +1,17 @@
 import networkx as nx
 
-def path_finder(a):
-    g=nx.Graph()
-    a=a.splitlines()
+def path_finder(area):
+    g,a=nx.Graph(),area.splitlines()
     n,m=len(a),len(a[0])
+    if [n,m]==[1]*2:
+        return 0
     for i in range(n):
         for j in range(m):
-            if a[i][j]=='.':
-                for k in [(x,y) for x,y in [(i+1,j),(i-1,j),(i,j+1),(i,j-1)] if 0<=x<n and 0<=y<m and a[x][y]=='.']:
-                    g.add_edge((i,j),k)
-    r=g,(0,0),(n-1,m-1)
-    if r[-1] not in g or r[1] not in g or not nx.has_path(*r):
-        return False
-    return nx.shortest_path_length(*r)
+            for x,y in [(x,y) for x,y in [(i+1,j),(i-1,j),(i,j+1),(i,j-1)] if 0<=x<n and 0<=y<m]:
+                g.add_edge((i,j),(x,y),weight=abs(int(a[i][j])-int(a[x][y])))
+    path=nx.shortest_path(g,(0,0),(n-1,m-1),lambda x,y,_:g[x][y]['weight'])
+    return sum(g[path[i]][path[i+1]]['weight'] for i in range(len(path)-1))
 
 print(path_finder("\n".join([
-  "......",
-  "......",
-  "......",
-  "......",
-  ".....W",
-  "....W."
-])))
+          "1"
+        ])))
