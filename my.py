e6df8b0f9cd6846e0edabf79e7ca474462749bd9
Jhonny <gonzodzen@mail.ru>
2024-10-11 02:23:02 +0300

    Generic number class

--- my.py
+++ my.py
@@ -1,25 +1,44 @@
-class Marine:
-    def __init__(self):
-        self.health=100
-        
-    def accept(self, visitor):
-        self.health=visitor.visit_light(self.health)
+from functools import reduce
 
-class Marauder:
-    def __init__(self):
-        self.health=125
-        
-    def accept(self, visitor):
-        self.health=visitor.visit_armored(self.health)
+def create_number_class(a):
+    class Number_class:
+        def __init__(self,s):
+            self.alf=a
+            self.num=s
+            self.notation=len(a)
 
-class TankBullet:
-    def visit_light(self, unit):
-        return unit-21
-        
-    def visit_armored(self, unit):
-        return unit-32
+        def convert_to(self,other):
+            return other('')._rev_convert(self._convert(self.num))
 
-bullet = TankBullet()
-light = Marine()
-light.accept(bullet)
-print(light.health)
+        def _rev_convert(self,n):
+            r=''
+            while n:
+                r=self.alf[n%self.notation]+r
+                n//=self.notation
+            return r or self.alf[0]
+
+        def _convert(self,n):
+            return reduce(lambda a,c:a*self.notation+self.alf.index(c),n,0)
+        
+        def __str__(self):
+            return self.num
+        
+        def __add__(self,other):
+            return Number_class(self._rev_convert(self._convert(self.num)+self._convert(other.num)))
+        
+        def __mul__(self,other):
+            return Number_class(self._rev_convert(self._convert(self.num)*self._convert(other.num)))
+        
+        def __sub__(self,other):
+            return Number_class(self._rev_convert(self._convert(self.num)-self._convert(other.num)))
+        
+        def __floordiv__(self,other):
+            return Number_class(self._rev_convert(self._convert(self.num)//self._convert(other.num)))
+    
+    return Number_class
+
+BinClass=create_number_class('01')
+HexClass=create_number_class('0123456789ABCDEF')
+b=BinClass('1010')
+
+print(b.convert_to(HexClass))
