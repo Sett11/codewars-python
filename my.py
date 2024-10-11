402522d4f21152febbdd3cc27235ead452ca4596
Jhonny <gonzodzen@mail.ru>
2024-10-11 16:34:15 +0300

    Hungry Hippos

--- my.py
+++ my.py
@@ -1,40 +1,25 @@
-def fill(a,d=0):
-    def f(a,i,d):
-        if a[i] is not None:
-            return a[i]
-        if d==-1:
-            i+=1
-            while i<len(a):
-                if a[i] is not None:
-                    return a[i]
-                i+=1
-            return
-        if d==1:
-            i-=1
-            while i>=0:
-                if a[i] is not None:
-                    return a[i]
-                i-=1
-            return
-        else:
-            k,r=i,[]
-            i+=1
-            while i<len(a):
-                if a[i] is not None:
-                    r.append((abs(k-i),a[i]))
-                    break
-                i+=1
-            i=k-1
-            while i>=0:
-                if a[i] is not None:
-                    r.append((abs(k-i),a[i]))
-                    break
-                i-=1
-            if r:
-                return sorted(r)[0][1]
-    r=[None]*len(a)
-    for i in range(len(a)):
-        r[i]=f(a,i,d)
-    return r
+import networkx as nx
 
-print(fill([None, 1, None, None, None, 2, None],0))
+class Game():
+    def __init__(self,a):
+        self.a=a
+        self.g=nx.Graph()
+        n,m=len(a),len(a[0])
+        for i in range(n):
+            for j in range(m):
+                if self.a[i][j]:
+                    t=(i,j)
+                    self.g.add_node(t)
+                    for k in [(x,y) for x,y in [(i+1,j),(i-1,j),(i,j+1),(i,j-1)] if 0<=x<n and 0<=y<m and self.a[x][y]]:
+                        self.g.add_edge(t,k)
+
+    def play(self):
+        return len(list(nx.algorithms.connected_components(self.g)))
+
+g=Game([[1,0,1,0,1],
+             [1,0,1,0,1],
+             [1,1,1,0,0],
+             [0,0,0,1,1],
+             [0,0,0,1,1]])
+
+print(g.play())
