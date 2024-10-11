d36e418ff3ebc01302db922eb24d83161a65c6f5
Jhonny <gonzodzen@mail.ru>
2024-10-11 15:00:11 +0300

    Fill None

--- my.py
+++ my.py
@@ -1,25 +1,40 @@
-def validate_args(*args):
-    def inner_func(func):
-        def check(*w):
-            for i in w:
-                if not isinstance(i,args):
-                    raise InvalidArgument()
-            return func(*w)
-        check.__name__=func.__name__
-        check.__doc__=func.__doc__
-        return check
-    return inner_func
+def fill(a,d=0):
+    def f(a,i,d):
+        if a[i] is not None:
+            return a[i]
+        if d==-1:
+            i+=1
+            while i<len(a):
+                if a[i] is not None:
+                    return a[i]
+                i+=1
+            return
+        if d==1:
+            i-=1
+            while i>=0:
+                if a[i] is not None:
+                    return a[i]
+                i-=1
+            return
+        else:
+            k,r=i,[]
+            i+=1
+            while i<len(a):
+                if a[i] is not None:
+                    r.append((abs(k-i),a[i]))
+                    break
+                i+=1
+            i=k-1
+            while i>=0:
+                if a[i] is not None:
+                    r.append((abs(k-i),a[i]))
+                    break
+                i-=1
+            if r:
+                return sorted(r)[0][1]
+    r=[None]*len(a)
+    for i in range(len(a)):
+        r[i]=f(a,i,d)
+    return r
 
-
-@validate_args(str)
-def say_hello(name):
-    """Say hello to {name}"""
-    return "Hello, " + str(name)
-
-@validate_args(int,float)
-def f(a,b):
-    return a+b
-
-print(say_hello('Jhonny'))
-print(f(1,2.0))
-print(say_hello.__name__)
+print(fill([None, 1, None, None, None, 2, None],0))
