ac72df8b6924d2a9e930f8b2de61894103b43746
Jhonny <gonzodzen@mail.ru>
2024-10-11 09:43:02 +0300

    Channelling power of Primes into a new Number

--- my.py
+++ my.py
@@ -1,44 +1,15 @@
-from functools import reduce
+import gmpy2
 
-def create_number_class(a):
-    class Number_class:
-        def __init__(self,s):
-            self.alf=a
-            self.num=s
-            self.notation=len(a)
+def e(n):
+    l=gmpy2.isqrt(n)+1
+    n+=1
+    b=gmpy2.xmpz(3)
+    b[4:n:2]=-1
+    for i in b.iter_clear(3,l):
+        b[i*i:n:i+i]=-1
+    return list(b.iter_clear(2,n))
 
-        def convert_to(self,other):
-            return other('')._rev_convert(self._convert(self.num))
+def channelling_primes(n):
+    return sum(map(lambda x:2**(x-1),e(n)))
 
-        def _rev_convert(self,n):
-            r=''
-            while n:
-                r=self.alf[n%self.notation]+r
-                n//=self.notation
-            return r or self.alf[0]
-
-        def _convert(self,n):
-            return reduce(lambda a,c:a*self.notation+self.alf.index(c),n,0)
-        
-        def __str__(self):
-            return self.num
-        
-        def __add__(self,other):
-            return Number_class(self._rev_convert(self._convert(self.num)+self._convert(other.num)))
-        
-        def __mul__(self,other):
-            return Number_class(self._rev_convert(self._convert(self.num)*self._convert(other.num)))
-        
-        def __sub__(self,other):
-            return Number_class(self._rev_convert(self._convert(self.num)-self._convert(other.num)))
-        
-        def __floordiv__(self,other):
-            return Number_class(self._rev_convert(self._convert(self.num)//self._convert(other.num)))
-    
-    return Number_class
-
-BinClass=create_number_class('01')
-HexClass=create_number_class('0123456789ABCDEF')
-b=BinClass('1010')
-
-print(b.convert_to(HexClass))
+print(channelling_primes(21))
