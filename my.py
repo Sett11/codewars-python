d0229dba421ec3bf37193838420362c5d4f4e9bd
Jhonny <gonzodzen@mail.ru>
2024-10-19 00:29:24 +0300

    Line Safari - Is that a line?

--- my.py
+++ my.py
@@ -1,160 +1,96 @@
-from collections import deque
-from copy import deepcopy
-import networkx as nx
-
-class Go:
-    def __init__(self,n,m=None):
-        if n>25 or (m and m>25):
-            raise Exception()
-        self.height=n
-        self.width=m if m is not None else n
-        self.size={"height":self.height,"width":self.width}
-        self.game_board=[['.']*self.width for _ in range(self.height)]
-        self.stones={'black':'x','white':'o'}
-        self.turn='black'
-        self.a,self.s=list(map(str,range(1,self.height+1)))[::-1],'ABCDEFGHJKLMNOPQRSTUVWXYZ'[:self.width]
-        self.handicap=False
-        self.g=nx.Graph()
-        self.res=False
-        self.current_move=None
-        self.board_states=deque([(deepcopy(self.game_board),self.g.copy(),self.current_move)])
-        self.stack=[self.height,self.width]
-        self.count_moves=0
-
-    @property
-    def board(self):
-        return self.game_board
-
-    def inner_check_liberties(self,a):
-        r=[]
-        for i in a:
-            c=0
-            for j,_ in i:
-                x,y=j
-                t=[(q,w) for q,w in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)] if 0<=q<self.height and 0<=w<self.width and self.game_board[q][w]=='.']
-                if not t:
-                    c+=1
-            if c==len(i):
-                r.append(i)
-        return r
-
-    def check_and_change(self,t):
-        self.g.add_node(t)
-        adj=self.g.adj
-        for i,j in adj:
-            x,y=i
-            k,z=t[0]
-            if abs(x-k)<=1 and abs(y-z)<=1 and t!=(i,j) and j==t[1] and (x==k or y==z):
-                self.g.add_edge(t,(i,j))
-        comp=self.inner_check_liberties(list(nx.algorithms.connected_components(self.g)))
-        if comp:
-            u=set(sum([[j[-1] for j in i] for i in comp],[]))
-            if len(u)==1 and t[1] in u:
-                return False
-            for i in comp:
-                if all(j[-1]!=t[-1] for j in i):
-                    for j,k in i:
-                        x,y=j
-                        self.game_board[x][y]='.'
-                        self.g.remove_node((j,k))
-        return True
-
-    def move(self,*moves):
-        self.stack.append(moves)
-        moves=[(i[:-1],i[-1]) for i in moves]
-        self.res=False
-        for i,j in moves:
-            self.count_moves+=1
-            if i in self.a and j in self.s:  
-                k,p=self.a.index(i),self.s.index(j)
-            else:
-                self.count_moves-=1
-                raise Exception('Cannot place a stone with out of bounds coordinates!')
-            curr_pos=self.game_board[k][p]
-            if curr_pos=='.':
-                self.current_move=((k,p),self.turn)
-                self.game_board[k][p]=self.stones[self.turn]
-                test=self.check_and_change(self.current_move)
-                self.turn='black' if self.turn=='white' else 'white'
-                self.board_states.append((deepcopy(self.game_board),self.g.copy(),self.current_move))
-                if test is False:
-                    self.stack.append('sui')
-                    self.rollback()
-                    raise Exception('Suicide move!')
-            else:
-                self.count_moves-=1
-                raise Exception('Cannot place a stone on an existing stone!')
-        if self.board_states[-1][-1] and len(self.board_states)>2:
-            x,y=(self.game_board,self.current_move),self.board_states[-3]
-            if x==(y[0],y[-1]):
-                self.rollback()
-                raise Exception('Repeated move!')
-            
-    def get_position(self,s):
-        return self.game_board[self.a.index(s[:-1])][self.s.index(s[-1])]
-
-    def handicap_stones(self,n):
-        self.stack.append('han')
-        x=(self.height,self.width)
-        if x not in [(9,9),(13,13),(19,19)] or n>9 or len(self.board_states)>1 or self.handicap:
-            raise Exception()
-        self.handicap=True
-        positions={(9,9):[(2,6),(6,2),(6,6),(2,2),(4,4)],
-                   (13,13):[(3,9),(9,3),(9,9),(3,3),(6,6),(6,3),(6,9),(3,6),(9,6)],
-                   (19,19):[(3,15),(15,3),(15,15),(3,3),(9,9),(9,3),(9,15),(3,9),(15,9)]}
-        curr_han,c=positions[x],0
-        if n>len(curr_han):
-            raise Exception()
-        while c<n:
-            i,j=curr_han[c]
-            self.game_board[i][j]=self.stones[self.turn]
-            c+=1
-        self.board_states.append((deepcopy(self.game_board),self.g.copy(),self.current_move))
-
-    def rollback(self,n=1):
-        self.stack.append(f'roll {n}')
-        if self.res or n>self.count_moves or n>=len(self.board_states):
-            raise Exception('Not rollback!')
-        while n and self.count_moves:
-            self.board_states.pop()
-            t=self.board_states[-1]
-            self.game_board,self.g,self.current_move=deepcopy(t[0]),t[1].copy(),t[2]
-            self.turn='black' if self.turn=='white' else 'white'
-            n-=1
-            self.count_moves-=1
+def line(a):
+    n,m,b,u,u2,u3,v=len(a),len(a[0]),sum([[(i,j) for j,k in enumerate(p) if k=='X'] for i,p in enumerate(a)],[]),set(),set(),set(),False
+    f=lambda i,j:[(x,y) for x,y in [(i+1,j),(i-1,j),(i,j+1),(i,j-1)] if 0<=x<n and 0<=y<m and (x,y) not in u2.union(u) and a[x][y]!=' ']
+    u3.update(b)
     
-    def pass_turn(self):
-        self.stack.append('pass')
-        self.res=False
-        self.turn='black' if self.turn=='white' else 'white'
-        self.current_move=None
-        self.board_states.append((deepcopy(self.game_board),self.g.copy(),self.current_move))
-        self.count_moves+=1
-
-    def reset(self):
-        self.res=True
-        t=self.board_states[0]
-        self.game_board,self.g,self.current_move=deepcopy(t[0]),t[1].copy(),t[2]
-        self.board_states.clear()
-        self.board_states.append((deepcopy(self.board),self.g.copy(),self.current_move))
-        self.turn='black'
-        self.handicap=False
-        self.stack=[self.height,self.width]
-        self.count_moves=0
-
-game=Go(8)
-
-a=[('6E',), ('5B',), ('7A',), ('3C',), 'pass', ('5D',), ('5C',), 'roll 1', ('2E',), ('3D',), ('4E',), ('2F',), ('3F',), ('4A',), ('4H',), ('7F',), 'roll 12', ('1D',), ('4D',), ('2C',), ('4C',), ('3A',), ('7G',), ('6D',), ('4B',), ('6C',), ('4F',), ('1F',), ('3G',), ('5F',), ('1E',), ('3E',), ('7B',), ('5A',), ('2H',), ('2D',), ('1G',), ('6A',), ('4G',), ('2A',), 'roll 5', ('1A',), ('1C',), ('2B',), ('7H',), ('3B',), ('5H',), ('6B',), 'pass', ('7E',), ('7C',), ('3H',), ('1B',), 'sui', 'roll 1', ('1H',), ('7D',)]
-
-for i in a:
-    if isinstance(i,str):
-        if i.startswith('roll'):
-            game.rollback(int(i.split()[1]))
-        if i=='sui':
-            pass
+    def dfs(i,j,d):
+        nonlocal v
+        if (i,j) in b:
+            v=True
+            return
+        u.add((i,j))
+        t=f(i,j)
+        if a[i][j]!='+':
+            if d in 'rl':
+                t=[(x,y) for x,y in t if y in (j-1,j+1)]
+            else:
+                t=[(x,y) for x,y in t if x in (i-1,i+1)]
+            if not t:
+                return
+            t=t[0]
+            if (a[t[0]][t[1]]=='-' and d in 'rl') or (a[t[0]][t[1]]=='|' and d in 'ud') or a[t[0]][t[1]] in '+X':
+                x,y=t
+                dfs(x,y,'u' if x<i else 'd' if x>i else 'r' if y>j else 'l')
+            else:
+                return
         else:
-            game.pass_turn()
-    else:
-        game.move(i[0])
+            if d in 'rl':
+                t=[(x,y) for x,y in t if x in (i-1,i+1)]
+                if not t or (len(t)>1 and ([1 for x,y in t if a[x][y] not in '+X'] or all(a[x][y]=='+' for x,y in t))):
+                    return
+                x,y=t[0]
+                dfs(x,y,'d' if x>i else 'u')
+            else:
+                t=[(x,y) for x,y in t if y in (j-1,j+1)]
+                if not t or (len(t)>1 and ([1 for x,y in t if a[x][y] not in '+X'] or all(a[x][y]=='+' for x,y in t))):
+                    return
+                x,y=t[0]
+                dfs(x,y,'r' if y>j else 'l')
 
-print(*game.board,sep='\n')
+    for i,j in b:
+        u.clear()
+        u2.clear()
+        u2.add((i,j))
+        t=f(i,j)
+        for k in range(len(t)):
+            x,y=t[k]
+            if (y in (j+1,j-1) and a[x][y] not in '-+X') or (x in (i+1,i-1) and a[x][y] not in '|+X'):
+                t[k]=0
+        t=[k for k in t if k]
+        if len(t)>1 or not t:
+            continue
+        x,y=t[0]
+        dfs(x,y,'u' if x<i else 'd' if x>i else 'r' if y>j else 'l')
+        u3.update(u)
+        if v:
+            break
+    for i in range(n):
+        for j in range(m):
+            if a[i][j]!=' ' and (i,j) not in u3:
+                return False
+    return v
+
+print(line(['   +-----+  ',
+            '   |+---+|  ',
+            '   ||+-+||  ',
+            '   |||X+||  ',
+            '   X|+--+|  ',
+            '    +----+  ']))
+print(line(['    +----+  ',
+            '    |+--+|  ',
+            '    ||X+||  ',
+            '    |+-+||  ',
+            '    +---+|  ',
+            'X--------+  ']))
+print(line(['    +-+    ',
+            '    | |    ',
+            '    ++++   ',
+            '    ++++   ',
+            '   X+++    ',
+            '     +---X ']))
+
+print(line(['     X        ', '     |   |    ', ' +   |  -+-   ', '     |   |    ', '     X        ']))
+print(line(["                      ",
+            "   +-------+          ",
+            "   |      +++---+     ",
+            "X--+      +-+   X     "]))
+print(line(["                    ",
+            "     +--------+     ",
+            "  X--+        +--+  ",
+            "                 |  ",
+            "                 X  ",
+            "                    "]))
+print(line(["           ",
+                "X---------X",
+                "           ",
+                "           "]))
