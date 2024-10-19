f37d1f615c20b23855a77d1c4bca56e79bbff7fd
Jhonny <gonzodzen@mail.ru>
2024-10-19 22:26:10 +0300

    Monty Hall Problem

--- my.py
+++ my.py
@@ -1,13 +1,4 @@
-def leaderboard_sort(a,b):
-    for i in b:
-        x,y=i.split()
-        k,p=int(y),a.index(x)
-        while k:
-            t=p-1 if k>0 else p+1
-            a[p],a[t]=a[t],a[p]
-            k=k+1 if k<0 else k-1
-            p=t
-    return a
+def monty_hall(n,a):
+    return round(100-100*len([i for i in a if i==n])/len(a))
 
-print(leaderboard_sort(['John', 'Brian', 'Jim', 'Dave', 'Fred'], ['Dave +1', 'Fred +4', 'Brian -1']))
-print(leaderboard_sort(['Bob', 'Larry', 'Kevin', 'Jack', 'Max'], ['Max +3', 'Kevin -1', 'Kevin +3']))
+print(monty_hall(2, [2,1,2,1,2,3,1,1,2,2,3]))
