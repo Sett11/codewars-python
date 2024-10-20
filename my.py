2f733cf48f30fcf995a03d22732fb5951cb0436f
Jhonny <gonzodzen@mail.ru>
2024-10-21 00:05:23 +0300

    Group prime numbers

--- my.py
+++ my.py
@@ -1,10 +1,21 @@
-from bisect import bisect as bs
 
-def leaderboard_climb(arr,kara):
-    n,a,b=len(kara),sorted(set(arr)),kara[:]
-    for i in range(n):
-        b[i]=(len(a)-bs(a,b[i])+1) or 1
-    return b
-        
 
-print(leaderboard_climb([92, 83, 80, 69, 64, 55, 52, 42, 39, 36, 28, 17, 12, 1], [4, 5, 14, 23, 27, 28, 35, 44, 46, 50, 56, 58, 61, 67, 74, 81, 88, 88, 90, 96, 100, 100]))
+# from gmpy2 import next_prime
+
+# def gen_prime(n,p=2):
+#     while n:
+#         yield p
+#         x=int(next_prime(p))
+#         p=x
+#         n-=1
+
+# def get_primes(n,m=2):
+#     g=gen_prime(n)
+#     for _ in range(0,n,m):
+#         t=[]
+#         for __ in range(m):
+#             t.append(next(g,None))
+#         yield tuple(t)
+
+# g=get_primes(11,5)
+# print([next(g) for _ in range(3)])
