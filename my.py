951c6ddd471515aaa9f8609fccc9a90452c4e71b
Jhonny <gonzodzen@mail.ru>
2024-10-21 00:03:58 +0300

    Climbing the Leaderboard

--- my.py
+++ my.py
@@ -1,10 +1,10 @@
-def climbing_stairs(a):
-    n=len(a)
-    r=a[:2]+[0]*(n-2)
-    for i in range(2,n):
-        r[i]=min(r[i-1],r[i-2])+a[i]
-    return min(r[-1],r[-2])
+from bisect import bisect as bs
 
-print(climbing_stairs([0, 2, 3, 2]))
-print(climbing_stairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
-print(climbing_stairs([10,15,20]))
+def leaderboard_climb(arr,kara):
+    n,a,b=len(kara),sorted(set(arr)),kara[:]
+    for i in range(n):
+        b[i]=(len(a)-bs(a,b[i])+1) or 1
+    return b
+        
+
+print(leaderboard_climb([92, 83, 80, 69, 64, 55, 52, 42, 39, 36, 28, 17, 12, 1], [4, 5, 14, 23, 27, 28, 35, 44, 46, 50, 56, 58, 61, 67, 74, 81, 88, 88, 90, 96, 100, 100]))
