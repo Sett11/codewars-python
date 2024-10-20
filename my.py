5f8425d8f86c4d53ffa5526e2be8d3cf1b269556
Jhonny <gonzodzen@mail.ru>
2024-10-20 19:35:54 +0300

    Climbing Stairs

--- my.py
+++ my.py
@@ -1,33 +1,10 @@
-class predicate:
-    def __init__(self,fn):
-        self.fn=fn
-    
-    def __invert__(self):
-        return predicate(lambda *args,**kwargs:not self.fn(*args,**kwargs))
+def climbing_stairs(a):
+    n=len(a)
+    r=a[:2]+[0]*(n-2)
+    for i in range(2,n):
+        r[i]=min(r[i-1],r[i-2])+a[i]
+    return min(r[-1],r[-2])
 
-    def __and__(self,other):
-        return predicate(lambda *args,**kwargs:self.fn(*args,**kwargs) and other.fn(*args,**kwargs))
-    
-    def __or__(self,other):
-        return predicate(lambda *args,**kwargs:self.fn(*args,**kwargs) or other.fn(*args,**kwargs))
-    
-    def __call__(self,*args,**kwargs):
-        return self.fn(*args,**kwargs)
-    
-@predicate
-def is_positive(num):
-    return num > 0
-
-@predicate
-def is_even(num):
-    return num % 2 == 0
-
-@predicate
-def is_less_than(a, b):
-    return a < b
-
-
-print((is_positive & ~is_even)(2))
-# print((is_even & is_positive)(3))
-# print((is_even | is_positive)(3))
-# print((~is_even | is_positive)(-4))
+print(climbing_stairs([0, 2, 3, 2]))
+print(climbing_stairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
+print(climbing_stairs([10,15,20]))
