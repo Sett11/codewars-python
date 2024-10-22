7be1fb57aba57b4531e817618a988c9af9dd5850
Jhonny <gonzodzen@mail.ru>
2024-10-23 00:23:55 +0300

    N smallest elements in original order (performance edition)

--- my.py
+++ my.py
@@ -1,52 +1,13 @@
-from itertools import permutations,product
+import numpy as np
+from collections import Counter
 
-class Nonogram:
-    def __init__(self,c):
-        self.c=c
+def performant_smallest(a,n):
+    u,r=Counter(a[i] for i in np.argpartition(a,n)[:n]),[]
+    for i in range(len(a)):
+        if u[a[i]]:
+            r.append(a[i])
+            u[a[i]]-=1
+    return r
 
-    def __gen_st(self,a):
-        r=[]
-        for i in a:
-            t=[]
-            for j in i:
-                t.extend([1]*j)
-                t.append(0)
-            if len(t)<5:
-                t.extend([0]*(5-len(t)))
-            r.append(t)
-        return r
-    
-    def __hard_check(self,a):
-        t=[[list(j) for j in i] for i in self.c]
-        for i in range(2):
-            for j in range(5):
-                for k in range(len(t[i][j])):
-                    if 1 not in a[i][j]:
-                        return False
-                    g=a[i][j].index(1)
-                    if t[i][j][k]==1 and g!=4 and a[i][j][g+1]==1:
-                        return False
-                    while t[i][j][k]:
-                        if g>=len(a[i][j]):
-                            break
-                        if not a[i][j][g]:
-                            return False
-                        a[i][j][g]=0
-                        t[i][j][k]-=1
-                        if t[i][j][k]<0:
-                            return False
-                        g+=1
-        return all(not i for i in sum(sum(a,[]),[]))
-                        
-    def solve(self):
-        a=[tuple(i) for i in product(*[list(j) for j in [set(permutations(i,5)) for i in self.__gen_st(self.c[1])]])]
-        for k in a:
-            if self.__hard_check([[list(i) for i in zip(*k)],[list(i) for i in k]]):
-                return k
-
-            
-            
-
-n=Nonogram((((3,), (4,), (1, 3), (1,), (1,)), ((1, 1, 1), (2,), (3,), (2,), (3,))))
-
-print(n.solve(),sep='\n')
+print(performant_smallest([2,1,3,2,3],3))
+print(performant_smallest([16, 20, 4, 4, 11, 5, 6, 4, 8, 11, 10, 9, 17, 14, 18, 16, 19, 16, 16, 19, 17, 20, 10, 17, 4, 18, 18, 18, 1, 4, 15, 4, 7, 14, 2, 14, 11, 13, 15, 20, 6, 12, 1, 17, 18, 16, 3, 6, 19, 19, 8, 17, 1, 13, 18, 5, 19, 11, 2, 12, 15, 1, 17, 5, 13, 11, 3, 13, 4, 6, 11, 3, 9, 4, 14, 6, 5, 1, 7, 18, 2, 12, 16, 10, 7, 15, 14, 8, 17, 16, 14, 11, 12, 6, 8, 10, 11, 16, 8, 18],35))
