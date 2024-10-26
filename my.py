c30bebe91de30eb9eb27c8f888ec7d407b46767b
Jhonny <gonzodzen@mail.ru>
2024-10-27 00:39:48 +0300

    Blobservation

--- my.py
+++ my.py
@@ -1,136 +1,127 @@
-# class Blob:
-#     def __init__(self,o):
-#         self.x,self.y,self.size=o['x'],o['y'],o['size']
+from collections import namedtuple
+from copy import deepcopy
 
-#     def __add__(self,other):
-#         self.size+=other.size
+Blob=namedtuple('Blob','x y size')
 
-#     def __str__(self):
-#         return f'(x:{self.x} y:{self.y} size:{self.size})'
+class Blobservation:
+    def __init__(self,h,w=None):
+        self.h=h
+        self.w=w or h
+        self._min=1e9
+        self._mat=[[0]*self.w for _ in range(self.h)]
+        self._board=deepcopy(self._mat)
     
-#     def __repr__(self):
-#         return str(self)
-
-# class Blobservation:
-#     def __init__(self,h,w=None):
-#         self.h=h
-#         self.w=w or h
-#         self._min=1e9
-#         self._blobs=None
-#         self._coords=set()
+    def _check(self,a):
+        for i in a:
+            try:
+                b,c,d=i.values()
+                if b<0 or b>=self.h or c<0 or c>=self.w or d<1 or d>21 or bool([j for j in i.values() if type(j)!=int]):
+                    return False
+            except:
+                return False
+        return True
     
-#     def _check(self,a):
-#         for i in a:
-#             try:
-#                 if not (0<=i['x']<self.h and 0<=i['y']<self.w and 0<i['size']<=20):
-#                     return False
-#             except:
-#                 return False
-#         return True
-    
-#     def populate(self,a):
-#         if not self._check(a):
-#             raise Exception('Is not valid populate!')
-#         self._blobs=[] if self._blobs is None else self._blobs
-#         for i in a:
-#             blob=Blob(i)
-#             self._min=min(self._min,blob.size)
-#             if (blob.x,blob.y) in self._coords:
-#                 for j in self._blobs:
-#                     if blob.x==j.x and blob.y==j.y:
-#                         j.size+=blob.size
-#             else:
-#                 self._coords.add((blob.x,blob.y))
-#                 self._blobs.append(blob)
+    def populate(self,a):
+        if not self._check(a):
+            raise Exception('Is not valid populate!')
+        for i in a:
+            x,y,z=i['x'],i['y'],i['size']
+            self._board[x][y]+=z
+            self._min=min(self._min,self._board[x][y])
 
-#     def print_state(self):
-#         return list(map(lambda x:[x.x,x.y,x.size],self._blobs))
-    
-#     def _next_step(self,curr,a):
 
-#         def hand(a,b):
-#             q,w=abs(a.x-b.x),abs(a.y-b.y)
-#             s=q+w-min(q,w)
-#             if s==1:
-#                 return b.x,b.y,b.size
-#             if a.x==b.x:
-#                 if a.y<b.y:
-#                     return a.x,a.y+1,0
-#                 else:
-#                     return a.x,a.y-1,0
-#             if a.y==b.y:
-#                 if a.x<b.x:
-#                     return a.x+1,a.y,0
-#                 else:
-#                     return a.x-1,a.y,0
-#             if a.x<b.x and a.y<b.y:
-#                 return a.x+1,a.y+1,0
-#             if a.x>b.x and a.y>b.y:
-#                 return a.x-1,a.y-1,0
-#             if a.x<b.x and a.y>b.y:
-#                 return a.x+1,a.y-1,0
-#             if a.x>b.x and a.y<b.y:
-#                 return a.x-1,a.y+1,0
+    def print_state(self):
+        r,m=[],1e9
+        for i in range(self.h):
+            for j in range(self.w):
+                if self._board[i][j]:
+                    m=min(m,self._board[i][j])
+                    r.append([i,j,self._board[i][j]])
+        self._min=m
+        return sorted(r,key=lambda x:(x[0],x[1]))
+    
+    def _next_step(self,curr,a):
+        if not a:
+            return curr.x,curr.y,0
+
+        def hand(a,b):
+            s=max(abs(a.x-b.x),abs(a.y-b.y))
+            if s==1:
+                return b.x,b.y,b.size
+            if a.x==b.x:
+                if a.y<b.y:
+                    return a.x,a.y+1,0
+                else:
+                    return a.x,a.y-1,0
+            if a.y==b.y:
+                if a.x<b.x:
+                    return a.x+1,a.y,0
+                else:
+                    return a.x-1,a.y,0
+            if a.x<b.x and a.y<b.y:
+                return a.x+1,a.y+1,0
+            if a.x>b.x and a.y>b.y:
+                return a.x-1,a.y-1,0
+            if a.x<b.x and a.y>b.y:
+                return a.x+1,a.y-1,0
+            if a.x>b.x and a.y<b.y:
+                return a.x-1,a.y+1,0
             
-#         if len(a)==1:
-#             return hand(curr,a[0])
-#         m=max(a,key=lambda e:e.size).size
-#         a=[i for i in a if i.size==m]
-#         if len(a)==1:
-#             return hand(curr,a[0])
-#         b=sorted([i for i in a if i.x<=curr.x and i.y>=curr.y],key=lambda e:(e.x,e.y))
-#         if b:return hand(curr,b[0])
-#         b=sorted([i for i in a if i.x>=curr.x and i.y>=curr.y],key=lambda e:(e.x,e.y))
-#         if b:return hand(curr,b[0])
-#         b=sorted([i for i in a if i.x>=curr.x and i.y<=curr.y],key=lambda e:(e.x,e.y))
-#         if b:return hand(curr,b[0])
-#         b=sorted([i for i in a if i.x<=curr.x and i.y<=curr.y],key=lambda e:(e.x,e.y))
-#         if b:return hand(curr,b[0])
-#         return 'What the fack!???'
+        if len(a)==1:
+            return hand(curr,a[0])
+        m=max(a,key=lambda e:e.size).size
+        a=[i for i in a if i.size==m]
+        if len(a)==1:
+            return hand(curr,a[0])
+        b=sorted([i for i in a if i.x<=curr.x and i.y>=curr.y],key=lambda e:(e.x,e.y))
+        if b:return hand(curr,b[0])
+        b=sorted([i for i in a if i.x>=curr.x and i.y>=curr.y],key=lambda e:(e.x,-e.y))
+        if b:return hand(curr,b[0])
+        b=sorted([i for i in a if i.x>=curr.x and i.y<=curr.y],key=lambda e:(-e.x,-e.y))
+        if b:return hand(curr,b[0])
+        b=sorted([i for i in a if i.x<=curr.x and i.y<=curr.y],key=lambda e:(-e.x,-e.y))
+        if b:return hand(curr,b[0])
+        return 'What the fack!???'
     
-#     def move(self,n=1):
-#         a,r=self._blobs,{}
-#         for i in a:
-#             if i.size!=self._min:
-#                 r[i]=[]
-#                 m=1e9
-#                 for j in self._blobs:
-#                     if j.size<i.size:
-#                         c1,c2=abs(i.x-j.x),abs(i.y-j.y)
-#                         d=c1+c2-min(c1,c2)
-#                         m=min(m,d)
-#                         r[i].append([j,d])
-#                 r[i]=[k[0] for k in r[i] if k[1]==m]
-#         r={i:self._next_step(i,r[i]) for i in r}
-#         return set(r.values())
-        
+    def move(self,n=1):
+        if type(n)!=int or n<1:
+            raise Exception('Not valid input!')
+        k=0
+        a=list(map(lambda x:Blob(*x),self.print_state()))
+        while n:
+            r={}
+            if len(a)<=1:
+                break
+            k=1
+            for i in a:
+                r[i]=[]
+                m=1e9
+                if i.size!=self._min:
+                    for j in a:
+                        if j.size<i.size:
+                            d=max(abs(i.x-j.x),abs(i.y-j.y))
+                            m=min(m,d)
+                            r[i].append([j,d])
+                    r[i]=[k[0] for k in r[i] if k[1]==m]
+            r,q,w={i:self._next_step(i,r[i]) for i in r},deepcopy(self._mat),{}
+            for i in r:
+                b,c,_=r[i]
+                d=i.size
+                t=b,c
+                q[b][c]+=d
+                if t not in w:
+                    w[t]=0
+                w[t]+=d
+            a=[Blob(*(list(i)+[w[i]])) for i in w]
+            n-=1
+        if k:
+            self._board=q
 
-# b=Blobservation(8)
-# b.populate([
-# 	{'x':0,'y':4,'size':3},
-# 	{'x':0,'y':7,'size':5},
-# 	{'x':2,'y':0,'size':2},
-# 	{'x':3,'y':7,'size':2},
-# 	{'x':4,'y':3,'size':4},
-# 	{'x':5,'y':6,'size':2},
-# 	{'x':6,'y':7,'size':1},
-# 	{'x':7,'y':0,'size':3},
-# 	{'x':7,'y':2,'size':1},])
+b=Blobservation(10,9)
+b.populate([{'x': 6, 'y': 6, 'size': 16}, {'x': 1, 'y': 6, 'size': 16}, {'x': 6, 'y': 3, 'size': 2}, {'x': 1, 'y': 4, 'size': 4}, {'x': 5, 'y': 1, 'size': 5}, {'x': 1, 'y': 8, 'size': 11}, {'x': 1, 'y': 1, 'size': 12}, {'x': 9, 'y': 0, 'size': 13}, {'x': 2, 'y': 3, 'size': 2}, {'x': 1, 'y': 0, 'size': 11}, {'x': 5, 'y': 7, 'size': 14}, {'x': 5, 'y': 2, 'size': 20}, {'x': 6, 'y': 1, 'size': 17}, {'x': 8, 'y': 4, 'size': 16}])
+print(b.move(6))
+print(b.move(2))
+print(b.move(1992))
+print(b.print_state())
 
-# # b.populate([
-# # {'x':0,'y':2,'size':2},
-# # {'x':0,'y':0,'size':2},
-# # {'x':1,'y':1,'size':1}])
-
-# # b.populate([{'x':0,'y':0,'size':5},
-# # 	{'x':0,'y':2,'size':10},
-# # 	{'x':2,'y':2,'size':3},
-# # 	{'x':2,'y':0,'size':9}])
-# print(b.move())
-
-import numpy as np
-
-def throw_rigged():
-    return np.random.choice(np.arange(1,7),p=([0.156]*5)+[.220])
-
-print(throw_rigged())
+# print(*[[(i,j) for j in range(5)] for i in range(5)],sep='\n')
