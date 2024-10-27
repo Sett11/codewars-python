35316a492ac2880de01587427d0b26e2c0d084b1
Jhonny <gonzodzen@mail.ru>
2024-10-28 01:35:43 +0300

    I am all alone (poor me)

--- my.py
+++ my.py
@@ -1,7 +1,25 @@
-from math import gcd
-from functools import reduce
+def all_alone(a):
+    n,m,u,v=len(a),len(a[0]),set(),True
+    def dfs(i,j):
+        nonlocal v
+        if not v or i<0 or i>=n or j<0 or j>=m or (i,j) in u or a[i][j] not in 'Xo ':
+            return
+        if a[i][j]=='o':
+            v=False
+            return
+        u.add((i,j))
+        dfs(i+1,j),dfs(i-1,j),dfs(i,j+1),dfs(i,j-1)
+    for i in range(n):
+        for j in range(m):
+            if a[i][j]=='X':
+                dfs(i,j)
+    return v
 
-def solution(a):
-    return reduce(gcd,a)*len(a)
-
-print(solution([6, 9, 21]))
+print(all_alone([
+            list("  o                o        #######"),
+            list("###############             #     #"),
+            list("#             #        o    #     #"),
+            list("#  X          ###############     #"),
+            list("#                                 #"),
+            list("###################################")
+        ]))
