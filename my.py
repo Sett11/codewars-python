7c39c1eea3a397bfe80217c33682c545ef92d848
Jhonny <gonzodzen@mail.ru>
2024-10-27 13:20:50 +0300

    Picking the best casino game

--- my.py
+++ my.py
@@ -1,9 +1,22 @@
-def missing(a,s):
-    s=s.replace(' ','')
-    try:
-        return ''.join(s[i].lower() for i in sorted(a))
-    except:
-        return "No mission today"
+from collections import namedtuple
+from operator import mul
 
-print(missing([0, 3, 5], "I love you"))
-print(missing([29, 31, 8], "The quick brown fox jumps over the lazy dog"))
+Game = namedtuple("Game", ["name", "outcomes"])
+
+def find_best_game(a):
+    return max([[sum(mul(*j) for j in i.outcomes),i.name] for i in a])[1]
+
+g1 = Game("Breakeven Steven", ((0.5, 20), (0.5, -20)))
+
+# EV = -0.1
+g2 = Game("Go big or go home", ((0.99, -10), (0.01, 980)))
+
+# EV = -0.2
+g3 = Game("Steady Eddy", ((0.51, -10), (0.49, 10)))
+
+# EV = -20
+g4 = Game("The staircase", ((0.75, -100), (0.10, 100),
+                            (0.05, 200),(0.05, 300),
+                            (0.05, 400)))
+
+print(find_best_game((g1,g2,g3,g4)))
