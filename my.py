5b8e63369dade8e514c9a75025a1379430fc5f30
Jhonny <gonzodzen@mail.ru>
2024-10-30 00:02:53 +0300

    Queue time counter

--- my.py
+++ my.py
@@ -1,17 +1,26 @@
-import networkx as nx
+from collections import deque
 
-def shortest_path(a,s,e):
-    g=nx.DiGraph()
-    [[g.add_edge(i,j,weight=a[i][j]) for j in a[i]] for i in a]
-    p=sorted(nx.all_shortest_paths(g,s,e,weight=lambda x,y,_:g[x][y]['weight']))
-    return [i for i in p if len(i)==len(min(p,key=len))]
+class Num(int):
+    def __init__(self,n):
+        self.n=n
 
-print(shortest_path({'a' : {'b': 10, 'c': 20, 'e':20},
-            'b' : {'a': 10, 'd': 20},
-            'c' : {'a': 10, 'f': 20},
-            'd' : {'b': 10, 'e': 20, 'g': 20},
-            'e' : {'a': 10, 'd': 20, 'f': 20},                      
-            'f' : {'c': 10, 'e': 20, 'h': 20},
-            'g' : {'d': 10, 'h': 20},
-            'h' : {'g': 10, 'f': 20},
-},'a','f'))
+    def __sub__(self,other):
+        return Num(self.n-other)
+
+def queue(a,i):
+    a[i]=Num(a[i])
+    q,c=deque(a),0
+    while True:
+        q[0]-=1
+        c+=1
+        if isinstance(q[0],Num) and not q[0]:
+            return c
+        elif not q[0]:
+            q.popleft()
+        else:
+            q.append(q.popleft())
+        
+
+print(queue([2, 5, 3, 6, 4],1))
+print(queue([2, 5, 3, 6, 4],4))
+print(queue([2, 5, 3, 6, 4], 3))
