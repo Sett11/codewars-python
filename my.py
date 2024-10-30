fcd1428b03e262498b9840d559d3a8a6942c51a2
Jhonny <gonzodzen@mail.ru>
2024-10-31 01:23:21 +0300

    Superheroes Convention #1 Pandemic

--- my.py
+++ my.py
@@ -1,14 +1,19 @@
-def phone_call(min1, min2_10, min11, s):
-    for i in range(1,100):
-        if i==1:
-            s-=min1
-        elif i in range(2,11):
-            s-=min2_10
-        else:
-            s-=min11
-        if s<=0:
-            break
-    return i if not s else i-1
+def is_possible(a):
+    v,d=True,{}
+    def dfs(x,c):
+        nonlocal v
+        d[x]=c
+        for i in a[x]:
+            if i not in d:
+                dfs(i,0 if c else 1)
+            if i in d and d[i]==c:
+                v=False
+                return
+    for i in a:
+        if i not in d:
+            dfs(i,0)
+    return v
 
-print(phone_call(6,2,6,58))
-print(phone_call(10,1,2,22))
+print(is_possible({0:[1, 2], 1: [0], 2: [0], 3: [], 4:[5,6], 5:[4], 6:[4]}))
+print(is_possible({0:[1, 2,3], 1: [0], 2: [0,3], 3: [2,0]}))
+print(is_possible({0: [1, 2], 1: [0, 3], 2: [0, 3], 3: [1, 2]}))
