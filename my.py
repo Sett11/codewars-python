dd320d8c794bfe676c73bf78466920f739df6117
Jhonny <gonzodzen@mail.ru>
2024-10-30 15:19:38 +0300

    Evaluate a postfix expression

--- my.py
+++ my.py
@@ -1,16 +1,15 @@
-def max_hexagon_beam(n,a):
-    r=[0 for _ in range(6*n-3)]
-    z,t=0,3*n-2
-    for i in range(2*n-1):
-        k=max(i-n+1,0)
-        for j in range(i+n-2*k):
-            v=a[z%len(a)]
-            z+=1
-            r[i]+=v
-            r[t+k+j-i]+=v
-            r[2*t-k-j]+=v
-    return max(r)
+from collections import deque
+from re import sub
 
-print(max_hexagon_beam(5,(1,0,4,-10)),sep='\n')
-print(max_hexagon_beam(4,(2, 4, 6, 8)),sep='\n')
-print(max_hexagon_beam(2,(5,8,3,8)),sep='\n')
+def postfix_evaluator(s):
+    q,a=deque(),s.replace('/','//').split()
+    for i in a:
+        if sub(r'-','',i).isdigit():
+            q.append(int(i))
+        else:
+            p,t=q.pop(),q.pop()
+            q.append(eval(f'{t}{i}{p}'))
+    return q[-1]
+
+print(postfix_evaluator("21 21 +"))
+print(postfix_evaluator("4 8 + 6 5 - * 3 2 - 2 2 + * /"))
