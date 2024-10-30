fbcb07ebffcb64a37e292f88c38fc298402a6d38
Jhonny <gonzodzen@mail.ru>
2024-10-30 14:04:30 +0300

    Hexagon Beam Max Sum

--- my.py
+++ my.py
@@ -1,40 +1,16 @@
-from collections import Counter
+def max_hexagon_beam(n,a):
+    r=[0 for _ in range(6*n-3)]
+    z,t=0,3*n-2
+    for i in range(2*n-1):
+        k=max(i-n+1,0)
+        for j in range(i+n-2*k):
+            v=a[z%len(a)]
+            z+=1
+            r[i]+=v
+            r[t+k+j-i]+=v
+            r[2*t-k-j]+=v
+    return max(r)
 
-def hand(a,n,s):
-    for i in range(10):
-        for j in range(10):
-            if j+n<=10 and all(a[i][k]==1 for k in range(j,j+n)):
-                for k in range(j,j+n):
-                    a[i][k]=s
-                return
-            if j+n<=10 and all(a[k][i]==1 for k in range(j,j+n)):
-                for k in range(j,j+n):
-                    a[k][i]=s
-                return
-    return
-
-def check(a):
-    b=[4]+[3]*2+[2]*3+[1]*4
-    for i in b:
-        hand(a,i,chr(96+i))
-    try:
-        c=Counter('\n'.join(''.join([j if j else ' ' for j in i]) for i in a))
-        return sorted([i for i in c.items() if i[0].isalpha()])==[('a',4),('b',6),('c',6),('d',4)]
-    except:
-        return False
-
-def validate_battlefield(a):
-    if a==[[1, 1, 1, 0, 0, 0, 0, 0, 0, 0], [1, 1, 0, 0, 0, 0, 0, 0, 1, 0], [1, 1, 0, 0, 1, 1, 1, 0, 1, 0], [1, 0, 0, 0, 0, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]:
-        return True
-    return check(a.copy()) or check([i[::-1] for i in a.copy()]) or check(a.copy()[::-1]) or check([i[::-1] for i in a.copy()][::-1])
-
-print(validate_battlefield([[1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-                            [1, 1, 0, 0, 0, 0, 0, 0, 1, 0],
-                            [1, 1, 0, 0, 1, 1, 1, 0, 1, 0],
-                            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [1, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                            [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]))
+print(max_hexagon_beam(5,(1,0,4,-10)),sep='\n')
+print(max_hexagon_beam(4,(2, 4, 6, 8)),sep='\n')
+print(max_hexagon_beam(2,(5,8,3,8)),sep='\n')
