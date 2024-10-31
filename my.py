f7802f1f93c63b1ae469b35cb18b21545619cc40
Jhonny <gonzodzen@mail.ru>
2024-11-01 01:10:09 +0300

    Count the divisible numbers

--- my.py
+++ my.py
@@ -1,49 +1,9 @@
-from operator import add,sub,mul,floordiv
-from re import sub as rs
+def divisible_count(x,y,k):
+    return (y//k)-((x-1)//k)
 
-ops={'+':add,'&':sub,'*':mul,'/':floordiv}
-
-class Placeholder:
-  def __init__(self,expr=None):
-    self.expr=expr if expr is not None else 'x'
-  
-  def __call__(self,*args):
-    wr=lambda s:rs(r'\(\-*\d+\)',lambda x:x.group()[1:-1],s)
-    s=self.expr.replace('x','{}').format(*args)
-
-    def f(s):
-      a=s.group().split()
-      return str(ops[a[1]](int(a[0]),int(a[-1])))
-    
-    while '(' in s:
-      s=rs(r'-*\d+ ([\+\&\*\/]) -*\d+',f,wr(s))
-    return int(s)
-  
-  def __add__(self,other):
-    return Placeholder(f'({self.expr} + {other.expr if isinstance(other,Placeholder) else other})')
-  
-  def __sub__(self,other):
-    return Placeholder(f'({self.expr} & {other.expr if isinstance(other,Placeholder) else other})')
-  
-  def __mul__(self,other):
-    return Placeholder(f'({self.expr} * {other.expr if isinstance(other,Placeholder) else other})')
-  
-  def __floordiv__(self,other):
-    return Placeholder(f'({self.expr} / {other.expr if isinstance(other,Placeholder) else other})')
-  
-  def __radd__(self,other):
-    return Placeholder(f'({other.expr if isinstance(other,Placeholder) else other} + {self.expr})')
-  
-  def __rsub__(self, other):
-    return Placeholder(f'({other.expr if isinstance(other,Placeholder) else other} & {self.expr})')
-  
-  def __rmul__(self,other):
-    return Placeholder(f'({other.expr if isinstance(other,Placeholder) else other} * {self.expr})')
-  
-  def __rfloordiv__(self,other):
-    return Placeholder(f'({other.expr if isinstance(other,Placeholder) else other} / {self.expr})')
-
-x = Placeholder()
-
-print((43 * (x // 11*(15+x)))(121,3))
-print((-28 - x)(4))
+print(divisible_count(x=1403301900265323690531138572053880024218531,
+y = 4600319395666899136859916446385340933332334,
+k = 3975))
+print(divisible_count(x = 5862413216007111840996441910940983970854906,
+y = 8353394985382020029144896187055386031808064,
+k = 1435))
