65baa61ccb78b3a93299cdfd34a0b604f22b7261
Jhonny <gonzodzen@mail.ru>
2024-11-03 00:58:15 +0300

    Topology #0: Converging Coins

--- my.py
+++ my.py
@@ -1,33 +1,31 @@
-class FuncAdd:
-    dp={}
+def converge(g,a,b,c):
+    if a==b==c:
+        return 0
+    q=tuple([tuple([a]),tuple([b]),tuple([c])])
+    w=set(q)
+    for i in range(len(g)):
+        q=[set(k for j in i for k in g[j]) for i in q]
+        if q[0]&q[1]&q[2]:
+            return i+1
+        q=tuple(map(tuple,q))
+        if q in w:
+            return
+        w.add(q)
+    return i
+        
 
-    def __init__(self,f):
-        self.name=s=f.__name__
-        if s not in FuncAdd.dp:
-            FuncAdd.dp[s]=[]
-        FuncAdd.dp[s].append(f)
+print(converge({
+            0: {5, 1, 3},
+            1: {0, 2},
+            2: {1},
+            3: {0, 4},
+            4: {3},
+            5: {6, 0},
+            6: {5},
+        },2,4,6))
 
-    def __call__(self,*args,**kwargs):
-        if self.name not in FuncAdd.dp:
-            raise NameError()
-        return tuple(i(*args,**kwargs) for i in FuncAdd.dp[self.name])
-    
-    @classmethod
-    def delete(cls,f):
-        del FuncAdd.dp[f.name]
-
-    @classmethod
-    def clear(cls):
-        FuncAdd.dp.clear()
-
-
-@FuncAdd
-def foo():
-    return 'Hello'
-
-@FuncAdd
-def foo():
-    return 'World'
-
-FuncAdd.delete(foo)
-print(foo())
+print(converge({
+            1: {2, 3},
+            2: {1, 3},
+            3: {1, 2},
+        },1,2,3))
