a61089dbd4c9db04a0d0438fd2b3741f5c56d9a0
Jhonny <gonzodzen@mail.ru>
2024-11-03 00:15:43 +0300

    Function Addition

--- my.py
+++ my.py
@@ -1,1 +1,33 @@
-from math import lcm
+class FuncAdd:
+    dp={}
+
+    def __init__(self,f):
+        self.name=s=f.__name__
+        if s not in FuncAdd.dp:
+            FuncAdd.dp[s]=[]
+        FuncAdd.dp[s].append(f)
+
+    def __call__(self,*args,**kwargs):
+        if self.name not in FuncAdd.dp:
+            raise NameError()
+        return tuple(i(*args,**kwargs) for i in FuncAdd.dp[self.name])
+    
+    @classmethod
+    def delete(cls,f):
+        del FuncAdd.dp[f.name]
+
+    @classmethod
+    def clear(cls):
+        FuncAdd.dp.clear()
+
+
+@FuncAdd
+def foo():
+    return 'Hello'
+
+@FuncAdd
+def foo():
+    return 'World'
+
+FuncAdd.delete(foo)
+print(foo())
