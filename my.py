1b1f7d443939bcae38cade0cd7da5d6ae4e5b22c
Jhonny <gonzodzen@mail.ru>
2024-11-03 02:17:31 +0300

    FIXME: Hello

--- my.py
+++ my.py
@@ -1,23 +1,30 @@
-from re import finditer
+class Dinglemouse:
+    def wrap(self,arg):
+        if isinstance(arg,int):
+            return f'I am {arg}.'
+        if arg in 'MF':
+            return f"I am {'male' if arg=='M' else 'female'}."
+        return f'My name is {arg}.'
 
-p,ix,cd=[0]*2,[(-1, 0), (0, 1), (1, 0), (0, -1)],0
-
-def i_am_here(s):
-    global p,ix,cd
-    for j in finditer(r'[qrRlL]\d*','q'+s):
-        i=j.group(0)
-        if i[0]=='r':
-            cd=(cd+1)%4
-        elif i[0]=='l':
-            cd=(cd+3)%4
-        elif i[0]=='q':
-            continue
-        else:
-            cd=(cd+2)%4
-        if len(i)>1:
-            i=int(i[1:])
-            p[0]+=i*ix[cd][0]
-            p[1]+=i*ix[cd][1]
-    return p if p!=[0,5] else [-10, 5]
-
-print(i_am_here('10r5r0'))
+    def __init__(self):
+        ...
+    
+    def setAge(self,age):
+        self.age=age
+        return self
+        
+    def setSex(self,sex):
+        self.sex=sex
+        return self
+        
+    def setName(self,name):
+        self.name=name
+        return self
+        
+    def hello(self):
+        return ('Hello. '+' '.join(map(self.wrap,self.__dict__.values()))).strip()
+    
+dm = Dinglemouse().setName("Bob").setAge(27).setSex('M')
+print(dm.hello())
+dm.name='Jhon'
+print(dm.hello())
