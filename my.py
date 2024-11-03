06e15bd37bbb4984d8e0c321de3a695935db04e0
Jhonny <gonzodzen@mail.ru>
2024-11-04 01:59:24 +0300

    Merged String Checker

--- my.py
+++ my.py
@@ -1,3 +1,11 @@
-def identify_bb(a,f):
-    n=len(a)
-    return a[f(*sum([[a[i]]*(i+1) for i in range(n)],[]))-sum([10*(i+1) for i in range(n)])-1]
+from collections import Counter
+from functools import cache
+
+@cache
+def f(a,b):
+    return '' if not (a and b) else a[0]+f(a[1:],b[1:]) if a[0]==b[0] else max(f(a[1:],b),f(a,b[1:]),key=len)
+
+def is_merge(s,a,b):
+    return f(s,a)==a and f(s,b)==b and Counter(s)==Counter(a+b)
+
+print(is_merge('codewars', 'cdw', 'oears'))
