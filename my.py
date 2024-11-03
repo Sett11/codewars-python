f8e6558028bee20607eb9beb2d0db6b52f7a633b
Jhonny <gonzodzen@mail.ru>
2024-11-04 01:36:28 +0300

    What Would They Have in Common?

--- my.py
+++ my.py
@@ -1,12 +1,27 @@
-from re import split,findall
-from functools import reduce
+from itertools import combinations
+from math import gcd
+from collections import defaultdict
 
-def f(e,x):
-    k,p=split(r'x\^?',e)
-    k,p=-1 if k=='-' else 1 if k=='' else int(k),1 if p=='' else int(p)
-    return p*k*pow(x,p-1)
+d=defaultdict(int)
 
-def differentiate(e,p):
-    return reduce(lambda a,c:a+f(c,p),[i[0] for i in findall(r'(-?\d*x(\^\d+)?)',e)],0)
+def find_int_inrange(a,b):
+    global d
+    m=0
+    for i in range(a,b+1):
+        if i not in d:
+            t=str(i)
+            x=sum(sum([[int(''.join(k)) for k in combinations(t,j)] for j in range(1,len(t))],[])+[int(t)])
+            y=sum(map(int,sum([[t[j:j+k] for j in range(len(t)-k+1)] for k in range(1,len(t))]+[[t]],[])))
+            z=gcd(x,y)
+            c=0
+            while z>1:
+                if x%z==0 and y%z==0:
+                    c+=1
+                z-=1
+            m=max(m,c)
+            d[i]=c
+        else:
+            m=max(m,d[i])
+    return [m]+[i for i in d if d[i]==m and a<=i<=b]
 
-print(differentiate("-5x^2+10x+4", 3))
+print(find_int_inrange(300,55000))
