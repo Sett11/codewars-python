358d029b1ccf59627f51e116f342bcc91852a0e0
Jhonny <gonzodzen@mail.ru>
2024-11-04 02:39:35 +0300

    I guess this is a 6kyu kata #5: Whac-A-Mole

--- my.py
+++ my.py
@@ -1,8 +1,36 @@
-def solve(arr):
-    n,a=1,sorted(arr)
-    for i in a:
-        if i<=n:
-            n+=i
-        else:
-            break
-    return n
+# def solve(n): 
+#     return list(map(int,str(n)))[::-1]
+
+# print(solve(45))
+
+
+def whac_a_mole(arr):
+    a,c=sorted(sum(arr,[]),reverse=True),0
+    def f(a):
+        while a[-1]<1:
+            a.pop()
+            if not a:
+                return
+    while a:
+        for _ in range(2):
+            f(a)
+            if not a:
+                break
+            a.pop()
+            c+=1
+            if not a:
+                break
+        a=[i-1 for i in a]
+    return c
+
+
+print(whac_a_mole([
+            [1, 1, 2, 2],
+            [3, 3, 4, 4],
+            [4, 8, 8, 8]
+        ]))
+print(whac_a_mole([
+            [6, 4, 1, 1],
+            [4, 4, 4, 4],
+            [1, 2, 3, 3]
+        ]))
