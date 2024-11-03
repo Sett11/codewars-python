85cd96f45d8a2c3ffe6a32684bc252d0532e3132
Jhonny <gonzodzen@mail.ru>
2024-11-03 21:24:52 +0300

    Differentiate a polynomial

--- my.py
+++ my.py
@@ -1,6 +1,12 @@
-from math import comb
+from re import split,findall
+from functools import reduce
 
-def get_participants(n):
-    return next(i for i in range(55) if comb(i,2)>=n)
+def f(e,x):
+    k,p=split(r'x\^?',e)
+    k,p=-1 if k=='-' else 1 if k=='' else int(k),1 if p=='' else int(p)
+    return p*k*pow(x,p-1)
 
-print(get_participants(700))
+def differentiate(e,p):
+    return reduce(lambda a,c:a+f(c,p),[i[0] for i in findall(r'(-?\d*x(\^\d+)?)',e)],0)
+
+print(differentiate("-5x^2+10x+4", 3))
