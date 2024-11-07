8db0210193e93f9bf58856d94e1465adee34ef83
Jhonny <gonzodzen@mail.ru>
2024-11-07 12:56:53 +0300

    Paginating a huge book

--- my.py
+++ my.py
@@ -1,11 +1,14 @@
-def solve(n):
-    if n<10:
-        return n
-    l=len(str(n))
-    s='9'*l
-    while int(s)>n:
-        s=s[1:]
-        l-=1
-    return 9*len(s)+sum(map(int,str(n-int(s))))
+def page_digits(n):
+    c,l,s=0,1,1
+    while s<=n:
+        e=min(n,s*10-1)
+        x=e-s+1
+        c+=x*l
+        l+=1
+        s*=10
+    return c
 
-print(solve(7019))
+
+print(page_digits(100))
+print(page_digits(4234))
+print(page_digits(6572))
