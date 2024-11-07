ee1518033ff507fd6817cd540274f9b6d8a0387b
Jhonny <gonzodzen@mail.ru>
2024-11-08 02:38:56 +0300

    Keith Numbers

--- my.py
+++ my.py
@@ -1,12 +1,15 @@
-def reverse(a):
-    r,i=[[a[0]]],1
-    for i in range(1,len(a)):
-        t=[a[i]]
-        k=len(r[-1])-1
-        while k>=0:
-            t.insert(0,r[-1][k]-t[0])
-            k-=1
-        r.append(t)
-    return r[-1]
+def is_keith_number(n):
+    if n<10:
+        return False
+    a,i=list(map(int,str(n))),1
+    while True:
+        s=sum(a)
+        if s==n:
+            return i
+        if i>100:
+            return False
+        a=a[1:]
+        a.append(s)
+        i+=1
 
-print(reverse([84, 42, 21, 10, 2]))
+print(is_keith_number(196))
