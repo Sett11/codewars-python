0a8f901d73ca2f14372ceefcf36119c009e22d32
Jhonny <gonzodzen@mail.ru>
2024-11-07 13:57:09 +0300

    Connect the Dots

--- my.py
+++ my.py
@@ -1,14 +1,38 @@
-def page_digits(n):
-    c,l,s=0,1,1
-    while s<=n:
-        e=min(n,s*10-1)
-        x=e-s+1
-        c+=x*l
-        l+=1
-        s*=10
-    return c
+def connect_the_dots(s):
+    a=[list(i) for i in s.splitlines()]
+    b=[i[1:] for i in sorted(sum([[(ord(p)-97,i,j) for j,p in enumerate(k) if p!=' '] for i,k in enumerate(a)],[]))]
+    def dfs(i,j,k,p):
+        a[i][j]='*'
+        if (i,j)==(k,p):
+            return
+        elif i==k:
+            if j<p:
+                dfs(i,j+1,k,p)
+            else:
+                dfs(i,j-1,k,p)
+        elif j==p:
+            if i<k:
+                dfs(i+1,j,k,p)
+            else:
+                dfs(i-1,j,k,p)
+        elif i<k and j<p:
+            dfs(i+1,j+1,k,p)
+        elif i>k and j>p:
+            dfs(i-1,j-1,k,p)
+        elif i<k and j>p:
+            dfs(i+1,j-1,k,p)
+        else:
+            dfs(i-1,j+1,k,p)
+    t=b.pop(0)
+    while b:
+        dfs(*t,*b[0])
+        t=b.pop(0)
+    return '\n'.join(''.join(i) for i in a)+'\n'
 
-
-print(page_digits(100))
-print(page_digits(4234))
-print(page_digits(6572))
+print(connect_the_dots((
+            "           \n" +
+            " a       b \n" +
+            " e         \n" +
+            "           \n" +
+            " d       c \n" +
+            "           \n" ))) 
