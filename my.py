4ee54120d0d2253fc318b5adf520848d49f5c461
Jhonny <gonzodzen@mail.ru>
2024-11-08 02:20:32 +0300

    Upside-Down Pyramid Addition...REVERSED!

--- my.py
+++ my.py
@@ -1,10 +1,12 @@
-def find_similar_dogs(s):
-    a,r,m=set(dogs[s]),{},0
-    for i in dogs:
-        if i!=s:
-            l=len(a&set(dogs[i]))
-            if l not in r:
-                r[l]=set()
-            r[l].add(i)
-            m=max(m,l)
-    return r[m]
+def reverse(a):
+    r,i=[[a[0]]],1
+    for i in range(1,len(a)):
+        t=[a[i]]
+        k=len(r[-1])-1
+        while k>=0:
+            t.insert(0,r[-1][k]-t[0])
+            k-=1
+        r.append(t)
+    return r[-1]
+
+print(reverse([84, 42, 21, 10, 2]))
