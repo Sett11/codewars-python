c2d62e1be5d75d958fb6aa3abbb37f7ff4354243
Jhonny <gonzodzen@mail.ru>
2024-11-08 00:00:32 +0300

    Dog recommendation system

--- my.py
+++ my.py
@@ -1,2 +1,10 @@
-def euclidean_distance(a,b):
-    return round(sum((j-i)**2 for i,j in zip(a,b))**.5,2)
+def find_similar_dogs(s):
+    a,r,m=set(dogs[s]),{},0
+    for i in dogs:
+        if i!=s:
+            l=len(a&set(dogs[i]))
+            if l not in r:
+                r[l]=set()
+            r[l].add(i)
+            m=max(m,l)
+    return r[m]
