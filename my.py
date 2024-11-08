1aa994a64509805ae5239914b18b6a9b5544b44e
Jhonny <gonzodzen@mail.ru>
2024-11-08 13:59:11 +0300

    Strongest even number in an interval

--- my.py
+++ my.py
@@ -1,23 +1,4 @@
-def word_game(grid,words,moves):
-    a,curr,ind=list(map(list,grid.splitlines())),[],[]
-    for i,j in moves:
-        if a[i][j]!=' ':
-            t=(i,j)
-            if t not in ind:
-                ind.append(t)
-                curr.append(a[i][j])
-                a[i][j]=a[i][j].upper()
-                if len(curr)==4:
-                    v=''.join(curr) in words
-                    for k,p in ind:
-                        a[k][p]=' ' if v else a[k][p].lower()
-                    curr,ind=[],[]
-            else:
-                ind.remove(t)
-                a[i][j]=a[i][j].lower()
-                curr.remove(a[i][j])
-    return '\n'.join([''.join(i) for i in a])
+def strongest_even(n,m):
+    return n if m-n<1 else n+(n%2) if m-n<2 else 2*strongest_even((n+(n%2))//2,(m-(m%2))//2)
 
-print(word_game( 'gdzuhn\nthcqyj\nantaaw\nkwzcab\nbcwwvg\nvsfjop',
-['cpca', 'bjya', 'gzuz', 'ksjv', 'wgfq', 'thvh', 'wnbo', 'tncw', 'daaw'],
-[(3, 3), (0, 5), (4, 1), (5, 4), (0, 3), (0, 2), (3, 2), (4, 5), (2, 4), (2, 3), (0, 1), (3, 1), (2, 4), (0, 2), (1, 1), (5, 5), (4, 0), (1, 5), (1, 4), (2, 0), (2, 0), (4, 0), (5, 0), (2, 4), (0, 2), (1, 0), (1, 1), (0, 4), (5, 0), (0, 4), (5, 2), (4, 1), (2, 5), (4, 1), (4, 0), (5, 3), (2, 0)]))
+print(strongest_even(129,193))
