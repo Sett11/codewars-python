e589e71e34fa1a0224983566a1a0df8364d1ea95
Jhonny <gonzodzen@mail.ru>
2024-11-10 01:17:49 +0300

    Tree Photography

--- my.py
+++ my.py
@@ -1,31 +1,11 @@
-from functools import reduce
+def tree_photography(a):
+    def f(a):
+        m,c=a[0],0
+        for i in a[1:]:
+            if i>m:
+                c+=1
+                m=i
+        return c
+    return 'left' if f(a)>f(a[::-1]) else 'right'
 
-CHARS="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-
-def f(n,b):
-    a,r=CHARS[:b],''
-    while n:
-        r=a[n%b]+r
-        n//=b
-    return r
-
-def ff(s,b):
-    r=CHARS[:b]
-    return str(reduce(lambda a,c:a*b+r.index(c),s,0))
-
-def is_polydivisible(s,b):
-    for i in range(1,len(s)+1):
-        if int(ff(s[:i],b))%i:
-            return False
-    return True
-
-def get_polydivisible(n,b):
-    c=i=r=0
-    while c<int(n):
-        if is_polydivisible(f(i,b),b):
-            c+=1
-            r=f(i,b)
-        i+=1
-    return r or '0'
-
-print(get_polydivisible('42',16))
+print(tree_photography([1, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2]))
