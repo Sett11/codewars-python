13caf260ec7a4659159e56ac88b270dd90e2570d
Jhonny <gonzodzen@mail.ru>
2024-11-14 00:07:18 +0300

    Ant Bridge

--- my.py
+++ my.py
@@ -1,19 +1,38 @@
-from copy import deepcopy
+from re import sub,findall
 
-def black_hole(n,i,j):
-	a,q,w=[[' ']*n for _ in range(n)],[(0,0),(0,n-1),(n-1,n-1),(n-1,0)],list(range(n**2,0,-1))
-	r=deepcopy(q)
-	while w:
-		for x,y in r:
-			if not w:
-				break
-			if a[x][y]!=' ':
-				q[0],q[1],q[2],q[3]=(q[0][0]+1,q[0][1]+1),(q[1][0]+1,q[1][1]-1),(q[2][0]-1,q[2][1]-1),(q[3][0]-1,q[3][1]+1)
-				r=deepcopy(q)
-				break
-			a[x][y]=w.pop()
-		else:
-			r[0],r[1],r[2],r[3]=(r[0][0],r[0][1]+1),(r[1][0]+1,r[1][1]),(r[2][0],r[2][1]-1),(r[3][0]-1,r[3][1])
-	return a[i][j]
-		
-print(black_hole(5,3,2),sep='\n')
+def ant_bridge(s,t):
+    t,curr,hole,back,b=findall(r'-|\d+',sub(r'\.+',lambda x:str(len(x.group())),t)),list(s),[],[],0
+    while t:
+        v=t.pop(0)
+        if v=='-':
+            if back:
+                if b==0:
+                    b=-1
+                    curr.append(back.pop())
+                else:
+                    curr.insert(b,back.pop())
+                    b-=1
+                if not back:
+                    b=0
+            elif hole:
+                curr.insert(0,hole.pop(0))
+        else:
+            k=int(v)+1
+            if curr:
+                hole.append(curr.pop())
+            while k:
+                if curr:
+                    hole.append(curr.pop())
+                elif back:
+                    hole.append(back.pop())
+                else:
+                    hole.append(hole.pop(0))
+                k-=1
+            back+=curr[:]
+            curr=[]
+            t.pop(0)
+    return ''.join(hole[::-1]+back+curr)
+        
+print(ant_bridge("IHGFEDCBA","-----.------..-----"))
+print(ant_bridge("HGFEDCBA","-----...---.....-......---....------"))
+print(ant_bridge('KJIHGFEDCBA','----....---.-..-----.....-----.....----'))
