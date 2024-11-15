84212e8165c6d160a10de64de911c7edd6919132
Jhonny <gonzodzen@mail.ru>
2024-11-16 01:28:46 +0300

    Plotting points on a grid.

--- my.py
+++ my.py
@@ -1,18 +1,13 @@
-def unpack(l):
-    r=[]
-    def f(x):
-        if isinstance(x,(str,int)) or not x:
-            r.append(x)
-            return
-        if isinstance(x,dict):
-            for i in x:
-                f(i)
-                f(x[i])
-        elif isinstance(x,(list,tuple,set)):
-            for i in x:
-                f(i)
-    for i in l:
-        f(i)
-    return r
+class Grid:
+    def __init__(self,w,h):
+        self.a=[['0']*w for _ in range(h)]
+    def plot_point(self,x,y):
+        self.a[y-1][x-1]='X'
+        pass
+    @property
+    def grid(self):
+        return '\n'.join(''.join(i) for i in self.a)
 
-print(unpack(['x', [1, 2, 4, [(1, 2), 3, 'y', 13], [1]], 1, 2, 12, {1, 2, 3}, {(1, 2): 'b', 'c': (3, 4)}]))
+g=Grid(5,7)
+g.plot_point(5,3)
+print(g.grid)
