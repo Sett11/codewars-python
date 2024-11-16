b199e9154d395da39c69b1c16ff1b39b9a3b5d7e
Jhonny <gonzodzen@mail.ru>
2024-11-17 00:11:36 +0300

    Histogram data

--- my.py
+++ my.py
@@ -1,13 +1,11 @@
-class Grid:
-    def __init__(self,w,h):
-        self.a=[['0']*w for _ in range(h)]
-    def plot_point(self,x,y):
-        self.a[y-1][x-1]='X'
-        pass
-    @property
-    def grid(self):
-        return '\n'.join(''.join(i) for i in self.a)
+from collections import Counter
 
-g=Grid(5,7)
-g.plot_point(5,3)
-print(g.grid)
+def histogram(a,b):
+    if not a:
+        return []
+    a,r=sorted(Counter(a).items()),[]
+    for i in range(0,a[-1][0]+1,b):
+        r.append(sum([j[1] for j in a if i<=j[0]<i+b]))
+    return r
+
+print(histogram([1, 1, 0, 1, 3, 2, 6], 1))
