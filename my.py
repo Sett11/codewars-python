5ff9ae8bf2bb27bc214dd7979fd11e5c2ed81887
Jhonny <gonzodzen@mail.ru>
2024-11-21 00:29:01 +0300

    The 5 Love Languages

--- my.py
+++ my.py
@@ -1,7 +1,8 @@
-from math import comb
+from preloaded import LOVE_LANGUAGES as l
 
-def est_subsets(a):
-    n=len(set(a))
-    return sum(comb(n,i) for i in range(1,n+1))
-
-print(est_subsets(['a', 'z', 'z', 'z', 'b', 'j', 'f', 'k', 'b', 'd', 'j', 'j', 'n', 'm', 'm']))
+def love_language(p,n):
+    r={i:0 for i in l}
+    for i in range(7*n):
+        if p.response(l[i%5])=='positive':
+            r[l[i%5]]+=1
+    return max(r.items(),key=lambda x:x[1])[0]
