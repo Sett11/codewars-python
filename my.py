dcbd9d5f1a79c52d1832efbab0a558603c901c27
Jhonny <gonzodzen@mail.ru>
2024-11-23 00:51:50 +0300

    Help Suzuki pack his coal basket!

--- my.py
+++ my.py
@@ -1,31 +1,10 @@
-from collections import defaultdict
+def pack_basket(w,s):
+    a,wt=sorted(map(int,s.replace('dust',' ').split())),[0]*(w+1)
+    for i in range(1,len(a)+1):
+        for j in range(w,0,-1):
+            if a[i-1]<=j:
+                wt[j]=max(wt[j],wt[j-a[i-1]]+a[i-1])
+    return f'The basket weighs {wt[-1]} kilograms'
+    
 
-class TreeNode:
-    def __init__(self, value, children = None):
-        self.value = value
-        self.children = [] if children is None else children
-
-
-def tree_printer(t):
-    d=defaultdict(list)
-    def dfs(x,k):
-        d[k].append(str(x.value))
-        if not x.children:
-            return
-        k+=1
-        for i in x.children:
-            dfs(i,k)
-        k-=1
-    dfs(t,0)
-    return '\n'.join(' '.join(i) for i in d.values())
-
-print(tree_printer(TreeNode('A', [
-                TreeNode('B', [
-                    TreeNode('D'),
-                    TreeNode('E')
-                ]),
-                TreeNode('C', [
-                    TreeNode('F'),
-                    TreeNode('G')
-                ])
-            ])))
+print(pack_basket(41,'dust 2 7 1 dust 4 75 dust 17 8dust 100dust'))
