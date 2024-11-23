eab18206b9baebb0b4e5893d6e8e133948f747ed
Jhonny <gonzodzen@mail.ru>
2024-11-23 13:54:36 +0300

    Weight assistant

--- my.py
+++ my.py
@@ -1,34 +1,15 @@
-from math import factorial as f
+def assistance(w):
+    if w<=25:
+        return 'Only lock' if w==25 else "Nothing to hang" if w==20 else 'Error'
+    plates,r={25: 'red', 20: 'blue', 15: 'yellow', 10: 'green', 5: 'black', 2.5: 'orange', 1.25: 'white'},[]
+    w-=25
+    for i in plates:
+        if w>i and i*2<=w:
+            k=0
+            while w>i and i*2<=w:
+                k+=1
+                w-=i*2
+            r.append(f'{k} {plates[i]}{"s" if k>1 else ""}')
+    return ', '.join(r)+', lock' if w==0 else 'Error'
 
-def count_the_ways(a):
-    n,m=len(a)//2,len(a[0])//2
-    return f(n+m)//(f(n)*f(m))*4 if (n and m) else 2 if (n or m) else 1
-
-print(count_the_ways(['X']))
-print(count_the_ways(['M', 'L', 'K', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M']))
-print(count_the_ways([
-['E','D','E'],
-['D','C','D'],
-['C','B','C'],
-['B','A','B'],
-['C','B','C'],
-['D','C','D'],
-['E','D','E']
-]))
-print(count_the_ways([
-            ['E','I','N','E','R','N','H','O','J','S','J','O','H','N','R','E','N','I','E'],
-            ['I','N','E','R','N','H','O','J','S','E','S','J','O','H','N','R','E','N','I'],
-            ['N','E','R','N','H','O','J','S','E','I','E','S','J','O','H','N','R','E','N'],
-            ['E','R','N','H','O','J','S','E','I','L','I','E','S','J','O','H','N','R','E'],
-            ['R','N','H','O','J','S','E','I','L','E','L','I','E','S','J','O','H','N','R'],
-            ['N','H','O','J','S','E','I','L','E','R','E','L','I','E','S','J','O','H','N'],
-            ['H','O','J','S','E','I','L','E','R','E','R','E','L','I','E','S','J','O','H'],
-            ['O','J','S','E','I','L','E','R','E','H','E','R','E','L','I','E','S','J','O'],
-            ['H','O','J','S','E','I','L','E','R','E','R','E','L','I','E','S','J','O','H'],
-            ['N','H','O','J','S','E','I','L','E','R','E','L','I','E','S','J','O','H','N'],
-            ['R','N','H','O','J','S','E','I','L','E','L','I','E','S','J','O','H','N','R'],
-            ['E','R','N','H','O','J','S','E','I','L','I','E','S','J','O','H','N','R','E'],
-            ['N','E','R','N','H','O','J','S','E','I','E','S','J','O','H','N','R','E','N'],
-            ['I','N','E','R','N','H','O','J','S','E','S','J','O','H','N','R','E','N','I'],
-            ['E','I','N','E','R','N','H','O','J','S','J','O','H','N','R','E','N','I','E']
-        ]))
+print(assistance(345))
