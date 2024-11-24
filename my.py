fe6c9793c5b2298750c9540c78bb31b8939192b7
Jhonny <gonzodzen@mail.ru>
2024-11-24 13:53:42 +0300

    Element equals its index

--- my.py
+++ my.py
@@ -1,18 +1,17 @@
-from functools import cache
+def bs(a):
+    l,r=0,len(a)
+    while r-l>1:
+        m=(l+r)//2
+        if a[m]<m:
+            l=m
+        else:
+            r=m
+    return l+1
 
-def target_game(a):
-    m=float('-inf')
-    @cache
-    def f(a,t):
-        nonlocal m
-        if not a:
-            m=max(m,t)
-            return
-        k=a[0]
-        if t+k>t:
-            f(a[2:],t+k)
-        f(a[1:],t)
-    f(tuple(a),0)
-    return m
+def index_equals_value(a):
+    r=bs(a)
+    return 0 if a[0]==0 else r if r<len(a) and a[r]==r else -1
 
-print(target_game([11, 82, 47, 48, 80, 35, 73, 99, 86, 32, 32]))
+print(index_equals_value((-3,0,1,3,10)))
+print(index_equals_value(((-5, 1, 2, 3, 4, 5, 7, 10, 15))))
+print(index_equals_value((0,)))
