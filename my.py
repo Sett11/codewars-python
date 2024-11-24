0c7d8a84bc9c050b701f8098394dfce41c49b390
Jhonny <gonzodzen@mail.ru>
2024-11-24 13:54:05 +0300

    Is This a Full Cycle?

--- my.py
+++ my.py
@@ -1,17 +1,13 @@
-def bs(a):
-    l,r=0,len(a)
-    while r-l>1:
-        m=(l+r)//2
-        if a[m]<m:
-            l=m
-        else:
-            r=m
-    return l+1
+def full_cycle(a):
+    n,u=len(a),set()
+    i=j=0
+    while j<=n:
+        t=a[i]
+        if t in u and len(u)==n:
+            return True
+        u.add(t)
+        i=t
+        j+=1
+    return False
 
-def index_equals_value(a):
-    r=bs(a)
-    return 0 if a[0]==0 else r if r<len(a) and a[r]==r else -1
-
-print(index_equals_value((-3,0,1,3,10)))
-print(index_equals_value(((-5, 1, 2, 3, 4, 5, 7, 10, 15))))
-print(index_equals_value((0,)))
+print(full_cycle([2, 0, 1]))
