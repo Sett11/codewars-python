743d6d52d75a3e58101c608ebc01b600e56c38d9
Jhonny <gonzodzen@mail.ru>
2024-11-26 01:29:14 +0300

    Simple square numbers

--- my.py
+++ my.py
@@ -1,1 +1,11 @@
-from math import comb as choose
+def solve(n):
+    for i in range(int(n**.5),0,-1):
+        k=n-i**2
+        if k>0 and k%(2*i)==0:
+            return (k//(2*i))**2
+    return -1
+
+print(solve(17))
+print(solve(3))
+print(solve(9))
+print(solve(88901))
