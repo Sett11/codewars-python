17e19be4101508340b57c7fa8c5f511fbf6b66d5
Jhonny <gonzodzen@mail.ru>
2024-11-26 02:20:06 +0300

    Pizza pieces

--- my.py
+++ my.py
@@ -1,9 +1,2 @@
-def reduce_by_rules(lst,r):
-    a,i=lst[::-1],0
-    while len(a)>1:
-        a.append(r[i%len(r)](a.pop(),a.pop()))
-        i+=1
-    return a[0]
-
-rules = [lambda a, b: a + b, lambda a, b: a - b]
-print(reduce_by_rules([2.0, 2.0, 3.0, 4.0],rules))
+def max_pizza(n):
+    return n*(n+1)//2+1 if n>=0 else -1
