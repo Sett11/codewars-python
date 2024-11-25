666f5a0344427a46db6693e59fa8ba13ccbe0091
Jhonny <gonzodzen@mail.ru>
2024-11-26 00:17:07 +0300

    Последовательность PaperFold

--- my.py
+++ my.py
@@ -1,6 +1,13 @@
-spoken=lambda s:s.title()+'.'
-shouted=lambda s:s.upper()+'!'
-whispered=lambda s:s.lower()+'.'
-greet=lambda f,s:f(s)
+def paper_fold():
+    s,i='1',0
+    def f():
+        nonlocal s
+        s=s+'1'+''.join('0' if i=='1' else '1' for i in s[::-1])
+    while True:
+        yield int(s[i])
+        i+=1
+        if i==len(s):
+            f()
 
-print(greet(spoken, "Hello"))
+g=paper_fold()
+print([next(g) for _ in range(20)])
