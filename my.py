c1bc3f6766a180423980d5bad917d39091f1419a
Jhonny <gonzodzen@mail.ru>
2024-11-26 01:03:39 +0300

    Quick (n choose k) calculator

--- my.py
+++ my.py
@@ -1,13 +1,1 @@
-def paper_fold():
-    s,i='1',0
-    def f():
-        nonlocal s
-        s=s+'1'+''.join('0' if i=='1' else '1' for i in s[::-1])
-    while True:
-        yield int(s[i])
-        i+=1
-        if i==len(s):
-            f()
-
-g=paper_fold()
-print([next(g) for _ in range(20)])
+from math import comb as choose
