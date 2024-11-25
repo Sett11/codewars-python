8a88f4b6440d2aeece6a5519eaff25dac563ee5e
Jhonny <gonzodzen@mail.ru>
2024-11-26 01:57:28 +0300

    Crazed Templating

--- my.py
+++ my.py
@@ -1,11 +1,4 @@
-def solve(n):
-    for i in range(int(n**.5),0,-1):
-        k=n-i**2
-        if k>0 and k%(2*i)==0:
-            return (k//(2*i))**2
-    return -1
+def letter_pattern(a):
+    return ''.join('*' if len(set(i))>1 else i[0] for i in zip(*a))
 
-print(solve(17))
-print(solve(3))
-print(solve(9))
-print(solve(88901))
+print(letter_pattern(['war', 'rad', 'dad']))
