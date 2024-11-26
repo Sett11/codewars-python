147b31f3093cd60cc2fd4ba080a325f6ab0a460c
Jhonny <gonzodzen@mail.ru>
2024-11-27 00:03:17 +0300

    Rule 30

--- my.py
+++ my.py
@@ -1,2 +1,12 @@
-def max_pizza(n):
-    return n*(n+1)//2+1 if n>=0 else -1
+def rule30(a,n):
+    r={(0,0,0):0,(0,0,1):1,(0,1,0):1,(0,1,1):1,(1,0,0):1,(1,0,1):0,(1,1,0):0,(1,1,1):0}
+    while n:
+        a=[0]+a+[0]
+        q=a[:]
+        for i in range(len(a)):
+            q[i]=r[tuple([a[i-1] if i else 0,a[i],a[i+1] if i!=len(a)-1 else 0])]
+        a=q
+        n-=1
+    return a
+
+print(rule30([1],5))
