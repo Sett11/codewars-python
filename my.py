0d82a10ec1711b494a91d73010d391f327bbbfaf
Jhonny <gonzodzen@mail.ru>
2024-12-03 00:06:26 +0300

    Make Me Slow

--- my.py
+++ my.py
@@ -1,8 +1,2 @@
-def vowel_recognition(s):
-    a,c,b,n=s.lower(),0,'aioue',len(s)
-    for i in range(len(a)):
-        if a[i] in b:
-            c+=(n-i)+(i*(n-i))
-    return c
-
-print(vowel_recognition('baceb'))
+def make_me_slow():
+    __import__('time').sleep(7)
