a1dd1afa532de4ea9f63a54de48ddeae51efd4b6
Jhonny <gonzodzen@mail.ru>
2024-12-21 00:13:34 +0300

    Ball and Cups

--- my.py
+++ my.py
@@ -1,4 +1,8 @@
-from math import pi
+def cup_and_balls(b,arr):
+    a=[0 if i!=b else 1 for i in range(1,4)]
+    for i,j in arr:
+        a[i-1],a[j-1]=a[j-1],a[i-1]
+    return a.index(1)+1
 
-def square_area(n):
-    return round(pow(n*4/(pi*2),2),2)
+print(cup_and_balls(2,[[1,3],[1,2],[2,1],[2,3]]))
+print(cup_and_balls(1,[[2,3],[1,2],[1,2]]))
