fc2085631b4670ec889dac3b9cb13589e5be79b0
Jhonny <gonzodzen@mail.ru>
2024-12-22 00:03:39 +0300

    Sorting by Exclusion

--- my.py
+++ my.py
@@ -1,8 +1,13 @@
-def cup_and_balls(b,arr):
-    a=[0 if i!=b else 1 for i in range(1,4)]
-    for i,j in arr:
-        a[i-1],a[j-1]=a[j-1],a[i-1]
-    return a.index(1)+1
+def sort_by_exclusion(a):
+    b,n=sorted(set(a)),len(a)
+    d,r=[b.index(i) for i in a],[0]*n
+    for i in range(n):
+        m=0
+        for j in range(i):
+            if d[i]>d[j] and r[j]>m:
+                m=r[j]
+        r[i]=m+1
+    return n-max(r)
 
-print(cup_and_balls(2,[[1,3],[1,2],[2,1],[2,3]]))
-print(cup_and_balls(1,[[2,3],[1,2],[1,2]]))
+
+print(sort_by_exclusion(["M","O","A"]))
