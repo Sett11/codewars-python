5b4c8b9be0b5b034e86fdf2a6f5c77803af0c8d7
Jhonny <gonzodzen@mail.ru>
2025-01-01 00:00:35 +0300

    Crossed words

--- my.py
+++ my.py
@@ -1,6 +1,13 @@
-from itertools import groupby
+def crossedwords(a,b):
+    r,k=[],next(i for i,j in enumerate(b) if j in a)
+    q=a.index(b[k])
+    for i in range(len(b)):
+        if i!=k:
+            t=''.join(' ' if j!=q else b[i] for j in range(len(a)))
+            r.append(t)
+        else:
+            r.append(a)
+    return '\n'.join(r)+'\n'
 
-def check_sequence(s,l,n):
-    return sum(1 for _,j in groupby(s) if len(list(j))==l)==n
-
-print(check_sequence('HTHHHHHHHTTHHTHHTHTTHH',2,5))
+print(crossedwords('ABXC', 'WXYZ'))
+print(crossedwords('GRAPHICAL', 'SYNTHESIS'))
