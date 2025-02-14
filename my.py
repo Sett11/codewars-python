4592e82e90d5e6a3978e51cf6db7b7161e4855e6
Jhonny <gonzodzen@mail.ru>
2025-02-15 00:17:23 +0300

    Make the small words big!

--- my.py
+++ my.py
@@ -1,8 +1,5 @@
-def four_piles(n,y):
-    for  i in range(1,n):
-        x=[(i+y),(i-y),(i*y),(i/y)]
-        if sum(x)==n and all(j for j in x):
-            return x
-    return []
+def small_word_helper(s):
+    return ' '.join(i.upper() if len(i)<4 else ''.join(j if j.lower() not in 'aouie' else '' for  j in i) for i in s.split())
 
-print(four_piles(48,3))
+print(small_word_helper("The quick brown fox jumps over the lazy dog"))
+print(small_word_helper("I'm just a small word from a small word family"))
