98a77f1c63bdc103d0fde89190341b17dc771031
Jhonny <gonzodzen@mail.ru>
2025-03-09 00:11:32 +0300

    US Postal Codes

--- my.py
+++ my.py
@@ -1,39 +1,5 @@
-# from itertools import permutations
+def abbr(s):
+    a,b='Alab,Ala,Ari,Ark,Ca,Col,Con,De,Fl,Ge,Ha,Id,Il,In,Io,Ka,Ke,Lo,Mai,Mar,Mas,Mic,Min,Missi,Mis,Mo,Neb,Nev,New H,New J,New M,New,North C,North D,Oh,Ok,Or,Pe,Rh,South C,Sou,Ten,Tex,Ut,Ve,Vi,Wa,We,Wi,Wy,Di,Am,Gu,Northern,Pu,U.'.split(','),'AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY DC AS GU MP PR VI'.split(' ')
+    return next(j for i,j in zip(a,b) if s.startswith(i))
 
-# def min_rook_distance(a,r):
-#     n=max(sum(a+[r],()))+1
-#     w=lambda x,y:abs(x[0]-y[0])+abs(x[1]-y[1])
-#     if n<10:
-#         def ff(a):
-#             a=[r]+list(a)
-#             t=0
-#             for i in range(len(a)-1):
-#                 t+=w(a[i],a[i+1])
-#             return t
-#         return min(map(ff,permutations(a)))
-#     m=float('inf')
-#     a=sorted(a,key=lambda x:w(r,x),reverse=True)
-#     def f(s,a,d):
-#         nonlocal m
-#         if not a:
-#             m=min(d,m)
-#             return
-#         q=a.copy()
-#         x=q.pop(q.index(min(q,key=lambda e:w(s,e))))
-#         f(x,q,d+w(s,x))
-#     for i in a:
-#         f(i,[j for j in a if j!=i],w(r,i))
-#     if n==14:
-#         if m==58:
-#             return 55
-#     if n==15:
-#         if m==70:
-#             return 66
-#     if n==10:
-#         if m==25:
-#             return 22
-#         if m==40:
-#             return 39
-#     return m
-    
-# print(min_rook_distance([(3, 0), (5, 4), (2, 2), (1, 5)], (2,3)))
+print(abbr('Alabama'))
