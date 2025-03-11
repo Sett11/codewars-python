7202a2a2ad78dcd74f9292494340ba53934eac26
Jhonny <gonzodzen@mail.ru>
2025-03-12 00:11:01 +0300

    Data Type Scramble

--- my.py
+++ my.py
@@ -1,9 +1,14 @@
-def pyramid_height(n):
-    c,i=0,1
-    while n>=i**2:
-        n-=i**2
-        i+=1
-        c+=1
-    return c
+def make_model_year(a):
+    r={'make':None,'model':None,'year':None,'new':None}
+    for i in a:
+        if isinstance(i,str):
+            r['make']=i
+        if isinstance(i,tuple):
+            r['model']=' '.join(i)
+        if isinstance(i,int) and not isinstance(i,bool):
+            r['year']=i
+        if isinstance(i,bool):
+            r['new']=i
+    return r
 
-print(pyramid_height(6201))
+print(make_model_year([1970, False, 'xmkd', ('xkdk', 'ytsvdylymeqifsmphugcspwd')]))
