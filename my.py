f7bf667d846a8db9cbe9a057fdd838eb25e50159
Jhonny <gonzodzen@mail.ru>
2025-03-15 00:01:53 +0300

    Shallowest path across the river

--- my.py
+++ my.py
@@ -1,14 +1,48 @@
-def make_model_year(a):
-    r={'make':None,'model':None,'year':None,'new':None}
-    for i in a:
-        if isinstance(i,str):
-            r['make']=i
-        if isinstance(i,tuple):
-            r['model']=' '.join(i)
-        if isinstance(i,int) and not isinstance(i,bool):
-            r['year']=i
-        if isinstance(i,bool):
-            r['new']=i
-    return r
+from collections import deque
 
-print(make_model_year([1970, False, 'xmkd', ('xkdk', 'ytsvdylymeqifsmphugcspwd')]))
+def can_reach_end(a,n,m,M):
+    q,u,ps=deque(),set(),{}
+    for i in range(n):
+        if a[i][0]<=M:
+            q.append((k:=(i,0))),u.add(k)
+            ps[k]=None
+    while q:
+        i,j=q.popleft()
+        if j==m-1:
+            path=[]
+            c=(i,j)
+            while c is not None:
+                path.insert(0,c)
+                c=ps[c]
+            return path
+        t=[(i+1,j),(i-1,j),(i,j+1),(i,j-1),(i-1,j-1),(i+1,j+1),(i+1,j-1),(i-1,j+1)]
+        for x,y in t:
+            if 0<=x<n and 0<=y<m and (x,y) not in u and a[x][y]<=M:
+                q.append((x,y)),u.add((x,y))
+                ps[(x,y)]=(i,j)
+    return None
+
+def shallowest_path(a):
+    n,m=len(a),len(a[0])
+    l,r,p=min(min(i) for i in a),max(max(i) for i in a),None
+    while l<=r:
+        M=(l+r)//2
+        path=can_reach_end(a,n,m,M)
+        if path:
+            p=path
+            r=M-1
+        else:
+            l=M+1
+    return p
+
+
+print(shallowest_path([
+[1, 8, 8],
+[8, 1, 8],
+[8, 1, 8],
+[8, 1, 8],
+[8, 1, 8],
+[8, 8, 1],
+[8, 1, 8],
+[8, 1, 1],
+[1, 8, 8]]))
