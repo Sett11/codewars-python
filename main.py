764aa01eecbdc87b9bfa5a7eccf70b21a810ba96
Jhonny <gonzodzen@mail.ru>
2025-04-17 01:30:18 +0300

    Splitting Numbers

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from concurrent.futures import ThreadPoolExecutor
+def split_numbers(n):
+    x=bin(n)[2:][::-1]
+    a,b=list('0'*len(x)),list('0'*len(x))
+    n=1
+    for i in range(len(x)):
+        if x[i]=='1' and n&1:
+            a[i]='1'
+            n += 1
+        elif x[i]=='1' and not n&1:
+            b[i]='1'
+            n += 1
+    return int(''.join(a[::-1]),2),int(''.join(b[::-1]),2)
 
-def task_master(functions):
-    total = 0
-    with ThreadPoolExecutor(max_workers=len(functions)) as executor:
-        futures = [executor.submit(func) for func in functions]
-        for future in futures:
-            total += future.result()
-    return total
+print(split_numbers(13))
