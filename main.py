4b312533d10f696095abce7f57de2c1d4357215d
Jhonny <gonzodzen@mail.ru>
2025-04-26 01:59:16 +0300

    [BUG] XCOM-409: Flight distance of Interceptor planes is miscalculated

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-def f(p, q):
-    if p == 0 and q == 0:
-        return None
-    if p == 1:
-        win_first = 1.0
-        win_second = 0.0 if q == 1 else 1 - q
-        return (win_first, win_second)
-    if q == 1:
-        win_first = p
-        win_second = 0.0
-        return (win_first, win_second)
-    if p == 0:
-        return (0.0, 0.0)
-    if q == 0:
-        return (1.0, 1.0)
-    P1 = p / (1 - (1-p)*(1-q))
-    P2 = (1-q)*p / (1 - (1-q)*(1-p))
-    return (P1, P2)
-
-print(f(1.0,0.8))
+def travel_distance(avg_speed, travel_time):
+    KM_PER_KNOT = 1.852
+    travel_hours = travel_time / 60
+    travel_kms = avg_speed * KM_PER_KNOT * travel_hours
+    return travel_kms
