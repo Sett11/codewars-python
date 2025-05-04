c0dcd399e0fc95cfce92e69ea5225965418df3b7
Jhonny <gonzodzen@mail.ru>
2025-05-05 00:02:26 +0300

    Find the cheapest path

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
 def travel_distance(avg_speed, travel_time):
-    KM_PER_KNOT = 1.852
-    travel_hours = travel_time / 60
-    travel_kms = avg_speed * KM_PER_KNOT * travel_hours
-    return travel_kms
+    return avg_speed * 1.852 * (travel_time / 60)
