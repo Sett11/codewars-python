d9d74d5d7f073bc2f705b968193bcbe70063e0a6
Jhonny <gonzodzen@mail.ru>
2025-05-06 00:02:47 +0300

    Simple directions reversal

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-def travel_distance(avg_speed, travel_time):
-    return avg_speed * 1.852 * (travel_time / 60)
+def solve(arr):
+    if not arr:
+        return []
+    
+    roads = []
+    directions = []
+    
+    for step in arr:
+        parts = step.split(' on ')
+        directions.append(parts[0])
+        roads.append(parts[1])
+    
+    reversed_roads = roads[::-1]
+    reversed_directions = ['Begin']
+    
+    for original_dir in directions[:0:-1]:
+        if original_dir == 'Left':
+            reversed_directions.append('Right')
+        elif original_dir == 'Right':
+            reversed_directions.append('Left')
+        else:
+            reversed_directions.append(original_dir)
+    
+    result = []
+    for dir, road in zip(reversed_directions, reversed_roads):
+        result.append(f"{dir} on {road}")
+    
+    return result
