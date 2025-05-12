53eda22ce783bfc6d2aab069f4fc815ced2445ee
Jhonny <gonzodzen@mail.ru>
2025-05-13 00:18:14 +0300

    Heads and Legs

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-count_squares=lambda x: 1 if x==0 else (x+1)**3 - (x-1)**3
+def animals(x, y):
+    if not (x or y):
+        return 0,0
+    co=0
+    while y>=0:
+        if x*2==y:
+            return x,co
+        y-=4
+        x-=1
+        co+=1
+    return 'No solutions'
+
+print(animals(72, 200))
