e29d7c2b98fea3b15ac7ca5ded12568c2e0bd9ee
Jhonny <gonzodzen@mail.ru>
2025-05-14 00:02:05 +0300

    Binary Bunch Transform of an Integer

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-def animals(x, y):
-    if not (x or y):
-        return 0,0
-    co=0
-    while y>=0:
-        if x*2==y:
-            return x,co
-        y-=4
-        x-=1
-        co+=1
-    return 'No solutions'
+from collections import defaultdict
 
-print(animals(72, 200))
+def bunch(n):
+    s=bin(n)[2:]
+    x,y,z,m,d=len(s),s.count('1'),0,1e3,defaultdict(list)
+    while 1:
+        t='0'*z+'1'*y+'0'*(x-y)
+        z+=1
+        x-=1
+        w=sum(1 for j,k in zip(s,t) if j!=k)
+        m=min(w,m)
+        d[w].append(int(t,2))
+        if t[-1]=='1':
+            break
+    return min(d[m])
+
+print(bunch(52))
+print(bunch(19))
