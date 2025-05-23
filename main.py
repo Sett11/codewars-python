0687f3ba86f3d15a84a80a2184b3e115fe281ead
Jhonny <gonzodzen@mail.ru>
2025-05-24 00:02:14 +0300

    Is this a valid Chess960 position?

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
-import math
-
-def solution(force1, force2, theta):
-    # Переводим угол theta из градусов в радианы
-    theta_rad = math.radians(theta)
-    # Вычисляем компоненты результирующей силы
-    R_x = force1 + force2 * math.cos(theta_rad)
-    R_y = force2 * math.sin(theta_rad)
-    # Вычисляем модуль результирующей силы
-    R = math.sqrt(R_x**2 + R_y**2)
-    # Вычисляем угол phi в радианах, затем переводим в градусы
-    if R_x == 0:
-        if R_y > 0:
-            phi_rad = math.pi / 2
-        elif R_y < 0:
-            phi_rad = -math.pi / 2
-        else:
-            phi_rad = 0  # Если R_y тоже 0, угол не определён, можно вернуть 0
-    else:
-        phi_rad = math.atan2(R_y, R_x)
-    phi_deg = math.degrees(phi_rad)
-
-    # Возвращаем результат с округлением до разумного количества знаков (например, 2)
-    return [R, phi_deg]
+def is_valid(positions):
+    # Проверяем, что король находится между двумя ладьями
+    rook_indices = [i for i, piece in enumerate(positions) if piece == 'R']
+    king_index = positions.index('K')
+    if not (rook_indices[0] < king_index < rook_indices[1]):
+        return False
+    # Проверяем, что слоны на клетках разного цвета
+    bishop_indices = [i for i, piece in enumerate(positions) if piece == 'B']
+    if (bishop_indices[0] % 2) == (bishop_indices[1] % 2):
+        return False
+    return True
