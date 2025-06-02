a39a1cf1773ab240106ac9a3050a1f74ded7bc54
Jhonny <gonzodzen@mail.ru>
2025-06-03 00:02:43 +0300

    Outrun your past self

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def dots_on_domino_bones(n):
-    return n*(n+1)*(n+2)/2
-
-print(dots_on_domino_bones(3))
+def number_lappings(my_speed, ghost_speed, time, round_length):
+    if my_speed <= ghost_speed:
+        return 0
+    relative_distance = (my_speed - ghost_speed) * time
+    full_laps = relative_distance // round_length
+    if relative_distance == full_laps * round_length:
+        return max(0, full_laps - 1)
+    else:
+        return int(full_laps)
