3912d4acb2e384855fadc873a7b75c8dd942adcd
Jhonny <gonzodzen@mail.ru>
2025-06-04 00:05:44 +0300

    Sting like a bee, float like a sponge?

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-def number_lappings(my_speed, ghost_speed, time, round_length):
-    if my_speed <= ghost_speed:
-        return 0
-    relative_distance = (my_speed - ghost_speed) * time
-    full_laps = relative_distance // round_length
-    if relative_distance == full_laps * round_length:
-        return max(0, full_laps - 1)
-    else:
-        return int(full_laps)
+decrypt_credits = lambda s: __import__('struct').unpack('<f', bytes.fromhex(s.zfill(8)))[0]
+
+print(decrypt_credits('803BC4'))
