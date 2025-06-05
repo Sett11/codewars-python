4224eec422ceba694b3c90013ca359d0eb438bc4
Jhonny <gonzodzen@mail.ru>
2025-06-06 00:07:11 +0300

    Simple Fun #399: Make Ascending Sequences

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
-def calculate_parity(cluster):
-    return cluster + [[sum(disk) % 2 for disk in zip(*cluster)]]
+from functools import lru_cache
 
-def recover_disk(cluster):
-    a = cluster.copy()
-    b = a.pop()
-    if None in b:
-        return calculate_parity(a)
-    c = [i for i in zip(*a)]
-    d = [sum(j for j in i if j is not None) % 2 for i in c]
-    for i in range(len(b)):
-        if len(set([b[i], d[i]])) != 1:
-            c[i] = [1 if j is None else j for j in c[i]]
-        else:
-            c[i] = [0 if j is None else j for j in c[i]]
-    return [list(i) for i in zip(*c)] + [b]
+@lru_cache(maxsize=None)
+def make_sequences(n):
+    if n <= 1:
+        return 1
+    result = 1
+    for i in range(1, n//2 + 1):
+        result += make_sequences(i)
+    return result
 
-print(calculate_parity([[0, 0, 0, 1, 1, 1, 1, 0], [0, 1, 0, 0, 1, 0, 1, 0]]))
-
-print(recover_disk([
-  [0,    0, 0, 1],
-  [None, 1, 0, 0],
-  [1,    1, 0, 1]
-]
-))
+print(make_sequences(40))
