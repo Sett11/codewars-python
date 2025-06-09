7e7843e8c7c15b155245c9cd6c7aa36ecc3a2e28
Jhonny <gonzodzen@mail.ru>
2025-06-10 00:03:43 +0300

    Get recursion limit

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-def is_cleanly_nested(a):
-    u = set()
-    def f(e):
-        nonlocal u
-        u.add(all(i for i in e) or all(not i for i in e))
-        for i in e:
-            f(i)
-        return
-    f(a)
-    return (len(u)==1 and u.pop())
-
-print(is_cleanly_nested([]))
-print(is_cleanly_nested([[],[]]))
-print(is_cleanly_nested([ [ [[]], [[]], [[]]  ], [[]] , [[]]  ]))
-print(is_cleanly_nested([ [ [ [[]],[[]], []  ], [[]] ] , [ [] ] ]))
+fetch_recursion_limit=lambda:eval('__imp'+'ort__')('sy'+'s').getrecursionlimit()
