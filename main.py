2925cffb39dbbc219e075515a6a91d802744b592
Jhonny <gonzodzen@mail.ru>
2025-06-11 00:04:20 +0300

    The Hackers’ Taunt

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-fetch_recursion_limit=lambda:eval('__imp'+'ort__')('sy'+'s').getrecursionlimit()
+def decrypt(a, k):
+    d = []
+    def f(e, n):
+        if n > k:
+            return
+        if isinstance(e, str):
+            if n == k:
+                d.insert(0, e)
+                return
+        else:
+            for i in e:
+                f(i, n+1)
+    f(a, -1)
+    return ''.join(d)
+
+print(decrypt(["a", "b", ["c"], ["world", "hello"]], 0))
+print(decrypt([ [  ["b"] , ["a"] ], ["world", "hello"]], 1))
+print(decrypt([ ["hello", [ [], [], "hiya", ["hiii", [[["lol"]]] ] ],"hi", [ [["he", "world"] ], [ ["hi", "!" ] ] ] ] , [ ["hi", "!","lol!",["lol"] ] ]], 2))
