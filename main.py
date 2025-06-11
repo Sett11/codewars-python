2f32ccec902c61d0e512a3fc7cb6afa0e9c4f7df
Jhonny <gonzodzen@mail.ru>
2025-06-12 00:02:07 +0300

    Build your Own AI Agent: Part 1

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-def decrypt(a, k):
-    d = []
-    def f(e, n):
-        if n > k:
-            return
-        if isinstance(e, str):
-            if n == k:
-                d.insert(0, e)
-                return
-        else:
-            for i in e:
-                f(i, n+1)
-    f(a, -1)
-    return ''.join(d)
+def next_states(s):
+    r, a, c = [], [], 0
+    if s[-1] == 'I':
+        r.append(s + 'U')
+    if s[0] == 'M':
+        r.append(s + s[1:])
+    while (i:=s.find('III', c)) != -1:
+        a.append(i)
+        c = i + 1
+    for i in a:
+        t = s[:i]+'U'+s[i+3:]
+        if r[-1] != t:
+            r.append(t)
+    c, a = 0, []
+    while (i:=s.find('UU', c)) != -1:
+        a.append(i)
+        c = i + 1
+    for i in a:
+        t = s[:i]+''+s[i+2:]
+        if r[-1] != t:
+            r.append(t)
+    return r
 
-print(decrypt(["a", "b", ["c"], ["world", "hello"]], 0))
-print(decrypt([ [  ["b"] , ["a"] ], ["world", "hello"]], 1))
-print(decrypt([ ["hello", [ [], [], "hiya", ["hiii", [[["lol"]]] ] ],"hi", [ [["he", "world"] ], [ ["hi", "!" ] ] ] ] , [ ["hi", "!","lol!",["lol"] ] ]], 2))
+print(next_states('MUUUI'))
