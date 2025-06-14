58f6c8cf70152e98d35a75c1738c0e35785bc69d
Jhonny <gonzodzen@mail.ru>
2025-06-15 00:23:48 +0300

    The Flying Deuces

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
-def next_states(s):
-    r, a, c = [], [], 0
-    if s[-1] == 'I':
-        r.append(s + 'U')
-    if s[0] == 'M':
-        r.append(s + s[1:])
-    while (i:=s.find('III', c)) != -1:
-        a.append(i)
-        c = i + 1
-    for i in a:
-        t = s[:i]+'U'+s[i+3:]
-        if r[-1] != t:
-            r.append(t)
-    c, a = 0, []
-    while (i:=s.find('UU', c)) != -1:
-        a.append(i)
-        c = i + 1
-    for i in a:
-        t = s[:i]+''+s[i+2:]
-        if r[-1] != t:
-            r.append(t)
-    return r
+def simulate(a, n, s, o):
+    if n == 0:
+        return a
+    b, c, l = a.copy(), [s, o], len(a)
+    if 0 not in c:
+        b[0] = a[1]
+    if l - 1 not in c:
+        b[-1] = a[-2]
+    for i in range(1, l-1):
+        if i not in c:
+            if a[i-1] == a[i+1]:
+                b[i] = a[i-1]
+            else:
+                t = (a[i-1] + a[i+1]) // 2
+                if t != a[i]:
+                    b[i] = b[i] + 1 if t > a[i] else b[i] - 1
+                else:
+                    b[i] = b[i] + 1 if max(a[i-1], a[i+1]) > a[i] else b[i] - 1
+    return simulate(b, n-1, s, o) 
 
-print(next_states('MUUUI'))
+
+print(simulate([0,1,0,2,5,4,8,0,3,4,2], 5, 1, 5))
