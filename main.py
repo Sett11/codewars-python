6aeab30c16724f32a55dd7774b44f86f6ea5ef40
Jhonny <gonzodzen@mail.ru>
2025-06-21 00:04:23 +0300

    Simple Fun #343: Add Spaces Between 1 To n

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
-def micro_world(a, k):
-    a.sort()
-    i, w = 0, True
-    while w:
-        w = False
-        for i in range(len(a)-1):
-            if a[i] < a[i+1] and a[i+1] <= a[i] + k:
-                w = True
-                a.pop(i)
-                break
-    return len(a)
+from collections import defaultdict
+from itertools import combinations_with_replacement
 
-print(micro_world([101, 53, 42, 102, 101, 55, 54], 1))
-print(micro_world([20, 15, 10, 15, 20, 25], 5))
+def add_spaces(s):
+    if '0' not in s:
+        return ' '.join(list(s))
+    n, l = 50, len(s)
+    while n > 9:
+        if len(''.join(map(str, range(10, n+1)))) == l - 9:
+            break
+        n -= 1
+    r, g, q, u = [], defaultdict(set), '1234567890', set(s[:i] for i in range(1, l+1))
+    nums = set([j for j in sum([[''.join(i), ''.join(i[::-1])] for  i in combinations_with_replacement(q,2)], []) + [i for i in q if i != '0'] + ['10'] if int(j) <= n and j[0] != '0'])
+    for i in nums:
+        for j in nums:
+            if i != j:
+                g[i].add(j)
+                g[j].add(i)
+    def dfs(e, a, w):
+        if len((x:=''.join(a))) > l or x not in u or r:
+            return
+        if ''.join(a) == s:
+            r.append(' '.join(a))
+            return
+        for i in g[e]:
+            if ''.join(a + [i]) in u and i not in w:
+                nw = w.copy()
+                nw.add(i)
+                dfs(i, a + [i], nw)
+    dfs(s[0], [s[0]], set([s[0]])), dfs(s[:2],[s[:2]], set([s[:2]]))
+    return r[0] if r else s
+
+
+print(add_spaces('206513188102221121415143923241619171127'))
+print(add_spaces('147113917185411610816122315'))
+print(add_spaces('121110987654312'))
