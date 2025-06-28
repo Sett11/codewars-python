5abbd07e5ab0d3084b06aee8dd8c158fa3f64165
Jhonny <gonzodzen@mail.ru>
2025-06-29 00:27:46 +0300

    Walking in the NASA facility

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
-def countdown(n):
-    v, t = n < 0, abs(n)
-    s = t / 1000
-    h = s // 3600
-    rs = s % 3600
-    m = rs // 60
-    sec = rs % 60
-    return f"{'-' if v else '+'}{str(int(h)).rjust(2,'0')}:{str(int(m)).rjust(2, '0')}:{str(int(sec)).rjust(2,'0')}"
+def collision(a):
+    n, m, c = len(a), len(a[0]), 1
+    while True:
+        pos, dr, na = {}, {}, [['-'] * m for _ in range(n)]
+        for i in range(n):
+            for j in range(m):
+                if a[i][j] in 'udlr':
+                    t, v = (i,j), a[i][j]
+                    if v == 'u':
+                        pos[t] = (i-1, j)
+                        dr[pos[t]] = 'u'
+                    if v == 'd':
+                        pos[t] = (i+1, j)
+                        dr[pos[t]] = 'd'
+                    if v == 'r':
+                        pos[t] = (i, j+1)
+                        dr[pos[t]] = 'r'
+                    if v == 'l':
+                        pos[t] = (i, j-1)
+                        dr[pos[t]] = 'l'
+        vals = pos.values()
+        if len(vals) > len(set(vals)):
+            return c
+        if not vals:
+            return -1
+        for i, j in pos.items():
+            if j in pos and i in vals:
+                return c
+            if 0 <= j[0] < n and 0 <= j[1] < m:
+                na[j[0]][j[1]] = dr[j]
+        c += 1
+        a = na
 
-print(countdown(-154800000))
-print(countdown(61000))
+print(collision([
+                    list("-----"),
+                    list("--d-l"),
+                    list("-----"),
+                    list("rr--u"),
+                    list("--l--")
+                ]),sep='\n')
