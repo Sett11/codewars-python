a7127a7393e6b4c90fa2a3d956a672fb3f3a5530
Jhonny <gonzodzen@mail.ru>
2025-06-30 00:03:00 +0300

    Ones and Zeroes

--- main.py
+++ main.py
@@ -1,40 +1,11 @@
-def collision(a):
-    n, m, c = len(a), len(a[0]), 1
-    while True:
-        pos, dr, na = {}, {}, [['-'] * m for _ in range(n)]
-        for i in range(n):
-            for j in range(m):
-                if a[i][j] in 'udlr':
-                    t, v = (i,j), a[i][j]
-                    if v == 'u':
-                        pos[t] = (i-1, j)
-                        dr[pos[t]] = 'u'
-                    if v == 'd':
-                        pos[t] = (i+1, j)
-                        dr[pos[t]] = 'd'
-                    if v == 'r':
-                        pos[t] = (i, j+1)
-                        dr[pos[t]] = 'r'
-                    if v == 'l':
-                        pos[t] = (i, j-1)
-                        dr[pos[t]] = 'l'
-        vals = pos.values()
-        if len(vals) > len(set(vals)):
-            return c
-        if not vals:
-            return -1
-        for i, j in pos.items():
-            if j in pos and i in vals:
-                return c
-            if 0 <= j[0] < n and 0 <= j[1] < m:
-                na[j[0]][j[1]] = dr[j]
-        c += 1
-        a = na
+from itertools import groupby
+from collections import Counter
 
-print(collision([
-                    list("-----"),
-                    list("--d-l"),
-                    list("-----"),
-                    list("rr--u"),
-                    list("--l--")
-                ]),sep='\n')
+def same_length(s):
+    if s[0] == '0':
+        return False
+    a, c = [len(list(j)) for i,j in groupby(s)], Counter(s)
+    return all(a[i] == a[i+1] for i in range(0, len(a) - 1, 2)) and c['1'] == c['0']
+
+print(same_length('1011100010'))
+print(same_length('00110100001111'))
