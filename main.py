6d4f6e1af8da56599e53721fedebfaf13d174727
Jhonny <gonzodzen@mail.ru>
2025-07-03 00:02:56 +0300

    We are the Robots d[(0)(0)]b

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-from string import ascii_uppercase as a, digits as d
-from collections import Counter
+from string import ascii_lowercase as s
 
-def convert(n, b, w=1):
-    r, s = (d + a)[:b], ''
-    while n > 0:
-        s = r[n%b] + s
-        n //= b
-    return s.rjust(w, '0')
+def count_robots(a):
+    aut = mec = 0
+    def inner_count(e):
+        nonlocal aut, mec
+        e = e.lower()
+        n, x, y, z = len(e), '|};&#[]/><()*', 'automatik' in e, 'mechanik' in e
+        if not (y or z):
+            return
+        for i in range(n - 9):
+            if e[i] in s and e[i+1] in x and\
+                e[i+2] in x and e[i+3] == '0' and e[i+4] in x and\
+                    e[i+5] in x and e[i+6] == '0' and e[i+7] in x and\
+                        e[i+8] in x and e[i+9] in s:
+                        if y:
+                            aut += 1
+                        else:
+                            mec += 1
+    [inner_count(i) for i in a]
+    return [f"{aut} robots functioning automatik", f"{mec} robots dancing mechanik"]
 
-def validate(b, w, a):
-    s = ''.join(convert(i, b, w) for i in a)
-    c = Counter(s)
-    for i in range(len(a)):
-        if c[convert(i, b)] != a[i]:
-            return False
-    return True
-
-print(validate(16,2,[0x1B, 0x03, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00,
-                 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00]))
+print(count_robots(["d[(0)(0)]b We're functioning automatik d[(0)(0)]b","And we are d[(0)(0)]b dancing mechanik d[(0)(0)]b d[(0)(0)]b"]))
