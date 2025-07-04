4ed1ecfcdde6684659c768caad2eadfca5e28fc2
Jhonny <gonzodzen@mail.ru>
2025-07-05 00:01:20 +0300

    Pokemon Damage Calculator

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-from string import ascii_lowercase as s
-
-def count_robots(a):
-    aut = mec = 0
-    def inner_count(e):
-        nonlocal aut, mec
-        e = e.lower()
-        n, x, y, z = len(e), '|};&#[]/><()*', 'automatik' in e, 'mechanik' in e
-        if not (y or z):
-            return
-        for i in range(n - 9):
-            if e[i] in s and e[i+1] in x and\
-                e[i+2] in x and e[i+3] == '0' and e[i+4] in x and\
-                    e[i+5] in x and e[i+6] == '0' and e[i+7] in x and\
-                        e[i+8] in x and e[i+9] in s:
-                        if y:
-                            aut += 1
-                        else:
-                            mec += 1
-    [inner_count(i) for i in a]
-    return [f"{aut} robots functioning automatik", f"{mec} robots dancing mechanik"]
-
-print(count_robots(["d[(0)(0)]b We're functioning automatik d[(0)(0)]b","And we are d[(0)(0)]b dancing mechanik d[(0)(0)]b d[(0)(0)]b"]))
+def calculate_damage(y, o, n, m):
+    r, t = 50 * (n / m), {'>':2, '==': 1, '<': .5}
+    if y == o:
+        return r * .5
+    a = ['fire > grass', 'fire < water', 'fire == electric', 'water < grass', 'water < electric', 'grass == electric', 'grass < fire', 'water > fire', 'grass > water', 'electric > water', 'electric == fire', 'electric == grass']
+    return r * t[[i.split()[1] for i in a if (s := i.split())[0] == y and s[-1] == o][0]]
+    
+print(calculate_damage("fire", "water", 100, 100))
