399fba74ba7124388704c5d4483ca472e584fcf2
Jhonny <gonzodzen@mail.ru>
2025-07-07 00:04:19 +0300

    Find the name of the lucky variable

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# def get_villain_name(d):
-#     s = str(d).split()[0].split('-')
-#     f = [ "The Evil","The Vile","The Cruel", "The Trashy","The Despicable", "The Embarrassing", "The Disreputable","The Atrocious", "The Twirling",  "The Orange","The Terrifying", "The Awkward"]
-#     l = ["Mustache", "Pickle", "Hood Ornament", "Raisin", "Recycling Bin", "Potato", "Tomato", "House Cat", "Teaspoon", "Laundry Basket"]
-#     return f'{f[int(s[1]) - 1]} {l[int(s[-1][-1])]}'
+def find_variable():
+    g = globals()
+    return [i for i in g if g[i] == 777][0]
