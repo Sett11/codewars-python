912acf25851faf00ecefde7ffab0135d786a39bf
Jhonny <gonzodzen@mail.ru>
2025-07-16 00:43:03 +0300

    Hero's root

--- main.py
+++ main.py
@@ -1,34 +1,11 @@
-from itertools import compress
-from math import isqrt
+def int_rac(n, x):
+    c = 1
+    while True:
+        y = (x + n // x) // 2
+        if abs(x - y) < 1:
+            break
+        c += 1
+        x = y
+    return c
 
-def primes(n):
-    sieve = bytearray([True]) * (n//2)
-    for i in range(3,int(n**0.5)+1,2):
-        if sieve[i//2]:
-            sieve[i*i//2::i] = bytearray((n-i*i-1)//(2*i)+1)
-    return [2,*compress(range(3,n,2), sieve[1:])]
-
-primes_list = primes(isqrt(10000000000000000))
-
-def factorization(n):
-    pf = []
-    for p in primes_list:
-      if p*p > n:
-        break
-      count = 0
-      while n % p == 0:
-        n //= p
-        count += 1
-      if count > 0:
-        pf.append((p, count))
-    if n > 1:
-        pf.append((n, 1))
-    return pf
-
-def divisor(n):
-    divs = set([1])
-    for p, e in factorization(n):
-        divs.update([x*p**k for k in range(1,e+1) for x in divs])
-    return sorted(divs)
-
-print(divisor(9999999999999994))
+print(int_rac(125348,300))
