0f8f90940e6716bf8fb6da0dc3dc90af7214e7a2
Jhonny <gonzodzen@mail.ru>
2025-07-23 01:15:38 +0300

    regex pattern to check if string has all characters

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from collections import Counter
+import re
 
-def boards_isomorphic(a, b):
-    q, w = Counter(a[1::2]), Counter(b[1::2])
-    def f(s, p):
-        z = [s[i:i+2] for i in range(0, len(s), 2)]
-        for i in range(len(z)):
-            z[i] = z[i].replace(z[i][1], str(p[z[i][1]]), 1)
-        return sorted(z)
-    return f(a, q) == f(b, w)
+def regex_contains_all(s):
+    return r'^'+r''.join('(?=.*' + i + ')' for i in s)+r'.+$'
 
+s = 'abc'
+ss = 'zzzaaacccbbbzzz'
 
-print(boards_isomorphic("4d2d", "2c4c"))
-print(boards_isomorphic("4d7dAc", "As4c7s"))
+pattern = regex_contains_all(s)
+
+print(re.match(pattern,ss))
