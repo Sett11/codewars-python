ba91bd5b1121fadfdbd171098828709d5e78e89c
Jhonny <gonzodzen@mail.ru>
2025-07-24 00:08:02 +0300

    Simple Fun #266: K Bits Digits

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-import re
+from itertools import combinations as c
 
-def regex_contains_all(s):
-    return r'^'+r''.join('(?=.*' + i + ')' for i in s)+r'.+$'
+def k_bits_digits(n, k):
+    if k == 0:
+        return [0]
+    if n < k:
+        return []
+    if n == k:
+        return [(1 << n) - 1]
+    a = []
+    for bits in c(range(n), k):
+        t = 0
+        for bit in bits:
+            t |= (1 << (n - 1 - bit))
+        a.append(t)
+    return sorted(a)
 
-s = 'abc'
-ss = 'zzzaaacccbbbzzz'
 
-pattern = regex_contains_all(s)
-
-print(re.match(pattern,ss))
+print(k_bits_digits(5, 3))
