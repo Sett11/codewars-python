77ea6a429017c9131256aec6cefa14f962c9bdfd
Jhonny <gonzodzen@mail.ru>
2025-07-29 02:24:31 +0300

    Has the Largest Product Horizontal or Vertical Direction? Find it!!

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-from itertools import combinations as c
+def highest_prod(a):
+    m = 0
+    def k(a):
+        nonlocal m
+        t = r = 1
+        for i in a:
+            m = max(m, i)
+            t *= i
+            if t > r:
+                r = t
+            if t < 1:
+                t = 1
+        return r
+    r = max(list(map(k, a)) + list(map(k, zip(*a))))
+    return r if r != m else 0
 
-def k_bits_digits(n, k):
-    if k == 0:
-        return [0]
-    if n < k:
-        return []
-    if n == k:
-        return [(1 << n) - 1]
-    a = []
-    for bits in c(range(n), k):
-        t = 0
-        for bit in bits:
-            t |= (1 << (n - 1 - bit))
-        a.append(t)
-    return sorted(a)
-
-
-print(k_bits_digits(5, 3))
+print(highest_prod([[2, 1, 4, 1], [0, 4, 8, 1], [1, 0, 10, 0]]))
+print(highest_prod([[2, 0, 4, 0], [0, 0, 0, 0], [1, 0, 10, 0]]))
