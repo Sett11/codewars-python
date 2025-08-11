291a5bc94228c1cfeb58802fb4d9434220f75fe2
Jhonny <gonzodzen@mail.ru>
2025-08-12 00:03:07 +0300

    myjinxin's Fairy tales #001 : Aladdin's lamp and three wishes

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-from collections import Counter
+from re import match
 
-def buy_fruits(a, b):
-    c = sorted(Counter(b).values(), reverse=True)
-    mn = mx = k = 0
-    x, y = sorted(a), sorted(a,reverse=True)
-    for i in c:
-        mn += x[k] * i
-        mx += y[k] * i
-        k += 1
-    return mn, mx
+def three_wishes(a, b, c, d):
+    r = [i.replace('I want ', '') for i in [b, c, d]]
+    if [i for i in r if match(r'\d+ wishes', i)]:
+        return []
+    for i in r:
+        t, v = i.split(), False
+        x = int(t[0])
+        y = ' '.join(t[1:])
+        if x > 1:
+            y = y[:-1]
+        for i in range(len(a)):
+            if a[i] == y:
+                while x:
+                    a.insert(i, y)
+                    x -= 1
+                v = True
+                break
+        if not v:
+            a.extend([y] * x)
+    return a
 
-print(buy_fruits([3,5,1,6,8,1],["peach","grapefruit","banana","orange","orange"]))
+print(three_wishes([
+            'gold coin','gold coin','gold coin', 'silver coin','silver coin',
+            'water','water','water','water', 'food','food','food','food',
+            'book','book', 'weapon','weapon', 'clothe','clothe', 'medicine','medicine',
+            'tool','tool'],
+        'I want 1 food',
+        'I want 2 books',
+        'I want 1 girl'))
