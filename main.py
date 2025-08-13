f6bc43c8fafc6e89fe062185bf0021dad0abd9de
Jhonny <gonzodzen@mail.ru>
2025-08-14 00:08:02 +0300

    The Rhinestone Cowboy ~ Count the dollars in his boots!

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-def f(a,b):
-    return  sum(i == j or i =='#' for i,j in zip(a,b)) == len(a)
+from re import search
 
-def scratch(a):
-    r = 0
-    ans = ["tiger","rabbit","dragon","snake","rat","ox","pig","dog","cock","sheep","horse","monkey"]
-    vals = ['5', '10', '20', '50', '100', '200', '500', '1000', '2000', '5000', '10000']
-    for i in [i.split() for i in a]:
-        if len(set(map(len,i[:-1]))) == 1:
-            an, va = [j for j in ans if len(j) == len(i[0])], [j for j in vals if len(j) == len(i[-1])]
-            x, y = [[f(j, k) for j in i[:-1]] for k in an], [f(i[-1], j) for j in va]
-            if [j for j in range(len(x)) if all(x[j])]:
-                r += int(max([va[j] for j in range(len(y)) if y[j]]))
-    return r
+def cowboys_dollars(a):
+    s, l, r = 'This Rhinestone Cowboy has {} dollar bills in his right boot and {} in the left', 0, 0
+    for i in range(2):
+        t = a[i].split('\n')
+        for j in t:
+            if j:
+                if '&' in j:
+                    break
+                if search(r'\[\(1\)\]', j):
+                    if i == 0:
+                        l += 1
+                    else:
+                        r += 1
+    return s.format(r, l)
 
-print(scratch(['#####t #####t #####t 5##', '###### ###### ###### 1#', 'c### c### c### 5', '###### ###### ###### 5', '### #a# #a# #0#', 'c### c### ###k 1####']))
+left ='''
+   ,|___|,
+   |     |
+   |     |
+   |[(1)]|
+   | ==  |
+   |[(1)]|
+   /    &|
+.-'`  ,   )****
+|         |   **
+`~~~~~~~~~~    ^'''
+right ='''
+   ,|___|,
+   |[(1)]|
+   |     |
+   |[(1)]|
+   | ==  |
+   |[(1)]|
+   /    &|
+.-'`  ,   )****
+|[(1)]    |   **
+`~~~~~~~~~~    ^'''
+boots = [left, right]
+
+print(cowboys_dollars(boots))
