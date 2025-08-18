8f584252ad858dd3236d7b45223ca1b4b07857b6
Jhonny <gonzodzen@mail.ru>
2025-08-19 00:13:00 +0300

    Simple Fun #12: Count Sum of Two Representions

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-from re import search, sub
-
-def roast_legacy(s):
-    s = s.lower()
-    print(s)
-    comp = set(["slow!", "expensive!", "manual!", "down!", "hostage!", "security!"])
-    points = {"cobol": 1000, "nonobject": 500, "monolithic": 500, "fax": 100, "modem": 100, "thickclient": 50, "tape": 50, "floppy": 50, "oldschoolit": 50}
-    r1 = 'These guys are already DevOps and in the Cloud and the business is happy!'
-    r2 = 'Burn baby burn disco inferno {} points earned in this roasting and {} complaints resolved!'
-    c = p = 0
-    for i in comp:
-        while search(i, s):
+def count_sum_of_two_representations(n, l, r):
+    k = t = n // 2
+    if n % 2 != 0:
+        t += 1
+    c = 0
+    while True:
+        if k >= l and t <= r and k <= t:
             c += 1
-            s = sub(i, '', s, count=1)
-    for i in points:
-        while search(i, s):
-            p += points[i]
-            s = sub(i, '', s, count=1)
-    return r1 if c+p == 0 else r2.format(p, c)
+        else:
+            break
+        k -= 1
+        t += 1
+    return c
 
-
-print(roast_legacy('expensive!NONOBJECTexpensive!NONOBJECThostage!JAVASCRIPTexpensive!DEVOPS'))
+print(count_sum_of_two_representations(93,24,58))
